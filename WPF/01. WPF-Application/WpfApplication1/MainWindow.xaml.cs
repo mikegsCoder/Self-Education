484554e80e61ce2651a074f1d7d4b1286bab0494
mikegsCoder <mikegsid@gmail.com>
2023-03-04 18:36:06 +0200

    Add WPF-Application

---
+++ WPF/01. WPF-Application/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Button click handler
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                MessageBox.Show("Please enter your name.");
+            }
+            else 
+            {
+                MessageBox.Show($"Hello, {textBoxName.Text.Trim()}!");
+            }
+        }
+    }
+}
