1e8a32d7956ff6ae0bcf8a71e3dc5a99ecfa4a3b
mikegsCoder <mikegsid@gmail.com>
2023-03-04 09:55:59 +0200

    Add simple windows forms app

---
+++ 01. Microsoft-Tutorials/03. Simple-Win-Forms-Apps/HelloWorld/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClickThis = New Button()
+        lblHelloWorld = New Label()
+        SuspendLayout()
+        ' 
+        ' btnClickThis
+        ' 
+        btnClickThis.Location = New Point(345, 148)
+        btnClickThis.Name = "btnClickThis"
+        btnClickThis.Size = New Size(112, 34)
+        btnClickThis.TabIndex = 0
+        btnClickThis.Text = "Click this"
+        btnClickThis.UseVisualStyleBackColor = True
+        ' 
+        ' lblHelloWorld
+        ' 
+        lblHelloWorld.AutoSize = True
+        lblHelloWorld.Location = New Point(345, 246)
+        lblHelloWorld.Name = "lblHelloWorld"
+        lblHelloWorld.Size = New Size(128, 30)
+        lblHelloWorld.TabIndex = 1
+        lblHelloWorld.Text = "This is Label"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 30F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblHelloWorld)
+        Controls.Add(btnClickThis)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnClickThis As Button
+    Friend WithEvents lblHelloWorld As Label
+End Class
---
+++ 01. Microsoft-Tutorials/03. Simple-Win-Forms-Apps/HelloWorld/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub btnClickThis_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+        lblHelloWorld.Text = "Hello World!"
+    End Sub
+End Class
