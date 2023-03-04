48a2d5421a9f0cb56e5bcdc76a70ff497411bafc
mikegsCoder <mikegsid@gmail.com>
2023-03-04 09:51:51 +0200

    Add HelloWpfApp

---
+++ 01. Microsoft-Tutorials/02. Hello-WPF-App/HelloWpfApp/Greetings.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        If HelloButton.IsChecked = True Then
+            MessageBox.Show("Hello.")
+        ElseIf GoodbyeButton.IsChecked = True Then
+            MessageBox.Show("Goodbye.")
+        End If
+    End Sub
+End Class
