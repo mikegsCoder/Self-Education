d651b7d02c64d695b23f5e4b80fe0057d177569a
mikegsCoder <mikegsid@gmail.com>
2023-03-05 10:03:13 +0200

    Add Events in Markup application

---
+++ WPF/03. Events-In-Markup/EventsInMarkup/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EventsInMarkup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // MouseEnter event handler
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Yellow;
+        }
+
+        // MouseLeave event handler
+        private void Rectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Green;
+        }
+    }
+}
