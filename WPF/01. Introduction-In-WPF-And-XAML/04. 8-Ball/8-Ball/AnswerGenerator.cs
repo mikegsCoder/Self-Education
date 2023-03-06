c9eb781552520d31844c3a6b65c3855e23bdee7c
mikegsCoder <mikegsid@gmail.com>
2023-03-06 18:00:19 +0200

    Add 8-Ball

---
+++ WPF/01. Introduction-In-WPF-And-XAML/04. 8-Ball/8-Ball/AnswerGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _8_Ball
+{
+    public static class AnswerGenerator
+    {
+        private static string[] answers = new string[]
+        {
+            "Ask again.",
+            "I can't answer immediately",
+            "Yes, ofcourse.",
+            "That's right.",
+            "Concentrate and ask again.",
+            "No.",
+            "Sure",
+            "I don't think so.",
+            "You better don't ask.",
+            "Bad point of view.",
+            "Propably yes.",
+            "It's difficult to answer.",
+            "Yes, as I see.",
+            "My answer is no.",
+            "Yes.",
+            "You can be sure.",
+            "Good point of view."
+        };
+
+        public static string GetRandomAnswer()
+        {
+            return answers[new Random().Next(answers.Length)];
+        }
+    }
+}
---
+++ WPF/01. Introduction-In-WPF-And-XAML/04. 8-Ball/8-Ball/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace _8_Ball
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Button click handler
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            // Set Cursor wait mode
+            this.Cursor = Cursors.Wait;
+
+            // Sleep the thread
+            Thread.Sleep(1000);
+
+            // Get random answer
+            txtAnswer.Text = AnswerGenerator.GetRandomAnswer();
+
+            // Return previous Cursor condition
+            this.Cursor = null;
+        }
+    }
+}
