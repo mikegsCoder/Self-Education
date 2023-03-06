f199fe3b125352a0c262a5aea64b72f8849e2b2e
mikegsCoder <mikegsid@gmail.com>
2023-03-06 18:05:56 +0200

    Add Custom-Main-Method

---
+++ WPF/02. Class-Application/01. Custom-Main-Method/MainMethod/StartUpClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using MainMethod;
+
+namespace ApplicationSample
+{
+    public class StartUpClass
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            MainWindow window = new MainWindow();
+            app.Run(window);
+        }
+    }
+}
