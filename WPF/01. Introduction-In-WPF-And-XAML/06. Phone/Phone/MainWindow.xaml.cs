8a7521d5277b192fbb21e68b9085ecf131b655d9
mikegsCoder <mikegsid@gmail.com>
2023-03-07 17:05:20 +0200

    Add Phone app

---
+++ WPF/01. Introduction-In-WPF-And-XAML/06. Phone/Phone/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Phone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Phone1
+    {
+        public string? Name { get; set; }
+
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Phone: {Name}, price: {Price}.";
+        }
+    }
+}
