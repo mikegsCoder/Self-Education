f8f669a786d3dc374e33ff4791ad3e587d612913
mikegsCoder <mikegsid@gmail.com>
2023-03-07 17:02:03 +0200

    Add MessageBox

---
+++ WPF/01. Introduction-In-WPF-And-XAML/04. MessageBox/MessageBox/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace MessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox1.Text;
+
+            if (text != "")
+            {
+                System.Windows.MessageBox.Show(text);
+            }
+        }
+    }
+}
