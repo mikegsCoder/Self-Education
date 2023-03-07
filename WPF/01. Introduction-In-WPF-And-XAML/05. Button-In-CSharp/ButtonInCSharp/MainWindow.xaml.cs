4a322ce4baeeb9c81669046af96e26142bef35be
mikegsCoder <mikegsid@gmail.com>
2023-03-07 17:03:56 +0200

    Add ButtonInCSharp app

---
+++ WPF/01. Introduction-In-WPF-And-XAML/05. Button-In-CSharp/ButtonInCSharp/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ButtonInCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button myButton = new Button();
+            myButton.Content = "Button in C#";
+            myButton.Width = 120;
+            myButton.Height = 40;
+            myButton.HorizontalAlignment = HorizontalAlignment.Center;
+            myButton.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+
+            grid.Children.Add(myButton);
+        }
+    }
+}
