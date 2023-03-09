24f454a3aed14e90069cfbe223ae72782ff7a8ae
mikegsCoder <mikegsid@gmail.com>
2023-03-09 16:57:51 +0200

    Add WrapPanel with Item Attributes

---
+++ WPF/03. Layouts/06. WrapPanel/WrapPanel_With_ItemAttributes/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WrapPanel_With_ItemAttributes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
