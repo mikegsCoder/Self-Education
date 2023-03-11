ff682a2d856dd897ef8ca36c436e20123c3fdcd8
mikegsCoder <mikegsid@gmail.com>
2023-03-11 12:15:05 +0200

    Add ToolTip

---
+++ WPF/04. Controls/02. Controls/ToolTip/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ToolTip
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Windows.Controls.ToolTip toolTip = new System.Windows.Controls.ToolTip();
+            StackPanel toolTipPanel = new StackPanel();
+            toolTipPanel.Children.Add(new TextBlock { Text = "Header", FontSize = 16 });
+            toolTipPanel.Children.Add(new TextBlock { Text = "Text" });
+            toolTip.Content = toolTipPanel;
+            button1.ToolTip = toolTip;
+        }
+    }
+}
