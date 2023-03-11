3b275d275453a28335ce7ff46a87cd25ed582bb8
mikegsCoder <mikegsid@gmail.com>
2023-03-11 12:12:33 +0200

    Add CheckBox

---
+++ WPF/04. Controls/02. Controls/CheckBox/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace CheckBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // create CheckBox
+            System.Windows.Controls.CheckBox checkBox5 = new System.Windows.Controls.CheckBox { Content = "New flag", MinHeight = 20, IsChecked = true };
+            // set eventhandler
+            checkBox5.Checked += checkBox5_Checked;
+            // add to StackPanel
+            stackPanel.Children.Add(checkBox5);
+        }
+
+        // Event handlers:
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox4.Content.ToString() + " checked");
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox4.Content.ToString() + " unchecked");
+        }
+
+        private void checkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox4.Content.ToString() + " intermediate");
+        }
+
+        private void checkBox5_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.CheckBox chBox = (System.Windows.Controls.CheckBox)sender;
+            MessageBox.Show(chBox.Content.ToString() + " checked");
+        }
+    }
+}
