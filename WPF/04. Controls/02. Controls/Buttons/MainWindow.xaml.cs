1d4696d80739ed2177509afddc961b466829ca99
mikegsCoder <mikegsid@gmail.com>
2023-03-11 12:10:47 +0200

    Add Controls

---
+++ WPF/04. Controls/02. Controls/Buttons/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Buttons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Action executed.");
+        }
+
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // close window
+        }
+    }
+}
