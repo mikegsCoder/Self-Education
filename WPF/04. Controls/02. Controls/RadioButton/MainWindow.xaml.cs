065c1407a64cd9dba063e214904300f0441d6048
mikegsCoder <mikegsid@gmail.com>
2023-03-11 12:13:47 +0200

    Add RadioButton

---
+++ WPF/04. Controls/02. Controls/RadioButton/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace RadioButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Windows.Controls.RadioButton rb = new System.Windows.Controls.RadioButton { IsChecked = true, GroupName = "Languages", Content = "JavaScript" };
+            rb.Checked += RadioButton_Checked;
+            stackPanel.Children.Add(rb);
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.RadioButton pressed = (System.Windows.Controls.RadioButton)sender;
+            MessageBox.Show(pressed.Content.ToString());
+        }
+    }
+}
