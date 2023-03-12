23adba3c145899e098ce1449e3db8f1cf2cc1a42
mikegsCoder <mikegsid@gmail.com>
2023-03-12 11:25:43 +0200

    Add Expander

---
+++ WPF/04. Controls/02. Controls/Expander/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Expander
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //example 3
+            StackPanel expanderPanel = new StackPanel();
+            expanderPanel.Children.Add(new CheckBox { Content = "WinForms" });
+            expanderPanel.Children.Add(new CheckBox { Content = "WPF" });
+            expanderPanel.Children.Add(new CheckBox { Content = "ASP.NET" });
+
+            System.Windows.Controls.Expander expander = new System.Windows.Controls.Expander();
+            expander.Header = "Choice your technology";
+            expander.Content = expanderPanel;
+
+            stackPanel.Children.Add(expander);
+        }
+
+        // event handlers:
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+            ((System.Windows.Controls.Expander)sender).Content = new Button() { Width = 80, Height = 30, Content = "Hello" };
+        }
+
+        private void Expander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Expander colapsed");
+        }
+    }
+}
