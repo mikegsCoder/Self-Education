400de685f947f694a4fa79fae4db2dffce425722
mikegsCoder <mikegsid@gmail.com>
2023-03-12 11:27:57 +0200

    Add ScrollViewer

---
+++ WPF/04. Controls/02. Controls/ScrollViewer/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace ScrollViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handlers:
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.LineUp();
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.LineDown();
+        }
+    }
+}
