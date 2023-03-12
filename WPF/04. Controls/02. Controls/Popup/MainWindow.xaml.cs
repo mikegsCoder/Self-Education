c0e50a4916d5df8b9e85368cc37b4e6df7e4ee39
mikegsCoder <mikegsid@gmail.com>
2023-03-12 11:23:37 +0200

    Add Popup

---
+++ WPF/04. Controls/02. Controls/Popup/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Popup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            popup1.IsOpen = true;
+        }
+    }
+}
