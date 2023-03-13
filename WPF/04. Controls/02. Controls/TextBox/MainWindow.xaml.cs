2615cd3f195ad904e3d30351546902f7583fdccd
mikegsCoder <mikegsid@gmail.com>
2023-03-13 17:34:48 +0200

    Add TextBox

---
+++ WPF/04. Controls/02. Controls/TextBox/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace TextBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handlers:
+        private void TextBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.TextBox textBox = (System.Windows.Controls.TextBox)sender;
+            MessageBox.Show(textBox.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.SelectionStart = 5;
+            textBox1.SelectionLength = 10;
+            textBox1.Focus();
+
+            //textBox1.Select(5, 10);
+        }
+    }
+}
