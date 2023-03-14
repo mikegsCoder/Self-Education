360a1a813a91ac009f58b3f696d66d5438a232ea
mikegsCoder <mikegsid@gmail.com>
2023-03-14 18:47:17 +0200

    Add TabControl

---
+++ WPF/04. Controls/02. Controls/TabControl/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TabControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Examle3();
+        }
+
+        private void Examle3()
+        {
+            ListBox notesList = new ListBox();
+
+            notesList.Items.Add("Macbook Pro");
+            notesList.Items.Add("HP Pavilion 5478");
+            notesList.Items.Add("Acer LK-08");
+
+            products1.Items.Add(new TabItem
+            {
+                Header = new TextBlock { Text = "Notebooks" }, // set header
+                Content = notesList // set content
+            });
+        }
+    }
+}
