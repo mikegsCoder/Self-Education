1e4e172b72951497230997ecf833d15fc14b6415
mikegsCoder <mikegsid@gmail.com>
2023-03-15 19:52:09 +0200

    Add TreeView

---
+++ WPF/04. Controls/02. Controls/TreeView/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handlers:
+        private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvItem = (TreeViewItem)sender;
+            MessageBox.Show("Item " + tvItem.Header.ToString() + " expanded");
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvItem = (TreeViewItem)sender;
+            MessageBox.Show("Selected item: " + tvItem.Header.ToString());
+        }
+    }
+}
