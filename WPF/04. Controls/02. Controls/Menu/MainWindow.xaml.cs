4465e913f97927c831fd97f1d436b70b00a16981
mikegsCoder <mikegsid@gmail.com>
2023-03-15 19:48:25 +0200

    Add Menu

---
+++ WPF/04. Controls/02. Controls/Menu/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Menu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+        }
+
+        // event handler
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            MessageBox.Show(menuItem.Header.ToString());
+        }
+    }
+}
