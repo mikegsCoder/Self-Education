ccb82365159ec301be762894ddeae5e6e8b26365
mikegsCoder <mikegsid@gmail.com>
2023-03-16 18:02:41 +0200

    Add ProgressBar

---
+++ WPF/04. Controls/02. Controls/ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ProgressBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handler
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
+            info.Content = e.NewValue.ToString();
+            progressBar.Value = e.NewValue;
+        }
+    }
+}
