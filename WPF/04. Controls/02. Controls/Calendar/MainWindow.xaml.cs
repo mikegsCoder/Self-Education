0c91c9b3cbdb48bb0ffe5ee4c17a11c8323a8fba
mikegsCoder <mikegsid@gmail.com>
2023-03-16 18:05:09 +0200

    Add Calendar

---
+++ WPF/04. Controls/02. Controls/Calendar/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handler:
+        private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime? selectedDate = calendar1.SelectedDate;
+
+            MessageBox.Show(selectedDate.Value.Date.ToShortDateString());
+        }
+    }
+}
