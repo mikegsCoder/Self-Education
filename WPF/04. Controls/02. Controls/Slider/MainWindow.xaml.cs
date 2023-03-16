dc34141396598c148813753766b14bd25d7c92f9
mikegsCoder <mikegsid@gmail.com>
2023-03-16 18:04:07 +0200

    Add Slider

---
+++ WPF/04. Controls/02. Controls/Slider/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Slider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //event handler:
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((System.Windows.Controls.Slider)sender).SelectionEnd = e.NewValue;
+            info.Content = e.NewValue.ToString();
+        }
+    }
+}
