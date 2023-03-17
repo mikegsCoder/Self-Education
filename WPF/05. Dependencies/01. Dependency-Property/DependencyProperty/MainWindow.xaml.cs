f0f9136b0a91aeae3adb6fdb9e7f4e4ed75aca1c
mikegsCoder <mikegsid@gmail.com>
2023-03-17 18:33:58 +0200

    Add Dependency Property

---
+++ WPF/05. Dependencies/01. Dependency-Property/DependencyProperty/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DependencyProperty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Example();
+        }
+
+        private void Example()
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Hello";
+            string text = (string)textBlock.ReadLocalValue(TextBlock.TextProperty); // Hello
+            // textBlock.ClearValue(TextBlock.TextProperty); // value cleared
+
+            grid.Children.Add(textBlock);
+        }
+    }
+}
