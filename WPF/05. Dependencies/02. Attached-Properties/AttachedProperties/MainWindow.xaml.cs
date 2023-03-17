d681ed8f91ccb9789644de6cc71e1c6ffc457e3f
mikegsCoder <mikegsid@gmail.com>
2023-03-17 18:35:32 +0200

    Add Attached Properties

---
+++ WPF/05. Dependencies/02. Attached-Properties/AttachedProperties/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AttachedProperties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetPosition();
+            ShowPosition();
+        }
+
+        private void SetPosition()
+        {
+            Grid.SetRow(button1, 1); // second row
+            Grid.SetColumn(button1, 1); // second column
+        }
+
+        private int GetRow()
+        {
+            return (int)button1.GetValue(Grid.RowProperty);
+        }
+
+        private int GetCol()
+        {
+            return (int)button1.GetValue(Grid.ColumnProperty);
+        }
+
+        private void ShowPosition()
+        {
+            MessageBox.Show($"Current position: row = {GetRow()}, column = {GetCol()}.");
+        }
+    }
+}
