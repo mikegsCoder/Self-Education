6b4bc3c677c3d53afd1e758c5c190171634d3661
mikegsCoder <mikegsid@gmail.com>
2023-03-18 12:45:32 +0200

    Add Routed Events

---
+++ WPF/06. Events/01. Routed-Events/RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button1.Click += Button1_Click;
+        }
+
+        // event handler from XAML:
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi from Button_Click.");
+        }
+
+        // event handler included in constructor:
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi from Button1_Click.");
+        }
+    }
+}
