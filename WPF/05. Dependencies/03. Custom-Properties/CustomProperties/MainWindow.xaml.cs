73300a9a62626d11f72b4a8af777476fea6e5d78
mikegsCoder <mikegsid@gmail.com>
2023-03-18 12:43:14 +0200

    Add Custom Properties

---
+++ WPF/05. Dependencies/03. Custom-Properties/CustomProperties/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace CustomProperties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handler:
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Phone phone = (Phone)this.Resources["iPhone6s"]; // get resource by key
+            MessageBox.Show(phone.Price.ToString());
+        }
+    }
+}
---
+++ WPF/05. Dependencies/03. Custom-Properties/CustomProperties/Phone.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+
+namespace CustomProperties
+{
+    public class Phone : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PriceProperty;
+
+        // static constructor:
+        static Phone()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Phone));
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone), metadata,
+                new ValidateValueCallback(ValidateValue));
+        }
+
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            int currentValue = (int)baseValue;
+
+            if (currentValue > 1000)
+            {
+                return 1000;
+            }
+
+            return currentValue;
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+
+            if (currentValue >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+    }
+}
