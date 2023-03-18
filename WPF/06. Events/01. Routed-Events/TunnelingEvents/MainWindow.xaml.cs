d05d833f05d24c1c5af50e226eb591d576828f48
mikegsCoder <mikegsid@gmail.com>
2023-03-18 12:48:41 +0200

    Add Tunneling Events

---
+++ WPF/06. Events/01. Routed-Events/TunnelingEvents/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TunnelingEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handler:
+        private void Control_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBlock1.Text = textBlock1.Text + "sender: " + sender.ToString() + "\n";
+            textBlock1.Text = textBlock1.Text + "source: " + e.Source.ToString() + "\n\n";
+        }
+    }
+}
