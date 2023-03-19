b29eb1559972b72af12802005efd9f0f23023516
mikegsCoder <mikegsid@gmail.com>
2023-03-19 11:44:56 +0200

    Add Attached Events

---
+++ WPF/06. Events/01. Routed-Events/AttachedEvents/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AttachedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // attach handler:
+            menuSelector.AddHandler(RadioButton.CheckedEvent, new RoutedEventHandler(RadioButton_Click));
+        }
+
+        // event handler:
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton selectedRadio = (RadioButton)e.Source;
+            textBlock1.Text = "Your choice is: " + selectedRadio.Content.ToString();
+        }
+    }
+}
