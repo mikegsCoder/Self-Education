4cdf70d8fcc85c41c466dca3c632e1c48bc54d00
mikegsCoder <mikegsid@gmail.com>
2023-03-20 17:38:56 +0200

    Add Command Routing

---
+++ WPF/08. Commands/01. Command/CommandRouting/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CommandRouting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Application Help");
+        }
+    }
+}
