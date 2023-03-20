796460d3374fec9e9608d8501786f52a484c594e
mikegsCoder <mikegsid@gmail.com>
2023-03-20 17:36:37 +0200

    Add Workshop - Text File Editor

---
+++ WPF/07. Workshop/01. Text-File-Editor/TextFileEditor/FileManager.cs
@@ -0,0 +1,102 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows;
+
+namespace TextFileEditor
+{
+    public class FileManager
+    {
+        private static string filePath = @"../../../";
+        private string textFile = filePath + "TextFile.txt";
+        private string zipFile = filePath + "TextFile.zip";
+
+        public string ReadAllText()
+        {
+            if (!File.Exists(textFile))
+            {
+                MessageBox.Show("File does not exist. Get it from the archive.");
+
+                return null;
+            }
+
+            return File.ReadAllText(textFile);
+        }
+
+        public void SaveAllText(string text) 
+        {
+            if (!File.Exists(textFile))
+            {
+                MessageBox.Show("File does not exist. Get it from the archive.");
+                return;
+            }
+
+            File.WriteAllText(textFile, text);
+
+            MessageBox.Show("Your text is saved successfully.");
+        }
+
+        public void AddToZip()
+        {
+            if (!File.Exists(textFile))
+            {
+                MessageBox.Show("File does not exist. Get it from the archive.");
+
+                return;
+            }
+
+            AddFilesToZip(zipFile, new string[] { textFile });
+
+            MessageBox.Show("TextFile added to zip successfully.");
+        }
+
+        public void ExtractFromZip()
+        {
+            if (!File.Exists(zipFile))
+            {
+                MessageBox.Show("Zip archive does not exist. Create it from the TextFile.");
+
+                return;
+            }
+
+            ExtractFileFromZip(zipFile, filePath);
+
+            MessageBox.Show("TextFile extracted from zip successfully.");
+        }
+
+        private void ExtractFileFromZip(string zipFile, string filePath)
+        {
+            if (File.Exists(textFile))
+            {
+                File.Delete(textFile);
+            }
+
+            ZipFile.ExtractToDirectory(zipFile, filePath);
+        }
+
+        private void AddFilesToZip(string zipFile, string[] files)
+        {
+            if (files == null || files.Length == 0)
+            {
+                return;
+            }
+
+            using (var zipArchive = ZipFile.Open(zipFile, ZipArchiveMode.Update))
+            {
+                foreach (var file in files)
+                {
+                    var fileInfo = new FileInfo(file);
+
+                    var existing = zipArchive.Entries.FirstOrDefault(e => e.Name == fileInfo.Name);
+
+                    if (existing != null)
+                    {
+                        existing.Delete();
+                    }
+
+                    zipArchive.CreateEntryFromFile(fileInfo.FullName, fileInfo.Name);
+                }
+            }
+        }
+    }
+}
---
+++ WPF/07. Workshop/01. Text-File-Editor/TextFileEditor/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+
+namespace TextFileEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FileManager fileManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fileManager = new FileManager();
+        }
+
+        private void readBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string text = fileManager.ReadAllText();
+
+            if (text != null)
+            {
+                textBox.Text = text;
+            }
+        }
+
+        private void writeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                MessageBox.Show("Your text is empty. Please write something.");
+
+                return;
+            }
+
+            fileManager.SaveAllText(text);
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+        }
+
+        private void addToZipBtn_Click(object sender, RoutedEventArgs e)
+        {
+            fileManager.AddToZip();
+        }
+
+        private void unzipBtn_Click(object sender, RoutedEventArgs e)
+        {
+            fileManager.ExtractFromZip();
+        }
+    }
+}
