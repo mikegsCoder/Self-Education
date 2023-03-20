3d91ea3dbead7947b90559b7272cd941318e6c5e
mikegsCoder <mikegsid@gmail.com>
2023-03-20 17:32:02 +0200

    Add Mouse Events

---
+++ WPF/06. Events/03. Mouse-Events/MouseEvents/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MouseEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // event handlers:
+        private void textBox1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(textBox1, textBox1.Text, DragDropEffects.Copy);
+        }
+
+        private void button1_Drop(object sender, DragEventArgs e)
+        {
+            button1.Content = e.Data.GetData(DataFormats.Text);
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Got Focus");
+        }
+    }
+}
