d9dce119ad95e7b45c97915f1c3a21137151495b
mikegsCoder <mikegsid@gmail.com>
2023-03-21 16:30:33 +0200

    Add Custom Exit Command

---
+++ WPF/08. Commands/02. Custom-Exit-Command/CustomCommand/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CustomCommand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine("Application exit: " + DateTime.Now.ToShortDateString() + " " +
+                DateTime.Now.ToLongTimeString());
+
+                writer.Flush();
+            }
+
+            this.Close();
+        }
+    }
+}
---
+++ WPF/08. Commands/02. Custom-Exit-Command/CustomCommand/WindowCommands.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace CustomCommand
+{
+    public class WindowCommands
+    {
+        static WindowCommands()
+        {
+            Exit = new RoutedCommand("Exit", typeof(MainWindow));
+        }
+
+        public static RoutedCommand Exit { get; set; }
+    }
+}
