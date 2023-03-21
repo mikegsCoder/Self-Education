6c0a57bcdbdbfc848b4fa1bebbb38ee92e064928
mikegsCoder <mikegsid@gmail.com>
2023-03-21 16:39:25 +0200

    Add Brushes

---
+++ WPF/09. Brushes/01. Brushes-Examples/SolidColorBrush/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace SolidColorBrush
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            button1.Background = new System.Windows.Media.SolidColorBrush(Colors.Blue);
+            button1.Foreground = new System.Windows.Media.SolidColorBrush(Colors.Yellow);
+
+            // or
+            // button1.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(207, 255, 255));
+        }
+    }
+}
