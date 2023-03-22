c21efd2304aed482b03010aae636000dea3c9c9b
mikegsCoder <mikegsid@gmail.com>
2023-03-22 17:29:11 +0200

    Add Resource Button

---
+++ WPF/10. Resources/01. Resources/ResourceButton/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ResourceButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
