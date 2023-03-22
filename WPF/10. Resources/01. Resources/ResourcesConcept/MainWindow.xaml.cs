7980c6f8c3fbc8b4719af24021099149db15fb81
mikegsCoder <mikegsid@gmail.com>
2023-03-22 17:27:02 +0200

    Add Resources

---
+++ WPF/10. Resources/01. Resources/ResourcesConcept/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ResourcesConcept
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetBuuton2Resources();
+        }
+
+        private void SetBuuton2Resources()
+        {
+            // set resource object
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.LightGray, 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.White, 1));
+
+            // add resource to the resource dictionary
+            this.Resources.Add("buttonGradientBrush", gradientBrush);
+
+            // set resource to the button
+            button2.Background = (Brush)this.TryFindResource("buttonGradientBrush");
+            // or like this
+            // button1.Background = (Brush)this.Resources["buttonGradientBrush"];
+        }
+    }
+}
