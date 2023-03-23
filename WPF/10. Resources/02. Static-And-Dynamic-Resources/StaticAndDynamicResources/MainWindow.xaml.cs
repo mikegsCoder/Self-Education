57ac7d5871e54a9254b31a3e9cf39ed5d45afe3a
mikegsCoder <mikegsid@gmail.com>
2023-03-23 17:15:16 +0200

    Add Static and Dynamic Resources

---
+++ WPF/10. Resources/02. Static-And-Dynamic-Resources/StaticAndDynamicResources/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace StaticAndDynamicResources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            // this will work with static resource
+            SolidColorBrush buttonBrush = (SolidColorBrush)this.TryFindResource("buttonBrush1");
+            
+            buttonBrush.Color = Colors.LimeGreen;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            // this will work only with dynamic resource
+            this.Resources["buttonBrush2"] = new SolidColorBrush(Colors.LimeGreen);
+        }
+    }
+}
