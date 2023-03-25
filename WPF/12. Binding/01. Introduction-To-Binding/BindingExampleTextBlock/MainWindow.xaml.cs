e0ed11bb6c2bf37ed0fbdf2c907138e788586e22
mikegsCoder <mikegsid@gmail.com>
2023-03-25 10:21:38 +0200

    Add Introduction to binding

---
+++ WPF/12. Binding/01. Introduction-To-Binding/BindingExampleTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BindingExampleTextBlock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bind();
+
+            // clear one binding:
+            // BindingOperations.ClearBinding(myTextBlock2, TextBlock.TextProperty);
+            
+            // clear all bindings:
+            // BindingOperations.ClearAllBindings(myTextBlock2);
+        }
+
+        private void Bind()
+        {
+            Binding binding = new Binding();
+
+            binding.ElementName = "myTextBox"; // source
+            binding.Path = new PropertyPath("Text"); // source property
+            myTextBlock2.SetBinding(TextBlock.TextProperty, binding); // set binding
+        }
+    }
+}
+
