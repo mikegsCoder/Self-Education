24ff68fa790879693b066079ae03a1537f26ae10
mikegsCoder <mikegsid@gmail.com>
2023-03-26 12:27:29 +0300

    Add Notify Property Changed Interface

---
+++ WPF/12. Binding/02. Notify-Property-Changed-Interface/NotifyPropertyChanged/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace NotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Phone phone = (Phone)this.Resources["nexusPhone"];
+
+            if (phone.Company == "Google")
+            {
+                phone.Company = "LG"; // change from Google to LG
+            }
+            else
+            {
+                phone.Company = "Google"; // change from LG to Google
+            }
+        }
+    }
+}
