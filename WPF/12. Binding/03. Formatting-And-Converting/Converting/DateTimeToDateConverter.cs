57249220ebc28b91c28653e7e4e843d45e742e41
mikegsCoder <mikegsid@gmail.com>
2023-03-27 17:53:40 +0300

    Add Converting

---
+++ WPF/12. Binding/03. Formatting-And-Converting/Converting/DateTimeToDateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converting
+{
+    public class DateTimeToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null && parameter.ToString() == "EN")
+            {
+                return ((DateTime)value).ToString("MM-dd-yyyy");
+            }
+
+            return ((DateTime)value).ToString("dd.MM.yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
