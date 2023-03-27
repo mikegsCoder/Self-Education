7aef2ff55404dd8047b62d67b56b1307ac608b9a
mikegsCoder <mikegsid@gmail.com>
2023-03-27 17:57:48 +0300

    Add Event Setter

---
+++ WPF/13. Styles-Triggers-And-Themes/01. Styles/EventSetter/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EventSetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            MessageBox.Show(clickedButton.Content.ToString());
+        }
+    }
+}
