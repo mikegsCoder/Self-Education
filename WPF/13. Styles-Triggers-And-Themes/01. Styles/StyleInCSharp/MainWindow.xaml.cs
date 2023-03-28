2444b56ee0dc5bde2f7bdeceeb1ddee8a62f7ad8
mikegsCoder <mikegsid@gmail.com>
2023-03-28 16:58:34 +0300

    Add Style in C#

---
+++ WPF/13. Styles-Triggers-And-Themes/01. Styles/StyleInCSharp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace StyleInCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StyleButtons();
+        }
+
+        private void StyleButtons()
+        {
+            Style buttonStyle = new Style();
+            buttonStyle.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            buttonStyle.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
+            buttonStyle.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Black) });
+            buttonStyle.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.White) });
+            buttonStyle.Setters.Add(new EventSetter { Event = Button.ClickEvent, Handler = new RoutedEventHandler(Button_Click) });
+
+            button1.Style = buttonStyle;
+            button2.Style = buttonStyle;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            MessageBox.Show(clickedButton.Content.ToString());
+        }
+    }
+}
