7c774196945cb71258203f5abbc97dbbb428a15e
mikegsCoder <mikegsid@gmail.com>
2023-03-30 19:33:10 +0300

    Add Application Events

---
+++ WPF/15. Class-Application/02. Work-With-Class-Application/ApplicationEvents/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace ApplicationEvents
+{
+    public partial class App : Application
+    {
+        // start one copy of the application
+        System.Threading.Mutex mutex;
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool createdNew;
+
+            string mutName = "Application";
+
+            mutex = new System.Threading.Mutex(true, mutName, out createdNew);
+            
+            if (!createdNew)
+            {
+                this.Shutdown();
+            }
+        }
+    }
+}
