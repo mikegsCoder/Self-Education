3597916f4f515c01dc694c202685a60792a310bf
mikegsCoder <mikegsid@gmail.com>
2023-03-30 19:34:04 +0300

    Add Application Current

---
+++ WPF/15. Class-Application/02. Work-With-Class-Application/ApplicationCurrent/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ApplicationCurrent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                MessageBox.Show(window.ToString());
+            }
+        }
+    }
+}
