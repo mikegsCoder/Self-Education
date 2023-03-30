32de60039b2275b0a44f25bee372361339798eda
mikegsCoder <mikegsid@gmail.com>
2023-03-30 19:35:02 +0300

    Add Shutdown Mode

---
+++ WPF/15. Class-Application/02. Work-With-Class-Application/ShutdownMode/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ShutdownMode
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // this.ShutdownMode = ShutdownMode.OnLastWindowClose;
+    }
+}
