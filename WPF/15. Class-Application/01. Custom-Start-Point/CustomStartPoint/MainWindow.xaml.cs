5bb5bb4a94d7e6c23b653c2392ed3a36a0c89e9f
mikegsCoder <mikegsid@gmail.com>
2023-03-30 19:31:53 +0300

    Add Custom Start Point

---
+++ WPF/15. Class-Application/01. Custom-Start-Point/CustomStartPoint/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace CustomStartPoint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [STAThread]
+        static void Main()
+        {
+            App app = new App();
+            MainWindow window = new MainWindow();
+
+            window.Title = "Custom Start Point";
+
+            app.Run(window);
+        }
+    }
+}
