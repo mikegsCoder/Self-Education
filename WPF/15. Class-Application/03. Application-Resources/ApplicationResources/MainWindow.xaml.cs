b4b2471018e86ca1c1b89cec24228d7787d15079
mikegsCoder <mikegsid@gmail.com>
2023-03-31 17:27:19 +0300

    Add Application Sources

---
+++ WPF/15. Class-Application/03. Application-Resources/ApplicationResources/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ApplicationResources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kitty.Source = new BitmapImage(new Uri("Images/kitty.jpg", UriKind.Relative));
+        }
+    }
+}
