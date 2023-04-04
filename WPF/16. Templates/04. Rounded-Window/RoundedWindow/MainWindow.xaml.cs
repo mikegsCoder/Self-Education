928bd9bbcc24916206d4e608b1a3fc87d399f3e1
mikegsCoder <mikegsid@gmail.com>
2023-04-04 16:35:12 +0300

    Add Rounded Window

---
+++ WPF/16. Templates/04. Rounded-Window/RoundedWindow/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RoundedWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // used to move window thru it's title
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
