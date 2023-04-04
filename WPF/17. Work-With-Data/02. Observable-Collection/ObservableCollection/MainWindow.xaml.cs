83cd8939d9eaf5659f1f6ab872ca33c844b37e6d
mikegsCoder <mikegsid@gmail.com>
2023-04-04 16:38:27 +0300

    Add Observable Collection

---
+++ WPF/17. Work-With-Data/02. Observable-Collection/ObservableCollection/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ObservableCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> phones;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            phones = new ObservableCollection<string> { "iPhone 6S Plus", "Nexus 6P", "Galaxy S7 Edge" };
+            
+            phonesList.ItemsSource = phones;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string phone = phoneTextBox.Text;
+
+            // add new phone
+            phones.Add(phone);
+        }
+    }
+}
