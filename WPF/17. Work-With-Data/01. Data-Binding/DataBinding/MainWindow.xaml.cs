7e37100846a463bf1518e072d6a9b82d470b5415
mikegsCoder <mikegsid@gmail.com>
2023-04-04 16:37:02 +0300

    Add Data Binding

---
+++ WPF/17. Work-With-Data/01. Data-Binding/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Phone MyPhone { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyPhone = new Phone
+            {
+                Name = "Lumia 630",
+                Company = new Company { Title = "Microsoft" },
+                Price = 500
+            };
+
+            this.DataContext = MyPhone;
+        }
+    }
+}
