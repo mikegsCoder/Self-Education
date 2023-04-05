3d5e6a69e3afadbd960ad138d6f0f5e45be4c8b4
mikegsCoder <mikegsid@gmail.com>
2023-04-05 17:43:32 +0300

    Add Item Template

---
+++ WPF/17. Work-With-Data/03. Item-Template/ItemTemplateAndDataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItemTemplateAndDataTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone {Id=1, ImagePath="/Images/iPhone6s.jpg", Title="iPhone 6S", Company="Apple" },
+                new Phone {Id=2, ImagePath="/Images/Lumia950.jpg", Title="Lumia 950", Company="Microsoft" },
+                new Phone {Id=3, ImagePath="/Images/Nexus5x.jpg", Title="Nexus 5X", Company="Google" },
+                new Phone {Id=4, ImagePath="/Images/Galaxys6.jpg", Title="Galaxy S6", Company="Samsung"}
+            };
+
+            phonesList.ItemsSource = Phones;
+        }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Phone p = (Phone)phonesList.SelectedItem;
+
+            MessageBox.Show(p.Title);
+        }
+    }
+}
---
+++ WPF/17. Work-With-Data/03. Item-Template/ItemTemplateAndDataTemplate/Phone.cs
@@ -0,0 +1,13 @@
+﻿namespace ItemTemplateAndDataTemplate
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; } // model
+
+        public string? Company { get; set; } // brand
+
+        public string? ImagePath { get; set; } // image uri
+    }
+}
