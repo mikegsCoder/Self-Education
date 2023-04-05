4d2a595faf297a968e9a0a3f19e08a0c4f882c52
mikegsCoder <mikegsid@gmail.com>
2023-04-05 17:44:37 +0300

    Add Data Triggers

---
+++ WPF/17. Work-With-Data/03. Item-Template/DataTriggers/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataTriggers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone {Id=1, ImagePath="/Images/iPhone6s.jpg", Title="iPhone 6S", Company="Apple" },
+                new Phone {Id=2, ImagePath="/Images/Lumia950.jpg", Title="Lumia 950", Company="Microsoft" },
+                new Phone {Id=3, ImagePath="/Images/Nexus5x.jpg", Title="Nexus 5X", Company="Google" },
+                new Phone {Id=4, ImagePath="/Images/Galaxys6.jpg", Title="Galaxy S6", Company="Samsung"}
+            };
+
+            phonesList.ItemsSource = Phones;
+        }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Phone p = (Phone)phonesList.SelectedItem;
+
+            MessageBox.Show(p.Title);
+        }
+    }
+}
