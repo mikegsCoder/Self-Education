b96745ed4acd2af223b38e52834f108eaeadba8b
mikegsCoder <mikegsid@gmail.com>
2023-04-06 16:40:09 +0300

    Add Data Providers

---
+++ WPF/17. Work-With-Data/05. Data-Providers/ObjectDataProvider/Phone.cs
@@ -0,0 +1,11 @@
+﻿namespace ObjectDataProvider
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; } // model
+
+        public string? Company { get; set; } // brand
+    }
+}
---
+++ WPF/17. Work-With-Data/05. Data-Providers/ObjectDataProvider/PhoneRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDataProvider
+{
+    public class PhoneRepository
+    {
+        private ObservableCollection<Phone> phones;
+
+        public PhoneRepository()
+        {
+            phones = new ObservableCollection<Phone>
+            {
+                new Phone {Id=1, Title="iPhone 6S", Company="Apple" },
+                new Phone {Id=2, Title="Lumia 950", Company="Microsoft" },
+                new Phone {Id=3, Title="Nexus 5X", Company="Google" },
+                new Phone {Id=4, Title="Galaxy S6", Company="Samsung"}
+            };
+        }
+
+        public ObservableCollection<Phone> GetPhones()
+        {
+            return phones;
+        }
+    }
+}
