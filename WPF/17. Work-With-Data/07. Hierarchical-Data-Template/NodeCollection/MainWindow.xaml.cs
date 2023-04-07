f96c641c45a771915e78811eb4119dac72bf946c
mikegsCoder <mikegsid@gmail.com>
2023-04-07 18:56:05 +0300

    Add Node Collection

---
+++ WPF/17. Work-With-Data/07. Hierarchical-Data-Template/NodeCollection/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace NodeCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Node> nodes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            nodes = new ObservableCollection<Node>
+            {
+                new Node
+                {
+                    Name ="Europe",
+                    Nodes = new ObservableCollection<Node>
+                    {
+                        new Node {Name="Germany" },
+                        new Node {Name="France" },
+                        new Node
+                        {
+                            Name ="Great Britain",
+                            Nodes = new ObservableCollection<Node>
+                            {
+                                new Node {Name="England" },
+                                new Node {Name="Scotland" },
+                                new Node {Name="Wales" },
+                                new Node {Name="Ireland" },
+                            }
+                        }
+                    }
+                },
+                new Node
+                {
+                    Name ="Asia",
+                    Nodes = new ObservableCollection<Node>
+                    {
+                        new Node {Name="China" },
+                        new Node {Name="Japan" },
+                        new Node { Name ="India" }
+                    }
+                },
+                new Node { Name="Africa" },
+                new Node { Name="America" },
+                new Node { Name="Australia" }
+            };
+
+            treeView1.ItemsSource = nodes;
+        }
+    }
+}
---
+++ WPF/17. Work-With-Data/07. Hierarchical-Data-Template/NodeCollection/Node.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NodeCollection
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public ObservableCollection<Node> Nodes { get; set; }
+    }
+}
