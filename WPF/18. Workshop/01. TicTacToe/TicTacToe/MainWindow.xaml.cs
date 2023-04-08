ab4bea7ff92db5f15d2d832c75a090b0a874e3ba
mikegsCoder <mikegsid@gmail.com>
2023-04-08 12:09:22 +0300

    Add instance of GameBoard into MainWindow.xaml.cs

--- WPF/18. Workshop/01. TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ WPF/18. Workshop/01. TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -9,9 +9,13 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        public GameBoard MyGameBoard = new GameBoard();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = MyGameBoard;
         }
 
         public void Button_Click(object sender, RoutedEventArgs e)
