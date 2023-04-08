d2c27dbcd9826e6bc26d93e04566a3916267e2a2
mikegsCoder <mikegsid@gmail.com>
2023-04-08 11:59:03 +0300

    Add TicTacToe initial commit

---
+++ WPF/18. Workshop/01. TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+    }
+}
