eb60a520da09488a07f122b2083dde0a9075856d
mikegsCoder <mikegsid@gmail.com>
2023-04-08 12:11:04 +0300

    Add implementation for Button_Click event handler

--- WPF/18. Workshop/01. TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ WPF/18. Workshop/01. TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -20,7 +20,23 @@ namespace TicTacToe
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
- 
+            //Updates UI and calls gameBoard update
+            var clickedButton = sender as Button;
+
+            if (MyGameBoard.currentPlayer == CurrentPlayer.X)
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#811717"));
+            }
+            else
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#126712"));
+            }
+
+            clickedButton.Background = Brushes.WhiteSmoke;
+            clickedButton.Content = MyGameBoard.currentPlayer;
+            clickedButton.IsHitTestVisible = false;
+
+            MyGameBoard.UpdateBoard(clickedButton.Name);
         }
 
         private void Restart_Click(object sender, RoutedEventArgs e)
