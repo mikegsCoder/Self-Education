92145ec0f744356007314c59b9e821547dd8f0f3
mikegsCoder <mikegsid@gmail.com>
2023-04-10 17:59:21 +0300

    Add Load and Save Data

---
+++ WPF/19. Documents/02. Flow-Document-Containers/LoadAndSaveData/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace LoadAndSaveData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "../../../mydoc.xaml";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Create))
+            {
+                if (docViewer.Document != null)
+                {
+                    XamlWriter.Save(docViewer.Document, fs);
+
+                    MessageBox.Show("File saved.");
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                FlowDocument document = XamlReader.Load(fs) as FlowDocument;
+
+                if (document != null)
+                {
+                    docViewer.Document = document;
+                }
+            }
+        }
+    }
+}
