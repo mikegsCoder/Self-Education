fceeea2f158f86a6519c1a84c50d53b48f14e194
mikegsCoder <mikegsid@gmail.com>
2023-04-10 18:02:02 +0300

    Add Fixed Document

---
+++ WPF/19. Documents/04. Fixed-Document/FixedDocument/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Xps.Packaging;
+using System.Windows.Xps;
+
+namespace FixedDocument
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "XPS Files (*.xps)|*.xps";
+
+            if (sfd.ShowDialog() == true)
+            {
+                XpsDocument doc = new XpsDocument(sfd.FileName, FileAccess.Write);
+                
+                XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
+                
+                writer.Write(documentViewer.Document as System.Windows.Documents.FixedDocument);
+                
+                doc.Close();
+            }
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            ofd.Filter = "XPS Files (*.xps)|*.xps";
+
+            if (ofd.ShowDialog() == true)
+            {
+                XpsDocument doc = new XpsDocument(ofd.FileName, FileAccess.Read);
+                
+                documentViewer.Document = doc.GetFixedDocumentSequence();
+            }
+        }
+    }
+}
