d7945db3d0c11a083bcdff3233587a654cf6117f
mikegsCoder <mikegsid@gmail.com>
2023-04-11 16:35:23 +0300

    Add Ellipse in C#

---
+++ WPF/20. Work-With-Graphics/01. Figures/EllipseInCSharp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EllipseInCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CreateEllipse();
+        }
+
+        private void CreateEllipse()
+        {
+            Ellipse el = new Ellipse();
+            el.Width = 200;
+            el.Height = 200;
+            el.VerticalAlignment = VerticalAlignment.Center;
+            el.Fill = Brushes.Green;
+            el.Stroke = Brushes.Red;
+            el.StrokeThickness = 3;
+            grid1.Children.Add(el);
+        }
+    }
+}
