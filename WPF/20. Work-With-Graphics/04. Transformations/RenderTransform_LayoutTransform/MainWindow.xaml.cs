c8de44f82df0bfd0ab044a77ec9993ab823a2eff
mikegsCoder <mikegsid@gmail.com>
2023-04-14 09:07:41 +0300

    Add RenderTransform LayoutTransform

---
+++ WPF/20. Work-With-Graphics/04. Transformations/RenderTransform_LayoutTransform/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RenderTransform_LayoutTransform
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
