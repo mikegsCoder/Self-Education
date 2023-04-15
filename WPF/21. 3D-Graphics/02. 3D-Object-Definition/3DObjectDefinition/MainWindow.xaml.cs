ca50ed59558b916bb4a2a789e4caefe33886bbba
mikegsCoder <mikegsid@gmail.com>
2023-04-15 13:52:15 +0300

    Add 3D Object Definition

---
+++ WPF/21. 3D-Graphics/02. 3D-Object-Definition/3DObjectDefinition/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _3DObjectDefinition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
