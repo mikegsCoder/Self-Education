a3a837419825dacbcdc1669f1ac2c27a6e4d4c58
mikegsCoder <mikegsid@gmail.com>
2023-04-16 11:03:58 +0300

    Add Cube

---
+++ WPF/21. 3D-Graphics/05. Cube/Cube/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Cube
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Vector3D CreateNormal(Point3D p0, Point3D p1, Point3D p2)
+        {
+            Vector3D v0 = new Vector3D(p1.X - p0.X, p1.Y - p0.Y, p1.Z - p0.Z);
+            Vector3D v1 = new Vector3D(p2.X - p1.X, p2.Y - p1.Y, p2.Z - p1.Z);
+
+            return Vector3D.CrossProduct(v0, v1);
+        }
+    }
+}
