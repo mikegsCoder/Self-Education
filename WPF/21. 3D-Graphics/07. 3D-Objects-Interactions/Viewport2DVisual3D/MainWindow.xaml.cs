51a23df2ba8dad645f1dbdac4e6f474e5ada40a7
mikegsCoder <mikegsid@gmail.com>
2023-04-17 10:16:06 +0300

    Add 3D Objects Interactions

---
+++ WPF/21. 3D-Graphics/07. 3D-Objects-Interactions/Viewport2DVisual3D/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Viewport2DVisual3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
