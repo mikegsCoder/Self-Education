9ac1f471bf0beb28b40d57a20431cb34fb9b416e
mikegsCoder <mikegsid@gmail.com>
2023-04-17 10:17:21 +0300

    Add Model UI Element 3D

---
+++ WPF/21. 3D-Graphics/07. 3D-Objects-Interactions/ModelUIElement3D/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ModelUIElement3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ModelUIElement3D_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse clicked.");
+        }
+    }
+}
