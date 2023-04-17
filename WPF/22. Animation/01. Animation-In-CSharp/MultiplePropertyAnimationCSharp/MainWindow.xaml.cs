e7ee24ce3aefa28c9ab48754076000ff88e4668a
mikegsCoder <mikegsid@gmail.com>
2023-04-17 10:20:39 +0300

    Add Multiple Property Animation C#

---
+++ WPF/22. Animation/01. Animation-In-CSharp/MultiplePropertyAnimationCSharp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace MultiplePropertyAnimationCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ButtonAnimation();
+        }
+
+        private void ButtonAnimation()
+        {
+            // widthAnimation
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = 70;
+            widthAnimation.To = 150;
+            widthAnimation.Duration = TimeSpan.FromSeconds(5);
+
+            // heightAnimation
+            DoubleAnimation heightAnimation = new DoubleAnimation();
+            heightAnimation.From = 30;
+            heightAnimation.To = 60;
+            heightAnimation.Duration = TimeSpan.FromSeconds(5);
+
+            helloButton.BeginAnimation(Button.WidthProperty, widthAnimation);
+            helloButton.BeginAnimation(Button.HeightProperty, heightAnimation);
+        }
+    }
+}
