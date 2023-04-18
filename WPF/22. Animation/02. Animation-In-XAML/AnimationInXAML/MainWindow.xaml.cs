17dfad51ce7d0a4b78cca62e6e43d5ff9510fd91
mikegsCoder <mikegsid@gmail.com>
2023-04-18 08:57:12 +0300

    Add Animation in XAML

---
+++ WPF/22. Animation/02. Animation-In-XAML/AnimationInXAML/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace AnimationInXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAnimation_Completed(object sender, EventArgs e)
+        {
+            MessageBox.Show("Animation completed.");
+        }
+    }
+}
