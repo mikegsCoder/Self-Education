d08ddd67a51dc85a3b9d3f00b120fb989eb9f119
mikegsCoder <mikegsid@gmail.com>
2023-04-19 10:10:00 +0300

    Add Animation Ease

---
+++ WPF/22. Animation/05. Animation-Ease/AnimationEase/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AnimationEase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
