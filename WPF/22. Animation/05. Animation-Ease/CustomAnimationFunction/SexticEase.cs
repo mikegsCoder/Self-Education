10dc0b20c0a1b70fca7742bd04349f8aec06e644
mikegsCoder <mikegsid@gmail.com>
2023-04-19 10:11:17 +0300

    Add Custom Animation Function

---
+++ WPF/22. Animation/05. Animation-Ease/CustomAnimationFunction/SexticEase.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media.Animation;
+using System.Windows;
+
+namespace CustomAnimationFunction
+{
+    public class SexticEase : EasingFunctionBase
+    {
+        protected override double EaseInCore(double normalizedTime)
+        {
+            return normalizedTime * normalizedTime * normalizedTime
+                   * normalizedTime * normalizedTime * normalizedTime;
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new SexticEase();
+        }
+    }
+}
