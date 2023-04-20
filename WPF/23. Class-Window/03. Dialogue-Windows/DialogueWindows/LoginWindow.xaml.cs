df179b2d256287e22262113868ae2b9241284a20
mikegsCoder <mikegsid@gmail.com>
2023-04-20 12:49:53 +0300

    Add Dialogue Windows

---
+++ WPF/23. Class-Window/03. Dialogue-Windows/DialogueWindows/LoginWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace DialogueWindows
+{
+    /// <summary>
+    /// Interaction logic for PasswordWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+        
+        public string Username
+        {
+            get { return usernameBox.Text; }
+        }
+
+        public string Password
+        {
+            get { return passwordBox.Text; }
+        }
+    }
+}
---
+++ WPF/23. Class-Window/03. Dialogue-Windows/DialogueWindows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace DialogueWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string username = "Peter";
+        private string password = "123456";    
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+
+            if (loginWindow.ShowDialog() == true)
+            {
+                if (loginWindow.Username == username && 
+                    loginWindow.Password == password)
+                {
+                    MessageBox.Show($"Welcome {username}.");
+                }
+                else
+                {
+                    MessageBox.Show("Wrong username or password.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You need to authenticate.");
+            }
+        }
+    }
+}
