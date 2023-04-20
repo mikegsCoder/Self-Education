b0b43c76d1077573c191ab948e2e8c1e32bb7e9c
mikegsCoder <mikegsid@gmail.com>
2023-04-20 12:47:08 +0300

    Add Window Lifecycle

---
+++ WPF/23. Class-Window/01. Window-Lifecycle/WindowLifecycle/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace WindowLifecycle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = @"../../../log.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+            Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Log("Loaded");
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Log("Closing");
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            Log("Closed");
+        }
+
+        private void Log(string eventName)
+        {
+            using (StreamWriter logger = new StreamWriter(path, true))
+            {
+                logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + eventName);
+            }
+        }
+    }
+}
