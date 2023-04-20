7dc04e9a349e731807e9f5b2a21410d573d623b7
mikegsCoder <mikegsid@gmail.com>
2023-04-20 12:48:26 +0300

    Add Windows Interaction

---
+++ WPF/23. Class-Window/02. Windows-Interaction/WindowsInteraction/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WindowsInteraction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TaskWindow taskWindow = new TaskWindow();
+
+            taskWindow.ViewModel = "ViewModel";
+            taskWindow.Owner = this;
+
+            taskWindow.Show();
+
+            taskWindow.ShowViewModel();
+        }
+
+        private void BlueButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in this.OwnedWindows)
+            {
+                window.Background = new SolidColorBrush(Colors.Blue);
+
+                if (window is TaskWindow)
+                {
+                    window.Title = "Blue Color";
+                }
+            }
+        }
+
+        private void WhiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in this.OwnedWindows)
+            {
+                window.Background = new SolidColorBrush(Colors.White);
+
+                if (window is TaskWindow)
+                {
+                    window.Title = "Task Window";
+                }
+            }
+        }
+
+        private void GreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in App.Current.Windows)
+            {
+                window.Background = new SolidColorBrush(Colors.Green);
+            }
+        }
+    }
+}
---
+++ WPF/23. Class-Window/02. Windows-Interaction/WindowsInteraction/TaskWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WindowsInteraction
+{
+    /// <summary>
+    /// Interaction logic for TaskWindow.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public string ViewModel { get; set; }
+
+        public TaskWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowViewModel()
+        {
+            MessageBox.Show(ViewModel);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RedButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void WhiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Background = new SolidColorBrush(Colors.White);
+        }
+    }
+}
