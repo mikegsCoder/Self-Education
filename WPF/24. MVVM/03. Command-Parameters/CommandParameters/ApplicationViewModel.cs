52c728ccdd0670cbd14d1461f00c7a036f68c145
mikegsCoder <mikegsid@gmail.com>
2023-04-21 10:06:33 +0300

    Add Command Parameters

---
+++ WPF/24. MVVM/03. Command-Parameters/CommandParameters/ApplicationViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CommandParameters
+{
+    // ViewModel:
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        // command to add new object
+        private RelayCommand addCommand;
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Phone phone = new Phone();
+                      Phones.Insert(0, phone);
+                      SelectedPhone = phone;
+                  }));
+            }
+        }
+
+        // command to remove
+        private RelayCommand removeCommand;
+
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Phone phone = obj as Phone;
+
+                      if (phone != null)
+                      {
+                          Phones.Remove(phone);
+                      }
+                  },
+                  (obj) => Phones.Count > 0));
+            }
+        }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
