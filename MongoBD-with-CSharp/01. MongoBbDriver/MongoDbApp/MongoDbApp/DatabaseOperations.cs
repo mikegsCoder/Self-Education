c8a67f67e6285c61fb42ebe78f6a82bd973415f2
mikegsCoder <mikegsid@gmail.com>
2023-05-27 10:53:15 +0300

    Add MongoDbApp

---
+++ MongoBD-with-CSharp/01. MongoBbDriver/MongoDbApp/MongoDbApp/DatabaseOperations.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDbApp
+{
+    public class DatabaseOperations
+    {
+        private MongoClient client; 
+
+        public DatabaseOperations() 
+        { 
+            client = new MongoClient("mongodb://localhost:27017");
+        }
+
+        public async void GetDatabases()
+        {
+            using (var cursor = client.ListDatabases())
+            {
+                var databases = await cursor.ToListAsync();
+
+                foreach (var database in databases)
+                {
+                    Console.WriteLine(database.ToString());
+                }
+            }
+        }
+
+        public async void GetDatabaseNames()
+        {
+            using (var cursor = client.ListDatabaseNames())
+            {
+                var databasesNames = await cursor.ToListAsync();
+
+                foreach (var name in databasesNames)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+        public void GetDatabase(string name)
+        {
+            client.GetDatabase(name);
+        }
+
+        public async void DropDatabase(string name)
+        {
+            await client.DropDatabaseAsync(name);
+        }
+    }
+}
---
+++ MongoBD-with-CSharp/01. MongoBbDriver/MongoDbApp/MongoDbApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+namespace MongoDbApp
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            DatabaseOperations db = new DatabaseOperations();
+
+            int input = 0;
+
+            while (input != 5)
+            {
+                Menu();
+                input = Convert.ToInt32(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        try
+                        {
+                            db.GetDatabases();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            db.GetDatabaseNames();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            Console.WriteLine("Enter databse name to get: ");
+                            string name = Console.ReadLine().Trim();
+
+                            db.GetDatabase(name);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 4:
+                        try
+                        {
+                            Console.WriteLine("Enter databse name to drop: ");
+                            string name = Console.ReadLine().Trim();
+
+                            db.DropDatabase(name);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("1. Get databases");
+            Console.WriteLine("2. Get datase names");
+            Console.WriteLine("3. Get database by name");
+            Console.WriteLine("4. Drop database");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("Enter your choise:");
+        }
+    }
+}
