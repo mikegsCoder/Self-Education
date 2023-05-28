0e726d2e914c2a6f4494bdae37a62f074112102a
mikegsCoder <mikegsid@gmail.com>
2023-05-28 11:47:47 +0300

    Add Menu

--- MongoBD-with-CSharp/02. Collections/Collections/Collections/Program.cs
+++ MongoBD-with-CSharp/02. Collections/Collections/Collections/Program.cs
@@ -11,6 +11,52 @@ namespace Collections
             string database = "test2";
 
             DatabaseOperations db = new DatabaseOperations(database);
+
+            int input = 0;
+
+            while (input != 7)
+            {
+                Menu();
+                input = Convert.ToInt32(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                      
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                       
+                        break;
+                    case 4:
+                       
+                        break;
+                    case 5:
+                       
+                        break;
+                    case 6:
+                       
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("1. Get collections");
+            Console.WriteLine("2. Get collection names");
+            Console.WriteLine("3. Create collection");
+            Console.WriteLine("4. Rename collection");
+            Console.WriteLine("5. Drop collection");
+            Console.WriteLine("6. Get collection");
+            Console.WriteLine("7. Exit");
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("Enter your choise:");
         }
     }
 }
