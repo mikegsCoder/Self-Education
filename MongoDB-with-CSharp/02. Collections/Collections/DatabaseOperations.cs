637b31fb82da5ef7c0c0016ba1aa5ece01e74ddd
mikegsCoder <mikegsid@gmail.com>
2023-05-28 12:07:17 +0300

    Changed folder structure

---
+++ MongoDB-with-CSharp/02. Collections/Collections/DatabaseOperations.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class DatabaseOperations
+    {
+        private MongoClient client;
+        private IMongoDatabase db;
+
+        public DatabaseOperations(string _databaseName)
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+
+            db = client.GetDatabase(_databaseName);
+        }
+
+        public async void GetCollections()
+        {
+            var collections = await db.ListCollections().ToListAsync();
+
+            if (collections.Count == 0)
+            {
+                await Console.Out.WriteLineAsync("No collections in database!");
+            }
+            else
+            {
+                foreach (var collection in collections)
+                {
+                    Console.WriteLine(collection);
+                }
+            }
+        }
+
+        public async void GetCollectionNames()
+        {
+            var collectionNames = await db.ListCollectionNames().ToListAsync();
+
+            if (collectionNames.Count == 0)
+            {
+                await Console.Out.WriteLineAsync("No collections in database!");
+            }
+            else
+            {
+                foreach (var name in collectionNames)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+        public async void CreateCollection(string name)
+        {
+            await db.CreateCollectionAsync(name);
+        }
+    }
+}
---
+++ MongoDB-with-CSharp/02. Collections/Collections/Program.cs
@@ -0,0 +1,86 @@
+﻿using MongoDB.Driver.Core.Operations;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string database = "test2";
+
+            DatabaseOperations db = new DatabaseOperations(database);
+
+            int input = 0;
+
+            while (input != 7)
+            {
+                Menu();
+                input = Convert.ToInt32(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        try
+                        {
+                            db.GetCollections();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            db.GetCollectionNames();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            Console.WriteLine("Enter new collection name: ");
+                            string name = Console.ReadLine().Trim();
+
+                            db.CreateCollection(name);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 4:
+                       
+                        break;
+                    case 5:
+                       
+                        break;
+                    case 6:
+                       
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("1. Get collections");
+            Console.WriteLine("2. Get collection names");
+            Console.WriteLine("3. Create collection");
+            Console.WriteLine("4. Rename collection");
+            Console.WriteLine("5. Drop collection");
+            Console.WriteLine("6. Get collection");
+            Console.WriteLine("7. Exit");
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("Enter your choise:");
+        }
+    }
+}
