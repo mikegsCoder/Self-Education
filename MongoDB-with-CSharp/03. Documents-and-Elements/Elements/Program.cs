c5c5359eff1220727587f4da8dc1de320429f11d
mikegsCoder <mikegsid@gmail.com>
2023-05-30 17:03:06 +0300

    Add Elements

---
+++ MongoDB-with-CSharp/03. Documents-and-Elements/Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+
+namespace Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string separator = new String('-', 50);
+
+            BsonElement el = new BsonElement("name", new BsonString("Tom"));
+
+            Console.WriteLine(el);       // name = Tom
+            Console.WriteLine(el.Name);  // name
+            Console.WriteLine(el.Value); // Tom
+
+            Console.WriteLine(separator);
+
+            BsonElement nameElement = new BsonElement("name", "Tom");
+            BsonElement ageElement = new BsonElement("age", 38);
+
+            Console.WriteLine(nameElement); // name = Tom 
+            Console.WriteLine(ageElement);  // age = 38
+
+            Console.WriteLine(separator);
+        }
+    }
+}
