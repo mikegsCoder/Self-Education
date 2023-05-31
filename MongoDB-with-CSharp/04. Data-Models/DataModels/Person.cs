41c997fe7e5707e39b837bdb56a9c2648c5ff851
mikegsCoder <mikegsid@gmail.com>
2023-05-31 18:21:53 +0300

    Add Person class

---
+++ MongoDB-with-CSharp/04. Data-Models/DataModels/Person.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class Person
+    {
+        public ObjectId Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Company? Company { get; set; }
+
+        public List<string>? Languages { get; set; } = new List<string>();
+    }
+}
