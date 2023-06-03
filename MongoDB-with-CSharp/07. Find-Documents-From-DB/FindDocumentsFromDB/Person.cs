6747b955d7f50e8d8dcab689bc8e02f6d24615ce
mikegsCoder <mikegsid@gmail.com>
2023-06-03 11:57:49 +0300

    Add Person

---
+++ MongoDB-with-CSharp/07. Find-Documents-From-DB/FindDocumentsFromDB/Person.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDocumentsFromDB
+{
+    [BsonIgnoreExtraElements]
+    public class Person
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; }
+    }
+}
--- MongoDB-with-CSharp/07. Find-Documents-From-DB/FindDocumentsFromDB/Program.cs
+++ MongoDB-with-CSharp/07. Find-Documents-From-DB/FindDocumentsFromDB/Program.cs
@@ -6,40 +6,7 @@ namespace FindDocumentsFromDB
     {
         static void Main(string[] args)
         {
-            string separator = new String('-', 50);    
-
-            DatabaseOperations db = new DatabaseOperations("test2", "users");
-
-            //BsonDocument tom = new BsonDocument
-            //{
-            //    {"Name", "Tom"},
-            //    {"Age", 38}
-            //};
-
-            //// insert one:
-            //db.InsertOne(tom);
-
-            //BsonDocument bob = new BsonDocument { { "Name", "Bob" }, { "Age", 42 } };
-            //BsonDocument sam = new BsonDocument { { "Name", "Sam" }, { "Age", 25 } };
-
-            //// insert many:
-            //db.InsertMany(new List<BsonDocument> { bob, sam });
-
-            // find:
-            db.Find();
-            Console.WriteLine(separator);
-
-            // find async:
-            db.FindAsync();
-            Console.WriteLine(separator);
-
-            // find persons:
-            db.FindPersons();
-            Console.WriteLine(separator);
-
-            // count:
-            db.GetCount();
-            Console.WriteLine(separator);
+           
         }
     }
 }
