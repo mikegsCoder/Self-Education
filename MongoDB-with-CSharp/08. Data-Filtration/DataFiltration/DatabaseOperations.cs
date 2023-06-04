aff1d7086654fbd92d6d695d7054d05ba750deb4
mikegsCoder <mikegsid@gmail.com>
2023-06-04 11:02:23 +0300

    Add DatabaseOperations

---
+++ MongoDB-with-CSharp/08. Data-Filtration/DataFiltration/DatabaseOperations.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFiltration
+{
+    public class DatabaseOperations
+    {
+        private MongoClient client;
+        private IMongoDatabase db;
+        private IMongoCollection<BsonDocument> collection;
+
+        public DatabaseOperations(string _databaseName, string _collection)
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+
+            db = client.GetDatabase(_databaseName);
+
+            collection = db.GetCollection<BsonDocument>(_collection);
+        }
+
+        public void FindWithFilter(BsonDocument filter)
+        {
+            var users = collection.Find(filter);
+
+            var usersList = users.ToList();
+
+            foreach (var user in usersList)
+            {
+                Console.WriteLine(user);
+            }
+        }
+
+        public void FindPersonsWithFilter(BsonDocument filter)
+        {
+            var personCollection = db.GetCollection<Person>("users");
+
+            //using var cursor = await personCollection.FindAsync(new BsonDocument());
+            //List<Person> users = cursor.ToList();
+
+            var users = personCollection.Find(filter);
+
+            var usersList = users.ToList();
+
+            foreach (var user in usersList)
+            {
+                Console.WriteLine($"{user.Name} - {user.Age}");
+            }
+        }
+    }
+}
