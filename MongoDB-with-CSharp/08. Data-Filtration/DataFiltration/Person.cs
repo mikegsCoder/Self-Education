fde6779b10d491fa725e7e8109eeb3a466121870
mikegsCoder <mikegsid@gmail.com>
2023-06-04 11:00:41 +0300

    Add Person

---
+++ MongoDB-with-CSharp/08. Data-Filtration/DataFiltration/Person.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFiltration
+{
+    public class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+
+        public List<string>? Languages { get; set; }
+    }
+}
