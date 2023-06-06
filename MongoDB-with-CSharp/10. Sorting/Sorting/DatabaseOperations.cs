ebe3d1c2e39af895452a2bdaf71264860f3fa8d9
mikegsCoder <mikegsid@gmail.com>
2023-06-06 16:02:13 +0300

    Add DatabaseOperations

---
+++ MongoDB-with-CSharp/10. Sorting/Sorting/DatabaseOperations.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class DatabaseOperations
+    {
+        private MongoClient client;
+        private IMongoDatabase db;
+        private IMongoCollection<BsonDocument> collection;
+
+        public DatabaseOperations(string _databaseName, string _collection)
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+
+            db = client.GetDatabase(_databaseName);
+
+            collection = db.GetCollection<BsonDocument>(_collection);
+        }
+
+        public IFindFluent<BsonDocument, BsonDocument> FindWithFilter(BsonDocument filter)
+        {
+            var users = collection.Find(filter);
+
+            return users;
+        }
+
+        public IFindFluent<Person, Person> FindPersonsWithFilter(BsonDocument filter)
+        {
+            var personCollection = db.GetCollection<Person>("users");
+
+            var users = personCollection.Find(filter);
+
+            return users;
+        }
+    }
+}
