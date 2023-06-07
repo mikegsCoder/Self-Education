6afc23ed87efc300a32c94519d40f138fec7bd2b
mikegsCoder <mikegsid@gmail.com>
2023-06-07 17:34:18 +0300

    Add Get Single Document

---
+++ MongoDB-with-CSharp/11. Get-Single-Document/GetSingleDocument/Program.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GetSingleDocument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+  
+        }
+    }
+}
