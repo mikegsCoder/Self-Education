e5d52c633ad7c1e26a3b8acc8d004b4413d37032
mikegsCoder <mikegsid@gmail.com>
2023-06-08 16:50:01 +0300

    Add examples

--- MongoDB-with-CSharp/12. Projection/Projection/Program.cs
+++ MongoDB-with-CSharp/12. Projection/Projection/Program.cs
@@ -8,7 +8,22 @@ namespace Projection
     {
         static void Main(string[] args)
         {
-          
+            string separator = new string('-', 50);
+
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            var collection = db.GetCollection<BsonDocument>("users");
+
+            // create ProjectionDefinition that includes Name and Age:
+            var projection = Builders<BsonDocument>.Projection.Include("Name").Include("Age").Exclude("_id");
+
+            // var users1 = await collection.Find(new BsonDocument()).Project(projection).ToListAsync();
+            var users1 = collection.Find(new BsonDocument()).Project(projection).ToList();
+
+            foreach (var user in users1) Console.WriteLine(user);
+            Console.WriteLine(separator);
+
         }
     }
 }
