f3f9936b5c0f138583d5e4b47570aeeadf31190a
mikegsCoder <mikegsid@gmail.com>
2023-06-08 16:47:13 +0300

    Add Projection

---
+++ MongoDB-with-CSharp/12. Projection/Projection/Program.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Net.Cache;
+
+namespace Projection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
