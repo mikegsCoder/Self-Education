393987f4f346c9e3d370c37e5144b896c85a0baa
mikegsCoder <mikegsid@gmail.com>
2023-06-08 16:48:26 +0300

    Add Person

---
+++ MongoDB-with-CSharp/12. Projection/Projection/Person.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projection
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; } = "";
+
+        public List<string>? Languages { get; set; }
+    }
+}
