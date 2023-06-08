f6d80be1968289c9ae361ee3fb2057c75bfb75d8
mikegsCoder <mikegsid@gmail.com>
2023-06-08 16:52:19 +0300

    Cleanup and code formatting

--- MongoDB-with-CSharp/12. Projection/Projection/Person.cs
+++ MongoDB-with-CSharp/12. Projection/Projection/Person.cs
@@ -1,11 +1,4 @@
-﻿using MongoDB.Bson;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projection
+﻿namespace Projection
 {
     public class Person
     {
--- MongoDB-with-CSharp/12. Projection/Projection/Program.cs
+++ MongoDB-with-CSharp/12. Projection/Projection/Program.cs
@@ -1,6 +1,5 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
-using System.Net.Cache;
 
 namespace Projection
 {
