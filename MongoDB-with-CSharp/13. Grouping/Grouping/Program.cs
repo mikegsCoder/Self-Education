0acd1f4bf2d8c71afee92ddfe7fe664ae81296a5
mikegsCoder <mikegsid@gmail.com>
2023-06-09 17:03:12 +0300

    Add Grouping

---
+++ MongoDB-with-CSharp/13. Grouping/Grouping/Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Grouping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string separator = new string('-', 50);
+
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            var collection = db.GetCollection<BsonDocument>("employees");
+
+ 
+        }
+    }
+}
