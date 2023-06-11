934a8e350da600c19718b382c74b11c8e77daad1
mikegsCoder <mikegsid@gmail.com>
2023-06-11 12:24:25 +0300

    Add UpdateAndPOCO

---
+++ MongoDB-with-CSharp/14. Edit-Documents/UpdateAndPOCO/Person.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateAndPOCO
+{
+    internal class Person
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; }
+
+        public List<string>? Languages { get; set; }
+
+        public DateTime? LastModified { get; set; }
+    }
+}
---
+++ MongoDB-with-CSharp/14. Edit-Documents/UpdateAndPOCO/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace UpdateAndPOCO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            var collection = db.GetCollection<Person>("users");
+
+            // set f1lter:
+            var filter = Builders<Person>.Filter.Eq(p => p.Name, "Tom");
+
+            // set update:
+            //var update = Builders<Person>.Update.Set(p => p.Age, 34);
+            //var update = Builders<Person>.Update.Set(p => p.Age, 35)
+            //  .Set(p => p.LastModified, DateTime.Now);
+            var update = Builders<Person>.Update
+                .Set(p => p.Age, 35)
+                .Set(p => p.Languages, new List<string>() { "new language" })
+                .Set(p => p.LastModified, DateTime.Now);
+
+            // update:
+            //var result = await collection.UpdateManyAsync(filter, update);
+            var result = collection.UpdateMany(filter, update);
+
+            Console.WriteLine($"Matched: {result.MatchedCount}; Updated: {result.ModifiedCount}");
+
+            var users = collection.Find("{}").ToList();
+            foreach (var user in users) Console.WriteLine(user.ToJson());
+        }
+    }
+}
