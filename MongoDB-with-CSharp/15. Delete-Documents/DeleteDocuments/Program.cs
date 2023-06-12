35fa6c8eae7db1820b6986f79875441607cce535
mikegsCoder <mikegsid@gmail.com>
2023-06-12 17:45:48 +0300

    Add examples

--- MongoDB-with-CSharp/15. Delete-Documents/DeleteDocuments/Program.cs
+++ MongoDB-with-CSharp/15. Delete-Documents/DeleteDocuments/Program.cs
@@ -12,6 +12,26 @@ namespace DeleteDocuments
             var db = client.GetDatabase("test2");
             var collection = db.GetCollection<BsonDocument>("users");
 
+            // delete one document where Name == "Sam":
+            //var result1 = await collection.DeleteOneAsync(new BsonDocument("Name", "Sam"));
+            var result1 = collection.DeleteOne(new BsonDocument("Name", "Sam"));
+
+            WriteResult(collection, result1);
+
+            // delete all documents where Name == "Tom":
+            //var result2 = await collection.DeleteManyAsync(new BsonDocument("Name", "Tom"));
+            var result2 = collection.DeleteMany(new BsonDocument("Name", "Tom"));
+
+            WriteResult(collection, result2);
+        }
+        private static void WriteResult(IMongoCollection<BsonDocument> collection, DeleteResult result)
+        {
+            Console.WriteLine($"Deleted documents: {result.DeletedCount}");
+
+            var users = collection.Find("{}").ToList();
+            foreach (var user in users) Console.WriteLine(user);
+
+            Console.WriteLine(new string('-', 50));
         }
     }
 }
