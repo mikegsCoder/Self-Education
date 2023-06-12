c063afbc5e8cc57a4a3a2f1c7a4475e030da1d28
mikegsCoder <mikegsid@gmail.com>
2023-06-12 17:47:25 +0300

    Add more examples

--- MongoDB-with-CSharp/15. Delete-Documents/DeleteDocuments/Program.cs
+++ MongoDB-with-CSharp/15. Delete-Documents/DeleteDocuments/Program.cs
@@ -23,6 +23,31 @@ namespace DeleteDocuments
             var result2 = collection.DeleteMany(new BsonDocument("Name", "Tom"));
 
             WriteResult(collection, result2);
+
+            // delete with POCO:
+            var collection1 = db.GetCollection<Person>("users");
+
+            // delete all documents where Name == "Bob":
+            //var result3 = await collection.DeleteManyAsync(p => p.Name == "Bob");
+            var result3 = collection1.DeleteMany(p => p.Name == "Bob");
+
+            Console.WriteLine($"Deleted documents: {result3.DeletedCount}");
+
+            var users = collection1.Find("{}").ToList();
+            foreach (var user in users) Console.WriteLine(user.Name);
+
+            Console.WriteLine(new string('-', 50));
+
+            // find one and delete where Name == "Alice":
+            //var result4 = await collection.FindOneAndDeleteAsync(new BsonDocument("Name", "Tom"));
+            var result4 = collection.FindOneAndDelete(new BsonDocument("Name", "Alice"));
+
+            if (result4 == null)
+                Console.WriteLine("Document not found.");
+            else
+                Console.WriteLine($"Deleted document: {result4}");
+
+            Console.WriteLine(new string('-', 50));
         }
         private static void WriteResult(IMongoCollection<BsonDocument> collection, DeleteResult result)
         {
