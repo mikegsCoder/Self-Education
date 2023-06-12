ccb94fe4791a9177ebd2616b6aaa10fdbffe9d28
mikegsCoder <mikegsid@gmail.com>
2023-06-12 17:41:56 +0300

    Add DeleteDocuments

---
+++ MongoDB-with-CSharp/15. Delete-Documents/DeleteDocuments/Program.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DeleteDocuments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            var collection = db.GetCollection<BsonDocument>("users");
+
+        }
+    }
+}
