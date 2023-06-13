0482241ecbf86ec19d29bc392e485144ecd6ffaf
mikegsCoder <mikegsid@gmail.com>
2023-06-13 16:15:17 +0300

    Add more examples

--- MongoDB-with-CSharp/16. Method-BulkWriteAsync/MethodBulkWriteAsync/Program.cs
+++ MongoDB-with-CSharp/16. Method-BulkWriteAsync/MethodBulkWriteAsync/Program.cs
@@ -29,6 +29,42 @@ namespace MethodBulkWriteAsync
                 new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Bob"}, {"Age", 42 } }),
                 new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Alice"}, {"Age", 33 } })
             });
+
+            WriteResult(collection);
+
+            // update and delete:
+            //await collection.BulkWriteAsync(new WriteModel<BsonDocument>[]
+            collection.BulkWrite(new WriteModel<BsonDocument>[]
+            {
+                // update:
+                new UpdateOneModel<BsonDocument>(new BsonDocument("Name", "Sam"), new BsonDocument("$set", new BsonDocument("Age", 30))),
+                // replace:
+                new ReplaceOneModel<BsonDocument>(new BsonDocument("Name", "Bob"), new BsonDocument{ {"Name", "Robert" }, {"Age", 44 } }),
+                // delete:
+                new DeleteOneModel<BsonDocument>(new BsonDocument("Name", "Alice"))
+            });
+
+            WriteResult(collection);
+
+            // BulkWriteResult<TDocument> properties:
+            //
+            // Upserts: holds collection of added documents
+            // InsertedCount: count of inserted documents
+            // MatchedCount: count of matched documents
+            // ModifiedCount: count of modified documents
+            // DeletedCount: count of deleted documents
+
+            //var result = await collection.BulkWriteAsync(new WriteModel<BsonDocument>[]
+            var result = collection.BulkWrite(new WriteModel<BsonDocument>[]
+            {
+                new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Alice"}, {"Age", 33 } }),
+                new UpdateOneModel<BsonDocument>(new BsonDocument("Name", "Sam"), new BsonDocument("$set", new BsonDocument("Age", 32))),
+                new ReplaceOneModel<BsonDocument>(new BsonDocument("Name", "Robert"), new BsonDocument("Name", "Bob")),
+                new DeleteOneModel<BsonDocument>(new BsonDocument("Name", "Tom"))
+            });
+
+            Console.WriteLine($"Matched: {result.MatchedCount};  Inserted: {result.InsertedCount}; " +
+                $"Modified: {result.ModifiedCount}; Deleted: {result.DeletedCount}");
         }
 
         private static void WriteResult(IMongoCollection<BsonDocument> collection)
