f8fa35a7becd8d5e7a83aa68e6d11c6019e19681
mikegsCoder <mikegsid@gmail.com>
2023-06-13 16:11:55 +0300

    Add Method BulkWriteAsync

---
+++ MongoDB-with-CSharp/16. Method-BulkWriteAsync/MethodBulkWriteAsync/Program.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MethodBulkWriteAsync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            var collection = db.GetCollection<BsonDocument>("people");
+ 
+        }
+    }
+}
