4c9ae3f010a15a005a06bea5f359c8864e0f00e5
mikegsCoder <mikegsid@gmail.com>
2023-06-13 16:13:45 +0300

    Add examples

--- MongoDB-with-CSharp/16. Method-BulkWriteAsync/MethodBulkWriteAsync/Program.cs
+++ MongoDB-with-CSharp/16. Method-BulkWriteAsync/MethodBulkWriteAsync/Program.cs
@@ -11,7 +11,35 @@ namespace MethodBulkWriteAsync
 
             var db = client.GetDatabase("test2");
             var collection = db.GetCollection<BsonDocument>("people");
- 
+
+            // add new document:
+            //await collection.BulkWriteAsync(new WriteModel<BsonDocument>[]
+            collection.BulkWrite(new WriteModel<BsonDocument>[]
+            {
+                new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Tom"}, {"Age", 38 } })
+            });
+
+            WriteResult(collection);
+
+            // add many documents:
+            //await collection.BulkWriteAsync(new WriteModel<BsonDocument>[]
+            collection.BulkWrite(new WriteModel<BsonDocument>[]
+            {
+                new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Sam"}, {"Age", 28 } }),
+                new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Bob"}, {"Age", 42 } }),
+                new InsertOneModel<BsonDocument>(new BsonDocument{{"Name", "Alice"}, {"Age", 33 } })
+            });
+        }
+
+        private static void WriteResult(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine("After BulkWrite:");
+
+            var people = collection.Find("{}").ToList();
+
+            foreach (var person in people) Console.WriteLine(person);
+
+            Console.WriteLine(new string('-', 50));
         }
     }
 }
