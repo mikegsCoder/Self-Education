dea75564dbc60f5c8cfc50b4099144696940b1bf
mikegsCoder <mikegsid@gmail.com>
2023-06-14 17:55:28 +0300

    Add examples

--- MongoDB-with-CSharp/17. Files-and-GridFS/FilesAndGridFS/Program.cs
+++ MongoDB-with-CSharp/17. Files-and-GridFS/FilesAndGridFS/Program.cs
@@ -13,6 +13,26 @@ namespace FilesAndGridFS
             var db = client.GetDatabase("test2");
             IGridFSBucket gridFS = new GridFSBucket(db);
 
+            // create stream:
+            using Stream fs1 = File.OpenRead("../../../kitty.jpeg");
+            // save in DB:
+            //ObjectId id = await gridFS.UploadFromStreamAsync("kitty.jpeg", fs);
+            ObjectId id = gridFS.UploadFromStream("kitty.jpeg", fs1);
+            Console.WriteLine($"Id: {id}");
+
+            Console.WriteLine(new string('-', 50));
+
+            // create stream
+            using Stream fs2 = File.OpenWrite("../../../new_kitty1.jpeg");
+            // download from DB:
+            //await gridFS.DownloadToStreamByNameAsync("cats.jpg", fs2);
+            gridFS.DownloadToStreamByName("kitty.jpeg", fs2);
+
+            // create stream
+            using Stream fs3 = File.OpenWrite("../../../new_kitty2.jpeg");
+            // download from DB:
+            //await gridFS.DownloadToStreamAsync("cats.jpg", fs3);
+            gridFS.DownloadToStream(id, fs3);
         }
     }
 }
