c958e760da4c4b3c40969e8e08c097477db27630
mikegsCoder <mikegsid@gmail.com>
2023-06-14 17:53:58 +0300

    Add Files and GridFS

---
+++ MongoDB-with-CSharp/17. Files-and-GridFS/FilesAndGridFS/Program.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace FilesAndGridFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("test2");
+            IGridFSBucket gridFS = new GridFSBucket(db);
+
+        }
+    }
+}
