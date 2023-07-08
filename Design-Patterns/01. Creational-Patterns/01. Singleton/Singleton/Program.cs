1b33a590d124156825748b1f0f999713288da0d9
mikegsCoder <mikegsid@gmail.com>
2023-07-08 11:13:45 +0300

    Add Singleton

---
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance(); // only one object invoked with different print details
+            
+            s1.PrintDetails("This is first message");
+            s1.PrintDetails("This is second message");
+        }
+    }
+}
---
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
