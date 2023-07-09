a98db5eaa2e2af7b7d3138ecad024d8b14a487ec
mikegsCoder <mikegsid@gmail.com>
2023-07-09 11:27:53 +0300

    Add SingletonLazyInitialization

---
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonLazyInitialization/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonLazyInitialization
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton()); // Thread-safe
+
+        private Singleton()
+        {}
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
