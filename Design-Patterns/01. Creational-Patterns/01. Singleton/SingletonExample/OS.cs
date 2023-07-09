8471f344e2e6ffccdf85e9dd084cb2e1d29372ab
mikegsCoder <mikegsid@gmail.com>
2023-07-09 11:35:44 +0300

    Cleanup and code formatting

--- Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonExample/OS.cs
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonExample/OS.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SingletonExample
+﻿namespace SingletonExample
 {
     public class OS
     {
--- Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonThreadSafetySecondVariant/Singleton.cs
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonThreadSafetySecondVariant/Singleton.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SingletonThreadSafetySecondVariant
+﻿namespace SingletonThreadSafetySecondVariant
 {
     public class Singleton
     {
