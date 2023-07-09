3a896ed189508503e91aed9e34557df088c8137e
mikegsCoder <mikegsid@gmail.com>
2023-07-09 11:32:13 +0300

    Add SingletonThreadSafetySecondVariant

---
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonThreadSafetySecondVariant/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonThreadSafetySecondVariant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                Singleton singleton1 = Singleton.GetInstance();
+                Console.WriteLine(singleton1.Date);
+            }).Start();
+
+            Singleton singleton2 = Singleton.GetInstance();
+            Console.WriteLine(singleton2.Date);
+        }
+    }
+}
---
+++ Design-Patterns/01. Creational-Patterns/01. Singleton/SingletonThreadSafetySecondVariant/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonThreadSafetySecondVariant
+{
+    public class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        public string Date { get; private set; }
+
+        private Singleton()
+        {
+            Date = DateTime.Now.TimeOfDay.ToString();
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
