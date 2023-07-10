c364c41eb9c2bb090ebf1c8f02d09280217a13e3
mikegsCoder <mikegsid@gmail.com>
2023-07-10 17:43:44 +0300

    Add ProductFactory

---
+++ Design-Patterns/01. Creational-Patterns/02. Simple-Factory/SimpleFactory/ProductFactory/Factory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SimpleFactory.Enum;
+using SimpleFactory.Products;
+
+
+namespace SimpleFactory.ProductFactory
+{
+   public static class Factory
+    {
+        public static IMobile CreateMobile(MobileType mobileType)
+        {
+            switch (mobileType)
+            {
+                case MobileType.Nokia:
+                {
+                    return new Nokia("N90", 400m);
+                }
+                case MobileType.Samsung:
+                {
+                    return new Samsung("Galaxy", 500m);
+                }
+                case MobileType.Sony:
+                {
+                    return new Sony("Xperia L4", 600m);
+                }
+                default:
+                {
+                    throw new ArgumentException("Invalid mobile type.");
+                }
+            }
+        }
+    }
+}
