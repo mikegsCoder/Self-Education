debec0507ba6ae496bf8708e2d4352e569664000
mikegsCoder <mikegsid@gmail.com>
2023-07-10 17:48:10 +0300

    Implemented examples

--- Design-Patterns/01. Creational-Patterns/02. Simple-Factory/SimpleFactory/Program.cs
+++ Design-Patterns/01. Creational-Patterns/02. Simple-Factory/SimpleFactory/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace SimpleFactory
+﻿using SimpleFactory.Enum;
+using SimpleFactory.ProductFactory;
+using SimpleFactory.Products;
+
+namespace SimpleFactory
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            IMobile firstMobile = Factory.CreateMobile(MobileType.Nokia);
+            IMobile secondMobile = Factory.CreateMobile(MobileType.Sony);
+            IMobile thirdMobile = Factory.CreateMobile(MobileType.Samsung);
+
+            Console.WriteLine(firstMobile.Price);
+            Console.WriteLine(secondMobile.Price);
+            Console.WriteLine(thirdMobile.Price);
+
+            Console.ReadLine();
         }
     }
 }
