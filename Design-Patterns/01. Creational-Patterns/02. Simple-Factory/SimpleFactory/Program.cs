32707907ad20d27e4bbf0ac2d7507fb86a6ca8f1
mikegsCoder <mikegsid@gmail.com>
2023-07-10 17:38:07 +0300

    Add SimpleFactory initial commit

---
+++ Design-Patterns/01. Creational-Patterns/02. Simple-Factory/SimpleFactory/Program.cs
@@ -0,0 +1,21 @@
+﻿using SimpleFactory.Enum;
+using SimpleFactory.ProductFactory;
+
+namespace SimpleFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMobile firstMobile = Factory.CreateMobile(MobileType.Nokia);
+            IMobile secondMobile = Factory.CreateMobile(MobileType.Sony);
+            IMobile thirdMobile = Factory.CreateMobile(MobileType.Samsung);
+
+            Console.WriteLine(firstMobile.Price);
+            Console.WriteLine(secondMobile.Price);
+            Console.WriteLine(thirdMobile.Price);
+
+            Console.ReadLine();
+        }
+    }
+}
