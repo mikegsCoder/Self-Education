4e9285f2e238e3ed5c5213daa62cdc693100393e
mikegsCoder <mikegsid@gmail.com>
2023-07-11 16:57:26 +0300

    Add PanelDeveloper and WoodDeveloper classes

---
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/PanelDeveloper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodExample
+{
+    public class PanelDeveloper : Developer
+    {
+        public PanelDeveloper(string name) : base(name)
+        { }
+
+        public override House Create()
+        {
+            return new PanelHouse();
+        }
+    }
+}
---
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/WoodDeveloper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodExample
+{
+    public class WoodDeveloper : Developer
+    {
+        public WoodDeveloper(string name) : base(name)
+        { }
+
+        public override House Create()
+        {
+            return new WoodHouse();
+        }
+    }
+}
