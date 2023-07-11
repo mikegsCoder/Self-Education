c540141021e376816724eda9c5ac9822272ced1f
mikegsCoder <mikegsid@gmail.com>
2023-07-11 16:58:48 +0300

    Implemented example

--- Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/Program.cs
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-      
+            Developer dev = new PanelDeveloper("Panel buildings");
+            House house2 = dev.Create();
+
+            dev = new WoodDeveloper("Wooden buildings");
+            House house = dev.Create();
+
+            Console.ReadLine();
         }
     }
 }
