c6f1928c0ddbaa8f15e939df9e85ab349d3c4265
mikegsCoder <mikegsid@gmail.com>
2023-07-11 16:55:49 +0300

    Add PanelHouse and WoodenHouse classes

---
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/PanelHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodExample
+{
+    public class PanelHouse : House
+    {
+        public PanelHouse()
+        {
+            Console.WriteLine("Panel house built.");
+        }
+    }
+}
---
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethodExample/WoodHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodExample
+{
+    public class WoodHouse : House
+    {
+        public WoodHouse()
+        {
+            Console.WriteLine("Wooden house built.");
+        }
+    }
+}
