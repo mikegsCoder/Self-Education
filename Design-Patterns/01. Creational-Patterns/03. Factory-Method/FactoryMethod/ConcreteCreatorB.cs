1ddab2dcd362500de62506982d3a32e14fbe67f6
mikegsCoder <mikegsid@gmail.com>
2023-07-11 17:01:13 +0300

    Cleanup and code formatting

--- Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethod/ConcreteCreatorB.cs
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethod/ConcreteCreatorB.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FactoryMethod
+﻿namespace FactoryMethod
 {
     public class ConcreteCreatorB : Creator
     {
