c9dc9064b48bae7fb73a23791fbb08fd51e8a548
mikegsCoder <mikegsid@gmail.com>
2023-07-11 16:50:38 +0300

    Add Factory Method

---
+++ Design-Patterns/01. Creational-Patterns/03. Factory-Method/FactoryMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productA = new ConcreteCreatorA().FactoryMethod();
+            Product productB = new ConcreteCreatorB().FactoryMethod();
+        }
+    }
+}
