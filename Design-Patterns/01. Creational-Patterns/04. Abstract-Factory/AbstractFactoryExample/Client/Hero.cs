1468be9f145d822444c7ba3b16ebacee05e2e8cf
mikegsCoder <mikegsid@gmail.com>
2023-07-12 17:38:39 +0300

    Add Client

---
+++ Design-Patterns/01. Creational-Patterns/04. Abstract-Factory/AbstractFactoryExample/Client/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactoryExample.Factories;
+using AbstractFactoryExample.Products;
+
+namespace AbstractFactoryExample.Client
+{
+    public class Hero
+    {
+        private Weapon weapon;
+        private Movement movement;
+
+        public Hero(HeroFactory factory)
+        {
+            weapon = factory.CreateWeapon();
+            movement = factory.CreateMovement();
+        }
+
+        public void Run()
+        {
+            movement.Move();
+        }
+
+        public void Hit()
+        {
+            weapon.Hit();
+        }
+    }
+}
