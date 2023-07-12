a12dae687073a927c42e0fe08097dd80f6c58e5b
mikegsCoder <mikegsid@gmail.com>
2023-07-12 17:40:28 +0300

    Implemented example

--- Design-Patterns/01. Creational-Patterns/04. Abstract-Factory/AbstractFactoryExample/Program.cs
+++ Design-Patterns/01. Creational-Patterns/04. Abstract-Factory/AbstractFactoryExample/Program.cs
@@ -1,11 +1,21 @@
-﻿
+﻿using AbstractFactoryExample.Client;
+using AbstractFactoryExample.Factories;
+
 namespace AbstractFactoryExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            Hero elf = new Hero(new ElfFactory());
+            elf.Hit();
+            elf.Run();
+
+            Hero warrior = new Hero(new WarriorFactory());
+            warrior.Hit();
+            warrior.Run();
+
+            Console.ReadLine();
         }
     }
 }
