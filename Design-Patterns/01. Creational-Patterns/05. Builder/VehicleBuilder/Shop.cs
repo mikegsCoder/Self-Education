1c8cadd54ccdfcdb5760cc971b3e9c0bb3d3029e
mikegsCoder <mikegsid@gmail.com>
2023-07-14 10:00:55 +0300

    Add Shop

---
+++ Design-Patterns/01. Creational-Patterns/05. Builder/VehicleBuilder/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleBuilder
+{
+    public class Shop
+    {
+        // Builder uses a complex series of steps
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+}
