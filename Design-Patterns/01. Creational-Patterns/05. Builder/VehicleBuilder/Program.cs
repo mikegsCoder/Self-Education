01cb403e8d2eb00ded085dbda23d84d1ca75bd85
mikegsCoder <mikegsid@gmail.com>
2023-07-14 10:02:58 +0300

    Implemented example

--- Design-Patterns/01. Creational-Patterns/05. Builder/VehicleBuilder/Program.cs
+++ Design-Patterns/01. Creational-Patterns/05. Builder/VehicleBuilder/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-           
+            VehicleBuilder builder;
+
+            // Create shop with vehicle builders
+            Shop shop = new Shop();
+
+            // Construct and display vehicles
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            // Wait for user
+            Console.ReadKey();
         }
     }
 }
