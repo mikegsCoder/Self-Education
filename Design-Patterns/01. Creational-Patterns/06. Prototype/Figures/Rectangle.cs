723afbbd76ea40c05e87f9a4a90692458542b82b
mikegsCoder <mikegsid@gmail.com>
2023-07-15 17:50:40 +0300

    Add Rectangle

---
+++ Design-Patterns/01. Creational-Patterns/06. Prototype/Figures/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Rectangle : IFigure
+    {
+        int width;
+        int height;
+
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public IFigure Clone()
+        {
+            return new Rectangle(width, height);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Rectangle with height {0} and width {1}.", height, width);
+        }
+    }
+}
