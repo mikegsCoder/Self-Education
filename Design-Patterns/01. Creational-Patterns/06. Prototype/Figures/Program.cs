4b9d3be467bf9df6d64e8816daeefc2202b1eebe
mikegsCoder <mikegsid@gmail.com>
2023-07-15 17:52:17 +0300

    Implemented example

--- Design-Patterns/01. Creational-Patterns/06. Prototype/Figures/Program.cs
+++ Design-Patterns/01. Creational-Patterns/06. Prototype/Figures/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
+            IFigure figure = new Rectangle(30, 40);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            figure = new Circle(30);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            Console.Read();
         }
     }
 }
