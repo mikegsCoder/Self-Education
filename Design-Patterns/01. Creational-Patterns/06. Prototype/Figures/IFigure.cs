ce70ed22aa4392e4e3363c234f89f34b22dabbf9
mikegsCoder <mikegsid@gmail.com>
2023-07-15 17:46:59 +0300

    Add IFigure

---
+++ Design-Patterns/01. Creational-Patterns/06. Prototype/Figures/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public interface IFigure
+    {
+        IFigure Clone();
+
+        void GetInfo();
+    }
+}
