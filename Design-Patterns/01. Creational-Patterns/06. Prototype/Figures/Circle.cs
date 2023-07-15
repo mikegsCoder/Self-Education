af02b605812ce2cc1968743fed5557a57364f424
mikegsCoder <mikegsid@gmail.com>
2023-07-15 17:48:47 +0300

    Add Circle

---
+++ Design-Patterns/01. Creational-Patterns/06. Prototype/Figures/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Circle : IFigure
+    {
+        int radius;
+
+        public Circle(int r)
+        {
+            radius = r;
+        }
+
+        public IFigure Clone()
+        {
+            return new Circle(radius);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Circle with radius {0}.", radius);
+        }
+    }
+}
