74119094c602b5b409ad23a3c045c4f5d56e4672
mikegsCoder <mikegsid@gmail.com>
2023-07-16 16:35:28 +0300

    Implemented example

--- Design-Patterns/01. Creational-Patterns/07. Fluent-Interface/FluentInterface/Program.cs
+++ Design-Patterns/01. Creational-Patterns/07. Fluent-Interface/FluentInterface/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            FluentEmployee fluentEmployee = new FluentEmployee();
+
+            fluentEmployee.NameOfTheEmployee("Peter Petrov")
+            .Born("10/11/1990")
+            .SetDepartment("IT")
+            .SetAddress("Varna, Bulgaria");
         }
     }
 }
