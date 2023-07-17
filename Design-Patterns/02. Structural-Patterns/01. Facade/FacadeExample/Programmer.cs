691ecd2b74461c859bf0b6e98544853ba2fe7ac6
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:17:32 +0300

    Add Programmer class

---
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Programmer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class Programmer
+    {
+        public void CreateApplication(VisualStudioFacade facade)
+        {
+            facade.Start();
+            facade.Stop();
+        }
+    }
+}
