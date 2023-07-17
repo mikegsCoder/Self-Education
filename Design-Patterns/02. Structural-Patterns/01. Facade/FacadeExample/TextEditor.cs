4ba7e5618bc6ad13417a87e9f33668cede2e4f12
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:12:42 +0300

    Add TextEditor

---
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/TextEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class TextEditor
+    {
+        public void CreateCode()
+        {
+            Console.WriteLine("Create code.");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Save code.");
+        }
+    }
+}
