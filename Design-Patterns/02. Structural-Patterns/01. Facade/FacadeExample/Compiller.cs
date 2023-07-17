cf916a827b4ca8b2e6f9340a18eefeb71995d4ea
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:21:50 +0300

    Cleanup and code formatting

--- Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Compiller.cs
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Compiller.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FacadeExample
+﻿namespace FacadeExample
 {
     public class Compiller
     {
