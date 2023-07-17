218f17b84ad9ecf7fadcfb4bf571e2a7f22ab90d
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:15:18 +0300

    Add Compiler class

---
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Compiller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class Compiller
+    {
+        public void Compile()
+        {
+            Console.WriteLine("Compile code.");
+        }
+    }
+}
