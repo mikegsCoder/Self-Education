6e177a1d89433e6d0bd06043457bcc8109b973ef
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:16:25 +0300

    Add VisualStudioFacade class

---
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/VisualStudioFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class VisualStudioFacade
+    {
+        TextEditor textEditor;
+        Compiller compiller;
+        CLR clr;
+
+        public VisualStudioFacade(TextEditor te, Compiller compil, CLR c)
+        {
+            textEditor = te;
+            compiller = compil;
+            clr = c;
+        }
+
+        public void Start()
+        {
+            textEditor.CreateCode();
+            textEditor.Save();
+            compiller.Compile();
+            clr.Execute();
+        }
+
+        public void Stop()
+        {
+            clr.Finish();
+        }
+    }
+}
