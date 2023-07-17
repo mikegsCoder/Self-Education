e7c43d6e46d1f3c5e7ff2aa87a880696e1056211
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:14:17 +0300

    Add CLR class

---
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/CLR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class CLR
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Execute application.");
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("Close application.");
+        }
+    }
+}
