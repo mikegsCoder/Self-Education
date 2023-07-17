d6daa34507c5f2f3b5c707c82b6be8d084620356
mikegsCoder <mikegsid@gmail.com>
2023-07-17 13:19:05 +0300

    Implemented example

--- Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Program.cs
+++ Design-Patterns/02. Structural-Patterns/01. Facade/FacadeExample/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-           
+            TextEditor textEditor = new TextEditor();
+            Compiller compiller = new Compiller();
+            CLR clr = new CLR();
+
+            VisualStudioFacade ide = new VisualStudioFacade(textEditor, compiller, clr);
+
+            Programmer programmer = new Programmer();
+            programmer.CreateApplication(ide);
+
+            Console.ReadLine();
         }
     }
 }
