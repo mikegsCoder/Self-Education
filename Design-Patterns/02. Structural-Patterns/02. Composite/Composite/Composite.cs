7a1034464e223cca14592eaf86a5fb2335d763ef
mikegsCoder <mikegsid@gmail.com>
2023-07-18 19:14:48 +0300

    Add Composite class

---
+++ Design-Patterns/02. Structural-Patterns/02. Composite/Composite/Composite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Composite : Component
+    {
+        List<Component> children = new List<Component>();
+
+        public Composite(string name)
+            : base(name)
+        { }
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine(name);
+
+            foreach (Component component in children)
+            {
+                component.Display();
+            }
+        }
+    }
+}
