3231dd5c52659211ae81afc453afa02e6460e94c
mikegsCoder <mikegsid@gmail.com>
2023-07-18 19:17:10 +0300

    Implemented Composite pattern

--- Design-Patterns/02. Structural-Patterns/02. Composite/Composite/Program.cs
+++ Design-Patterns/02. Structural-Patterns/02. Composite/Composite/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-          
+            // Create components
+            Component root = new Composite("Root");
+            Composite subtree = new Composite("Subtree");
+            Component leaf1 = new Leaf("Leaf");
+            Component leaf2 = new Leaf("Leaf");
+
+            // Create treee
+            subtree.Add(leaf2);
+            root.Add(subtree);
+            root.Add(leaf1);
+
+            // Display
+            root.Display();
         }
     }
 }
