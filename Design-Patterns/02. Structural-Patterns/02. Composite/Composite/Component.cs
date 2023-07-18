efde731e0a0e646f52d7029a4c07bf6fef09de49
mikegsCoder <mikegsid@gmail.com>
2023-07-18 19:13:49 +0300

    Add Component class

---
+++ Design-Patterns/02. Structural-Patterns/02. Composite/Composite/Component.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public abstract class Component
+    {
+        protected string name;
+
+        public Component(string _name)
+        {
+            name = _name;
+        }
+
+        public abstract void Display();
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+    }
+}
