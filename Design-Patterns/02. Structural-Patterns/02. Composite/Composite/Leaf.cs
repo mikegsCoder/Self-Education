8e334c353d94669eca35838a99afd26470aecffc
mikegsCoder <mikegsid@gmail.com>
2023-07-18 19:15:42 +0300

    Add Leaf class

---
+++ Design-Patterns/02. Structural-Patterns/02. Composite/Composite/Leaf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Leaf : Component
+    {
+        public Leaf(string name)
+         : base(name)
+        { }
+
+        public override void Display()
+        {
+            Console.WriteLine(name);
+        }
+
+        public override void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
