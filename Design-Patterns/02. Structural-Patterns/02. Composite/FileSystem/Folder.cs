d42b0fff46ab1a4cfe02131c60b47e025ada8c10
mikegsCoder <mikegsid@gmail.com>
2023-07-19 17:39:26 +0300

    Add Folder class

---
+++ Design-Patterns/02. Structural-Patterns/02. Composite/FileSystem/Folder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    public class Folder : Component
+    {
+        private List<Component> components = new List<Component>();
+
+        public Folder(string name)
+            : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            components.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            components.Remove(component);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Node: " + name);
+            Console.WriteLine("SubNodes:");
+
+            for (int i = 0; i < components.Count; i++)
+            {
+                components[i].Print();
+            }
+        }
+    }
+}
