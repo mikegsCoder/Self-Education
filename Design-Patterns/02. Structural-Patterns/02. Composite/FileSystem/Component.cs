a28bdc14576daf9c7f582901e4c909b607c39a71
mikegsCoder <mikegsid@gmail.com>
2023-07-19 17:37:13 +0300

    Add Component

---
+++ Design-Patterns/02. Structural-Patterns/02. Composite/FileSystem/Component.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    public abstract class Component
+    {
+        protected string name;
+
+        public Component(string _name)
+        {
+            name = _name;
+        }
+
+        public virtual void Add(Component component) { }
+
+        public virtual void Remove(Component component) { }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
