69ac0fa7648c4f6d903ab3a3f686ca8c1b1b2fb6
mikegsCoder <mikegsid@gmail.com>
2023-07-20 17:10:20 +0300

    Add Proxy

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Proxy();
+
+            subject.Request();
+        }
+    }
+}
