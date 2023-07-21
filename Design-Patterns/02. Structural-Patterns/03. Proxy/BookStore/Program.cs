f93ab86310e7fecf56bd3aeedaab12923eaff3c6
mikegsCoder <mikegsid@gmail.com>
2023-07-21 18:02:53 +0300

    Implemented example

--- Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/Program.cs
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            using (IBook book = new BookStoreProxy())
+            {
+                // get first page
+                Page page1 = book.GetPage(1);
+                Console.WriteLine(page1.Text);
+
+                // get second page
+                Page page2 = book.GetPage(2);
+                Console.WriteLine(page2.Text);
+
+                // go back to the first page
+                page1 = book.GetPage(1);
+                Console.WriteLine(page1.Text);
+            }
+
+            Console.Read();
         }
     }
 }
