264c1f102079480bb67f1b38a9c0181b3ff7a228
mikegsCoder <mikegsid@gmail.com>
2023-07-21 18:00:47 +0300

    Add BookStoreProxy class

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/BookStoreProxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class BookStoreProxy : IBook
+    {
+        List<Page> pages;
+        BookStore bookStore;
+
+        public BookStoreProxy()
+        {
+            pages = new List<Page>();
+        }
+
+        public Page GetPage(int number)
+        {
+            Page page = pages.FirstOrDefault(p => p.Number == number);
+
+            if (page == null)
+            {
+                if (bookStore == null)
+                {
+                    bookStore = new BookStore();
+                }
+
+                page = bookStore.GetPage(number);
+
+                pages.Add(page);
+            }
+
+            return page;
+        }
+
+        public void Dispose()
+        {
+            if (bookStore != null)
+            {
+                bookStore.Dispose();
+            }
+        }
+    }
+}
