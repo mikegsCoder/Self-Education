7c05e89dd635b618923be946c4770838ab6a64d9
mikegsCoder <mikegsid@gmail.com>
2023-07-21 17:57:09 +0300

    Add IBook interface

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/IBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public interface IBook : IDisposable
+    {
+        Page GetPage(int number);
+    }
+}
