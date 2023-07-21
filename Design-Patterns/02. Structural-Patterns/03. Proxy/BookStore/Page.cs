e0460116c0e9469ffb53dbd8cca288abed29e612
mikegsCoder <mikegsid@gmail.com>
2023-07-21 18:05:21 +0300

    Cleanup and code formatting

--- Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/Page.cs
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/Page.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookStore
+﻿namespace BookStore
 {
     public class Page
     {
         public int Id { get; set; }
+
         public int Number { get; set; }
+
         public string Text { get; set; }
     }
 }
