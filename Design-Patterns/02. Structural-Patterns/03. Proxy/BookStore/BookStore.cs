ac74359a29bd966519c9cb55a05a01dc27cbeb2b
mikegsCoder <mikegsid@gmail.com>
2023-07-21 17:59:34 +0300

    Add BookStore class

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/BookStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class BookStore : IBook
+    {
+        PageContext db;
+
+        public BookStore()
+        {
+            db = new PageContext();
+        }
+
+        public Page GetPage(int number)
+        {
+            return db.Pages.FirstOrDefault(p => p.Number == number);
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
