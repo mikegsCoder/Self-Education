d9debc7d0883e343871ebd24c195adc9cb6c197e
mikegsCoder <mikegsid@gmail.com>
2023-07-21 17:55:49 +0300

    Add Page class

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/Page.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
+}
