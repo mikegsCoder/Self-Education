4801838d69c9f2a225130923ff2eb7d8a916fde1
mikegsCoder <mikegsid@gmail.com>
2023-07-21 17:58:22 +0300

    Add PageContext

---
+++ Design-Patterns/02. Structural-Patterns/03. Proxy/BookStore/PageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class PageContext : DbContext
+    {
+        public DbSet<Page> Pages { get; set; }
+    }
+}
