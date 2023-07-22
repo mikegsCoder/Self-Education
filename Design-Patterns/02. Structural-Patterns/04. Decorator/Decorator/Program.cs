8499d03e1f9d5c0fa7cbc626880abef171dcefba
mikegsCoder <mikegsid@gmail.com>
2023-07-22 14:42:52 +0300

    Implemented example

--- Design-Patterns/02. Structural-Patterns/04. Decorator/Decorator/Program.cs
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Decorator/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            // Create ConcreteComponent and two Decorators
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+            d2.Operation();
+
+            // Wait for user
+            Console.ReadKey();
         }
     }
 }
