a1d837b82e5ff3ceb32d9b00abf8293d59e13367
mikegsCoder <mikegsid@gmail.com>
2023-07-22 14:37:35 +0300

    Add Decorator class

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Decorator/Decorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component _component)
+        {
+            component = _component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+}
