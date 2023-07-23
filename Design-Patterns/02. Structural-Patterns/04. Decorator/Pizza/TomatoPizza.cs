b3ae185b42f1e2ffa0ab7750a36f4442d704c022
mikegsCoder <mikegsid@gmail.com>
2023-07-23 13:02:43 +0300

    Add TomatoPizza

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Pizza/TomatoPizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class TomatoPizza : PizzaDecorator
+    {
+        public TomatoPizza(Pizza p)
+           : base(p.Name + ", with tomatoes", p)
+        { }
+
+        public override int GetCost()
+        {
+            return pizza.GetCost() + 3;
+        }
+    }
+}
