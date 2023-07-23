d2589f11c0d9695a9d0211d1feee1d69d95c736a
mikegsCoder <mikegsid@gmail.com>
2023-07-23 13:03:49 +0300

    Add CheesePizza

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Pizza/CheesePizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class CheesePizza : PizzaDecorator
+    {
+        public CheesePizza(Pizza p)
+            : base(p.Name + ", with cheese", p)
+        { }
+
+        public override int GetCost()
+        {
+            return pizza.GetCost() + 5;
+        }
+    }
+}
