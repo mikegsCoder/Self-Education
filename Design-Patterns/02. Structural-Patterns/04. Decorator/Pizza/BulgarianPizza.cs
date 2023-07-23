f17588ecb102d5d55c4e552413b159d3c94d1c86
mikegsCoder <mikegsid@gmail.com>
2023-07-23 13:05:52 +0300

    Add BulgarianPizza

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Pizza/BulgarianPizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class BulgarianPizza : Pizza
+    {
+        public BulgarianPizza()
+           : base("Bulgarian pizza")
+        { }
+
+        public override int GetCost()
+        {
+            return 8;
+        }
+    }
+}
