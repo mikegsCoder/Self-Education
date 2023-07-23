479579936c531a2643345cdd90ffe05f5273189e
mikegsCoder <mikegsid@gmail.com>
2023-07-23 13:01:39 +0300

    Add PizzaDecorator class

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Pizza/PizzaDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public abstract class PizzaDecorator : Pizza
+    {
+        protected Pizza pizza;
+
+        public PizzaDecorator(string n, Pizza _pizza) : base(n)
+        {
+            pizza = _pizza;
+        }
+    }
+}
