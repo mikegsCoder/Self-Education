2edb9306e43ef9c9a7ea8f06504f284695353688
mikegsCoder <mikegsid@gmail.com>
2023-07-23 13:04:54 +0300

    Add ItalianPizza

---
+++ Design-Patterns/02. Structural-Patterns/04. Decorator/Pizza/ItalianPizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class ItalianPizza : Pizza
+    {
+        public ItalianPizza() : base("Italian pizza")
+        { }
+
+        public override int GetCost()
+        {
+            return 10;
+        }
+    }
+}
