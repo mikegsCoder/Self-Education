493a580199abb01051838cea6f1b696c81b6c1fb
mikegsCoder <mikegsid@gmail.com>
2023-07-24 17:49:55 +0300

    Add Client class

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Adapter/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class Client
+    {
+        public void Request(Target target)
+        {
+            target.Request();
+        }
+    }
+}
