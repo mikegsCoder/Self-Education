90b8f891be6e28082059c2092f79b4b37d0cf56d
mikegsCoder <mikegsid@gmail.com>
2023-07-24 17:48:54 +0300

    Add Target class

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Adapter/Target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class Target
+    {
+        public virtual void Request()
+        { }
+    }
+}
