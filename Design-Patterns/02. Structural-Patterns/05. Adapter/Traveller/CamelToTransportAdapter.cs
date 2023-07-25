6c2993fa319f9fbb862a632c08bde752a2e7a747
mikegsCoder <mikegsid@gmail.com>
2023-07-25 16:30:47 +0300

    Add CamelToTransportAdapter

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/CamelToTransportAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveller
+{
+    public class CamelToTransportAdapter : ITransport
+    {
+        Camel camel;
+        public CamelToTransportAdapter(Camel c)
+        {
+            camel = c;
+        }
+
+        public void Drive()
+        {
+            camel.Move();
+        }
+    }
+}
