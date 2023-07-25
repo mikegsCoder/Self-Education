ce13d19c1d45be30115d8db0deaefc6e8013871d
mikegsCoder <mikegsid@gmail.com>
2023-07-25 16:29:11 +0300

    Add Driver class

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveller
+{
+    public class Driver
+    {
+        public void Travel(ITransport transport)
+        {
+            transport.Drive();
+        }
+    }
+}
