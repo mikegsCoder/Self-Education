12e7017322ad70fcc314303a3a269ed862e2fc76
mikegsCoder <mikegsid@gmail.com>
2023-07-25 16:28:16 +0300

    Add Auto class

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveller
+{
+    public class Auto : ITransport
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving car on the road.");
+        }
+    }
+}
