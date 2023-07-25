b851461ee78a9e12ac7cd7ae6a0828cad5a0671f
mikegsCoder <mikegsid@gmail.com>
2023-07-25 16:27:15 +0300

    Add Camel class

---
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/Camel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveller
+{
+    public class Camel : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Camel walks in the desert.");
+        }
+    }
+}
