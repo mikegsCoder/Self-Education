a6620fcf6bdd1d9665babc2c403a12009491371c
mikegsCoder <mikegsid@gmail.com>
2023-07-25 16:32:20 +0300

    Implemented example

--- Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/Program.cs
+++ Design-Patterns/02. Structural-Patterns/05. Adapter/Traveller/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            // traveller
+            Driver driver = new Driver();
+
+            // car
+            Auto auto = new Auto();
+
+            // travelling with car
+            driver.Travel(auto);
+
+            // camel
+            Camel camel = new Camel();
+
+            // using Adapter
+            ITransport camelTransport = new CamelToTransportAdapter(camel);
+
+            // continue travelling in the desert
+            driver.Travel(camelTransport);
+
+            Console.Read();
         }
     }
 }
