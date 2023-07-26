b6284e57f16ad957e29b8d8f762325e0a2589a97
mikegsCoder <mikegsid@gmail.com>
2023-07-26 17:28:32 +0300

    Implemented Bridge design pattern

--- Design-Patterns/02. Structural-Patterns/06. Bridge/Bridge/Program.cs
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Bridge/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            Abstraction abstraction;
+            abstraction = new RefinedAbstraction(new ConcreteImplementorA());
+            abstraction.Operation();
+
+            abstraction.Implementor = new ConcreteImplementorB();
+            abstraction.Operation();
         }
     }
 }
