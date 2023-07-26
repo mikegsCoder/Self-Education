e30d36e00cbe777a9a995bb7c32adc777d9d5c6e
mikegsCoder <mikegsid@gmail.com>
2023-07-26 17:26:47 +0300

    Add RefinedAbstraction

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Bridge/RefinedAbstraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class RefinedAbstraction : Abstraction
+    {
+        public RefinedAbstraction(Implementor imp)
+            : base(imp)
+        { }
+
+        public override void Operation()
+        {
+        }
+    }
+}
