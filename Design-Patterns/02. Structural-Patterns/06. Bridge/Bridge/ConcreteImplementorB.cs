a578825394ffc40d8dee8f1f83a3924b8c42d79c
mikegsCoder <mikegsid@gmail.com>
2023-07-26 17:25:32 +0300

    Add ConcreteImplementorB

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Bridge/ConcreteImplementorB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class ConcreteImplementorB : Implementor
+    {
+        public override void OperationImp()
+        {
+        }
+    }
+}
