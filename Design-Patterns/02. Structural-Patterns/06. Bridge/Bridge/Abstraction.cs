f88f4f885c61c6b9e898787e6916c2414b1c13d9
mikegsCoder <mikegsid@gmail.com>
2023-07-26 17:23:15 +0300

    Add Abstraction class

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Bridge/Abstraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public abstract class Abstraction
+    {
+        protected Implementor implementor;
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public Abstraction(Implementor imp)
+        {
+            implementor = imp;
+        }
+
+        public virtual void Operation()
+        {
+            implementor.OperationImp();
+        }
+    }
+}
