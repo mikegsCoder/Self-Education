d847210dafe11184417f4cb4d2876535b1a11dbd
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:42:59 +0300

    Add FreelanceProgrammer class

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/FreelanceProgrammer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    public class FreelanceProgrammer : Programmer
+    {
+        public FreelanceProgrammer(ILanguage lang) : base(lang)
+        {}
+
+        public override void EarnMoney()
+        {
+            Console.WriteLine("Get money for completed project.");
+        }
+    }
+}
