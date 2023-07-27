48bb8aa06e2ddf91c9fe67924b5fd44c64370512
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:38:06 +0300

    Add Programmers initial commit

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/ILanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    public interface ILanguage
+    {
+        void Build();
+        void Execute();
+    }
+}
