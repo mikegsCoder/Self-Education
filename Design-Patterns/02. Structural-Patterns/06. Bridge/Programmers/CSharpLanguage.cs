273ee6672795886160bf5872e2cef7d599ab6176
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:41:46 +0300

    Add CSharpLanguage class

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/CSharpLanguage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    public class CSharpLanguage : ILanguage
+    {
+        public void Build()
+        {
+            Console.WriteLine("Using Roslyn compiller to produce exe file.");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("JIT compiles to binary code.");
+            Console.WriteLine("CLR executes the binary code.");
+        }
+    }
+}
