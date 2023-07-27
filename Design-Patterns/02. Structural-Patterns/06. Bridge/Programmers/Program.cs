40f2c8c11354c5d320283cbeb12f2ea1c9bd30d9
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:45:54 +0300

    Implemented example

--- Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/Program.cs
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
+            // create new C++ programmer
+            Programmer freelancer = new FreelanceProgrammer(new CPPLanguage());
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+
+            // new project needs C# language
+            freelancer.Language = new CSharpLanguage();
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+
+            Console.Read();
         }
     }
 }
