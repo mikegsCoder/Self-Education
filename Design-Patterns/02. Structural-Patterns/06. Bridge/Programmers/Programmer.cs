58d3aee9f4088afcf0bd983f25833935011bcfa6
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:39:23 +0300

    Add Programmer class

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/Programmer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    public abstract class Programmer
+    {
+        protected ILanguage language;
+
+        public ILanguage Language
+        {
+            set { language = value; }
+        }
+
+        public Programmer(ILanguage lang)
+        {
+            language = lang;
+        }
+
+        public virtual void DoWork()
+        {
+            language.Build();
+            language.Execute();
+        }
+
+        public abstract void EarnMoney();
+    }
+}
