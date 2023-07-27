9ad64f23e048cb7a9b600ab91a782cbc9cf881d4
mikegsCoder <mikegsid@gmail.com>
2023-07-27 16:40:37 +0300

    Add CPPLanguage class

---
+++ Design-Patterns/02. Structural-Patterns/06. Bridge/Programmers/CPPLanguage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    public class CPPLanguage : ILanguage
+    {
+        public void Build()
+        {
+            Console.WriteLine("Using C++ compiller to create binary code.");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Executing binary code.");
+        }
+    }
+}
