78e557a2f0b394a76c4d574192ea68393999ddf0
mikegsCoder <mikegsid@gmail.com>
2023-07-28 17:55:34 +0300

    Add ConcreteFlyweight class

---
+++ Design-Patterns/02. Structural-Patterns/07. Flyweight/Flyweight/ConcreteFlyweight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public class ConcreteFlyweight : Flyweight
+    {
+        int intrinsicState;
+
+        public override void Operation(int extrinsicState)
+        {}
+    }
+}
