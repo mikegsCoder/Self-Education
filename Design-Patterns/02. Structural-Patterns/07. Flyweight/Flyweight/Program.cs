8fe36065a0abc5f1b67106e8ce927e5db2b5b11f
mikegsCoder <mikegsid@gmail.com>
2023-07-28 18:01:22 +0300

    Implemented Flyweight design pattern

--- Design-Patterns/02. Structural-Patterns/07. Flyweight/Flyweight/Program.cs
+++ Design-Patterns/02. Structural-Patterns/07. Flyweight/Flyweight/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-           
+            int extrinsicstate = 22;
+
+            FlyweightFactory f = new FlyweightFactory();
+
+            Flyweight fx = f.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = f.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fd = f.GetFlyweight("D");
+            fd.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight uf = new UnsharedConcreteFlyweight();
+
+            uf.Operation(--extrinsicstate);
         }
     }
 }
