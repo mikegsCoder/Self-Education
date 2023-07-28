93bace911a8010f6e8fb467909d9d42876247e54
mikegsCoder <mikegsid@gmail.com>
2023-07-28 17:58:52 +0300

    Add FlyweightFactory

---
+++ Design-Patterns/02. Structural-Patterns/07. Flyweight/Flyweight/FlyweightFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public class FlyweightFactory
+    {
+        Hashtable flyweights = new Hashtable();
+
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            if (!flyweights.ContainsKey(key))
+            {
+                flyweights.Add(key, new ConcreteFlyweight());
+            }
+
+            return flyweights[key] as Flyweight;
+        }
+    }
+}
