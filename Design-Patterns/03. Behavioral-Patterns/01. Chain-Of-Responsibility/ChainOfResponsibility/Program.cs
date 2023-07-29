e8ff26298b5277e02e27ed20ec61de5507ace224
mikegsCoder <mikegsid@gmail.com>
2023-07-29 12:30:41 +0300

    Implemented Chain Of Responsibility pattern

--- Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/ChainOfResponsibility/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/ChainOfResponsibility/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+
+            h1.Successor = h2;
+            h1.HandleRequest(2);
         }
     }
 }
