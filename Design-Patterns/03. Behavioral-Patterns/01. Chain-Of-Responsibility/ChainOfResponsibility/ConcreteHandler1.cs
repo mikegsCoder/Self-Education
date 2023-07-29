eb8c88f26eb4c9ea5fff9daba278d9e0ef01fdd3
mikegsCoder <mikegsid@gmail.com>
2023-07-29 12:27:47 +0300

    Add ConcreteHandler1 class

---
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/ChainOfResponsibility/ConcreteHandler1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int condition)
+        {
+            // request aggregation
+
+            if (condition == 1)
+            {
+                // request handled
+            }
+
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(condition);
+            }
+        }
+    }
+}
