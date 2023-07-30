bae8149eb4ead3994e3e0a94619e8f9d591dba85
mikegsCoder <mikegsid@gmail.com>
2023-07-30 11:18:26 +0300

    Add MoneyPaymentHandler class

---
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/MoneyPaymentHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    public class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer == true)
+            {
+                Console.WriteLine("Executing WesternUnion transfer.");
+            }
+            else if (Successor != null)
+            {
+                Successor.Handle(receiver);
+            }
+        }
+    }
+}
