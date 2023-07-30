91ec075d6663eab2e9ebc9854fd7a48e5b49c07d
mikegsCoder <mikegsid@gmail.com>
2023-07-30 11:14:44 +0300

    Add MoneyTransfer app initial commit

---
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/Receiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    public class Receiver
+    {
+        // bank transfer
+        public bool BankTransfer { get; set; }
+
+        // money transfers
+        public bool MoneyTransfer { get; set; }
+
+        // PayPal transfer
+        public bool PayPalTransfer { get; set; }
+
+        public Receiver(bool bt, bool mt, bool ppt)
+        {
+            BankTransfer = bt;
+            MoneyTransfer = mt;
+            PayPalTransfer = ppt;
+        }
+    }
+}
