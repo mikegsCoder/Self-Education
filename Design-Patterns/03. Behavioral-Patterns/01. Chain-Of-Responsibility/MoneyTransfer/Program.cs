99b7fb5010dbe1bcedcc99a042e767453d7e5cbb
mikegsCoder <mikegsid@gmail.com>
2023-07-30 11:21:32 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            Receiver receiver = new Receiver(false, true, true);
+
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHadler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+
+            bankPaymentHandler.Successor = paypalPaymentHandler;
+            paypalPaymentHandler.Successor = moneyPaymentHadler;
+
+            bankPaymentHandler.Handle(receiver);
+
+            Console.Read();
         }
     }
 }
