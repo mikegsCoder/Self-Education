683fe432baf757907633523f12cc008dc0d84f62
mikegsCoder <mikegsid@gmail.com>
2023-07-30 11:16:08 +0300

    Add PaymentHandler class

---
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/PaymentHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    public abstract class PaymentHandler
+    {
+        public PaymentHandler Successor { get; set; }
+
+        public abstract void Handle(Receiver receiver);
+    }
+}
