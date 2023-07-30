a6c7c1293fae5b2cb494db04dda07691b369af32
mikegsCoder <mikegsid@gmail.com>
2023-07-30 11:17:22 +0300

    Add BankPaymentHandler class

---
+++ Design-Patterns/03. Behavioral-Patterns/01. Chain-Of-Responsibility/MoneyTransfer/BankPaymentHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    public class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.BankTransfer == true)
+            {
+                Console.WriteLine("Executing bank transfer.");
+            }
+            else if (Successor != null)
+            {
+                Successor.Handle(receiver);
+            }
+        }
+    }
+}
