9ecd3d40226b2db99f02e2c57a963cc9829b4e5f
mikegsCoder <mikegsid@gmail.com>
2023-07-31 17:28:25 +0300

    Add Aggregate class

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/Iterator/Aggregate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+
+        public abstract int Count { get; protected set; }
+
+        public abstract object this[int index] { get; set; }
+    }
+}
