75e84870b8488fe7b4828e96bba8eb1f189e5bdc
mikegsCoder <mikegsid@gmail.com>
2023-07-31 17:29:30 +0300

    Add ConcreteIterator class

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/Iterator/ConcreteIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class ConcreteIterator : Iterator
+    {
+        private readonly Aggregate aggregate;
+        private int current;
+
+        public ConcreteIterator(Aggregate _aggregate)
+        {
+            aggregate = _aggregate;
+        }
+
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+
+            current++;
+
+            if (current < aggregate.Count)
+            {
+                ret = aggregate[current];
+            }
+
+            return ret;
+        }
+
+        public override object CurrentItem()
+        {
+            return aggregate[current];
+        }
+
+        public override bool IsDone()
+        {
+            return current >= aggregate.Count;
+        }
+    }
+}
