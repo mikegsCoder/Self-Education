234bbe0d196589a136dd0e8cc11abe562499bfcd
mikegsCoder <mikegsid@gmail.com>
2023-08-01 16:39:12 +0300

    Add LibraryNumerator

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/BookLibrary/LibraryNumerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class LibraryNumerator : IBookIterator
+    {
+        IBookNumerable aggregate;
+        int index = 0;
+
+        public LibraryNumerator(IBookNumerable a)
+        {
+            aggregate = a;
+        }
+
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
