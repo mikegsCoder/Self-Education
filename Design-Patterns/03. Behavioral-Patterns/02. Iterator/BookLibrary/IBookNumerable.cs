3fb6f23763db9d34163188c24d0626f1df84e990
mikegsCoder <mikegsid@gmail.com>
2023-08-01 16:37:14 +0300

    Add IBookNumerable

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/BookLibrary/IBookNumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public interface IBookNumerable
+    {
+        IBookIterator CreateNumerator();
+
+        int Count { get; }
+
+        Book this[int index] { get; }
+    }
+}
