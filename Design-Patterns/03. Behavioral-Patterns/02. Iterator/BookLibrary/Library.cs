c3d875650b937463aa8035b36b11aebef56df0fd
mikegsCoder <mikegsid@gmail.com>
2023-08-01 16:40:14 +0300

    Add Library class

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/BookLibrary/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class Library : IBookNumerable
+    {
+        private Book[] books;
+
+        public Library()
+        {
+            books = new Book[]
+            {
+                new Book{Name="C# development"},
+                new Book {Name="JS development"},
+                new Book {Name="Design patterns"}
+            };
+        }
+
+        public int Count
+        {
+            get { return books.Length; }
+        }
+
+        public Book this[int index]
+        {
+            get { return books[index]; }
+        }
+
+        public IBookIterator CreateNumerator()
+        {
+            return new LibraryNumerator(this);
+        }
+    }
+}
