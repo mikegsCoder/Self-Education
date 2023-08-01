ec1daf15f89a455f2042073441feb79e53b78265
mikegsCoder <mikegsid@gmail.com>
2023-08-01 16:41:20 +0300

    Add Reader class

---
+++ Design-Patterns/03. Behavioral-Patterns/02. Iterator/BookLibrary/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class Reader
+    {
+        public void SeeBooks(Library library)
+        {
+            IBookIterator iterator = library.CreateNumerator();
+
+            while (iterator.HasNext())
+            {
+                Book book = iterator.Next();
+
+                Console.WriteLine(book.Name);
+            }
+        }
+    }
+}
