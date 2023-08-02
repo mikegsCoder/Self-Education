cae5db7de386f1df9b10c870d2bebde2b3d39a3c
mikegsCoder <mikegsid@gmail.com>
2023-08-02 16:44:19 +0300

    Add Invoker class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/Command/Invoker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class Invoker
+    {
+        Command command;
+
+        public void SetCommand(Command c)
+        {
+            command = c;
+        }
+
+        public void Run()
+        {
+            command.Execute();
+        }
+
+        public void Cancel()
+        {
+            command.Undo();
+        }
+    }
+}
