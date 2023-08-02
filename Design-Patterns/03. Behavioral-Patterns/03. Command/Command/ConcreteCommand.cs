44a4f49194d6df89124e7fefd1824ad6ef631008
mikegsCoder <mikegsid@gmail.com>
2023-08-02 16:45:20 +0300

    Add ConcreteCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/Command/ConcreteCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class ConcreteCommand : Command
+    {
+        Receiver receiver;
+
+        public ConcreteCommand(Receiver r)
+        {
+            receiver = r;
+        }
+
+        public override void Execute()
+        {
+            receiver.Operation();
+        }
+
+        public override void Undo()
+        { }
+    }
+}
