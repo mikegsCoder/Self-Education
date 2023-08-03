4e5f511bf66452481d7731817f90fc66a070f56a
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:31:59 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-           
+            TV tv = new TV();
+            Volume volume = new Volume();
+            MultiPult mPult = new MultiPult();
+            mPult.SetCommand(0, new TVOnCommand(tv));
+            mPult.SetCommand(1, new VolumeCommand(volume));
+
+            // turn on TV
+            mPult.PressButton(0);
+
+            // increase volume
+            mPult.PressButton(1);
+            mPult.PressButton(1);
+            mPult.PressButton(1);
+
+            // cancel
+            mPult.PressUndoButton();
+            mPult.PressUndoButton();
+            mPult.PressUndoButton();
+            mPult.PressUndoButton();
+
+            Console.Read();
         }
     }
 }
