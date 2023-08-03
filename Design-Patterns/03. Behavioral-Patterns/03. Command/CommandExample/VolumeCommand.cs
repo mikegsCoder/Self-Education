de98fe46479f8b0e26049d1accd00f688dbd1029
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:19:56 +0300

    Add VolumeCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/VolumeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class VolumeCommand : ICommand
+    {
+        Volume volume;
+
+        public VolumeCommand(Volume v)
+        {
+            volume = v;
+        }
+
+        public void Execute()
+        {
+            volume.RaiseLevel();
+        }
+
+        public void Undo()
+        {
+            volume.DropLevel();
+        }
+    }
+}
