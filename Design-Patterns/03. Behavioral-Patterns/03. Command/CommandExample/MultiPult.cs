51c49f0412a4da58fcabcc2d5c427e4ad10fb828
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:29:49 +0300

    Add MultiPult class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/MultiPult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class MultiPult
+    {
+        ICommand[] buttons;
+        Stack<ICommand> commandsHistory;
+
+        public MultiPult()
+        {
+            buttons = new ICommand[2];
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new NoCommand();
+            }
+
+            commandsHistory = new Stack<ICommand>();
+        }
+
+        public void SetCommand(int number, ICommand com)
+        {
+            buttons[number] = com;
+        }
+
+        public void PressButton(int number)
+        {
+            buttons[number].Execute();
+
+            // add executed command into history
+            commandsHistory.Push(buttons[number]);
+        }
+
+        public void PressUndoButton()
+        {
+            if (commandsHistory.Count > 0)
+            {
+                ICommand undoCommand = commandsHistory.Pop();
+
+                undoCommand.Undo();
+            }
+        }
+    }
+}
