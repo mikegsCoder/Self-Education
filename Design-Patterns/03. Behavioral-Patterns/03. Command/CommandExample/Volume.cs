1fbae66aa7525145499521ba9c9a6aafc748b3b6
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:18:55 +0300

    Add Volume class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/Volume.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class Volume
+    {
+        public const int OFF = 0;
+        public const int HIGH = 20;
+        private int level;
+
+        public Volume()
+        {
+            level = OFF;
+        }
+
+        public void RaiseLevel()
+        {
+            if (level < HIGH)
+            {
+                level++;
+            }
+
+            Console.WriteLine("Volume level {0}.", level);
+        }
+
+        public void DropLevel()
+        {
+            if (level > OFF)
+            {
+                level--;
+            }
+
+            Console.WriteLine("Volume level {0}.", level);
+        }
+    }
+}
