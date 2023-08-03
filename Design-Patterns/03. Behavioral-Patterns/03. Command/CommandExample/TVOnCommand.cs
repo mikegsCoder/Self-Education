b694ed5b423dc1a331e36628353af98cfe2788a6
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:18:04 +0300

    Add TVOnCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/TVOnCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class TVOnCommand : ICommand
+    {
+        TV tv;
+
+        public TVOnCommand(TV tvSet)
+        {
+            tv = tvSet;
+        }
+
+        public void Execute()
+        {
+            tv.On();
+        }
+
+        public void Undo()
+        {
+            tv.Off();
+        }
+    }
+}
