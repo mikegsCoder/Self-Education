f34282e1736914dc8a155fad53715762ed2e962b
mikegsCoder <mikegsid@gmail.com>
2023-08-03 14:17:05 +0300

    Add TV class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/CommandExample/TV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("TV is switched ON.");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("TV is switched OFF.");
+        }
+    }
+}
