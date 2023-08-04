2694dec3b8be929fa3cd389e57120e23cc4d4819
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:16:31 +0300

    Add TestCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/TestCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class TestCommand : ICommand
+    {
+        QAEngineer qa;
+
+        public TestCommand(QAEngineer q)
+        {
+            qa = q;
+        }
+
+        public void Execute()
+        {
+            qa.StartTest();
+        }
+
+        public void Undo()
+        {
+            qa.StopTest();
+        }
+    }
+}
