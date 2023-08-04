79ad151f803c91e55aa659f3ac1a425a5f0271d1
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:11:17 +0300

    Add SalesManager class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/SalesManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class SalesManager
+    {
+        public void StartAdvertize()
+        {
+            Console.WriteLine("Sales Manager starts advertising.");
+        }
+
+        public void StopAdvertize()
+        {
+            Console.WriteLine("Sales Manager stopped advertising.");
+        }
+    }
+}
