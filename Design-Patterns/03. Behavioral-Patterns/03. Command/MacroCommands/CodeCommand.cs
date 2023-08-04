f7ce63bdf0a29214505d98a33ac08ee648f38f3f
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:13:14 +0300

    Add CodeCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/CodeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class CodeCommand : ICommand
+    {
+        Developer dev;
+
+        public CodeCommand(Developer d)
+        {
+            dev = d;
+        }
+
+        public void Execute()
+        {
+            dev.StartCoding();
+        }
+
+        public void Undo()
+        {
+            dev.StopCoding();
+        }
+    }
+}
