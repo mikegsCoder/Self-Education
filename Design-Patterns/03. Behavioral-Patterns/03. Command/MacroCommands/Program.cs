e7fd0e110f07437ed8708ce7dde73477e63b849c
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:20:59 +0300

    Implemented Macro command example

--- Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/Program.cs
@@ -6,7 +6,23 @@ namespace MacroCommands
     {
         static void Main(string[] args)
         {
-           
+            Developer dev = new Developer();
+            QAEngineer qa = new QAEngineer();
+            SalesManager manager = new SalesManager();
+
+            List<ICommand> commands = new List<ICommand>
+            {
+                new CodeCommand(dev),
+                new TestCommand(qa),
+                new AdvertiseCommand(manager)
+            };
+
+            CEO ceo = new CEO();
+            ceo.SetCommand(new MacroCommand(commands));
+            ceo.StartProject();
+            ceo.StopProject();
+
+            Console.Read();
         }
     }
 }
