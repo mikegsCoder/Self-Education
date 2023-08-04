f8c8c9ef20c043247c375031b0247e96dad1d174
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:19:11 +0300

    Add MacroCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/MacroCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class MacroCommand : ICommand
+    {
+        List<ICommand> commands;
+
+        public MacroCommand(List<ICommand> comms)
+        {
+            commands = comms;
+        }
+
+        public void Execute()
+        {
+            foreach (ICommand c in commands)
+            {
+                c.Execute();
+            }
+        }
+
+        public void Undo()
+        {
+            foreach (ICommand c in commands)
+            {
+                c.Undo();
+            }
+        }
+    }
+}
