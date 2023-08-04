472ce2601282090e585048e0ba15595b4d041736
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:12:20 +0300

    Add CEO

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/CEO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class CEO
+    {
+        ICommand command;
+
+        public void SetCommand(ICommand comm)
+        {
+            command = comm;
+        }
+
+        public void StartProject()
+        {
+            if (command != null)
+            {
+                command.Execute();
+            }
+        }
+
+        public void StopProject()
+        {
+            if (command != null)
+            {
+                command.Undo();
+            }
+        }
+    }
+}
