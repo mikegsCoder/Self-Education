d23646c3c3e474c77f72e4d3763b25540863a455
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:06:21 +0300

    Add MacroCommands initial commit

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace MacroCommands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
