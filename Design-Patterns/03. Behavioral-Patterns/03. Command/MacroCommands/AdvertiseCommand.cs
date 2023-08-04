51eab80f4e97be1d90ea892cca2d0a5a6d7edac0
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:17:45 +0300

    Add AdvertiseCommand class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/AdvertiseCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class AdvertiseCommand : ICommand
+    {
+        SalesManager manager;
+
+        public AdvertiseCommand(SalesManager m)
+        {
+            manager = m;
+        }
+
+        public void Execute()
+        {
+            manager.StartAdvertize();
+        }
+
+        public void Undo()
+        {
+            manager.StopAdvertize();
+        }
+    }
+}
