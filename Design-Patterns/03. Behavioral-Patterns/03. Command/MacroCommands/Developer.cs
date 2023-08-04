4b70260cdee0dfe122e6879568c3cfb409d27c6a
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:09:19 +0300

    Add Developer class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/Developer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class Developer
+    {
+        public void StartCoding()
+        {
+            Console.WriteLine("Developer starts coding.");
+        }
+
+        public void StopCoding()
+        {
+            Console.WriteLine("Developer finished coding.");
+        }
+    }
+}
