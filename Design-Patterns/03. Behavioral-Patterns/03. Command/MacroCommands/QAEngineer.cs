9afdbe5d930ba5909783c74fa014585135ba571a
mikegsCoder <mikegsid@gmail.com>
2023-08-04 14:10:17 +0300

    Add QAEngineer class

---
+++ Design-Patterns/03. Behavioral-Patterns/03. Command/MacroCommands/QAEngineer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroCommands
+{
+    public class QAEngineer
+    {
+        public void StartTest()
+        {
+            Console.WriteLine("QA Engineer starts testing.");
+        }
+
+        public void StopTest()
+        {
+            Console.WriteLine("QA Engineer finished testing.");
+        }
+    }
+}
