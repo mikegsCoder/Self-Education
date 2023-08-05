8a91208140e14a036523c70c4d469eb7b581e099
mikegsCoder <mikegsid@gmail.com>
2023-08-05 09:53:21 +0300

    Add ConcreteClass

---
+++ Design-Patterns/03. Behavioral-Patterns/04. Template-Method/TemplateMethod/ConcreteClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public class ConcreteClass : AbstractClass
+    {
+        public override void Operation1()
+        {
+            // some implementation
+        }
+
+        public override void Operation2()
+        {
+            // some implementation
+        }
+    }
+}
