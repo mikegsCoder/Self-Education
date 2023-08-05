ddcfb11b917c64f3834cad78340a95f8d8f0b317
mikegsCoder <mikegsid@gmail.com>
2023-08-05 09:52:23 +0300

    Add AbstractClass

---
+++ Design-Patterns/03. Behavioral-Patterns/04. Template-Method/TemplateMethod/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            Operation1();
+            Operation2();
+        }
+
+        public abstract void Operation1();
+
+        public abstract void Operation2();
+    }
+}
