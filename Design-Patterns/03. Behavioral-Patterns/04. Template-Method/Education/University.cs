82003d80fe456bbd9e780c0f270e82cdf8d64be6
mikegsCoder <mikegsid@gmail.com>
2023-08-06 11:04:13 +0300

    Add University class

---
+++ Design-Patterns/03. Behavioral-Patterns/04. Template-Method/Education/University.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education
+{
+    public class University : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Taking exams to enter into University.");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Learning software development concepts.");
+            Console.WriteLine("Developing projects.");
+        }
+
+        public override void PassExams()
+        {
+            Console.WriteLine("Passing project defence.");
+        }
+
+        public override void GetDocument()
+        {
+            Console.WriteLine("Getting diploma for software developer.");
+        }
+    }
+}
