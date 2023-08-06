b9a516cc21d34c1f75f57fbfa7c4a0b70fed848b
mikegsCoder <mikegsid@gmail.com>
2023-08-06 11:01:32 +0300

    Add Eduation class

---
+++ Design-Patterns/03. Behavioral-Patterns/04. Template-Method/Education/Education.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education
+{
+    public abstract class Education
+    {
+        public void Learn()
+        {
+            Enter();
+            Study();
+            PassExams();
+            GetDocument();
+        }
+
+        public abstract void Enter();
+
+        public abstract void Study();
+
+        public virtual void PassExams()
+        {
+            Console.WriteLine("Passing exams.");
+        }
+
+        public abstract void GetDocument();
+    }
+}
