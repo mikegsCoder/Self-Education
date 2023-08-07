fd1261bdc7c883724f2ee08363cee23a69d9ef96
mikegsCoder <mikegsid@gmail.com>
2023-08-07 17:55:37 +0300

    Add Context

---
+++ Design-Patterns/03. Behavioral-Patterns/05. Strategy/Strategy/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Context
+    {
+        public IStrategy ContextStrategy { get; set; }
+
+        public Context(IStrategy _strategy)
+        {
+            ContextStrategy = _strategy;
+        }
+
+        public void ExecuteAlgorithm()
+        {
+            ContextStrategy.Algorithm();
+        }
+    }
+}
