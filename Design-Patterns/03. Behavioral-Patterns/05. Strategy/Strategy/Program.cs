43b2c6d019f862b2855899117c176a6c2f11063a
mikegsCoder <mikegsid@gmail.com>
2023-08-07 17:59:17 +0300

    Implemented Strategy pattern

--- Design-Patterns/03. Behavioral-Patterns/05. Strategy/Strategy/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/05. Strategy/Strategy/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-           
+            ConcreteStrategy1 strategy1 = new ConcreteStrategy1();
+            ConcreteStrategy2 strategy2 = new ConcreteStrategy2();
+
+            Context context = new Context(strategy1);
+            context.ExecuteAlgorithm();
+
+            context.ContextStrategy = strategy2;
+            context.ExecuteAlgorithm();
         }
     }
 }
