839c2a6a9cef9527f3155e7780092fd5737f1427
mikegsCoder <mikegsid@gmail.com>
2023-08-08 16:57:03 +0300

    Add Car class

---
+++ Design-Patterns/03. Behavioral-Patterns/05. Strategy/Car/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Car
+    {
+        protected int passengers; 
+        protected string model; 
+
+        public Car(int num, string model, IMovable mov)
+        {
+            passengers = num;
+            model = model;
+            Movable = mov;
+        }
+
+        public IMovable Movable { private get; set; }
+
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
