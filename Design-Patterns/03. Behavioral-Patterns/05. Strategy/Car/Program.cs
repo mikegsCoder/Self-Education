fb8dfde025625a4507ff3ecd7a9f250bca0018db
mikegsCoder <mikegsid@gmail.com>
2023-08-08 17:00:44 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/05. Strategy/Car/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/05. Strategy/Car/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            Car car = new Car(4, "Opel", new PetrolMove());
+            car.Move();
+
+            car.Movable = new ElectricMove();
+            car.Move();
+
+            Console.ReadLine();
         }
     }
 }
