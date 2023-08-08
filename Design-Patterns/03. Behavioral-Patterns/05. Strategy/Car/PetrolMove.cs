996cb09720df9eae92a49f23df88b25c8678f31b
mikegsCoder <mikegsid@gmail.com>
2023-08-08 16:58:10 +0300

    Add PetrolMove class

---
+++ Design-Patterns/03. Behavioral-Patterns/05. Strategy/Car/PetrolMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Moving on petrol.");
+        }
+    }
+}
