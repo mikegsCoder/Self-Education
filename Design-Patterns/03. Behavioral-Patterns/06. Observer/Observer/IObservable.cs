31852b20ab6902c155c3e6b7f761ac6b7cb6f476
mikegsCoder <mikegsid@gmail.com>
2023-08-09 17:24:37 +0300

    Add IObservable interface

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/Observer/IObservable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public interface IObservable
+    {
+        void AddObserver(IObserver o);
+
+        void RemoveObserver(IObserver o);
+
+        void NotifyObservers();
+    }
+}
