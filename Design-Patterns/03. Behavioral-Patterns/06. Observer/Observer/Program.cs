d4cdf8ff313804492471dabb8901cd0da4997c1c
mikegsCoder <mikegsid@gmail.com>
2023-08-09 17:29:27 +0300

    Implemented Observer pattern

--- Design-Patterns/03. Behavioral-Patterns/06. Observer/Observer/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/Observer/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+
+            ConcreteObservable observable = new ConcreteObservable();
+
+            observable.AddObserver(observer1);
+            observable.AddObserver(observer2);
+
+            observable.NotifyObservers();
         }
     }
 }
