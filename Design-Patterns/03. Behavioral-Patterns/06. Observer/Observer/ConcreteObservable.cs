b2d390b11cf84a97dae1488663087d2bbfcb5dc8
mikegsCoder <mikegsid@gmail.com>
2023-08-09 17:27:21 +0300

    Add ConcreteObservable

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/Observer/ConcreteObservable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class ConcreteObservable : IObservable
+    {
+        private List<IObserver> observers;
+
+        public ConcreteObservable()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void AddObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+}
