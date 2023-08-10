4c233133d4fe1653f5a64756bf6b90fb6706e44c
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:17:49 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-           
+            Stock stock = new Stock();
+            Bank bank = new Bank("New Bank", stock);
+            Broker broker = new Broker("John Smith", stock);
+
+            stock.Market();
+            broker.StopTrade();
+            stock.Market();
+
+            Console.Read();
         }
     }
 }
