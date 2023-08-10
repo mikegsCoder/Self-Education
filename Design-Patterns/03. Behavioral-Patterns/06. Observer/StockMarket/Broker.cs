59e405ad32df7683ce740105ca6ad8343ad1119f
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:16:14 +0300

    Add Broker class

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/Broker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Broker : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+
+        public Broker(string name, IObservable obs)
+        {
+            Name = name;
+            stock = obs;
+
+            stock.RegisterObserver(this);
+        }
+
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.USD > 1.5)
+            {
+                Console.WriteLine("Broker {0} sells USD at price: {1}.", Name, sInfo.USD);
+            }
+            else
+            {
+                Console.WriteLine("Broker {0} buys USD at price: {1}", Name, sInfo.USD);
+            }
+        }
+
+        public void StopTrade()
+        {
+            stock.RemoveObserver(this);
+            stock = null;
+        }
+    }
+}
