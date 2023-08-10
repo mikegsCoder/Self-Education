09f216625d18e218281a6925a6c033efb4f276a3
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:09:38 +0300

    Add IObservable interface

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/IObservable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public interface IObservable
+    {
+        void RegisterObserver(IObserver o);
+
+        void RemoveObserver(IObserver o);
+
+        void NotifyObservers();
+    }
+}
