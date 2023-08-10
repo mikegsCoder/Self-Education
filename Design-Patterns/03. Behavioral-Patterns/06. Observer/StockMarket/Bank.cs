20c0aa0fbc43832e5baeb42f79062dbaec6204e4
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:14:04 +0300

    Add Bank class

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/Bank.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Bank : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+
+        public Bank(string name, IObservable obs)
+        {
+            Name = name;
+            stock = obs;
+
+            stock.RegisterObserver(this);
+        }
+
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.Euro > 2.5)
+            {
+                Console.WriteLine("Bank {0} sells Euro at price: {1}", Name, sInfo.Euro);
+            }
+            else
+            {
+                Console.WriteLine("Bank {0} buys Euro at price: {1}", Name, sInfo.Euro);
+            }
+        }
+    }
+}
