fa9efcf594f4b50b18980695ab8950407e03d4f9
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:12:50 +0300

    Add Stock class

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/Stock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Stock : IObservable
+    {
+        StockInfo sInfo;
+
+        List<IObserver> observers;
+
+        public Stock()
+        {
+            observers = new List<IObserver>();
+            sInfo = new StockInfo();
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.Update(sInfo);
+            }
+        }
+
+        public void Market()
+        {
+            sInfo.USD = GetRandomNumber(1, 2);
+            sInfo.Euro = GetRandomNumber(2, 3);
+
+            NotifyObservers();
+        }
+
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
