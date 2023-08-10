8ba2359ec8b236b59a86d0e9da1ae6a5c9698f0d
mikegsCoder <mikegsid@gmail.com>
2023-08-10 16:11:45 +0300

    Add StockInfo

---
+++ Design-Patterns/03. Behavioral-Patterns/06. Observer/StockMarket/StockInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class StockInfo
+    {
+        public double USD { get; set; }
+
+        public double Euro { get; set; }
+    }
+}
