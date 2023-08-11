38a7b07a509d8e43ac59a514eee435d8ecfdbb13
mikegsCoder <mikegsid@gmail.com>
2023-08-11 18:05:54 +0300

    Add ConcreteColleague2

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/Mediator/ConcreteColleague2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public class ConcreteColleague2 : Colleague
+    {
+        public ConcreteColleague2(Mediator mediator)
+           : base(mediator)
+        { }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        { }
+    }
+}
