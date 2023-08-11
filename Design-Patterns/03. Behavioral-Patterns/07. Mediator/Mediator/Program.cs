18940a6cc14896130f3cb1c10ab739447bf45332
mikegsCoder <mikegsid@gmail.com>
2023-08-11 18:08:45 +0300

    Implemented Mediator pattern

--- Design-Patterns/03. Behavioral-Patterns/07. Mediator/Mediator/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/Mediator/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            ConcreteMediator mediator = new ConcreteMediator();
+            ConcreteColleague1 colleague1 = new ConcreteColleague1(mediator);
+            ConcreteColleague2 colleague2 = new ConcreteColleague2(mediator);
+
+            mediator.Send("Message", colleague2);
         }
     }
 }
