7ce4c87bfded0c750a4508735e4291e64471cb13
mikegsCoder <mikegsid@gmail.com>
2023-08-11 18:10:21 +0300

    Cleanup and code formatting

--- Design-Patterns/03. Behavioral-Patterns/07. Mediator/Mediator/ConcreteColleague2.cs
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/Mediator/ConcreteColleague2.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mediator
+﻿namespace Mediator
 {
     public class ConcreteColleague2 : Colleague
     {
