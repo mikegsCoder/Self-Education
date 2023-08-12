cc3acb604ceaf4cc02320407e4a3b4a35fe7823e
mikegsCoder <mikegsid@gmail.com>
2023-08-12 12:03:19 +0300

    Add ManagerMediator class

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/ManagerMediator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment
+{
+    public class ManagerMediator : Mediator
+    {
+        public Colleague Customer { get; set; }
+        public Colleague Developer { get; set; }
+        public Colleague QA { get; set; }
+
+        public override void Send(string msg, Colleague colleague)
+        {
+            if (Customer == colleague)
+            {
+                Developer.Notify(msg);
+            }
+            else if (Developer == colleague)
+            {
+                QA.Notify(msg);
+            }
+            else if (QA == colleague)
+            {
+                Customer.Notify(msg);
+            }
+        }
+    }
+}
