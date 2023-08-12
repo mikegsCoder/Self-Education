b3f1dff977149796ec73a5aed25bcfd447b80f28
mikegsCoder <mikegsid@gmail.com>
2023-08-12 11:59:17 +0300

    Add QAColleague class

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/QAColleague.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment
+{
+    public class QAColleague : Colleague
+    {
+        public QAColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Message for QA: " + message);
+        }
+    }
+}
