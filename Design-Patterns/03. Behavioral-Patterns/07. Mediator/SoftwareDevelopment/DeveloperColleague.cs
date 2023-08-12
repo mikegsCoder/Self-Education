a886cb56259bd3a2c9cb641a2b14155d1b916655
mikegsCoder <mikegsid@gmail.com>
2023-08-12 11:57:41 +0300

    Add DeveloperColleague class

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/DeveloperColleague.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment
+{
+    public class DeveloperColleague : Colleague
+    {
+        public DeveloperColleague(Mediator mediator)
+           : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Message for developer: " + message);
+        }
+    }
+}
