cbb64e9166d07cb5d6891981d649fa925d1b19bf
mikegsCoder <mikegsid@gmail.com>
2023-08-12 11:55:44 +0300

    Add Colleague class

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/Colleague.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment
+{
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator _mediator)
+        {
+            mediator = _mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public abstract void Notify(string message);
+    }
+}
