67a76fbe933ba1e70c3f922e13be41b38b4b111a
mikegsCoder <mikegsid@gmail.com>
2023-08-12 12:04:59 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            ManagerMediator mediator = new ManagerMediator();
+            Colleague customer = new CustomerColleague(mediator);
+            Colleague developer = new DeveloperColleague(mediator);
+            Colleague qa = new QAColleague(mediator);
+
+            mediator.Customer = customer;
+            mediator.Developer = developer;
+            mediator.QA = qa;
+
+            customer.Send("I need new software.");
+            developer.Send("Software is ready. Need to be tested.");
+            qa.Send("Tests are done. Software is ready for sale.");
+
+            Console.Read();
         }
     }
 }
