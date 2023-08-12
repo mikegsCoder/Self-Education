8725d4ea2bc542b9e4c44776188ff9bf8dea7b91
mikegsCoder <mikegsid@gmail.com>
2023-08-12 12:00:52 +0300

    Add CustomerColleague class

---
+++ Design-Patterns/03. Behavioral-Patterns/07. Mediator/SoftwareDevelopment/CustomerColleague.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment
+{
+    public class CustomerColleague : Colleague
+    {
+        public CustomerColleague(Mediator mediator)
+           : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Message for customer: " + message);
+        }
+    }
+}
