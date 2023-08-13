d7e0ad3d850acc7837a94e1508d65d8e91f6abdc
mikegsCoder <mikegsid@gmail.com>
2023-08-13 11:12:14 +0300

    Implemented Memento pattern

--- Design-Patterns/03. Behavioral-Patterns/08. Memento/Memento/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/Memento/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            Originator o = new Originator();
+            o.State = "On";
+
+            // Store internal state
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            // Continue changing originator
+            o.State = "Off";
+
+            // Restore saved state
+            o.SetMemento(c.Memento);
+
+            // Wait for user
+            Console.ReadKey();
         }
     }
 }
