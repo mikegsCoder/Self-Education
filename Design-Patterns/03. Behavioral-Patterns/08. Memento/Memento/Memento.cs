ceaba2d5bedc8de1e038f008fb4cd6ad008216c4
mikegsCoder <mikegsid@gmail.com>
2023-08-13 11:08:34 +0300

    Add Memento class

---
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/Memento/Memento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Memento
+    {
+        public string State { get; private set; }
+
+        public Memento(string state)
+        {
+            State = state;
+        }
+    }
+}
