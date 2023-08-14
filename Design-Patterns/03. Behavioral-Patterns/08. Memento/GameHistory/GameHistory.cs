71e6dc6bafe6a8a5f857b2dd1b33889fcd3af611
mikegsCoder <mikegsid@gmail.com>
2023-08-14 17:57:19 +0300

    Add GameHistory

---
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/GameHistory/GameHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHistory
+{
+    // Caretaker
+    public class GameHistory
+    {
+        public Stack<HeroMemento> History { get; private set; }
+
+        public GameHistory()
+        {
+            History = new Stack<HeroMemento>();
+        }
+    }
+}
