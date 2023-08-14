b75001053151eb7dc33ab00953ad7547ed517247
mikegsCoder <mikegsid@gmail.com>
2023-08-14 17:56:15 +0300

    Add Hero class

---
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/GameHistory/Hero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHistory
+{
+    // Originator
+    public class Hero
+    {
+        private int bullets = 10; 
+        private int lives = 5; 
+
+        public void Shoot()
+        {
+            if (bullets > 0)
+            {
+                bullets--;
+
+                Console.WriteLine("We shot. We have {0} more bullets.", bullets);
+            }
+            else
+            {
+                Console.WriteLine("No more bullets.");
+            }
+        }
+
+        // Saving state
+        public HeroMemento SaveState()
+        {
+            Console.WriteLine("Saving game state. Parameters: {0} bullets, {1} lives.", bullets, lives);
+            
+            return new HeroMemento(bullets, lives);
+        }
+
+        // Restoring state
+        public void RestoreState(HeroMemento memento)
+        {
+            bullets = memento.Bullets;
+            lives = memento.Lives;
+
+            Console.WriteLine("Restoring game state. Parameters: {0} bullets, {1} lives.", bullets, lives);
+        }
+    }
+}
