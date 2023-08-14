4f803200cc522757bbda31be0711ea4e082a1be9
mikegsCoder <mikegsid@gmail.com>
2023-08-14 17:55:23 +0300

    Add HeroMemento

---
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/GameHistory/HeroMemento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHistory
+{
+    // Memento
+    public class HeroMemento
+    {
+        public int Bullets { get; private set; }
+        public int Lives { get; private set; }
+
+        public HeroMemento(int bullets, int lives)
+        {
+            Bullets = bullets;
+            Lives = lives;
+        }
+    }
+}
