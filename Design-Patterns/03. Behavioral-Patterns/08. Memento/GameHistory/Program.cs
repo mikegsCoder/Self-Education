fdb57aac46ff79f3d5d9b64c71386831cac94864
mikegsCoder <mikegsid@gmail.com>
2023-08-14 17:59:07 +0300

    Implemented Memento example

--- Design-Patterns/03. Behavioral-Patterns/08. Memento/GameHistory/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/08. Memento/GameHistory/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-           
+            Hero hero = new Hero();
+            hero.Shoot(); // we shot, we have 9 more bullets
+            GameHistory game = new GameHistory();
+
+            game.History.Push(hero.SaveState()); // saving state
+
+            hero.Shoot(); // we shot, we have 8 more bullets
+
+            hero.RestoreState(game.History.Pop()); // restoring state
+
+            hero.Shoot(); // we shot, we have 8 more bullets
+
+            Console.Read();
         }
     }
 }
