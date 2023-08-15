ce06b054e0c17ec0b575d29fa392e127e2d3394a
mikegsCoder <mikegsid@gmail.com>
2023-08-15 11:39:55 +0300

    Add StateA class

---
+++ Design-Patterns/03. Behavioral-Patterns/09. State/State/StateA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class StateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new StateB();
+        }
+    }
+}
