f96c645e6402f7c0549407915f357e8a87527b8d
mikegsCoder <mikegsid@gmail.com>
2023-08-16 17:40:38 +0300

    Add Water class

---
+++ Design-Patterns/03. Behavioral-Patterns/09. State/Water/Water.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Water
+{
+    public class Water
+    {
+        public WaterState State { get; set; }
+
+        public Water(WaterState ws)
+        {
+            State = ws;
+        }
+
+        public void Heat()
+        {
+            if (State == WaterState.ICE)
+            {
+                Console.WriteLine("Melting ice to liquid.");
+                State = WaterState.LIQUID;
+            }
+            else if (State == WaterState.LIQUID)
+            {
+                Console.WriteLine("Vaporizing liquid to steam.");
+                State = WaterState.GAS;
+            }
+            else if (State == WaterState.GAS)
+            {
+                Console.WriteLine("Increasing temperature of the steam.");
+            }
+        }
+        public void Frost()
+        {
+            if (State == WaterState.LIQUID)
+            {
+                Console.WriteLine("Cooling water to ice.");
+                State = WaterState.ICE;
+            }
+            else if (State == WaterState.GAS)
+            {
+                Console.WriteLine("Cooling steam to water.");
+                State = WaterState.LIQUID;
+            }
+        }
+    }
+}
