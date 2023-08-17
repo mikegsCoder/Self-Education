43f9d94f31f055ea2ae66a04bb30b9796a5fafb3
mikegsCoder <mikegsid@gmail.com>
2023-08-17 16:17:08 +0300

    Add TerminalExpression class

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/Interpreter/TerminalExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        { }
+    }
+}
