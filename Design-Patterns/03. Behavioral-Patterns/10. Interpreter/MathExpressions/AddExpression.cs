6b714781683fcf7413cd5da3d2387d9459ed3737
mikegsCoder <mikegsid@gmail.com>
2023-08-18 17:00:36 +0300

    Cleanup and code formatting

--- Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/AddExpression.cs
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/AddExpression.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MathExpressions
+﻿namespace MathExpressions
 {
     // nonterminal add expression
     public class AddExpression : IExpression
