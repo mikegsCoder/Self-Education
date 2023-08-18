51244e10a68c9de2a04f27188b8238474dbd2241
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:53:15 +0300

    Add MathExpressins initial commit

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/Context.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    public class Context
+    {
+        Dictionary<string, int> variables;
+
+        public Context()
+        {
+            variables = new Dictionary<string, int>();
+        }
+
+        // get variable by name
+        public int GetVariable(string name)
+        {
+            return variables[name];
+        }
+
+        public void SetVariable(string name, int value)
+        {
+            if (variables.ContainsKey(name))
+            {
+                variables[name] = value;
+            }
+            else
+            {
+                variables.Add(name, value);
+            }
+        }
+    }
+}
---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
