c9b01cf407920af9f4b2a61a7ed31614e207c787
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:57:25 +0300

    Add NumberExpression

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/NumberExpression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    // terminal expression
+    public class NumberExpression : IExpression
+    {
+        string name; // variable name
+
+        public NumberExpression(string variableName)
+        {
+            name = variableName;
+        }
+
+        public int Interpret(Context context)
+        {
+            return context.GetVariable(name);
+        }
+    }
+}
