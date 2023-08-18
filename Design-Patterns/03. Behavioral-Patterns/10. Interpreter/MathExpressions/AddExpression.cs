edecbca01d88950a74dd0d101d6f5e8651e32ada
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:55:28 +0300

    Add AddExpression

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/AddExpression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    // nonterminal add expression
+    public class AddExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public AddExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) + rightExpression.Interpret(context);
+        }
+    }
+}
