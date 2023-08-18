1821b685eb3b7a5f987ec2ca87d21c7d766a7055
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:54:24 +0300

    Add IExpression interface

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/IExpression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    public interface IExpression
+    {
+        int Interpret(Context context);
+    }
+}
