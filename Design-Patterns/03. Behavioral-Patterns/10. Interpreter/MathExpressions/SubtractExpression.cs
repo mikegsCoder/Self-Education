1d21bd43607e74589d5f2b0a955d76f7945edfd7
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:56:28 +0300

    Add SubtractExpression

---
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/SubtractExpression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    // nonterminal subtract expression
+    public class SubtractExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public SubtractExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) - rightExpression.Interpret(context);
+        }
+    }
+}
