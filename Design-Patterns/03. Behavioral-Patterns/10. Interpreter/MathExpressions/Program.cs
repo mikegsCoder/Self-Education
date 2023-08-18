be78ffbd12ddc9b2afdb7ee33e6fb59fbb63767f
mikegsCoder <mikegsid@gmail.com>
2023-08-18 16:58:54 +0300

    Implemented example

--- Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/10. Interpreter/MathExpressions/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-           
+            Context context = new Context();
+
+            // set variables
+            int x = 5;
+            int y = 8;
+            int z = 2;
+
+            // add variables into the context
+            context.SetVariable("x", x);
+            context.SetVariable("y", y);
+            context.SetVariable("z", z);
+
+            // create object fot calculation expression: x + y - z
+            IExpression expression = new SubtractExpression(
+                new AddExpression(
+                    new NumberExpression("x"),
+                    new NumberExpression("y")),
+                new NumberExpression("z")
+            );
+
+            int result = expression.Interpret(context);
+            Console.WriteLine("Result: {0}", result);
+
+            Console.Read();
         }
     }
 }
