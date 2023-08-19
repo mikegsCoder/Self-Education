fd2f6e128f027bd3ee8878b708ec13a9105d2f23
mikegsCoder <mikegsid@gmail.com>
2023-08-19 12:45:55 +0300

    Add ConcreteVisitor1

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/Visitor/ConcreteVisitor1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Visitor
+{
+    public class ConcreteVisitor1 : Visitor
+    {
+        public override void VisitElementA(ElementA elementA)
+        {
+            elementA.OperationA();
+        }
+
+        public override void VisitElementB(ElementB elementB)
+        {
+            elementB.OperationB();
+        }
+    }
+}
