bb42d73970353ac984aeb9911c05aafcde927e07
mikegsCoder <mikegsid@gmail.com>
2023-08-19 12:45:00 +0300

    Add ElementB

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/Visitor/ElementB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public class ElementB : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitElementB(this);
+        }
+
+        public void OperationB()
+        { }
+    }
+}
