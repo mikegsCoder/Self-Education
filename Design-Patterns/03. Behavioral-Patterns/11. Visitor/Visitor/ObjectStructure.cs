8eda61c5080ef0fc0d217e178cdc547f73fb3410
mikegsCoder <mikegsid@gmail.com>
2023-08-19 12:48:26 +0300

    Add ObjectStructure

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/Visitor/ObjectStructure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Visitor
+{
+    public class ObjectStructure
+    {
+        List<Element> elements = new List<Element>();
+
+        public void Add(Element element)
+        {
+            elements.Add(element);
+        }
+
+        public void Remove(Element element)
+        {
+            elements.Remove(element);
+        }
+
+        public void Accept(Visitor visitor)
+        {
+            foreach (Element element in elements)
+            {
+                element.Accept(visitor);
+            }
+        }
+    }
+}
