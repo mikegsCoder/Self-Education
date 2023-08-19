b447b9420ef44c3a2d1042254d248121906f036c
mikegsCoder <mikegsid@gmail.com>
2023-08-19 12:50:09 +0300

    Implemented Visitor pattern

--- Design-Patterns/03. Behavioral-Patterns/11. Visitor/Visitor/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/Visitor/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            var structure = new ObjectStructure();
+
+            structure.Add(new ElementA());
+            structure.Add(new ElementB());
+
+            structure.Accept(new ConcreteVisitor1());
+            structure.Accept(new ConcreteVisitor2());
         }
     }
 }
