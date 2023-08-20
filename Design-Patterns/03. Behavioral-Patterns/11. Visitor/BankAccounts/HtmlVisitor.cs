ada60e6efc62524c1d3272b03594ef140810b047
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:50:48 +0300

    Add HtmlVisitor

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/HtmlVisitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class HtmlVisitor : IVisitor
+    {
+        public void VisitPersonAcc(Person acc)
+        {
+            string result = "<table><tr><td>Property<td><td>Value</td></tr>";
+            result += "<tr><td>Name<td><td>" + acc.Name + "</td></tr>";
+            result += "<tr><td>Number<td><td>" + acc.Number + "</td></tr></table>";
+           
+            Console.WriteLine(result);
+        }
+
+        public void VisitCompanyAcc(Company acc)
+        {
+            string result = "<table><tr><td>Property<td><td>Value</td></tr>";
+            result += "<tr><td>Name<td><td>" + acc.Name + "</td></tr>";
+            result += "<tr><td>RegNumber<td><td>" + acc.RegNumber + "</td></tr>";
+            result += "<tr><td>Number<td><td>" + acc.Number + "</td></tr></table>";
+            
+            Console.WriteLine(result);
+        }
+    }
+}
