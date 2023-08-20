b86a2e64ba067d4194c9bbd9118553d99e36b0d3
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:48:44 +0300

    Add Company class

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class Company : IAccount
+    {
+        public string Name { get; set; }
+        public string RegNumber { get; set; }
+        public string Number { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitCompanyAcc(this);
+        }
+    }
+}
