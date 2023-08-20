3e2364dd38dc977b86cbff61dcc374cef640ee70
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:51:45 +0300

    Add XmlVisitor

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/XmlVisitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class XmlVisitor : IVisitor
+    {
+        public void VisitPersonAcc(Person acc)
+        {
+            string result = "<Person><Name>" + acc.Name + "</Name>" +
+                "<Number>" + acc.Number + "</Number><Person>";
+
+            Console.WriteLine(result);
+        }
+
+        public void VisitCompanyAcc(Company acc)
+        {
+            string result = "<Company><Name>" + acc.Name + "</Name>" +
+                "<RegNumber>" + acc.RegNumber + "</RegNumber>" +
+                "<Number>" + acc.Number + "</Number><Company>";
+
+            Console.WriteLine(result);
+        }
+    }
+}
