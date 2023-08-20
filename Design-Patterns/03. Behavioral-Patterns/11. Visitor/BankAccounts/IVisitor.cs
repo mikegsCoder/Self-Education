24919c693e2bef33b04fe90ee10fbfb668bd5115
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:47:08 +0300

    Add BankAccounts initial commit

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public interface IVisitor
+    {
+        void VisitPersonAcc(Person acc);
+
+        void VisitCompanyAcc(Company acc);
+    }
+}
---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccounts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
