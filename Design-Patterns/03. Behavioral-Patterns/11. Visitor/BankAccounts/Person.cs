d610e5a81026053cf6cf3bfb86e0614b35c8d499
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:49:39 +0300

    Add Person class

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class Person : IAccount
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitPersonAcc(this);
+        }
+    }
+}
