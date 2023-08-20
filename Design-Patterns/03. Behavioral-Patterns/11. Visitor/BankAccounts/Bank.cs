b4c57be8e2d2b26976470da3a7e86e8a54c7dad7
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:53:03 +0300

    Add Bank class

---
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class Bank
+    {
+        List<IAccount> accounts = new List<IAccount>();
+
+        public void Add(IAccount acc)
+        {
+            accounts.Add(acc);
+        }
+
+        public void Remove(IAccount acc)
+        {
+            accounts.Remove(acc);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (IAccount acc in accounts)
+            {
+                acc.Accept(visitor);
+            }
+        }
+    }
+}
