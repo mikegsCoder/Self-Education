61a429619a6cb2cca3489ea94a8a0e6855232da9
mikegsCoder <mikegsid@gmail.com>
2023-08-20 09:54:33 +0300

    Implemented Visitor example

--- Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Program.cs
+++ Design-Patterns/03. Behavioral-Patterns/11. Visitor/BankAccounts/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            var structure = new Bank();
+
+            structure.Add(new Person { Name = "Ivan Ivanov", Number = "82184931" });
+            structure.Add(new Company { Name = "Microsoft", RegNumber = "ewuir32141324", Number = "3424131445" });
+
+            structure.Accept(new HtmlVisitor());
+            structure.Accept(new XmlVisitor());
+
+            Console.Read();
         }
     }
 }
