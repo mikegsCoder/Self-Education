8b6482b4bbd10f511178f631d460f226a0eca9ae
mikegsCoder <mikegsid@gmail.com>
2023-08-24 16:02:54 +0300

    Add raiseEvents function

--- JS-Back-End/02. Node.js-Streams-and-Utilities/events/index.js
+++ JS-Back-End/02. Node.js-Streams-and-Utilities/events/index.js
@@ -2,3 +2,15 @@ const events = require('events');
 
 const publisher = new events.EventEmitter();
 
+function raiseEvents(){
+    console.log('before');
+    publisher.emit('ping', 'Hello world!');
+    console.log('after');
+    publisher.emit('ping', 'Hello again!');
+    publisher.emit('pong', 5, 8);
+}
+
+module.exports = {
+    raiseEvents,
+    publisher
+}
