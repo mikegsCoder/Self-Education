fb3043456799b1a4018168545f2d9d756bfaa522
mikegsCoder <mikegsid@gmail.com>
2023-08-26 11:19:56 +0300

    Import and use logger

--- JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
+++ JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const logger = require("./logger");
 
 const app = express();
+
+app.use(logger);
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
