179e94b8ad2793e66575b0d26639ab9d46d7bc9a
mikegsCoder <mikegsid@gmail.com>
2023-08-27 11:59:45 +0300

    Add app.get /

--- JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
+++ JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
@@ -5,4 +5,9 @@ const app = express();
 
 app.use(logger);
 
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Hello, Express!');
+});
+
 app.listen(3000, () => console.log("Server listening on port 3000"));
