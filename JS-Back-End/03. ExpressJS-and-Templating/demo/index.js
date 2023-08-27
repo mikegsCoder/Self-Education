7bf3d8cb95e32524af2a78be9f8716a8888b6186
mikegsCoder <mikegsid@gmail.com>
2023-08-27 12:04:44 +0300

    Add static

--- JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
+++ JS-Back-End/03. ExpressJS-and-Templating/demo/index.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const logger = require("./logger");
+const express = require('express');
+const logger = require('./logger');
 
 const app = express();
 
+app.use(express.static('static'));
 app.use(logger);
 
-app.get('/', (req, res) => {
-    res.status(200);
-    res.send('Hello, Express!');
-});
+// app.get('/', (req, res) => {
+//     res.status(200);
+//     res.send('Hello, Express!');
+// });
 
-app.get('/doc', (req, res) => {
-    res.download('./demo.pdf');
-});
+// app.get('/doc', (req, res) => {
+//     res.download('./demo.pdf');
+// });
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.listen(3000, () => console.log('Server listening on port 3000'));
