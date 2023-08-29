6c581ea0afba26d25cb0aa5e7b5a0e2dc067661c
mikegsCoder <mikegsid@gmail.com>
2023-08-29 16:14:23 +0300

    Starting server

--- JS-Back-End/03. ExpressJS-and-Templating/hbs/server.js
+++ JS-Back-End/03. ExpressJS-and-Templating/hbs/server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+// const bodyParser = require('body-parser').urlencoded({
+//     extended: true
+// });
+
+async function start() {
+    const port = 3000;
+    const app = express();
+
+    //app.use(bodyParser);
+    app.use(express.urlencoded({extended: true}));
+
+    app.engine(
+        ".hbs",
+        engine({
+            extname: ".hbs",
+        })
+    );
+
+    app.set("view engine", ".hbs");
+
+    app.listen(port, () => console.log(`Server listening on port ${port}.`));
+}
+
+start();
