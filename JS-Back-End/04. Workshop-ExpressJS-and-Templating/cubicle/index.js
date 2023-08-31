823a2e22914c657b8e69edbcee256640fcfa6a5c
mikegsCoder <mikegsid@gmail.com>
2023-08-31 16:31:30 +0300

    Starting server

--- JS-Back-End/04. Workshop-ExpressJS-and-Templating/cubicle/index.js
+++ JS-Back-End/04. Workshop-ExpressJS-and-Templating/cubicle/index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+
+start();
+
+async function start() {
+    const port = 3000;
+    const app = express();
+
+    app.engine('hbs', hbs({
+        extname: '.hbs'
+    }));
+    app.set('view engine', 'hbs');
+    
+    app.listen(port, () => console.log(`Server listening on port ${port}.`))
+}
