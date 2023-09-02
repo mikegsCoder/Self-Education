03715f9fd02b52f1c90c4e1b8fe2a6ee34a8ae85
mikegsCoder <mikegsid@gmail.com>
2023-09-02 12:16:57 +0300

    Use static and js

--- JS-Back-End/04. Workshop-ExpressJS-and-Templating/cubicle/index.js
+++ JS-Back-End/04. Workshop-ExpressJS-and-Templating/cubicle/index.js
@@ -13,7 +13,9 @@ async function start() {
         extname: '.hbs'
     }));
     app.set('view engine', 'hbs');
-
+    app.use('/static', express.static('static'));
+    app.use('/js', express.static('js'));
+    app.use(express.urlencoded({ extended: false }));
     app.use(await storage());
     
     app.listen(port, () => console.log(`Server listening on port ${port}.`))
