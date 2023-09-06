61ea593025e1e94772f809621f41d3aa2a838729
mikegsCoder <mikegsid@gmail.com>
2023-09-06 10:58:09 +0300

    Connect to database

--- JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
+++ JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 async function start() {
-    console.log('It works!');
+    const connectionStr = "mongodb://localhost:27017/testdb";
+
+    const client = await mongoose.connect(connectionStr, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    console.log("Database connected.");
 }
 
 start();
