714027cd574d98c2a512d382295e48c72b6c9cb6
mikegsCoder <mikegsid@gmail.com>
2023-09-06 11:05:07 +0300

    Add new cat into cats collection

--- JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
+++ JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
@@ -1,14 +1,28 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const Cat = require('./models/Cat');
 
 async function start() {
-    const connectionStr = "mongodb://localhost:27017/testdb";
+    const connectionStr = 'mongodb://localhost:27017/testdb';
 
     const client = await mongoose.connect(connectionStr, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 
-    console.log("Database connected.");
+    console.log('Database connected.');
+
+    // Cats collection:
+    try {
+        const someCat = new Cat({
+            name: 'fluffy',
+            color: 'White'
+        });
+        await someCat.save();
+    } catch (err) {
+        console.log('Caught the error.');
+        console.log('>>>', err.message);
+    }
 }
 
 start();
