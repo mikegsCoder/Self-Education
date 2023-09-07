8d79c5d0b76ed186cf4c4638c0ac2b0880043f4b
mikegsCoder <mikegsid@gmail.com>
2023-09-07 17:29:34 +0300

    Import and use Person model

--- JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
+++ JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Cat = require('./models/Cat');
+const Person = require("./models/Person");
 
 async function start() {
     const connectionStr = 'mongodb://localhost:27017/testdb';
@@ -13,12 +14,23 @@ async function start() {
     console.log('Database connected.');
 
     // Cats collection:
+    // try {
+    //     const someCat = new Cat({
+    //         name: 'fluffy',
+    //         color: 'White'
+    //     });
+    //     await someCat.save();
+    // } catch (err) {
+    //     console.log('Caught the error.');
+    //     console.log('>>>', err.message);
+    // }
+
+    // People collection:
     try {
-        const someCat = new Cat({
-            name: 'fluffy',
-            color: 'White'
+        const person = new Person({
+            age: -5
         });
-        await someCat.save();
+        await person.save();
     } catch (err) {
         console.log('Caught the error.');
         console.log('>>>', err.message);
