007624dd914bfdb9bb0317c77d737ec8f36fadbe
mikegsCoder <mikegsid@gmail.com>
2023-09-07 17:34:03 +0300

    Search person collection by different criteria

--- JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
+++ JS-Back-End/05. NoSQL-and-MongoDB/demo/index.js
@@ -26,15 +26,26 @@ async function start() {
     // }
 
     // People collection:
-    try {
-        const person = new Person({
-            age: -5
-        });
-        await person.save();
-    } catch (err) {
-        console.log('Caught the error.');
-        console.log('>>>', err.message);
-    }
+    // try {
+    //     const person = new Person({
+    //         age: -5
+    //     });
+    //     await person.save();
+    // } catch (err) {
+    //     console.log('Caught the error.');
+    //     console.log('>>>', err.message);
+    // }
+
+    // find:
+    // const data = await Person.find({ firstName: 'John' });
+    // const data = await Person.find({ _id: '63deaa5c5cb8e74b3d9eda9f' }); // returns array
+    // const data = await Person.findById('63deaa5c5cb8e74b3d9eda9f'); // returns object
+    // const data = await Person.find({ lastName: /s/i }); // search by regex
+    // const data = await Person.find({ age: { $gt: 30 } }); // age > 30
+    // const data = await Person.find({ age: { $gte: 30 } }); // age >= 30
+    // const data = await Person.find({ age: { $lt: 30 } }); // age < 30
+    const data = await Person.find({ age: { $lte: 30 } }); // age <= 30
+    console.log(data);
 }
 
 start();
