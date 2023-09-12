4a36e9b181803cb4e9a32141f1e9381da7d60da5
mikegsCoder <mikegsid@gmail.com>
2023-09-12 16:23:48 +0300

    Import and use storage

--- JS-Back-End/08. Workshop-MongoDB-and-Mongoose/cubicle/index.js
+++ JS-Back-End/08. Workshop-MongoDB-and-Mongoose/cubicle/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+
 const expressConfig = require('./config/express');
 const databaseConfig = require('./config/database');
 const routesConfig = require('./config/routes');
+
+const { init: storage } = require('./services/storage');
 
 start();
 
@@ -11,6 +14,7 @@ async function start() {
 
     expressConfig(app);
     await databaseConfig(app);
+    app.use(await storage());
     routesConfig(app);
     
     app.listen(port, () => console.log(`Server listening on port ${port}.`))
