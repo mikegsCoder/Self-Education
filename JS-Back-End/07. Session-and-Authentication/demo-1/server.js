84f47472a604ddf725589f5855cc2c69a8a9ca3b
mikegsCoder <mikegsid@gmail.com>
2023-09-16 12:10:07 +0300

    Add mySessionStorage

--- JS-Back-End/07. Session-and-Authentication/demo-1/server.js
+++ JS-Back-End/07. Session-and-Authentication/demo-1/server.js
@@ -2,4 +2,30 @@ const express = require('express');
 
 const app = express();
 
+const sessions = {};
+
+function mySessionStorage(req, res, next) {
+    let session = {};
+    if (req.headers.cookie) {
+        const id = req.headers.cookie.split('=')[1];
+        if (sessions[id] == undefined) {
+            console.log('>>> Invalid session cookie, generating new one.');
+            createSession();
+        } else {
+            session = sessions[id];
+            console.log('>>> Existing session ', session);
+        }
+    } else {
+        createSession();
+    }
+
+    req.session = session;
+
+    next();
+
+    function createSession() {
+        // some implementation here
+    }
+}
+
 app.listen(3000);
