dfbe3a3d12ba2e5cd4e0bacfd5d10e0d251492af
mikegsCoder <mikegsid@gmail.com>
2023-09-17 10:52:30 +0300

    Code cleanup

--- JS-Back-End/07. Session-and-Authentication/demo-2/server.js
+++ JS-Back-End/07. Session-and-Authentication/demo-2/server.js
@@ -1,38 +1,5 @@
 const express = require('express');
 const expressSession = require('express-session');
-// const cookieParser = require('cookie-parser');
-
-/*
-const sessions = {};
-
-function mySessionStorage(req, res, next) {
-    let session = {};
-    if (req.cookies.sessionId && sessions[req.cookies.sessionId] != undefined) {
-        const id = req.cookies.sessionId;
-        session = sessions[id];
-        console.log('>>> Existing session ', session);
-    } else {
-        createSession();
-    }
-
-    req.session = session;
-    
-    next();
-    
-    function createSession() {
-        const id = ('00000000' + (Math.random() * 99999999 | 0).toString(16)).slice(-8);
-        sessions[id] = session;
-
-        res.cookie('sessionId', id);
-        console.log('New user generated session with Id: ', id);
-        
-        session.visited = 0;
-    }
-}
-
-app.use(cookieParser());
-app.use(mySessionStorage);
-*/
 
 const app = express();
 
@@ -44,8 +11,6 @@ app.use(expressSession({
 }));
 
 app.get('/', (req, res) => {
-    // req.session.visited++;
-    // res.send(`<h1>Hello</h1><p>Your session has data ${JSON.stringify(req.session)} </p>`);
     res.send('Hello');
 });
 
