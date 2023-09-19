1ef6fb8eb1141f9209a5fee2d563b187ad8ad634
mikegsCoder <mikegsid@gmail.com>
2023-09-19 16:38:36 +0300

    Add demo-4 initial commit

---
+++ JS-Back-End/07. Session-and-Authentication/demo-4/server.js
@@ -0,0 +1,17 @@
+const { request } = require('express');
+const express = require('express');
+const bodyParser = require('express').urlencoded;
+const expressSession = require('express-session');
+// const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.use(bodyParser({ extended: false }));
+app.use(expressSession({
+    secret: 'my random secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.listen(3000);
