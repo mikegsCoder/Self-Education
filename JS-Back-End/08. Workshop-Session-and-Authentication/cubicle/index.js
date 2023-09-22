79bf0a99f2b49d87ea82be1341bb28d6bd0826a9
mikegsCoder <mikegsid@gmail.com>
2023-09-22 11:12:06 +0300

    Add cubicle last version

---
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routesConfig = require('./config/routes');
+
+const { init: storage } = require('./services/storage');
+
+start();
+
+async function start() {
+    const port = 3000;
+    const app = express();
+
+    expressConfig(app);
+    await databaseConfig(app);
+    app.use(await storage());
+    routesConfig(app);
+    
+    app.listen(port, () => console.log(`Server listening on port ${port}.`))
+}
