fed3563e6a3700ff1b1c4273d9dbfd24a5fe981c
mikegsCoder <mikegsid@gmail.com>
2023-09-22 11:29:05 +0300

    Update author

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/models/Comment.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/models/Comment.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
-    author: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
     content: { type: String, required: true, maxLength: 250 }
 });
 
