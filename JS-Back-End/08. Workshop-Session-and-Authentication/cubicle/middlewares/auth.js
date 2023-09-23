b164d02e1af55a6e8ccb58b05640a7f165cc3bcd
mikegsCoder <mikegsid@gmail.com>
2023-09-23 14:01:34 +0300

    Add readToken

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/middlewares/auth.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/middlewares/auth.js
@@ -10,5 +10,26 @@ module.exports = () => (req, res, next) => {
         login,
         logout
     };
+
+    if (readToken(req)) {
+        next();
+    }
+
+    function readToken(req) {
+        const token = req.cookies[COOKIE_NAME];
+        if (token) {
+            try {
+                const userData = jwt.verify(token, TOKEN_SECRET);
+                req.user = userData;
+                res.locals.user = userData;
+                console.log('Known user', userData.username);
+            } catch (err) {
+                res.clearCookie(COOKIE_NAME);
+                res.redirect('/auth/login');
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
