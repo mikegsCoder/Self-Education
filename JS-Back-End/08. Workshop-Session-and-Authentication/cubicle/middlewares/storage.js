6c37445ba66721f56e4cf73ba586860ba785e28e
mikegsCoder <mikegsid@gmail.com>
2023-09-27 14:23:23 +0300

    Add storage middleware

---
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/middlewares/storage.js
@@ -0,0 +1,12 @@
+const productService = require('../services/product');
+const accessoryService = require('../services/accessory');
+
+async function init() {
+    return (req, res, next) => {
+        const storage = Object.assign({}, productService, accessoryService);
+        req.storage = storage;
+        next();
+    };
+}
+
+module.exports = init;
