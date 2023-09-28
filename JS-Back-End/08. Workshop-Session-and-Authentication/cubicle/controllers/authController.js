3b245fbfa80c15ce8b05f5e57e610276cbc34991
mikegsCoder <mikegsid@gmail.com>
2023-09-28 17:42:28 +0300

    Add register post

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/authController.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/authController.js
@@ -7,4 +7,18 @@ router.get('/register', isGuest(), (req, res) => {
     res.render('register', { title: 'Register' });
 });
 
+router.post('/register', isGuest(), async (req, res) => {
+    try {
+        await req.auth.register(req.body);
+        res.redirect('/products');
+    } catch (err) {
+        const ctx = { 
+            title: 'Register', 
+            error: err.message, 
+            data: { username: req.body.username } 
+        };
+        res.render('register', ctx);
+    }
+});
+
 module.exports = router;
