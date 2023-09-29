1c24da6f348ed249c40434ebcff0722706fc123f
mikegsCoder <mikegsid@gmail.com>
2023-09-29 15:11:45 +0300

    Import and use authController

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
@@ -1,6 +1,8 @@
 const homeController = require('../controllers/homeController');
+const authController = require('../controllers/authController');
 
 module.exports = (app) => {
+    app.use('/auth', authController);
    
     app.use('/', homeController);
 }
