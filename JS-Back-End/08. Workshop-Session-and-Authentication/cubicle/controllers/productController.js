1755ce8bd963c882a180affee1df7823a0d5c415
mikegsCoder <mikegsid@gmail.com>
2023-09-30 10:33:34 +0300

    Add create post

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/productController.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/productController.js
@@ -23,4 +23,24 @@ router.get('/create', isAuth(), (req, res) => {
     res.render('create', {title: 'Create Cube'});
 });
 
+router.post('/create', isAuth(), async (req, res) => {
+    const cube = {
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        difficulty: Number(req.body.difficulty),
+        author: req.user._id
+    };
+
+    try {
+        await req.storage.create(cube);
+    } catch (err) {
+        if (err.name == 'ValidationError') {
+            return res.render('create', { title: 'Create Cube', error: 'Invalid data provided.' });
+        }
+    }
+
+    res.redirect('/');
+});
+
 module.exports = router;
