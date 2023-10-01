7fdb63dfe9f214d8ad965a8605c5814ea34cc22c
mikegsCoder <mikegsid@gmail.com>
2023-10-01 11:37:55 +0300

    Import and use productController

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
@@ -1,7 +1,9 @@
 const homeController = require('../controllers/homeController');
+const productController = require('../controllers/productController');
 const authController = require('../controllers/authController');
 
 module.exports = (app) => {
+    app.use('/products', productController);
     app.use('/auth', authController);
    
     app.use('/', homeController);
