95a9c21c1f088bf7272576d9fe93022c45cd3710
mikegsCoder <mikegsid@gmail.com>
2023-10-02 17:10:38 +0300

    Add create post

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/accessoryController.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/controllers/accessoryController.js
@@ -6,4 +6,16 @@ router.get('/create', (req, res) => {
     res.render('createAccessory', { title: 'Create New Accessory' });
 });
 
+router.post('/create', async (req, res) => {
+    const accessory = new Accessory({
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl
+    });
+
+    await req.storage.createAccessory(accessory);
+
+    res.redirect('/');
+});
+
 module.exports = router;
