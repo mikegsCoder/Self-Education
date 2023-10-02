8236b729291fa15aca33cc74b4538f2822044f21
mikegsCoder <mikegsid@gmail.com>
2023-10-02 17:12:24 +0300

    Import and use accessoryController

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
@@ -1,9 +1,11 @@
 const homeController = require('../controllers/homeController');
 const productController = require('../controllers/productController');
+const accessoryController = require('../controllers/accessoryController');
 const authController = require('../controllers/authController');
 
 module.exports = (app) => {
     app.use('/products', productController);
+    app.use('/accessory', accessoryController);
     app.use('/auth', authController);
    
     app.use('/', homeController);
