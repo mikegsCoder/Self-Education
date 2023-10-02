29f919174947086603be830d7d731abc96e59ec4
mikegsCoder <mikegsid@gmail.com>
2023-10-02 17:15:00 +0300

    Import and use comments

--- JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
+++ JS-Back-End/08. Workshop-Session-and-Authentication/cubicle/config/routes.js
@@ -1,12 +1,18 @@
+const { post: commentPost } = require('../controllers/comments');
+
 const homeController = require('../controllers/homeController');
 const productController = require('../controllers/productController');
 const accessoryController = require('../controllers/accessoryController');
 const authController = require('../controllers/authController');
 
+const { isAuth } = require('../middlewares/guards');
+
 module.exports = (app) => {
     app.use('/products', productController);
     app.use('/accessory', accessoryController);
     app.use('/auth', authController);
-   
+
+    app.post('/comments/:cubeId/create', isAuth(), commentPost);
+
     app.use('/', homeController);
 }
