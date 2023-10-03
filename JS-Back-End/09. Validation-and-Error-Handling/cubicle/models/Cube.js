f227c962d4e31a5c1d003bb30e6c820814734ba5
mikegsCoder <mikegsid@gmail.com>
2023-10-03 16:20:09 +0300

    Update cube model

--- JS-Back-End/09. Validation-and-Error-Handling/cubicle/models/Cube.js
+++ JS-Back-End/09. Validation-and-Error-Handling/cubicle/models/Cube.js
@@ -1,9 +1,27 @@
 const { Schema, model } = require('mongoose');
 
+const NAME_PATTERN = /^[a-zA-Z0-9 ]+$/;
+const IMAGE_PATTERN = /^https?:\/\//;
+
 const schema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true, maxLength: 500 },
-    imageUrl: { type: String, required: true, match: /^https?:\/\// },
+    name: { 
+        type: String, 
+        required: [true, 'Cube name is required.'], 
+        minLength: [5, 'Cube name must be at least 5 characters long.'], 
+        match: [NAME_PATTERN, 'Cube name may contain only latin alphanumeric characters.'] 
+    },
+    description: { 
+        type: String, 
+        required: [true, 'Cube description is required.'], 
+        minLength: [20, 'Cube description must be at least 20 characters long.'], 
+        maxLength: [500, 'Cube description must be at most 500 characters long.'], 
+        match: [NAME_PATTERN, 'Cube description may contain only latin alphanumeric characters.'] 
+    },
+    imageUrl: { 
+        type: String, 
+        required: [true, 'Image URL is required.'], 
+        match: [IMAGE_PATTERN, 'Invalid image URL.'] 
+    },
     difficulty: { type: Number, min: 1, max: 6 },
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
     accessories: [{ type: Schema.Types.ObjectId, ref: 'Accessory' }],
