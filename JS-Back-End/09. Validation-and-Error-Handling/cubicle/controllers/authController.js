3d9818343ae6292d61ef517f1e434d90f12cc84b
mikegsCoder <mikegsid@gmail.com>
2023-10-03 16:10:28 +0300

    Add cubicle last version

---
+++ JS-Back-End/09. Validation-and-Error-Handling/cubicle/controllers/authController.js
@@ -0,0 +1,47 @@
+const { Router } = require('express');
+const { isGuest, isAuth } = require('../middlewares/guards');
+
+const router = Router();
+
+router.get('/register', isGuest(), (req, res) => {
+    res.render('register', { title: 'Register' });
+});
+
+router.post('/register', isGuest(), async (req, res) => {
+    try {
+        await req.auth.register(req.body);
+        res.redirect('/products');
+    } catch (err) {
+        const ctx = { 
+            title: 'Register', 
+            error: err.message, 
+            data: { username: req.body.username } 
+        };
+        res.render('register', ctx);
+    }
+});
+
+router.get('/login', isGuest(), (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/login', isGuest(), async (req, res) => {
+    try {
+        await req.auth.login(req.body);
+        res.redirect('/products');
+    } catch (err) {
+        const ctx = { 
+            title: 'Login', 
+            error: err.message, 
+            data: { username: req.body.username } 
+        };
+        res.render('login', ctx);
+    }
+});
+
+router.get('/logout', isAuth(), (req, res) => {
+    req.auth.logout();
+    res.redirect('/products');
+});
+
+module.exports = router;
---
+++ JS-Back-End/09. Validation-and-Error-Handling/cubicle/controllers/homeController.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => res.redirect('/products'));
+
+router.get('/about', (req, res) => {
+    res.render('about', {title: 'About Page'});
+});
+
+router.all('*', (req, res) => {
+    res.render('404', {title: 'Page Not Found'});
+});
+
+module.exports = router;
