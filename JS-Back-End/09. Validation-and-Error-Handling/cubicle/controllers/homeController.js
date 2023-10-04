9b458b09651fb50119e95c0decdc39c8ea11e046
mikegsCoder <mikegsid@gmail.com>
2023-10-04 17:43:24 +0300

    Import and use asyncWrapper

--- JS-Back-End/09. Validation-and-Error-Handling/cubicle/controllers/homeController.js
+++ JS-Back-End/09. Validation-and-Error-Handling/cubicle/controllers/homeController.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
+const asyncWrapper = require('../util/asyncWrapper');
 
 const router = Router();
 
 router.get('/', (req, res) => res.redirect('/products'));
 
-router.get('/about', (req, res) => {
+router.get('/about', asyncWrapper( async (req, res) => {
+    // throw new Error('Test Error');
     res.render('about', {title: 'About Page'});
-});
+}));
 
 router.all('*', (req, res) => {
     res.render('404', {title: 'Page Not Found'});
