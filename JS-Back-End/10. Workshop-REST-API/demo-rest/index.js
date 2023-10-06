5f7249cf2251970c88df80082b9fc2539acbf583
mikegsCoder <mikegsid@gmail.com>
2023-10-06 17:39:04 +0300

    Set headers

--- JS-Back-End/10. Workshop-REST-API/demo-rest/index.js
+++ JS-Back-End/10. Workshop-REST-API/demo-rest/index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
 app.listen(5000, () => console.log('Server listening on port 5000.'));
