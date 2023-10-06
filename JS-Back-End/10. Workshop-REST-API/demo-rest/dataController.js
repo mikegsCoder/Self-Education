9f78741b76ec796f500be407cc0ba09c95a8dd33
mikegsCoder <mikegsid@gmail.com>
2023-10-06 17:41:47 +0300

    Add dataController

---
+++ JS-Back-End/10. Workshop-REST-API/demo-rest/dataController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const data = {
+    'a1': {
+        name: 'First'
+    },
+    'a2': {
+        name: 'Second'
+    },
+    'a3': {
+        name: 'Third'
+    }
+};
+
+module.exports = router;
