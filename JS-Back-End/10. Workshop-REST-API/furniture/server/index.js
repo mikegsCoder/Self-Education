7fbdb9b3fe72a29f3c550d1a8bbeb4d07dd797a1
mikegsCoder <mikegsid@gmail.com>
2023-10-08 10:47:20 +0300

    Add mongoose connection

--- JS-Back-End/10. Workshop-REST-API/furniture/server/index.js
+++ JS-Back-End/10. Workshop-REST-API/furniture/server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 start();
 
 async function start() {
+    await new Promise((resolve, reject) => {
+        mongoose.connect('mongodb://0.0.0.0:27017/furniture-rest', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const db = mongoose.connection;
+        db.once('open', () => {
+            console.log('Database connected.');
+            resolve();
+        });
+        db.on('error', (err) => {
+            reject(err);
+        });
+    });
+
     const app = express();
 
     app.get('/', (req, res) => res.send('It works!'));
