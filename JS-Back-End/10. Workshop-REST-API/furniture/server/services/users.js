db187d4b26b93645237b2b51ecf74ccdb893a111
mikegsCoder <mikegsid@gmail.com>
2023-10-10 16:31:29 +0300

    Add users service

---
+++ JS-Back-End/10. Workshop-REST-API/furniture/server/services/users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+const { SECRET } = require('../config');
+
+async function register(email, password) {
+    // check if user exists
+    const existing = await User.findOne({ email });
+
+    if (existing) {
+        const err = new Error('User with this email already exists in the database.');
+        err.status = 409;
+        throw err;
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // store user
+    const user = new User({
+        email,
+        hashedPassword
+    });
+
+    await user.save();
+
+    return {
+        _id: user._id,
+        email: user.email,
+        accessToken: createToken(user)
+    };
+}
