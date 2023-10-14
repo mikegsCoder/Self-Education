e0aae1686987c77aba3a43ae20f48b8c9e75c7f4
mikegsCoder <mikegsid@gmail.com>
2023-10-14 10:39:54 +0300

    Add update

--- JS-Back-End/10. Workshop-REST-API/furniture/server/controllers/furnitureController.js
+++ JS-Back-End/10. Workshop-REST-API/furniture/server/controllers/furnitureController.js
@@ -39,4 +39,25 @@ router.get('/:id', preload(), async (req, res) => {
     res.json(item);
 });
 
+router.put('/:id', isAuth(), preload(), isOwner(), async (req, res) => {
+    const updated = {
+        make: req.body.make,
+        model: req.body.model,
+        year: Number(req.body.year),
+        description: req.body.description,
+        price: Number(req.body.price),
+        img: req.body.img,
+        material: req.body.material
+    };
+
+    try {
+        const result = await update(req.data, updated);
+        
+        res.json(result);
+    } catch (err) {
+        const message = parseError(err); 
+        res.status(err.status || 400).json({ message });       
+    }
+});
+
 module.exports = router;
