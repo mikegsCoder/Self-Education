a05d8a6c94746e62e12dc845676449698dd6e6e0
mikegsCoder <mikegsid@gmail.com>
2023-10-15 08:20:02 +0300

    Add booking-uni initial commit

---
+++ JS-Back-End/11. Exam-Preparation/booking-uni/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: 3000,
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/booking-uni',
+    TOKEN_SECRET: 'this is my secret',
+    COOKIE_NAME: 'SESSION_TOKEN'
+};
---
+++ JS-Back-End/11. Exam-Preparation/booking-uni/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { PORT } = require('./config');
+const databaseConfig = require('./config/database');
+const expressConfig = require('./config/express');
+const routesConfig = require('./config/routes');
+
+start();
+
+async function start(){
+    const app = express();
+    
+    await databaseConfig(app);
+    expressConfig(app);
+    routesConfig(app);
+    
+    app.listen(PORT, () => 
+        console.log(`Application startted at http://localhost:${PORT}`));
+}
+
+
