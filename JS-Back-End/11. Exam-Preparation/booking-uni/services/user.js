baa5fd5ef046ac2ef651136e20ed8f5fde361a31
mikegsCoder <mikegsid@gmail.com>
2023-10-15 08:26:25 +0300

    Add getUserByUsername

--- JS-Back-End/11. Exam-Preparation/booking-uni/services/user.js
+++ JS-Back-End/11. Exam-Preparation/booking-uni/services/user.js
@@ -12,6 +12,14 @@ async function createUser(username, email, hashedPassword) {
     return user;
 }
 
+async function getUserByUsername(username) {
+    const pattern = new RegExp(`^${username}$`, 'i');
+    const user = await User.findOne({ username: { $regex: pattern } });
+
+    return user;
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUserByUsername
 };
