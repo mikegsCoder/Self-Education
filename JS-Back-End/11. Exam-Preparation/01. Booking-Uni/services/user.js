a025df7617ec588c538c3b25f30df79890b19772
mikegsCoder <mikegsid@gmail.com>
2023-10-16 17:40:15 +0300

    Add getUserByEmail

--- JS-Back-End/11. Exam-Preparation/01. Booking-Uni/services/user.js
+++ JS-Back-End/11. Exam-Preparation/01. Booking-Uni/services/user.js
@@ -19,7 +19,15 @@ async function getUserByUsername(username) {
     return user;
 }
 
+async function getUserByEmail(email) {
+    const pattern = new RegExp(`^${email}$`, 'i');
+    const user = await User.findOne({ email: { $regex: pattern } });
+
+    return user;
+}
+
 module.exports = {
     createUser,
-    getUserByUsername
+    getUserByUsername,
+    getUserByEmail
 };
