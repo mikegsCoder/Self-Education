79062940fa0d58ede01193297621bf67961381e9
mikegsCoder <mikegsid@gmail.com>
2023-10-25 17:12:17 +0300

    Import and use userController

--- JS-Back-End/11. Exam-Preparation/01. Booking-Uni/config/routes.js
+++ JS-Back-End/11. Exam-Preparation/01. Booking-Uni/config/routes.js
@@ -1,9 +1,11 @@
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
 const hotelController = require('../controllers/hotelController');
+const userController = require('../controllers/userController');
 
 module.exports = (app) => {
     app.use('/', homeController);
     app.use('/auth', authController);
     app.use('/hotels', hotelController);
+    app.use('/user', userController);
 };
