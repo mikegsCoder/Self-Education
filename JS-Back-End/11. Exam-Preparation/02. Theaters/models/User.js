492909f122877c33fd99a7e4d7b4d8966a3f7c97
mikegsCoder <mikegsid@gmail.com>
2023-10-26 05:34:36 +0300

    Add user model

---
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/models/User.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+    username: { type: String, required: true },
+    hashedPassword: { type: String, required: true },
+    likedPlays: [{ type: Schema.Types.ObjectId, ref: 'Play' }]
+});
+
+module.exports = model('User', schema);
