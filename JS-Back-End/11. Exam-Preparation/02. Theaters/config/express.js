e5db68e3e1b3c4e4e8aa0e49c7ab958c6c4f7f37
mikegsCoder <mikegsid@gmail.com>
2023-10-27 17:41:18 +0300

    Import and use authMiddleware

--- JS-Back-End/11. Exam-Preparation/02. Theaters/config/express.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/config/express.js
@@ -2,12 +2,16 @@ const hbs = require("express-handlebars");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+const authMiddleware = require("../middlewares/auth");
+
 module.exports = (app) => {
     app.engine("hbs", hbs.engine({ extname: ".hbs" }));
     app.set("view engine", "hbs");
 
     app.use("/static", express.static("static"));
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(authMiddleware());
 
     app.use((req, res, next) => {
         if (!req.url.includes("favicon")) {
