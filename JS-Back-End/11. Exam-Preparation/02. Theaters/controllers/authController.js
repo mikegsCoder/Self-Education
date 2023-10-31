a321ac63b6a628df9bdd6f40ac4fad06795afc5b
mikegsCoder <mikegsid@gmail.com>
2023-10-31 07:12:56 +0200

    Add register post

--- JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/authController.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/authController.js
@@ -6,4 +6,42 @@ router.get('/register', isGuest(), (req, res) => {
     res.render('register');
 });
 
+router.post('/register', 
+    isGuest(),
+    body('username')
+        .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long.').bail()
+        .isAlphanumeric().withMessage('Username may contain only English letters and digits.'),
+    body('password')
+        .isLength({ min: 3 }).withMessage('Password must be at least 3 characters long.').bail()
+        .isAlphanumeric().withMessage('Password may contain only English letters and digits.'),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error ('Passwords don\'t match.');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        
+        try {
+            if (errors.length > 0) {
+                throw new Error(Object.values(errors).map(e => e.msg).join('\n'));
+            }
+
+            await req.auth.register(req.body.username.trim(), req.body.password.trim());
+
+            res.redirect('/');
+        } catch (err) {
+            console.log(err.message)
+            const ctx = {
+                errors: err.message.split('\n'),
+                userData: {
+                    username: req.body.username
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
 module.exports = router;
