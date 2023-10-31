81e39d56204c91aa65968e9f83b8cf0d604c6f5b
mikegsCoder <mikegsid@gmail.com>
2023-10-31 07:07:43 +0200

    Add authController

---
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/authController.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+const { isGuest } = require('../middlewares/guards');
+
+router.get('/register', isGuest(), (req, res) => {
+    res.render('register');
+});
+
+module.exports = router;
