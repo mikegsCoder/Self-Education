287283d6e279278344f8ed3adf1bb083f035bbc5
mikegsCoder <mikegsid@gmail.com>
2023-11-01 07:00:19 +0200

    Import and use playController

--- JS-Back-End/11. Exam-Preparation/02. Theaters/config/routes.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/config/routes.js
@@ -1,7 +1,9 @@
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
+const playController = require('../controllers/playController');
 
 module.exports = (app) => {
     app.use('/', homeController);
     app.use('/auth', authController);
+    app.use('/play', playController);
 };
