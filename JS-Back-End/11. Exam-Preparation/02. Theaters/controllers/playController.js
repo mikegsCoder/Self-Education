fe24205c1bd8cd4196cf1fa4283c33457cb22625
mikegsCoder <mikegsid@gmail.com>
2023-11-02 06:37:35 +0200

    Add create post

--- JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
@@ -6,4 +6,31 @@ router.get('/create', isUser(), (req, res) => {
     res.render('play/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const playData = {
+            title: req.body.title.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            public: Boolean(req.body.public),
+            author: req.user._id
+        };
+
+        await req.storage.createPlay(playData);
+
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            playData: {
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                public: Boolean(req.body.public)
+            }
+        };
+        res.render('play/create', ctx);
+    }
+});
+
 module.exports = router;
