a43a234849c9c0c553c49d3b0b78c39e33430c18
mikegsCoder <mikegsid@gmail.com>
2023-11-03 17:33:01 +0200

    Add delete get

--- JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
@@ -89,4 +89,20 @@ router.post('/edit/:id', isUser(), async (req, res) => {
     }
 });
 
+router.get('/delete/:id', isUser(), async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+
+        if (play.author != req.user._id) {
+            throw new Error('Cannot delete play you haven\'t created.');
+        }
+
+        await req.storage.deletePlay(req.params.id);
+        res.redirect('/');
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/play/details/' + req.params.id);
+    }
+});
+
 module.exports = router;
