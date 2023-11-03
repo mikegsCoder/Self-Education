09126722ddedc2018f22892ca357b83a03da8ff6
mikegsCoder <mikegsid@gmail.com>
2023-11-03 17:36:01 +0200

    Add parse error

--- JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const { isUser, isGuest } = require('../middlewares/guards');
+const { parseError } = require('../util/parsers');
 
 router.get('/create', isUser(), (req, res) => {
     res.render('play/create');
