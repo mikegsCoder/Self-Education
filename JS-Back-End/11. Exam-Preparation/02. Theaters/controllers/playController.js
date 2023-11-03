9c5e7b1c5ceffb311a841c318b7f37ae9d547606
mikegsCoder <mikegsid@gmail.com>
2023-11-03 17:31:35 +0200

    Add edit post

--- JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
+++ JS-Back-End/11. Exam-Preparation/02. Theaters/controllers/playController.js
@@ -62,4 +62,31 @@ router.get('/edit/:id', isUser(), async (req, res) => {
     }
 });
 
+router.post('/edit/:id', isUser(), async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+        
+        if (play.author != req.user._id) {
+            throw new Error('Cannot edit play you haven\'t created.');
+        }
+
+        await req.storage.editPlay(req.params.id, req.body);
+
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            play: {
+                _id: req.params.id,
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                public: Boolean(req.body.public)
+            }
+        };
+
+        res.render('play/edit', ctx);
+    }
+});
+
 module.exports = router;
