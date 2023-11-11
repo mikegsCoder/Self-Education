cbd4efba6d208e387cefcdbe5ce8cbba184f67bf
mikegsCoder <mikegsid@gmail.com>
2023-11-11 08:27:19 +0200

    Import and use courseController

--- JS-Back-End/12. Exams/01. Tutorials/config/routes.js
+++ JS-Back-End/12. Exams/01. Tutorials/config/routes.js
@@ -1,7 +1,9 @@
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
+const courseController = require('../controllers/courseController');
 
 module.exports = (app) => {
     app.use('/', homeController);
     app.use('/auth', authController);
+    app.use('/course', courseController);
 };
