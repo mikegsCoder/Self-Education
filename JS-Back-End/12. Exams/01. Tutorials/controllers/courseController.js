de827db0972a41469a2a0ba7e2aaff6a34bc10d0
mikegsCoder <mikegsid@gmail.com>
2023-11-12 13:27:05 +0200

    Add create post

--- JS-Back-End/12. Exams/01. Tutorials/controllers/courseController.js
+++ JS-Back-End/12. Exams/01. Tutorials/controllers/courseController.js
@@ -7,4 +7,31 @@ router.get('/create', isUser(), (req, res) => {
     res.render('course/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const courseData = {
+            title: req.body.title.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            duration: req.body.duration.trim(),
+            author: req.user._id
+        };
+        
+        await req.storage.createCourse(courseData);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            courseData: {
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                duration: req.body.duration
+            }
+        };
+
+        res.render('course/create', ctx);
+    }
+});
+
 module.exports = router;
