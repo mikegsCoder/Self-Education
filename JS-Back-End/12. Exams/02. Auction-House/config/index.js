89386dec9ccda6214434a68fa974b27fb6214419
mikegsCoder <mikegsid@gmail.com>
2023-11-14 06:51:46 +0200

    Add auction house initial commit

---
+++ JS-Back-End/12. Exams/02. Auction-House/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DB_CONNECTION_STRING: 'mongodb://0.0.0.0:27017/auctions',
+    ITEM: 'Auction',
+    PORT: 3000,
+    TOKEN_SECRET: 'this is my secret',
+    COOKIE_NAME: 'SESSION_TOKEN'
+};
