4bd03ea9d8bcea88122d8e7c532e8dfd8a290cf0
mikegsCoder <mikegsid@gmail.com>
2023-11-15 06:38:45 +0200

    Add createItem

--- JS-Back-End/12. Exams/02. Auction-House/services/item.js
+++ JS-Back-End/12. Exams/02. Auction-House/services/item.js
@@ -12,7 +12,22 @@ async function getItemById(id) {
     return Item.findById(id).populate('author').populate('bidder').lean();
 }
 
+async function createItem(itemData) {
+    const pattern = new RegExp(`^${itemData.title}$`, 'i');
+    const existing = await Item.findOne({ title: {$regex: pattern }});
+
+    if (existing) {
+        throw new Error(`${ITEM} with this name already exists.`);
+    }
+
+    const item = new Item(itemData);
+    await item.save();
+
+    return item;
+}
+
 module.exports = {
     getAllItems,
-    getItemById
+    getItemById,
+    createItem
 };
