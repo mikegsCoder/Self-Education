0ac16f3899016fb7d394b2be35a515e6067f20f6
mikegsCoder <mikegsid@gmail.com>
2023-11-16 07:05:18 +0200

    Add auth middleware

---
+++ JS-Back-End/12. Exams/02. Auction-House/middlewares/auth.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { TOKEN_SECRET, COOKIE_NAME } = require('../config');
+const userService = require('../services/user');
+
+module.exports = () => (req, res, next) => {
+    if (parseToken(req, res)) {
+        req.auth = {
+            async register(email, password, firstName, lastName) {
+                const token = await register(email, password, firstName, lastName);
+                res.cookie(COOKIE_NAME, token);
+            },
+            async login(email, password) {
+                const token = await login(email, password);
+                res.cookie(COOKIE_NAME, token);
+            },
+            logout() {
+                res.clearCookie(COOKIE_NAME);
+            },
+        };
+
+        next();
+    }
+};
+
+async function register(email, password, firstName, lastName) {
+}
+
+async function login(email, password) {
+}
+
+function generateToken(userData) {
+}
+
+function parseToken(req, res) {
+}
