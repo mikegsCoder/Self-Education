66e9c7ecb6857f9bdc7e5d7eb7e2bb7dd8574a14
mikegsCoder <mikegsid@gmail.com>
2023-11-19 09:02:20 +0200

    Add browse get

--- JS-Back-End/12. Exams/02. Auction-House/controllers/homeController.js
+++ JS-Back-End/12. Exams/02. Auction-House/controllers/homeController.js
@@ -4,4 +4,16 @@ router.get('/', async (req, res) => {
     res.render('home');
 });
 
+router.get('/browse', async (req, res) => {
+    const items = await req.storage.getAllItems();
+
+    const ctx = {
+        items,
+        title: 'Auctions',
+        closed: false
+    };
+
+    res.render('browse', ctx);
+});
+
 module.exports = router;
