ab9399012f18accab68559a75ec92239dc17ddff
mikegsCoder <mikegsid@gmail.com>
2023-11-21 06:21:34 +0200

    Add create post

--- JS-Back-End/12. Exams/02. Auction-House/controllers/itemController.js
+++ JS-Back-End/12. Exams/02. Auction-House/controllers/itemController.js
@@ -1,10 +1,40 @@
 const router = require('express').Router();
 
 const { isUser } = require('../middlewares/guards');
+const { parseError } = require('../util/parsers');
 const { ITEM } = require('../config/index');
 
 router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
+});
+
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            category: req.body.category,
+            price: req.body.price,
+            author: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                category: req.body.category,
+                price: req.body.price,
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
 });
 
 module.exports = router;
