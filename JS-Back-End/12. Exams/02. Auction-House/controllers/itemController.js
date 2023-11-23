41dc5daf7b3faac241d5cde152dfb868f834d1b4
mikegsCoder <mikegsid@gmail.com>
2023-11-23 07:02:36 +0200

    Add edit post

--- JS-Back-End/12. Exams/02. Auction-House/controllers/itemController.js
+++ JS-Back-End/12. Exams/02. Auction-House/controllers/itemController.js
@@ -70,4 +70,32 @@ router.get('/edit/:id', isUser(), async (req, res) => {
     }
 });
 
+router.post('/edit/:id', isUser(), async (req, res) => {
+    try {
+        const item = await req.storage.getItemById(req.params.id);
+        
+        if (item.author._id != req.user._id) {
+            throw new Error(`Cannot edit ${ITEM.toLowerCase()} you haven\'t created.`);
+        }
+
+        await req.storage.editItem(req.params.id, req.body);
+
+        res.redirect('/item/details/' + req.params.id); 
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            item: {
+                _id: req.params.id,
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                category: req.body.category,
+                price: req.body.price
+            }
+        };
+
+        res.render('item/edit', ctx);
+    }
+});
+
 module.exports = router;
