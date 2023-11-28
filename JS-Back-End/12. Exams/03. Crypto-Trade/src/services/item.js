556318954b47064e2f3ee4df36cc3de6a933133c
mikegsCoder <mikegsid@gmail.com>
2023-11-28 07:04:35 +0200

    Add editItem

--- JS-Back-End/12. Exams/03. Crypto-Trade/src/services/item.js
+++ JS-Back-End/12. Exams/03. Crypto-Trade/src/services/item.js
@@ -18,8 +18,21 @@ async function createItem(itemData) {
     return item;
 }
 
+async function editItem(id, itemData) {
+    const item = await Item.findById(id);
+
+    item.title = itemData.title.trim();
+    item.description = itemData.description.trim();
+    item.imageUrl = itemData.imageUrl.trim();
+    item.paymentMethod = itemData.paymentMethod;
+    item.price = itemData.price;
+
+    return item.save();
+}
+
 module.exports = {
     getAllItems,
     getItemById,
-    createItem
+    createItem,
+    editItem
 };
