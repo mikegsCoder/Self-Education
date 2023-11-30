4646120e19e17aeca13c0e651d2524d70a97d923
mikegsCoder <mikegsid@gmail.com>
2023-11-30 07:10:28 +0200

    Add register post

--- JS-Back-End/12. Exams/03. Crypto-Trade/src/controllers/authController.js
+++ JS-Back-End/12. Exams/03. Crypto-Trade/src/controllers/authController.js
@@ -6,4 +6,46 @@ router.get('/register', isGuest(), (req, res) => {
     res.render('register');
 });
 
+router.post('/register', isGuest(),
+    body('username')
+        .isLength({ min: 5 }).withMessage('Username must be at least 5 characters long.'),
+    body('email')
+        .isEmail().withMessage('Plese enter a valid email.').bail()
+        .isLength({ min: 10 }).withMessage('Email must be at least 10 characters long.'),
+    body('password')
+        .isLength({ min: 4 }).withMessage('Password must be at least 4 characters long.'),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error ('Passwords don\'t match.');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        
+        try {
+            if (errors.length > 0) {
+                throw new Error(Object.values(errors).map(e => e.msg).join('\n'));
+            }
+
+            await req.auth.register(
+                req.body.email, 
+                req.body.password, 
+                req.body.username);
+
+            res.redirect('/');
+        } catch (err) {
+            console.log(err.message)
+            const ctx = {
+                errors: err.message.split('\n'),
+                userData: {
+                    username: req.body.username,
+                    email: req.body.email
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
 module.exports = router;
