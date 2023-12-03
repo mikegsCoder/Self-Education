a7e822403e370b8a4b0b2730bef021ba5fa6c62c
mikegsCoder <mikegsid@gmail.com>
2023-12-03 09:06:17 +0200

    Add create post

--- JS-Back-End/12. Exams/03. Crypto-Trade/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/03. Crypto-Trade/src/controllers/itemController.js
@@ -8,4 +8,33 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            paymentMethod: req.body.paymentMethod,
+            price: req.body.price,
+            author: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                paymentPethod: req.body.paymentMethod,
+                price: req.body.price,
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
