bdcd77c28f147eb5f9fe8fa7c81c59cbb04c946e
mikegsCoder <mikegsid@gmail.com>
2023-12-07 07:18:36 +0200

    Add getItemById

--- JS-Back-End/12. Exams/04. Job-Ads/src/services/item.js
+++ JS-Back-End/12. Exams/04. Job-Ads/src/services/item.js
@@ -7,6 +7,13 @@ async function getAllItems() {
     return Item.find({}).lean();
 }
 
+async function getItemById(id) {
+    let item = await Item.findById(id).populate('author').populate('usersApplyed').lean();
+
+    return item;
+}
+
 module.exports = {
-    getAllItems
+    getAllItems,
+    getItemById
 };
