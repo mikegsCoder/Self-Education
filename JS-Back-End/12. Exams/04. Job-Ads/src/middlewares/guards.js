6da0bafba8471fdcfe0512bce7633d48a1f6c40c
mikegsCoder <mikegsid@gmail.com>
2023-12-08 07:17:02 +0200

    Add guards middleware

---
+++ JS-Back-End/12. Exams/04. Job-Ads/src/middlewares/guards.js
@@ -0,0 +1,13 @@
+function isUser() {
+    return (req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.redirect("/auth/login");
+        }
+    };
+}
+
+module.exports = {
+    isUser
+};
