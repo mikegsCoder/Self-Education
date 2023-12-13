730f9671f33c2b78cccf7cb4a3ebcf32f9f302d4
mikegsCoder <mikegsid@gmail.com>
2023-12-13 07:07:54 +0200

    Add register post

--- JS-Back-End/12. Exams/04. Job-Ads/src/controllers/authController.js
+++ JS-Back-End/12. Exams/04. Job-Ads/src/controllers/authController.js
@@ -6,4 +6,46 @@ router.get('/register', isGuest(), (req, res) => {
     res.render('register');
 });
 
+router.post('/register', isGuest(),
+    body('email')
+        .isEmail().withMessage('Plese enter a valid email.'),
+    body('skills')
+        .isLength({ min: 1 }).withMessage('Skills are required.')
+        .isLength({ max: 40 }).withMessage('Skills description must be less than 40 characters long.'),
+    body('password')
+        .isLength({ min: 5 }).withMessage('Password must be at least 5 characters long.'),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error ('Passwords don\'t match.');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        
+        try {
+            if (errors.length > 0) {
+                throw new Error(Object.values(errors).map(e => e.msg).join('\n'));
+            }
+
+            await req.auth.register(
+                req.body.email, 
+                req.body.password, 
+                req.body.skills);
+
+            res.redirect('/');
+        } catch (err) {
+            console.log(err.message)
+            const ctx = {
+                errors: err.message.split('\n'),
+                userData: {
+                    skills: req.body.skills,
+                    email: req.body.email
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
 module.exports = router;
