76f8536423df6f6952e7ba18b62f76503203a484
mikegsCoder <mikegsid@gmail.com>
2023-12-15 07:17:53 +0200

    Add create post

--- JS-Back-End/12. Exams/04. Job-Ads/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/04. Job-Ads/src/controllers/itemController.js
@@ -8,4 +8,31 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            location: req.body.location.trim(),
+            companyName: req.body.companyName.trim(),
+            companyDescription: req.body.companyDescription.trim(),
+            author: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/catalog');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title.trim(),
+                location: req.body.location.trim(),
+                companyName: req.body.companyName.trim(),
+                companyDescription: req.body.companyDescription.trim()
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
