a43f604091a8799aa26cbbd01fd69699929f73d1
mikegsCoder <mikegsid@gmail.com>
2023-12-18 07:21:27 +0200

    Add getUserByUsername

--- JS-Back-End/12. Exams/05. Art-Gallery/src/services/user.js
+++ JS-Back-End/12. Exams/05. Art-Gallery/src/services/user.js
@@ -11,6 +11,14 @@ async function createUser(address, hashedPassword, username) {
     return user;
 }
 
+async function getUserByUsername(username) {
+    const pattern = new RegExp(`^${username}$`, 'i');
+    const user = await User.findOne({ username: { $regex: pattern } });
+
+    return user;
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUserByUsername
 };
