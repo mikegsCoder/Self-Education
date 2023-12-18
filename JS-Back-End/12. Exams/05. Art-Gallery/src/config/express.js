9bc59b2170ae45e5ba20e9eee6d7e4d91bf8af3c
mikegsCoder <mikegsid@gmail.com>
2023-12-18 07:15:16 +0200

    Add Art-Gallery initial commit

---
+++ JS-Back-End/12. Exams/05. Art-Gallery/src/config/express.js
@@ -0,0 +1,25 @@
+const hbs = require("express-handlebars");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+module.exports = (app) => {
+    app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+    app.set("view engine", "hbs");
+
+    app.use("/static", express.static("static"));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+
+    app.use((req, res, next) => {
+        if (!req.url.includes("favicon")) {
+            console.log(">>>", req.method, req.url);
+
+            if (req.user) {
+                // change to project req. (email or username ...):
+                console.log("Known user:", req.user.username);
+            }
+        }
+
+        next();
+    });
+};
