4cc8546767bc75dd8d56e99775ecfbfc4c828a65
mikegsCoder <mikegsid@gmail.com>
2023-12-23 11:02:24 +0200

    Add catalog get

--- JS-Back-End/12. Exams/05. Art-Gallery/src/controllers/homeController.js
+++ JS-Back-End/12. Exams/05. Art-Gallery/src/controllers/homeController.js
@@ -6,4 +6,10 @@ router.get('/', async (req, res) => {
     res.render('home', { items });
 });
 
+router.get('/catalog', async (req, res) => {
+    const items = await req.storage.getAllItems();
+
+    res.render('catalog', { items });
+});
+
 module.exports = router;
