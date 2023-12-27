5ca67042619962de0b60c6389abf4a135f613294
mikegsCoder <mikegsid@gmail.com>
2023-12-27 10:32:13 +0200

    Add create post

--- JS-Back-End/12. Exams/05. Art-Gallery/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/05. Art-Gallery/src/controllers/itemController.js
@@ -8,4 +8,31 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            technique: req.body.technique.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            certificate: req.body.certificate.trim(),
+            author: req.user._id
+        };
+        
+        await req.storage.createItem(itemData, req.user._id);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title,
+                technique: req.body.technique,
+                imageUrl: req.body.imageUrl,
+                certificate: req.body.certificate
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
