7859e134608b6ed169fb81015bdf4c93ccf98e5e
mikegsCoder <mikegsid@gmail.com>
2024-01-02 09:58:12 +0200

    Import and use auth middleware

--- JS-Back-End/12. Exams/06. Book-Talk/src/config/express.js
+++ JS-Back-End/12. Exams/06. Book-Talk/src/config/express.js
@@ -2,6 +2,7 @@ const hbs = require("express-handlebars");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+const authMiddleware = require("../middlewares/auth");
 const storageMiddleware = require("../middlewares/storage");
 
 module.exports = (app) => {
@@ -11,6 +12,7 @@ module.exports = (app) => {
     app.use("/static", express.static("static"));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(authMiddleware());
 
     app.use((req, res, next) => {
         if (!req.url.includes("favicon")) {
