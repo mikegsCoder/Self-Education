83431c2ef8348149d1ba2d90b957b8d74a779c34
mikegsCoder <mikegsid@gmail.com>
2024-01-03 11:28:41 +0200

    Add homeController

---
+++ JS-Back-End/12. Exams/06. Book-Talk/src/controllers/homeController.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    const items = await req.storage.getAllItems();
+
+    res.render('home' , { items });
+});
+
+module.exports = router;
