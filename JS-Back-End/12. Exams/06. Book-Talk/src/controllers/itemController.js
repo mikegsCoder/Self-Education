de6a1e469dff834884accb71090c38c36ae26dd4
mikegsCoder <mikegsid@gmail.com>
2024-01-08 07:04:16 +0200

    Add create post

--- JS-Back-End/12. Exams/06. Book-Talk/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/06. Book-Talk/src/controllers/itemController.js
@@ -8,4 +8,35 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            author: req.body.author.trim(),
+            genre: req.body.genre.trim(),
+            stars: req.body.stars,
+            imageUrl: req.body.imageUrl.trim(),
+            review: req.body.review.trim(),
+            owner: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre,
+                stars: req.body.stars,
+                imageUrl: req.body.imageUrl,
+                review: req.body.review
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
