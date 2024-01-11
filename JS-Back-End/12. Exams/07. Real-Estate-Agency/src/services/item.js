fc2ec4b60a517de073e9e1f78974d94548fb7910
mikegsCoder <mikegsid@gmail.com>
2024-01-11 07:01:19 +0200

    Add item serice

---
+++ JS-Back-End/12. Exams/07. Real-Estate-Agency/src/services/item.js
@@ -0,0 +1,12 @@
+const Item = require('../models/Item');
+const User = require('../models/User');
+
+const { ITEM } = require('../config/index');
+
+async function getAllItems() {
+    return Item.find({}).lean();
+}
+
+module.exports = {
+    getAllItems
+};
