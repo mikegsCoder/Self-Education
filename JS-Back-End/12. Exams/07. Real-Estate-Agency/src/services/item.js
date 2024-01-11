f872fc3731ad343c1f1bf634f9cb010455165bca
mikegsCoder <mikegsid@gmail.com>
2024-01-11 07:04:01 +0200

    Add getItemById

--- JS-Back-End/12. Exams/07. Real-Estate-Agency/src/services/item.js
+++ JS-Back-End/12. Exams/07. Real-Estate-Agency/src/services/item.js
@@ -11,7 +11,12 @@ async function getTopItems() {
     return Item.find({}).sort({ createdAt: -1 }).limit(3).lean();
 }
 
+async function getItemById(id) {
+    return Item.findById(id).populate('owner').populate('rentedBy').lean();
+}
+
 module.exports = {
     getAllItems,
-    getTopItems
+    getTopItems,
+    getItemById
 };
