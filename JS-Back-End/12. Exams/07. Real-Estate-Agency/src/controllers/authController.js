cfb493ea0711cba42e9dc2ab2171e139ee142998
mikegsCoder <mikegsid@gmail.com>
2024-01-19 07:11:47 +0200

    Add register post

--- JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/authController.js
+++ JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/authController.js
@@ -6,4 +6,45 @@ router.get('/register', isGuest(), (req, res) => {
     res.render('register');
 });
 
+router.post('/register', isGuest(),
+    body('name')
+    .matches(/^([A-Z]{1})([a-z]+)(\s)([A-Z]{1})([a-z]+){1}$/).withMessage('Incorrect name'),
+    body('username')
+        .isLength({ min: 5 }).withMessage('Username must be at least 5 characters long.'),
+    body('password')
+        .isLength({ min: 4 }).withMessage('Password must be at least 4 characters long.'),
+    body('rePass').custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error ('Passwords don\'t match.');
+        }
+        return true;
+    }),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        
+        try {
+            if (errors.length > 0) {
+                throw new Error(Object.values(errors).map(e => e.msg).join('\n'));
+            }
+
+            await req.auth.register(
+                req.body.name, 
+                req.body.password, 
+                req.body.username);
+
+            res.redirect('/');
+        } catch (err) {
+            console.log(err.message)
+            const ctx = {
+                errors: err.message.split('\n'),
+                userData: {
+                    username: req.body.username,
+                    name: req.body.name
+                }
+            }
+            res.render('register', ctx);
+        }
+    }
+);
+
 module.exports = router;
