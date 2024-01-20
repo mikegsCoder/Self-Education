418c50d99b4c9a7f109566c395ffff4bc4e4e3a3
mikegsCoder <mikegsid@gmail.com>
2024-01-20 10:32:30 +0200

    Add create post

--- JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/itemController.js
@@ -8,4 +8,37 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            title: req.body.title.trim(),
+            type: req.body.type.trim(),
+            year: req.body.year,
+            city: req.body.city.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            description: req.body.description.trim(),
+            pieces: req.body.pieces,
+            owner: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                title: req.body.title,
+                type: req.body.type,
+                year: req.body.year,
+                city: req.body.city,
+                imageUrl: req.body.imageUrl,
+                description: req.body.description,
+                pieces: req.body.pieces,
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
