b54982101f059729635425f3d1e35ee3918a2843
mikegsCoder <mikegsid@gmail.com>
2024-01-21 08:41:21 +0200

    Add edit post

--- JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/07. Real-Estate-Agency/src/controllers/itemController.js
@@ -79,4 +79,34 @@ router.get('/edit/:id', isUser(), async (req, res) => {
     }
 });
 
+router.post('/edit/:id', isUser(), async (req, res) => {
+    try {
+        const item = await req.storage.getItemById(req.params.id);
+        
+        if (item.owner._id != req.user._id) {
+            throw new Error(`Cannot edit ${ITEM.toLowerCase()} you haven\'t created.`);
+        }
+
+        await req.storage.editItem(req.params.id, req.body);
+
+        res.redirect('/item/details/' + req.params.id); 
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            item: {
+                _id: req.params.id,
+                title: req.body.title,
+                type: req.body.type,
+                year: req.body.year,
+                city: req.body.city,
+                imageUrl: req.body.imageUrl,
+                description: req.body.description,
+                pieces: req.body.pieces
+            }
+        };
+
+        res.render('item/edit', ctx);
+    }
+});
+
 module.exports = router;
