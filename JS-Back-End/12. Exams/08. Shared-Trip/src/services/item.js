5f8a9912f834b1937773814262399a0cf9fd07e9
mikegsCoder <mikegsid@gmail.com>
2024-01-24 07:23:15 +0200

    Add getItemById

--- JS-Back-End/12. Exams/08. Shared-Trip/src/services/item.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/services/item.js
@@ -7,6 +7,11 @@ async function getAllItems() {
     return Item.find({}).lean();
 }
 
+async function getItemById(id) {
+    return Item.findById(id).populate('author').populate('buddies').lean();
+}
+
 module.exports = {
-    getAllItems
+    getAllItems,
+    getItemById
 };
