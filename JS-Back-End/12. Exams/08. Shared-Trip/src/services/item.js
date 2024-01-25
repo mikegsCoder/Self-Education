d8a1a551dfc59c946b969e8b51931e0d671b977c
mikegsCoder <mikegsid@gmail.com>
2024-01-25 07:32:53 +0200

    Add joinItem

--- JS-Back-End/12. Exams/08. Shared-Trip/src/services/item.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/services/item.js
@@ -38,10 +38,18 @@ async function deleteItem(id) {
     return Item.findByIdAndDelete(id);
 }
 
+async function joinItem(itemId, userId) {
+    const item = await Item.findById(itemId);
+    item.buddies.push(userId);
+
+    return item.save();
+}
+
 module.exports = {
     getAllItems,
     getItemById,
     createItem, 
     editItem,
-    deleteItem
+    deleteItem,
+    joinItem
 };
