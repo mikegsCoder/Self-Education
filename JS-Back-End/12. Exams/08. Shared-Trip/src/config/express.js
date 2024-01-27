6696af6fd47679d020a65d0a91a84cc741d6ed6c
mikegsCoder <mikegsid@gmail.com>
2024-01-27 09:30:24 +0200

    Import and use storage middleware

--- JS-Back-End/12. Exams/08. Shared-Trip/src/config/express.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/config/express.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const authMiddleware = require("../middlewares/auth");
+const storageMiddleware = require("../middlewares/storage");
 
 module.exports = (app) => {
     app.engine("hbs", hbs.engine({ extname: ".hbs" }));
@@ -24,4 +25,6 @@ module.exports = (app) => {
 
         next();
     });
+    
+    app.use(storageMiddleware());
 };
