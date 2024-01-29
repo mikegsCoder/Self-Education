ec0bd4b98a883111d9a53d55f27fdb230364d194
mikegsCoder <mikegsid@gmail.com>
2024-01-29 07:06:36 +0200

    Add catalog get

--- JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/homeController.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/homeController.js
@@ -6,4 +6,10 @@ router.get('/', async (req, res) => {
     res.render('home');
 });
 
+router.get('/catalog', async (req, res) => {
+    const items = await req.storage.getAllItems();
+
+    res.render('catalog', { items });
+});
+
 module.exports = router;
