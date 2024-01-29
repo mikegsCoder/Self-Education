47fbbd8c20fd5813592c79081b218fa1ceeecf7f
mikegsCoder <mikegsid@gmail.com>
2024-01-29 07:11:41 +0200

    Add profile get

--- JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/homeController.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/homeController.js
@@ -12,4 +12,13 @@ router.get('/catalog', async (req, res) => {
     res.render('catalog', { items });
 });
 
+router.get('/profile', isUser(), async(req, res) => {
+    const userProfile = await req.storage.getUserProfile(req.user._id);
+    userProfile.isMale = Boolean(userProfile.gender == 'male');
+    userProfile.tripsCount = userProfile.trips.length;
+    userProfile.hasTrips = Boolean(userProfile.tripsCount > 0);
+
+    res.render('profile', { userProfile });
+});
+
 module.exports = router;
