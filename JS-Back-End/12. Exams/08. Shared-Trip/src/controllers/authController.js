89718f8b61bd4c9982329801bd5b151e6f47b3ce
mikegsCoder <mikegsid@gmail.com>
2024-01-31 07:19:24 +0200

    Add logout

--- JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/authController.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/authController.js
@@ -71,4 +71,9 @@ router.post('/login', isGuest(), async (req, res) => {
     }
 });
 
+router.get('/logout', (req, res) => {
+    req.auth.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
