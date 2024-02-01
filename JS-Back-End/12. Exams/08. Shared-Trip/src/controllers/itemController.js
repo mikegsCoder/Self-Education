020c2db1f3c16685e77d26b559e17832740c377d
mikegsCoder <mikegsid@gmail.com>
2024-02-01 07:15:12 +0200

    Add create post

--- JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/itemController.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/controllers/itemController.js
@@ -8,4 +8,41 @@ router.get('/create', isUser(), (req, res) => {
     res.render('item/create');
 });
 
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const itemData = {
+            startPoint: req.body.startPoint.trim(),
+            endPoint: req.body.endPoint.trim(),
+            date: req.body.date,
+            time: req.body.time,
+            carImage: req.body.carImage.trim(),
+            carBrand: req.body.carBrand.trim(),
+            seats: Number(req.body.seats),
+            price: Number(req.body.price),
+            description: req.body.description.trim(),
+            author: req.user._id
+        };
+        
+        await req.storage.createItem(itemData);
+        res.redirect('/catalog');
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            itemData: {
+                startPoint: req.body.startPoint,
+                endPoint: req.body.endPoint,
+                date: req.body.date,
+                time: req.body.time,
+                carImage: req.body.carImage,
+                carBrand: req.body.carBrand,
+                seats: req.body.seats,
+                price: req.body.price,
+                description: req.body.description,
+            }
+        };
+
+        res.render('item/create', ctx);
+    }
+});
+
 module.exports = router;
