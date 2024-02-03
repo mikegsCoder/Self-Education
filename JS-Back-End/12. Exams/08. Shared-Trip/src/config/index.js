195bacafb8ee9e050906bc14c96418d1a483c62f
mikegsCoder <mikegsid@gmail.com>
2024-02-03 15:57:45 +0200

    Add environment variable in db connection string

--- JS-Back-End/12. Exams/08. Shared-Trip/src/config/index.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/config/index.js
@@ -2,8 +2,7 @@ const itemName = 'Trip'
 
 module.exports = {
     ITEM: itemName,
-    // DB_CONNECTION_STRING: `mongodb://0.0.0.0:27017/${itemName.toLowerCase()}`,
-    DB_CONNECTION_STRING: `mongodb+srv://testUser:testqaws123@cluster0.xnq7krw.mongodb.net/${itemName.toLowerCase()}`,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || `mongodb://0.0.0.0:27017/${itemName.toLowerCase()}`,
     PORT: 3000,
     TOKEN_SECRET: 'this is my secret',
     COOKIE_NAME: 'SESSION_TOKEN'
