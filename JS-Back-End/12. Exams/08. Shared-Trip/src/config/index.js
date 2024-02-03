4b62f42abe9c7429026e79bc7792a11de1794415
mikegsCoder <mikegsid@gmail.com>
2024-02-03 12:02:52 +0200

    Add MongoDB Atlass connection string

--- JS-Back-End/12. Exams/08. Shared-Trip/src/config/index.js
+++ JS-Back-End/12. Exams/08. Shared-Trip/src/config/index.js
@@ -2,7 +2,8 @@ const itemName = 'Trip'
 
 module.exports = {
     ITEM: itemName,
-    DB_CONNECTION_STRING: `mongodb://0.0.0.0:27017/${itemName.toLowerCase()}`,
+    // DB_CONNECTION_STRING: `mongodb://0.0.0.0:27017/${itemName.toLowerCase()}`,
+    DB_CONNECTION_STRING: `mongodb+srv://testUser:testqaws123@cluster0.xnq7krw.mongodb.net/${itemName.toLowerCase()}`,
     PORT: 3000,
     TOKEN_SECRET: 'this is my secret',
     COOKIE_NAME: 'SESSION_TOKEN'
