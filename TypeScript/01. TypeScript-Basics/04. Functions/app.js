0545b0a37e604a21c655a55e3bf43ef1219b4c0a
mikegsCoder <mikegsid@gmail.com>
2024-02-07 07:14:56 +0200

    Add more functions examples

--- TypeScript/01. TypeScript-Basics/04. Functions/app.js
+++ TypeScript/01. TypeScript-Basics/04. Functions/app.js
@@ -10,4 +10,18 @@ function addExample2(a, b) {
     console.log(result);
 }
 addExample2(20, 30); // 50
-addExample2(10, 15); //25
+addExample2(10, 15); // 25
+// ---------- using external variables in function ----------
+var x = 1.5;
+function multiplication(a) {
+    var result = a * x;
+    console.log(result);
+}
+multiplication(20); // 30
+multiplication(10); // 15
+// ---------- function return type ----------
+function addExample3(a, b) {
+    return a + b;
+}
+var result = addExample3(1, 2);
+console.log(result); // 3
--- TypeScript/01. TypeScript-Basics/04. Functions/app.ts
+++ TypeScript/01. TypeScript-Basics/04. Functions/app.ts
@@ -13,4 +13,23 @@ function addExample2(a: number, b: number) {
 }
 
 addExample2(20, 30);  // 50
-addExample2(10, 15);  //25
+addExample2(10, 15);  // 25
+
+// ---------- using external variables in function ----------
+let x: number = 1.5;
+
+function multiplication(a: number) {
+  let result = a * x;
+  console.log(result);
+}
+
+multiplication(20);  // 30
+multiplication(10);  // 15
+
+// ---------- function return type ----------
+function addExample3(a: number, b: number): number {
+  return a + b;
+}
+
+let result = addExample3(1, 2);
+console.log(result);  // 3
