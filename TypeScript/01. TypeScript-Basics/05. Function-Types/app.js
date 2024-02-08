7ac41872254243537476a9b66d28f20ba3962508
mikegsCoder <mikegsid@gmail.com>
2024-02-08 07:07:13 +0200

    Add number example

--- TypeScript/01. TypeScript-Basics/05. Function-Types/app.js
+++ TypeScript/01. TypeScript-Basics/05. Function-Types/app.js
@@ -4,3 +4,15 @@ function hello() {
 };
 var message = hello;
 message();
+// ---------- number ----------
+function sumFunction(x, y) {
+    return x + y;
+};
+function subtractFunction(a, b) {
+    return a - b;
+};
+var op;
+op = sumFunction;
+console.log(op(2, 4)); // 6
+op = subtractFunction;
+console.log(op(6, 4)); // 2
--- TypeScript/01. TypeScript-Basics/05. Function-Types/app.ts
+++ TypeScript/01. TypeScript-Basics/05. Function-Types/app.ts
@@ -5,3 +5,20 @@ function hello() {
 
 const message: () => void = hello;
 message();
+
+// ---------- number ----------
+function sumFunction(x: number, y: number): number {
+  return x + y;
+};
+
+function subtractFunction(a: number, b: number): number {
+  return a - b;
+};
+
+let op: (x: number, y: number) => number;
+
+op = sumFunction;
+console.log(op(2, 4));  // 6
+
+op = subtractFunction;
+console.log(op(6, 4));  // 2
