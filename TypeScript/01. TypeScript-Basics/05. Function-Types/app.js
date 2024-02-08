2b1863dc61ba1d35158fe8e4f9efd3a65fe43b2d
mikegsCoder <mikegsid@gmail.com>
2024-02-08 07:09:59 +0200

    Add functions as parameter to another functions example

--- TypeScript/01. TypeScript-Basics/05. Function-Types/app.js
+++ TypeScript/01. TypeScript-Basics/05. Function-Types/app.js
@@ -1,7 +1,8 @@
 // ---------- void ----------
 function hello() {
     console.log("Hello TypeScript!");
-};
+}
+;
 var message = hello;
 message();
 // ---------- number ----------
@@ -16,3 +17,15 @@ op = sumFunction;
 console.log(op(2, 4)); // 6
 op = subtractFunction;
 console.log(op(6, 4)); // 2
+// ---------- functions as parameters to another functions ----------
+function sum(x, y) {
+    return x + y;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function mathOp(x, y, op) {
+    return op(x, y);
+}
+console.log(mathOp(10, 20, sum)); // 30 
+console.log(mathOp(10, 20, multiply)); // 200 
--- TypeScript/01. TypeScript-Basics/05. Function-Types/app.ts
+++ TypeScript/01. TypeScript-Basics/05. Function-Types/app.ts
@@ -22,3 +22,19 @@ console.log(op(2, 4));  // 6
 
 op = subtractFunction;
 console.log(op(6, 4));  // 2
+
+// ---------- functions as parameters to another functions ----------
+function sum(x: number, y: number): number {
+  return x + y;
+};
+
+function multiply(a: number, b: number): number {
+  return a * b;
+};
+
+function mathOp(x: number, y: number, op: (a: number, b: number) => number): number {
+  return op(x, y);
+}
+
+console.log(mathOp(10, 20, sum));       // 30 
+console.log(mathOp(10, 20, multiply));  // 200 
