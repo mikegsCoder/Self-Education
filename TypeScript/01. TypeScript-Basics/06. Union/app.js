d7b1aff8ba8b59a3f22521c2c5ff0fe3cf069dae
mikegsCoder <mikegsid@gmail.com>
2024-02-09 07:06:21 +0200

    Add another union example

--- TypeScript/01. TypeScript-Basics/06. Union/app.js
+++ TypeScript/01. TypeScript-Basics/06. Union/app.js
@@ -12,3 +12,14 @@ var id2 = 'xyz123';
 printId('1h2e3l4o5');
 printId(9876);
 printId(id2);
+// ---------- union as function parameter example ----------
+function printSentence(words) {
+    if (typeof words === 'string') {
+        console.log(words);
+    }
+    else {
+        console.log(words.join(' '));
+    }
+}
+printSentence(['Programming', 'language', 'TypeScript']);
+printSentence('Programming language JavaScript');
