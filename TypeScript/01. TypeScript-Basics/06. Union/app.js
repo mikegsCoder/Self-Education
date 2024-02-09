84fd231d0b273ce56fce49608620baa367d0bb55
mikegsCoder <mikegsid@gmail.com>
2024-02-09 07:02:08 +0200

    Add union

---
+++ TypeScript/01. TypeScript-Basics/06. Union/app.js
@@ -0,0 +1,6 @@
+// ---------- union ----------
+var id1;
+id1 = "1345dgg5";
+console.log(id1); // 1345dgg5
+id1 = 234;
+console.log(id1); // 234
---
+++ TypeScript/01. TypeScript-Basics/06. Union/app.ts
@@ -0,0 +1,8 @@
+// ---------- union ----------
+let id1: number | string;
+
+id1 = "1345dgg5";
+console.log(id1);  // 1345dgg5
+
+id1 = 234;
+console.log(id1);  // 234
