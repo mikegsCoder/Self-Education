f0beb3dc5aac1e1a10ff21324585b106f70fe8b6
mikegsCoder <mikegsid@gmail.com>
2024-02-09 07:04:11 +0200

    Add union as function parameter example

--- TypeScript/01. TypeScript-Basics/06. Union/app.js
+++ TypeScript/01. TypeScript-Basics/06. Union/app.js
@@ -4,3 +4,11 @@ id1 = "1345dgg5";
 console.log(id1); // 1345dgg5
 id1 = 234;
 console.log(id1); // 234
+// ---------- union as function parameter example ----------
+function printId(id2) {
+    console.log("Id: ".concat(id2));
+}
+var id2 = 'xyz123';
+printId('1h2e3l4o5');
+printId(9876);
+printId(id2);
--- TypeScript/01. TypeScript-Basics/06. Union/app.ts
+++ TypeScript/01. TypeScript-Basics/06. Union/app.ts
@@ -6,3 +6,14 @@ console.log(id1);  // 1345dgg5
 
 id1 = 234;
 console.log(id1);  // 234
+
+// ---------- union as function parameter example ----------
+function printId(id2: number | string) {
+  console.log(`Id: ${id2}`);
+}
+
+let id2: string | number = 'xyz123';
+
+printId('1h2e3l4o5');
+printId(9876);
+printId(id2);
