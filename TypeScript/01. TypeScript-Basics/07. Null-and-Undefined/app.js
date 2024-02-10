507ccbdd697fe588ac25266f15e5b63e3726ca77
mikegsCoder <mikegsid@gmail.com>
2024-02-10 09:27:25 +0200

    Add null and undefined

---
+++ TypeScript/01. TypeScript-Basics/07. Null-and-Undefined/app.js
@@ -0,0 +1,16 @@
+// ---------- null and undefined ----------
+var a = undefined;
+var b = null;
+// ---------- null and undefined example ----------
+var userId = null;
+function printId(id) {
+    if (id === null) {
+        console.log('No such user!');
+    }
+    else {
+        console.log("User id: ".concat(id, "."));
+    }
+}
+printId(userId); // No such user!
+userId = 34;
+printId(userId); // User id: 34.
---
+++ TypeScript/01. TypeScript-Basics/07. Null-and-Undefined/app.ts
@@ -0,0 +1,19 @@
+// ---------- null and undefined ----------
+let a: undefined = undefined;
+let b: null = null;
+
+// ---------- null and undefined example ----------
+let userId: number | null = null;
+
+function printId(id: number | null) {
+  if (id === null) {
+    console.log('No such user!');
+  } else {
+    console.log(`User id: ${id}.`);
+  }
+}
+
+printId(userId);  // No such user!
+
+userId = 34;
+printId(userId);  // User id: 34.
