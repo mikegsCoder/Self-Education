c31918a5fa17f79cf1455140d66d32743f7ef734
mikegsCoder <mikegsid@gmail.com>
2024-02-11 09:52:37 +0200

    Add objects

---
+++ TypeScript/01. TypeScript-Basics/08. Objects/app.js
@@ -0,0 +1,7 @@
+// ---------- objects ----------
+var person1 = { name: 'Tom', age: 34 };
+console.log(person1.name);
+console.log(person1['name']);
+// ---------- objects example ----------
+var person2 = { name: 'John', age: 35 };
+console.log(person2.name); // John
---
+++ TypeScript/01. TypeScript-Basics/08. Objects/app.ts
@@ -0,0 +1,10 @@
+// ---------- objects ----------
+let person1 = { name: 'Tom', age: 34 };
+
+console.log(person1.name);
+console.log(person1['name']);
+
+// ---------- objects example ----------
+let person2: { name: string; age: number } = { name: 'John', age: 35 };
+
+console.log(person2.name);   // John
