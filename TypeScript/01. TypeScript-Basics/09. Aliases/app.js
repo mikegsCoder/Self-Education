1ddf1df838ee179fad1894cce579fb778557d86f
mikegsCoder <mikegsid@gmail.com>
2024-02-13 07:09:16 +0200

    Add alias example

--- TypeScript/01. TypeScript-Basics/09. Aliases/app.js
+++ TypeScript/01. TypeScript-Basics/09. Aliases/app.js
@@ -2,3 +2,18 @@ var userId = 2;
 console.log("Id: ".concat(userId)); // Id: 2
 userId = 'qwerty';
 console.log("Id: ".concat(userId)); // Id: qwerty
+// ---------- alias example ----------
+// function parameter is alias
+function printId(inputId) {
+    console.log("Id: ".concat(inputId));
+}
+// return type is alias
+function getId(isNumber) {
+    if (isNumber)
+        return 1;
+    else
+        return '1';
+}
+printId(12345);
+printId('qwerty');
+console.log(getId(true));
--- TypeScript/01. TypeScript-Basics/09. Aliases/app.ts
+++ TypeScript/01. TypeScript-Basics/09. Aliases/app.ts
@@ -6,3 +6,20 @@ console.log(`Id: ${userId}`); // Id: 2
 
 userId = 'qwerty';
 console.log(`Id: ${userId}`); // Id: qwerty
+
+// ---------- alias example ----------
+// function parameter is alias
+function printId(inputId: id) {
+  console.log(`Id: ${inputId}`);
+}
+// return type is alias
+function getId(isNumber: boolean): id {
+  if (isNumber)
+    return 1;
+  else
+    return '1';
+}
+
+printId(12345);
+printId('qwerty');
+console.log(getId(true));
