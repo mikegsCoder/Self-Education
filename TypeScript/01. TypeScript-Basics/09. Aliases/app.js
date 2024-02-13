68a83aa038e51cf0ff2f9ca4da36997b5cf8e781
mikegsCoder <mikegsid@gmail.com>
2024-02-13 07:06:31 +0200

    Add aliases

---
+++ TypeScript/01. TypeScript-Basics/09. Aliases/app.js
@@ -0,0 +1,4 @@
+var userId = 2;
+console.log("Id: ".concat(userId)); // Id: 2
+userId = 'qwerty';
+console.log("Id: ".concat(userId)); // Id: qwerty
---
+++ TypeScript/01. TypeScript-Basics/09. Aliases/app.ts
@@ -0,0 +1,8 @@
+// ---------- aliases ----------
+type id = number | string;
+
+let userId: id = 2;
+console.log(`Id: ${userId}`); // Id: 2
+
+userId = 'qwerty';
+console.log(`Id: ${userId}`); // Id: qwerty
