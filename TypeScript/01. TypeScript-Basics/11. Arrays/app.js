ce86146c70fd2d359d4ee77bedff8fa1c7026a27
mikegsCoder <mikegsid@gmail.com>
2024-02-14 07:11:52 +0200

    Add array example

--- TypeScript/01. TypeScript-Basics/11. Arrays/app.js
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.js
@@ -3,3 +3,6 @@ var list = [10, 20, 30];
 var colors = ['red', 'green', 'blue'];
 console.log(list[0]); // 10
 console.log(colors[1]); // green
+// ---------- arrays example ----------
+var names = ['Tom', 'Bob', 'Alice'];
+console.log(names[1]); // Bob
--- TypeScript/01. TypeScript-Basics/11. Arrays/app.ts
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.ts
@@ -4,3 +4,7 @@ let colors: string[] = ['red', 'green', 'blue'];
 
 console.log(list[0]);   // 10
 console.log(colors[1]); // green
+
+// ---------- arrays example ----------
+let names: Array<string> = ['Tom', 'Bob', 'Alice'];
+console.log(names[1]);  // Bob
