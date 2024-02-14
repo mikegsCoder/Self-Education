ffcdc07137af83cc7af9463d0a847c63ed9323c1
mikegsCoder <mikegsid@gmail.com>
2024-02-14 07:10:30 +0200

    Add arrays

---
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.js
@@ -0,0 +1,5 @@
+// ---------- arrays ----------
+var list = [10, 20, 30];
+var colors = ['red', 'green', 'blue'];
+console.log(list[0]); // 10
+console.log(colors[1]); // green
---
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.ts
@@ -0,0 +1,6 @@
+// ---------- arrays ----------
+let list: number[] = [10, 20, 30];
+let colors: string[] = ['red', 'green', 'blue'];
+
+console.log(list[0]);   // 10
+console.log(colors[1]); // green
