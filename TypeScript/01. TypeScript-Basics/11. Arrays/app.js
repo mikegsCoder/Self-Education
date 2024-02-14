c6aff0e23aefb7c88e974e932406231c09f4bf2b
mikegsCoder <mikegsid@gmail.com>
2024-02-14 07:14:11 +0200

    Add ReadonyArray example

--- TypeScript/01. TypeScript-Basics/11. Arrays/app.js
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.js
@@ -6,3 +6,10 @@ console.log(colors[1]); // green
 // ---------- arrays example ----------
 var names = ['Tom', 'Bob', 'Alice'];
 console.log(names[1]); // Bob
+// ---------- ReadonlyArray ----------
+var people1 = ['Tom', 'Bob', 'Sam'];
+/*
+people1[1] = 'Kate';   // Error!
+people1.push('Kate');  // Error!
+people1.pop();         // Error!
+*/
--- TypeScript/01. TypeScript-Basics/11. Arrays/app.ts
+++ TypeScript/01. TypeScript-Basics/11. Arrays/app.ts
@@ -8,3 +8,12 @@ console.log(colors[1]); // green
 // ---------- arrays example ----------
 let names: Array<string> = ['Tom', 'Bob', 'Alice'];
 console.log(names[1]);  // Bob
+
+// ---------- ReadonlyArray ----------
+const people1: ReadonlyArray<string> = ['Tom', 'Bob', 'Sam'];
+
+/*
+people1[1] = 'Kate';   // Error!
+people1.push('Kate');  // Error!
+people1.pop();         // Error!
+*/
