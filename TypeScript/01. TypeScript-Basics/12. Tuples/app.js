b4b0eada1b8404c5d6dd46db65dbda796ee91ed8
mikegsCoder <mikegsid@gmail.com>
2024-02-17 10:22:25 +0200

    Add tuple example

--- TypeScript/01. TypeScript-Basics/12. Tuples/app.js
+++ TypeScript/01. TypeScript-Basics/12. Tuples/app.js
@@ -38,3 +38,14 @@ function printUser1(user) {
 }
 printUser1(bob);
 printUser1(john);
+// ---------- tuple example ----------
+function printMarks(marks) {
+    for (var _i = 0, marks_1 = marks; _i < marks_1.length; _i++) {
+        var mark = marks_1[_i];
+        console.log(mark);
+    }
+}
+var math = ['Math', 5, 4, 5, 4, 4];
+var physics = ['Physics', 5, 5, 5];
+printMarks(math);
+printMarks(physics);
--- TypeScript/01. TypeScript-Basics/12. Tuples/app.ts
+++ TypeScript/01. TypeScript-Basics/12. Tuples/app.ts
@@ -46,3 +46,17 @@ function printUser1(user: [string, number, boolean?]) {
 
 printUser1(bob);
 printUser1(john);
+
+// ---------- tuple example ----------
+function printMarks(marks: [string, ...number[]]) {
+  for (const mark of marks) {
+    console.log(mark);
+  }
+}
+
+let math: [string, ...number[]] = ['Math', 5, 4, 5, 4, 4];
+let physics: [string, ...number[]] = ['Physics', 5, 5, 5];
+
+printMarks(math);
+printMarks(physics);
+
