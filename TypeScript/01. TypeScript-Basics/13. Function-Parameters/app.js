f8f60b9f4ebc6913a4794a9e6f757636129c0b21
mikegsCoder <mikegsid@gmail.com>
2024-02-18 11:16:11 +0200

    Add function parameters example

--- TypeScript/01. TypeScript-Basics/13. Function-Parameters/app.js
+++ TypeScript/01. TypeScript-Basics/13. Function-Parameters/app.js
@@ -14,3 +14,18 @@ var num1 = addNumbers(3, 7, 8);
 console.log(num1); // 18
 var num2 = addNumbers(3, 7, 8, 9, 4);
 console.log(num2); // 31
+// ---------- function parameters example ----------
+function sum() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    var result = 0;
+    for (var i = 0; i < args.length; i++) {
+        result += args[i];
+    }
+    return result;
+}
+var numbers = [1, 3, 5, 7, 9];
+var num = sum.apply(void 0, numbers);
+console.log(num); // 25
--- TypeScript/01. TypeScript-Basics/13. Function-Parameters/app.ts
+++ TypeScript/01. TypeScript-Basics/13. Function-Parameters/app.ts
@@ -14,3 +14,18 @@ console.log(num1);  // 18
 
 let num2 = addNumbers(3, 7, 8, 9, 4);
 console.log(num2);  // 31
+
+// ---------- function parameters example ----------
+function sum(...args: number[]): number {
+  let result = 0;
+
+  for (let i = 0; i < args.length; i++) {
+    result += args[i];
+  }
+
+  return result;
+}
+
+const numbers = [1, 3, 5, 7, 9] as const;
+let num = sum(...numbers);
+console.log(num);  // 25
