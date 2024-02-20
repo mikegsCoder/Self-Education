ee224d0456e35b084df5fb65a90ebfb30a902ef6
mikegsCoder <mikegsid@gmail.com>
2024-02-20 07:06:09 +0200

    Add playground

---
+++ TypeScript/01. TypeScript-Basics/15. Playground/length.js
@@ -0,0 +1,6 @@
+// Write a simple function that calculates the length of the string.
+// ---------- solution: ----------
+function stringLength(str) {
+    console.log("The length is ".concat(str.length, " symbols."));
+}
+stringLength('Hello TypeScript!'); // The length is 17 symbols.
---
+++ TypeScript/01. TypeScript-Basics/15. Playground/length.ts
@@ -0,0 +1,8 @@
+// Write a simple function that calculates the length of the string.
+
+// ---------- solution: ----------
+function stringLength(str: string) {
+  console.log(`The length is ${str.length} symbols.`);
+}
+
+stringLength('Hello TypeScript!') // The length is 17 symbols.
