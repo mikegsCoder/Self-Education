3cf21faa92dd28bf4cd9bfd34970cb92735a30a7
mikegsCoder <mikegsid@gmail.com>
2024-02-20 07:11:32 +0200

    Add solution 2

--- TypeScript/01. TypeScript-Basics/15. Playground/max-min.js
+++ TypeScript/01. TypeScript-Basics/15. Playground/max-min.js
@@ -8,3 +8,20 @@ function maxMin1(arr) {
 var result1 = maxMin1([-2, 0, 7, 3, 5]);
 console.log("The min value is ".concat(result1[0], "."));
 console.log("The max value is ".concat(result1[1], "."));
+// ---------- solution2: ----------
+function maxMin2(arr) {
+    var min = arr[0];
+    var max = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return [min, max];
+}
+var result2 = maxMin2([-2, 0, 7, 3, 5]);
+console.log("The min value is ".concat(result2[0], "."));
+console.log("The max value is ".concat(result2[1], "."));
--- TypeScript/01. TypeScript-Basics/15. Playground/max-min.ts
+++ TypeScript/01. TypeScript-Basics/15. Playground/max-min.ts
@@ -11,3 +11,24 @@ function maxMin1(arr: number[]): [number, number] {
 const result1 = maxMin1([-2, 0, 7, 3, 5]);
 console.log(`The min value is ${result1[0]}.`);
 console.log(`The max value is ${result1[1]}.`);
+
+// ---------- solution2: ----------
+function maxMin2(arr: number[]): [number, number] {
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return [min, max];
+}
+
+const result2 = maxMin2([-2, 0, 7, 3, 5]);
+console.log(`The min value is ${result2[0]}.`);
+console.log(`The max value is ${result2[1]}.`);
