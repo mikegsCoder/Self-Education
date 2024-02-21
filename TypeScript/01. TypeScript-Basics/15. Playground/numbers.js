d1ce995a6303f65db532e68840d1c3baedd99029
mikegsCoder <mikegsid@gmail.com>
2024-02-21 07:03:34 +0200

    Add second solution

--- TypeScript/01. TypeScript-Basics/15. Playground/numbers.js
+++ TypeScript/01. TypeScript-Basics/15. Playground/numbers.js
@@ -22,3 +22,24 @@ function numbers1(m, n) {
 numbers1(3, 7); // 3, 4, 5, 6, 7
 numbers1(7, 3); // 7, 6, 5, 4, 3
 numbers1(5, 5); // 5
+// ---------- solution 2 ----------
+function numbers2(m, n) {
+    var arr = [];
+    if (m < n) {
+        for (var i = m; i <= n; i++) {
+            arr.push(i);
+        }
+    }
+    else if (m > n) {
+        for (var i = m; i >= n; i--) {
+            arr.push(i);
+        }
+    }
+    else {
+        arr = [m];
+    }
+    console.log(arr.join(', '));
+}
+numbers2(3, 7); // 3, 4, 5, 6, 7
+numbers2(7, 3); // 7, 6, 5, 4, 3
+numbers2(5, 5); // 5
--- TypeScript/01. TypeScript-Basics/15. Playground/numbers.ts
+++ TypeScript/01. TypeScript-Basics/15. Playground/numbers.ts
@@ -26,3 +26,26 @@ function numbers1(m: number, n: number) {
 numbers1(3, 7); // 3, 4, 5, 6, 7
 numbers1(7, 3); // 7, 6, 5, 4, 3
 numbers1(5, 5); // 5
+
+// ---------- solution 2 ----------
+function numbers2(m: number, n: number) {
+  let arr: number[] = [];
+
+  if (m < n) {
+    for (let i = m; i <= n; i++) {
+      arr.push(i);
+    }
+  } else if (m > n) {
+    for (let i = m; i >= n; i--) {
+      arr.push(i);
+    }
+  } else {
+    arr = [m];
+  }
+
+  console.log(arr.join(', '));
+}
+
+numbers2(3, 7); // 3, 4, 5, 6, 7
+numbers2(7, 3); // 7, 6, 5, 4, 3
+numbers2(5, 5); // 5
