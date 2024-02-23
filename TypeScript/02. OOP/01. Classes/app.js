74e10f5e70fe245fe156cc91898bda8646c6054b
mikegsCoder <mikegsid@gmail.com>
2024-02-23 07:10:05 +0200

    Add classes

---
+++ TypeScript/02. OOP/01. Classes/app.js
@@ -0,0 +1,10 @@
+// ---------- classes ----------
+var User1 = /** @class */ (function () {
+    function User1() {
+    }
+    return User1;
+}());
+var tom = new User1();
+tom.name = 'Tom';
+tom.age = 36;
+console.log("name: ".concat(tom.name, "  age: ").concat(tom.age)); // name: Tom  age: 36
---
+++ TypeScript/02. OOP/01. Classes/app.ts
@@ -0,0 +1,11 @@
+// ---------- classes ----------
+class User1 {
+  name: string;
+  age: number;
+}
+
+let tom = new User1();
+tom.name = 'Tom';
+tom.age = 36;
+
+console.log(`name: ${tom.name}  age: ${tom.age}`);  // name: Tom  age: 36
