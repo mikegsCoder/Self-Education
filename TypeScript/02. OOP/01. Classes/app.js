333c5cf6d9c3596062c1c504b1c539ea2d8e95b1
mikegsCoder <mikegsid@gmail.com>
2024-02-23 07:12:15 +0200

    Add class example

--- TypeScript/02. OOP/01. Classes/app.js
+++ TypeScript/02. OOP/01. Classes/app.js
@@ -8,3 +8,13 @@ var tom = new User1();
 tom.name = 'Tom';
 tom.age = 36;
 console.log("name: ".concat(tom.name, "  age: ").concat(tom.age)); // name: Tom  age: 36
+// ---------- classes example ----------
+var User2 = /** @class */ (function () {
+    function User2() {
+        this.name = 'Tom Smith';
+        this.age = 43;
+    }
+    return User2;
+}());
+var user = new User2();
+console.log("name: ".concat(user.name, "  age: ").concat(user.age)); // name: Tom Smith  age: 43
--- TypeScript/02. OOP/01. Classes/app.ts
+++ TypeScript/02. OOP/01. Classes/app.ts
@@ -9,3 +9,13 @@ tom.name = 'Tom';
 tom.age = 36;
 
 console.log(`name: ${tom.name}  age: ${tom.age}`);  // name: Tom  age: 36
+
+// ---------- classes example ----------
+class User2 {
+  name: string = 'Tom Smith';
+  age: number = 43;
+}
+
+let user = new User2();
+
+console.log(`name: ${user.name}  age: ${user.age}`);   // name: Tom Smith  age: 43
