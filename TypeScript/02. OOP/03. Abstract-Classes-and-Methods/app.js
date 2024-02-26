ca304616dc6261c154e1346a1565cdefa713e458
mikegsCoder <mikegsid@gmail.com>
2024-02-26 06:58:40 +0200

    Add abstract classes and methods

---
+++ TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.js
@@ -0,0 +1,7 @@
+// ---------- abstract class ----------
+var Figure1 = /** @class */ (function () {
+    function Figure1() {
+    }
+    return Figure1;
+}());
+// let someFigure = new Figure1()  // Error!
---
+++ TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.ts
@@ -0,0 +1,5 @@
+// ---------- abstract class ----------
+abstract class Figure1 {
+}
+
+// let someFigure = new Figure1()  // Error!
