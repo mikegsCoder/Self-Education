1b31ada6315a005afaa9243d8fb5ab0f81da5ee4
mikegsCoder <mikegsid@gmail.com>
2024-02-26 07:00:56 +0200

    Add abstract class example

--- TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.js
+++ TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // ---------- abstract class ----------
 var Figure1 = /** @class */ (function () {
     function Figure1() {
@@ -5,3 +20,28 @@ var Figure1 = /** @class */ (function () {
     return Figure1;
 }());
 // let someFigure = new Figure1()  // Error!
+// ---------- abstract class example ----------
+var Figure2 = /** @class */ (function () {
+    function Figure2() {
+    }
+    Figure2.prototype.getArea = function () {
+        console.log('Not Implemented!');
+    };
+    return Figure2;
+}());
+var Rectangle2 = /** @class */ (function (_super) {
+    __extends(Rectangle2, _super);
+    function Rectangle2(width, height) {
+        var _this = _super.call(this) || this;
+        _this.width = width;
+        _this.height = height;
+        return _this;
+    }
+    Rectangle2.prototype.getArea = function () {
+        var square = this.width * this.height;
+        console.log('area =', square);
+    };
+    return Rectangle2;
+}(Figure2));
+var someFigure2 = new Rectangle2(20, 30);
+someFigure2.getArea(); // area = 600
--- TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.ts
+++ TypeScript/02. OOP/03. Abstract-Classes-and-Methods/app.ts
@@ -3,3 +3,24 @@ abstract class Figure1 {
 }
 
 // let someFigure = new Figure1()  // Error!
+
+// ---------- abstract class example ----------
+abstract class Figure2 {
+  getArea(): void {
+    console.log('Not Implemented!')
+  }
+}
+
+class Rectangle2 extends Figure2 {
+  constructor(public width: number, public height: number) {
+    super();
+  }
+
+  getArea(): void {
+    let square = this.width * this.height;
+    console.log('area =', square);
+  }
+}
+
+let someFigure2: Figure2 = new Rectangle2(20, 30)
+someFigure2.getArea();   // area = 600
