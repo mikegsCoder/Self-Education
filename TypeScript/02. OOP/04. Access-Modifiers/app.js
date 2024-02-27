ecf75c1cc0194f5aa093d1a62be641167d2d3a9a
mikegsCoder <mikegsid@gmail.com>
2024-02-27 07:00:37 +0200

    Add access modifiers

---
+++ TypeScript/02. OOP/04. Access-Modifiers/app.js
@@ -0,0 +1,6 @@
+// ---------- public ----------
+var Person1 = /** @class */ (function () {
+    function Person1() {
+    }
+    return Person1;
+}());
---
+++ TypeScript/02. OOP/04. Access-Modifiers/app.ts
@@ -0,0 +1,5 @@
+// ---------- public ----------
+class Person1 {
+  public name: string;
+  public year: number;
+}
