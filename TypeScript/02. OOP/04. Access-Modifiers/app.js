3b2631ea3cdf2d5b93cd76cac17e4c12007b59d6
mikegsCoder <mikegsid@gmail.com>
2024-02-27 07:08:20 +0200

    Add protected

--- TypeScript/02. OOP/04. Access-Modifiers/app.js
+++ TypeScript/02. OOP/04. Access-Modifiers/app.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // ---------- public ----------
 var Person1 = /** @class */ (function () {
     function Person1() {
@@ -22,3 +37,38 @@ var tom = new Person2('Tom', 24);
 tom.print(); // Name: Tom  Year of birth: 2000
 // console.log(tom._name);  // Error!
 // tom.setYear(45);         // Error!
+// ---------- protected ----------
+var Person3 = /** @class */ (function () {
+    function Person3(name, age) {
+        this.name = name;
+        this.year = this.setYear(age);
+    }
+    Person3.prototype.printPerson = function () {
+        console.log("Name: ".concat(this.name, "  Year of birth: ").concat(this.year));
+    };
+    Person3.prototype.setYear = function (age) {
+        return new Date().getFullYear() - age;
+    };
+    return Person3;
+}());
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, age, company) {
+        var _this = _super.call(this, name, age) || this;
+        _this.company = company;
+        return _this;
+    }
+    Employee.prototype.printEmployee = function () {
+        // console.log("Year: " + this.year);   // field year is inaccessible, because it is private
+        // setYear(25);                         // method setYear is inaccessible, because it is private
+        this.printPerson(); // method printPerson is accessible, because it is protected
+        console.log("Company: ".concat(this.company));
+    };
+    return Employee;
+}(Person3));
+var sam = new Employee('Sam', 31, 'Microsoft');
+sam.printEmployee();
+/*
+Name: Sam  Year of birth: 1993
+Company: Microsoft
+*/
--- TypeScript/02. OOP/04. Access-Modifiers/app.ts
+++ TypeScript/02. OOP/04. Access-Modifiers/app.ts
@@ -28,3 +28,45 @@ tom.print(); // Name: Tom  Year of birth: 2000
 
 // console.log(tom._name);  // Error!
 // tom.setYear(45);         // Error!
+
+// ---------- protected ----------
+class Person3 {
+  protected name: string;
+  private year: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.year = this.setYear(age);
+  }
+
+  protected printPerson(): void {
+    console.log(`Name: ${this.name}  Year of birth: ${this.year}`);
+  }
+
+  private setYear(age: number): number {
+    return new Date().getFullYear() - age;
+  }
+}
+
+class Employee extends Person3 {
+  protected company: string;
+
+  constructor(name: string, age: number, company: string) {
+    super(name, age);
+    this.company = company;
+  }
+
+  public printEmployee(): void {
+    // console.log("Year: " + this.year);   // field year is inaccessible, because it is private
+    // setYear(25);                         // method setYear is inaccessible, because it is private
+    this.printPerson();                     // method printPerson is accessible, because it is protected
+    console.log(`Company: ${this.company}`);
+  }
+}
+
+let sam = new Employee('Sam', 31, 'Microsoft');
+sam.printEmployee();
+/*
+Name: Sam  Year of birth: 1993
+Company: Microsoft
+*/
