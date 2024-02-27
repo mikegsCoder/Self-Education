ee12d6b63c1f6d6668a200c3db63fe563e789821
mikegsCoder <mikegsid@gmail.com>
2024-02-27 07:03:51 +0200

    Add private

--- TypeScript/02. OOP/04. Access-Modifiers/app.js
+++ TypeScript/02. OOP/04. Access-Modifiers/app.js
@@ -4,3 +4,21 @@ var Person1 = /** @class */ (function () {
     }
     return Person1;
 }());
+// ---------- private ----------
+var Person2 = /** @class */ (function () {
+    function Person2(name, age) {
+        this._name = name;
+        this._year = this.setYear(age);
+    }
+    Person2.prototype.print = function () {
+        console.log("Name: ".concat(this._name, "  Year of birth: ").concat(this._year));
+    };
+    Person2.prototype.setYear = function (age) {
+        return new Date().getFullYear() - age;
+    };
+    return Person2;
+}());
+var tom = new Person2('Tom', 24);
+tom.print(); // Name: Tom  Year of birth: 2000
+// console.log(tom._name);  // Error!
+// tom.setYear(45);         // Error!
--- TypeScript/02. OOP/04. Access-Modifiers/app.ts
+++ TypeScript/02. OOP/04. Access-Modifiers/app.ts
@@ -3,3 +3,28 @@ class Person1 {
   public name: string;
   public year: number;
 }
+
+// ---------- private ----------
+class Person2 {
+  private _name: string;
+  private _year: number;
+
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._year = this.setYear(age);
+  }
+
+  public print(): void {
+    console.log(`Name: ${this._name}  Year of birth: ${this._year}`);
+  }
+
+  private setYear(age: number): number {
+    return new Date().getFullYear() - age;
+  }
+}
+
+let tom = new Person2('Tom', 24);
+tom.print(); // Name: Tom  Year of birth: 2000
+
+// console.log(tom._name);  // Error!
+// tom.setYear(45);         // Error!
