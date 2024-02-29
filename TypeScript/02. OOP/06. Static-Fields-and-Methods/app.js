36d4ce3eb4b031020b5b5b34105ecaa13deb7f23
mikegsCoder <mikegsid@gmail.com>
2024-02-29 07:03:39 +0200

    Add access modifier

--- TypeScript/02. OOP/06. Static-Fields-and-Methods/app.js
+++ TypeScript/02. OOP/06. Static-Fields-and-Methods/app.js
@@ -15,3 +15,15 @@ var tom = new Person1('Tom', 36);
 var years = Person1.calculateYears(36);
 console.log(Person1.retirementAge); // 70
 console.log(years); // 34
+// ---------- access modifiers ----------
+var Person2 = /** @class */ (function () {
+    function Person2(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person2.calculateYears = function (age) {
+        return Person2.retirementAge - age;
+    };
+    Person2.retirementAge = 70;
+    return Person2;
+}());
--- TypeScript/02. OOP/06. Static-Fields-and-Methods/app.ts
+++ TypeScript/02. OOP/06. Static-Fields-and-Methods/app.ts
@@ -21,3 +21,21 @@ let years = Person1.calculateYears(36);
 
 console.log(Person1.retirementAge); // 70
 console.log(years);                 // 34
+
+// ---------- access modifiers ----------
+class Person2 {
+  age: number;
+  name: string;
+
+  private static retirementAge: number = 70;
+
+  static calculateYears(age: number): number {
+    return Person2.retirementAge - age;
+  }
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
