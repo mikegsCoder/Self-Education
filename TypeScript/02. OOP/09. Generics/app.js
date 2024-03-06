bcd93fb150dd229c9e61fc26b09d6753afca7c4f
mikegsCoder <mikegsid@gmail.com>
2024-03-06 07:12:49 +0200

    Add generic array

--- TypeScript/02. OOP/09. Generics/app.js
+++ TypeScript/02. OOP/09. Generics/app.js
@@ -6,3 +6,9 @@ var result1 = getId(5);
 console.log(result1); // 5
 var result2 = getId('abc');
 console.log(result2); // abc
+// ---------- generic array ----------
+function getString(args) {
+    return args.join(', ');
+}
+var result3 = getString([1, 2, 34, 5]);
+console.log(result3); // 1, 2, 34, 5
--- TypeScript/02. OOP/09. Generics/app.ts
+++ TypeScript/02. OOP/09. Generics/app.ts
@@ -8,3 +8,11 @@ console.log(result1);  // 5
 
 let result2 = getId<string>('abc');
 console.log(result2);  // abc
+
+// ---------- generic array ----------
+function getString<T>(args: Array<T>): string {
+  return args.join(', ');
+}
+
+let result3 = getString<number>([1, 2, 34, 5]);
+console.log(result3);  // 1, 2, 34, 5
