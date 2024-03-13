58718de959a80eb0b1f4d8a48c7f840dd9eefa4f
mikegsCoder <mikegsid@gmail.com>
2024-03-13 07:06:48 +0200

    Add namespaces example

--- TypeScript/03. Modules-and-Namespaces/01. Namespaces/app.js
+++ TypeScript/03. Modules-and-Namespaces/01. Namespaces/app.js
@@ -11,3 +11,24 @@ var Personnel1;
 })(Personnel1 || (Personnel1 = {}));
 var alice = new Personnel1.Employee1('Alice');
 console.log(alice.name); // Alice
+// ---------- namespaces example ---------
+var Personnel2;
+(function (Personnel2) {
+    var Employee2 = /** @class */ (function () {
+        function Employee2(name) {
+            this.name = name;
+        }
+        return Employee2;
+    }());
+    Personnel2.Employee2 = Employee2;
+    function work(emp) {
+        console.log(emp.name, 'is working');
+    }
+    Personnel2.work = work;
+    Personnel2.defaultUser = { name: 'Kate' };
+    Personnel2.value = 'Hello';
+})(Personnel2 || (Personnel2 = {}));
+var tom = new Personnel2.Employee2('Tom');
+Personnel2.work(tom); // Tom is working
+console.log(Personnel2.defaultUser.name); // Kate
+console.log(Personnel2.value); // Hello
--- TypeScript/03. Modules-and-Namespaces/01. Namespaces/app.ts
+++ TypeScript/03. Modules-and-Namespaces/01. Namespaces/app.ts
@@ -8,3 +8,29 @@ namespace Personnel1 {
 
 let alice = new Personnel1.Employee1('Alice');
 console.log(alice.name);    // Alice
+
+// ---------- namespaces example ---------
+namespace Personnel2 {
+  export interface IUser {
+    displayInfo(): void;
+  }
+
+  export class Employee2 {
+    constructor(public name: string) {
+    }
+  }
+
+  export function work(emp: Employee2): void {
+    console.log(emp.name, 'is working');
+  }
+
+  export let defaultUser = { name: 'Kate' };
+
+  export let value = 'Hello';
+}
+
+let tom = new Personnel2.Employee2('Tom');
+Personnel2.work(tom);  // Tom is working
+
+console.log(Personnel2.defaultUser.name);  // Kate
+console.log(Personnel2.value);             // Hello
