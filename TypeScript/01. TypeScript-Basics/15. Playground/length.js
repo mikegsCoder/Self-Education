b122fd2444fa898ad64ae83e9eeb0398a6b1f21a
mikegsCoder <mikegsid@gmail.com>
2024-03-17 09:58:52 +0200

    Removed js files

--- TypeScript/01. TypeScript-Basics/15. Playground/length.js
+++
@@ -1,6 +0,0 @@
-// Write a simple function that calculates the length of the string.
-// ---------- solution: ----------
-function stringLength(str) {
-    console.log("The length is ".concat(str.length, " symbols."));
-}
-stringLength('Hello TypeScript!'); // The length is 17 symbols.
