c35b927166a8249882dfdbe0c9cbe4fd2af436ff
mikegsCoder <mikegsid@gmail.com>
2024-03-21 07:10:05 +0200

    Add class decorator example

--- TypeScript/05. Decorators/01. Class-Decorators/app.ts
+++ TypeScript/05. Decorators/01. Class-Decorators/app.ts
@@ -17,3 +17,39 @@ class User1 {
     console.log(this.name);
   }
 }
+
+// ---------- class decorator example ----------
+function logger<TFunction extends Function>(target: TFunction): TFunction {
+  let newConstructor: Function = function (name: string) {
+    console.log('Creating new instance');
+    this.name = name;
+    this.age = 23;
+    this.print = function (): void {
+      console.log(this.name, this.age);
+    }
+  }
+
+  return <TFunction>newConstructor;
+}
+
+@logger
+class User2 {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  print(): void {
+    console.log(this.name);
+  }
+}
+
+let tom = new User2('Tom');
+let bob = new User2('Bob');
+tom.print();
+bob.print();
+// Creating new instance
+// Creating new instance
+// Tom 23
+// Bob 23
