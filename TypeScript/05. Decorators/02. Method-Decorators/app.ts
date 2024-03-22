8dac5d6650c99ca6661dd02b992f213916c9e6f0
mikegsCoder <mikegsid@gmail.com>
2024-03-22 07:19:58 +0200

    Add Method parameters decorators

--- TypeScript/05. Decorators/02. Method-Decorators/app.ts
+++ TypeScript/05. Decorators/02. Method-Decorators/app.ts
@@ -67,3 +67,68 @@ class Calculator {
 let calc = new Calculator();
 let z = calc.add(4, 5);
 z = calc.add(6, 7);
+
+// ---------- method parameters decorators ----------
+function logParameter(target: any, key: string, index: number) {
+  var metadataKey = `__log_${key}_parameters`;
+
+  if (Array.isArray(target[metadataKey])) {
+    target[metadataKey].push(index);
+  }
+  else {
+    target[metadataKey] = [index];
+  }
+}
+
+function logMethod(target: any, key: string, descriptor: PropertyDescriptor) {
+  var originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    var metadataKey = `__log_${key}_parameters`;
+    var indices = target[metadataKey];
+
+    if (Array.isArray(indices)) {
+      for (var i = 0; i < args.length; i++) {
+        if (indices.indexOf(i) !== -1) {
+          var arg = args[i];
+          var argStr = JSON.stringify(arg) || arg.toString();
+          console.log(`${key} arg[${i}]: ${argStr}`);
+        }
+      }
+
+      var result = originalMethod.apply(this, args);
+      return result;
+    }
+    else {
+      var a = args.map(a => (JSON.stringify(a) || a.toString())).join();
+      var result = originalMethod.apply(this, args);
+      var r = JSON.stringify(result);
+      console.log(`Call: ${key}(${a}) => ${r}`);
+
+      return result;
+    }
+  }
+
+  return descriptor;
+}
+
+class User2 {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  @logMethod
+  setName(@logParameter name: string) {
+    this.name = name;
+  }
+
+  print(): void {
+    console.log(this.name);
+  }
+}
+
+let bob = new User2('Bob');
+bob.setName('John');
+bob.setName('Sam');
