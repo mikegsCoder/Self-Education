2a1257c0208d4f6cccaf2458ff56a8de1e11bbda
mikegsCoder <mikegsid@gmail.com>
2024-03-22 07:11:09 +0200

    Add method decorator example

--- TypeScript/05. Decorators/02. Method-Decorators/app.ts
+++ TypeScript/05. Decorators/02. Method-Decorators/app.ts
@@ -14,3 +14,28 @@ interface PropertyDescriptor {
   get?(): any;
   set?(v: any): void;
 }
+
+// ---------- method decorator example ----------
+function readable(onlyRead: boolean) {
+  return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.writable = !onlyRead;
+  };
+}
+
+class User1 {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  @readable(true)
+  print(): void {
+    console.log(this.name);
+  }
+}
+
+let tom = new User1('Tom');
+tom.print = function () {
+  console.log('print has been changed');
+}
+tom.print();  // Tom
