cc5196b93577a7f068ee760ee6da2cacc0062073
mikegsCoder <mikegsid@gmail.com>
2024-03-22 07:14:14 +0200

    Add method parameters and return type

--- TypeScript/05. Decorators/02. Method-Decorators/app.ts
+++ TypeScript/05. Decorators/02. Method-Decorators/app.ts
@@ -39,3 +39,31 @@ tom.print = function () {
   console.log('print has been changed');
 }
 tom.print();  // Tom
+
+// ---------- method parameters and return type ----------
+function log(
+  target: Object,
+  method: string,
+  descriptor: PropertyDescriptor) {
+  let originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: number[]) {
+    console.log(JSON.stringify(args));
+
+    let returnValue = originalMethod.apply(this, args);
+    console.log(`${JSON.stringify(args)} => ${returnValue}`)
+
+    return returnValue;
+  }
+}
+
+class Calculator {
+  @log
+  add(x: number, y: number): number {
+    return x + y;
+  }
+}
+
+let calc = new Calculator();
+let z = calc.add(4, 5);
+z = calc.add(6, 7);
