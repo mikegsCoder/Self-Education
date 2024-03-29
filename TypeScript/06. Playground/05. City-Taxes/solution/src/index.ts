dcf2c9352aed63dfa66667c1c08d60430d600cd9
mikegsCoder <mikegsid@gmail.com>
2024-03-29 07:24:06 +0200

    Add function cityTaxes

--- TypeScript/06. Playground/05. City-Taxes/solution/src/index.ts
+++ TypeScript/06. Playground/05. City-Taxes/solution/src/index.ts
@@ -7,3 +7,23 @@ type City = {
   applyGrowth: (percentage: number) => void;
   applyRecession: (percentage: number) => void;
 };
+
+function cityTaxes(name: string, population: number, treasury: number): City {
+  const city: City = {
+    name,
+    population,
+    treasury,
+    taxRate: 10,
+    collectTaxes: function () {
+      this.treasury += Math.floor(this.population * this.taxRate);
+    },
+    applyGrowth: function (percentage: number) {
+      this.population = Math.floor(this.population * (1 + percentage / 100));
+    },
+    applyRecession: function (percentage: number) {
+      this.treasury -= Math.floor((this.treasury * percentage) / 100);
+    },
+  };
+
+  return city;
+}
