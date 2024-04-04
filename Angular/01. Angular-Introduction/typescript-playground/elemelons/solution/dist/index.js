8b965e5efbc0ee316c5b87fe5b8b260f73c4b0e5
mikegsCoder <mikegsid@gmail.com>
2024-04-04 07:34:02 +0300

    Add Melon class

---
+++ Angular/01. Angular-Introduction/typescript-playground/elemelons/solution/dist/index.js
@@ -0,0 +1,15 @@
+var Melon = (function () {
+    function Melon(weight, melonSort) {
+        this.weight = weight;
+        this.melonSort = melonSort;
+    }
+    Object.defineProperty(Melon.prototype, "elementIndex", {
+        get: function () {
+            return this.weight * this.melonSort.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Melon;
+}());
+//# sourceMappingURL=index.js.map
