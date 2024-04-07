a01332a160f3d292c2fa7a984d1e0ce63b9f1426
mikegsCoder <mikegsid@gmail.com>
2024-04-07 10:29:11 +0300

    Add method examples including generics

--- Angular/01. Angular-Introduction/demo/dist/index.js
+++ Angular/01. Angular-Introduction/demo/dist/index.js
@@ -1,8 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("underscore");
 var User = (function () {
     function User(name, age) {
         this.name = name;
         this.age = age;
     }
+    User.prototype.returnInfo = function () {
+        return {};
+    };
+    User.prototype.doSomething = function (obj) {
+        return _.range(0, 10, 1);
+    };
+    User.prototype.doSomethingElse = function (obj) {
+        return obj;
+    };
     return User;
 }());
 //# sourceMappingURL=index.js.map
--- Angular/01. Angular-Introduction/demo/src/index.ts
+++ Angular/01. Angular-Introduction/demo/src/index.ts
@@ -1,3 +1,5 @@
+import * as _ from 'underscore';
+
 interface IMyObj {
   name: string;
   age: number;
@@ -9,11 +11,25 @@ interface IMyObj2 extends IMyObj {
 }
 
 class User {
-  name: string;
-  age: number;
+  // name: string;
+  // age: number;
   
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  // constructor(name: string, age: number) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  constructor(public name: string, public age: number){}
+
+  returnInfo() {
+    return {};
+  }
+
+  doSomething(obj: IMyObj) {
+    return _.range(0, 10, 1);
+  }
+
+  doSomethingElse<T extends IMyObj>(obj: T): T {
+    return obj;
   }
 }
