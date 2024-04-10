6e637354afafcee8b1f073bba2e97e27b6f07113
mikegsCoder <mikegsid@gmail.com>
2024-04-10 07:18:38 +0300

    Add first-app

---
+++ Angular/02. Angular-Components-and-Data-Binding/first-app/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+const users = [
+    {
+        firstName: 'Peter',
+        lastName: 'Petrov',
+        age: '30'
+    },
+    {
+        firstName: 'George',
+        lastName: 'Smith',
+        age: '35'
+    },
+    {
+        firstName: 'Ana',
+        lastName: 'Nikol',
+        age: '32'
+    }
+];
+
+@Component({
+  selector: 'app-root',
+//   template: `<div class="welcome-message">HELLO WORLD</div>`,
+//   styles:[
+//     `.welcome-message {color: red;}`
+//   ]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  welcomeMessage = 'Hello!';
+
+  users = users;
+
+  constructor() {
+    
+  }
+
+  nameInputBtnClickHandler(data: { inputEl: HTMLInputElement }): void {
+    console.log(data.inputEl.value);
+  }
+}
