ca6a7d76f386167b2d32a189690c54042206f488
mikegsCoder <mikegsid@gmail.com>
2024-04-10 07:30:43 +0300

    Add examples

--- Angular/02. Angular-Components-and-Data-Binding/first-app/src/app/app.component.ts
+++ Angular/02. Angular-Components-and-Data-Binding/first-app/src/app/app.component.ts
@@ -1,38 +1,49 @@
 import { Component } from '@angular/core';
 
 const users = [
-  {
-    firstName: 'Peter',
-    lastName: 'Petrov',
-    age: '30',
-  },
-  {
-    firstName: 'George',
-    lastName: 'Smith',
-    age: '35',
-  },
-  {
-    firstName: 'Ana',
-    lastName: 'Nikol',
-    age: '32',
-  },
+    {
+        firstName: 'Peter',
+        lastName: 'Petrov',
+        age: '30'
+    },
+    {
+        firstName: 'George',
+        lastName: 'Smith',
+        age: '35'
+    },
+    {
+        firstName: 'Ana',
+        lastName: 'Nikol',
+        age: '32'
+    }
 ];
 
 @Component({
   selector: 'app-root',
-  //   template: `<div class="welcome-message">HELLO WORLD</div>`,
-  //   styles:[
-  //     `.welcome-message {color: red;}`
-  //   ]
+//   template: `<div class="welcome-message">HELLO WORLD</div>`,
+//   styles:[
+//     `.welcome-message {color: red;}`
+//   ]
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   welcomeMessage = 'Hello!';
+
+  nameInputValue = 'TEST TEST';
+
   users = users;
 
+  isVisible = false;
+
   constructor() {
-    
+    setTimeout(() => {
+        this.nameInputValue = 'BEST!';
+    }, 4000);
+  }
+
+  toggleHandler() : void {
+    this.isVisible = !this.isVisible;
   }
 
   nameInputBtnClickHandler(data: { inputEl: HTMLInputElement }): void {
