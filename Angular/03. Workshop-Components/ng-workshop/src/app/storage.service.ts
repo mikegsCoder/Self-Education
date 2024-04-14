fe58268eca675f1c7b072f34ec5a3b2bf4c29997
mikegsCoder <mikegsid@gmail.com>
2024-04-14 09:11:25 +0300

    Add ServerStorage

--- Angular/03. Workshop-Components/ng-workshop/src/app/storage.service.ts
+++ Angular/03. Workshop-Components/ng-workshop/src/app/storage.service.ts
@@ -38,3 +38,38 @@ export class BrowserStorage {
     return item;
   }
 }
+
+export class ServerStorage {
+  localStorage = {
+    data: {},
+    setItem<T>(key: string, item: T): void {
+      this.data[key] = item;
+    },
+    getItem<T>(key: string): T {
+      return this.data[key];
+    },
+  };
+
+  setItem<T>(key: string, item: T): T {
+    this.localStorage.setItem(key, JSON.stringify(item));
+
+    return item;
+  }
+
+  getItem<T>(key: string): T {
+    let item;
+    const tmp = this.localStorage.getItem(key) as any;
+
+    if (!tmp) {
+      return null;
+    }
+
+    try {
+      item = JSON.parse(tmp);
+    } catch {
+      item = tmp;
+    }
+
+    return item;
+  }
+}
