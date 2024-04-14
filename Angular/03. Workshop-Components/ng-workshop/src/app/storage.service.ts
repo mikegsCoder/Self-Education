e976b6c69fd13d40e9055f6b45cde2a0cb0ed221
mikegsCoder <mikegsid@gmail.com>
2024-04-14 09:10:03 +0300

    Add BrowserStorage

--- Angular/03. Workshop-Components/ng-workshop/src/app/storage.service.ts
+++ Angular/03. Workshop-Components/ng-workshop/src/app/storage.service.ts
@@ -11,3 +11,30 @@ export class StorageService implements IStorage {
     return null;
   }
 }
+
+export class BrowserStorage {
+  localStorage = localStorage;
+
+  setItem<T>(key: string, item: T): T {
+    const str = typeof item === 'string' ? item : JSON.stringify(item);
+    this.localStorage.setItem(key, str);
+    return item;
+  }
+
+  getItem<T>(key: string): T {
+    let item;
+    const tmp = this.localStorage.getItem(key);
+
+    if (!tmp) {
+      return null;
+    }
+
+    try {
+      item = JSON.parse(tmp || '');
+    } catch {
+      item = tmp;
+    }
+
+    return item;
+  }
+}
