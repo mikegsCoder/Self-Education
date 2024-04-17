9664e3278925b7b703472670f4a2c380b389fd99
mikegsCoder <mikegsid@gmail.com>
2024-04-17 06:14:48 +0300

    Add aside component

--- Angular/03. Workshop-Components/ng-workshop/src/app/app.module.ts
+++ Angular/03. Workshop-Components/ng-workshop/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { AsideComponent } from './aside/aside.component';
 import { storageServiceProvider } from './storage.service';
 import { UserService } from './user.service';
 import { ThemeService } from './theme.service';
@@ -19,6 +20,7 @@ import { PostService } from './post.service';
     FooterComponent,
     ThemeListComponent,
     ThemeListItemComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule, 
---
+++ Angular/03. Workshop-Components/ng-workshop/src/app/aside/aside.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { IPost } from '../interfaces/post';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.css'],
+})
+export class AsideComponent implements OnInit {
+  postList: IPost[];
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.loadPostList(5).subscribe((postList) => {
+      this.postList = postList;
+    });
+  }
+}
