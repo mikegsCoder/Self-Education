28ae5706175fbda7ea8684da00ff2116f8df794e
mikegsCoder <mikegsid@gmail.com>
2024-04-18 08:35:53 +0300

    Add core module with navigation component

---
+++ Angular/04. Modules-and-Routing/app-with-router/src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavigationComponent
+  ]
+})
+export class CoreModule { }
