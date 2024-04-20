0e82ea9491c186f902e4870924e2173992190141
mikegsCoder <mikegsid@gmail.com>
2024-04-20 09:15:50 +0300

    Add About component

--- Angular/04. Modules-and-Routing/app-with-router/src/app/app.module.ts
+++ Angular/04. Modules-and-Routing/app-with-router/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 import { AccessGuard } from './guards/access.guard';
 import { TestResolver } from './test.resolver';
@@ -15,6 +16,7 @@ import { TestResolver } from './test.resolver';
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
