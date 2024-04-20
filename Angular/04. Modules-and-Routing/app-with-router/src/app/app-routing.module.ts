b6782b827ce1dacbedd6c4885e7b2a0dbf6cdd21
mikegsCoder <mikegsid@gmail.com>
2024-04-20 09:20:06 +0300

    Add Access guard for About component route

--- Angular/04. Modules-and-Routing/app-with-router/src/app/app-routing.module.ts
+++ Angular/04. Modules-and-Routing/app-with-router/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TestResolver } from './test.resolver';
+import { AccessGuard } from './guards/access.guard';
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+    canActivate: [AccessGuard],
     data: {
       isLogged: true,
     },
