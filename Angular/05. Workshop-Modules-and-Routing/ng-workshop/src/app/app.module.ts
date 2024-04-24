c52cd1258434a642d46fa48a3f95de98c962893f
mikegsCoder <mikegsid@gmail.com>
2024-04-24 10:33:49 +0300

    Add Core module

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/app.module.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/app.module.ts
@@ -3,31 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 import { AsideComponent } from './shared/aside/aside.component';
-import { storageServiceProvider } from './storage.service';
 import { UserService } from './user.service';
 import { ThemeService } from './theme.service';
 import { PostService } from './post.service';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     ThemeListComponent,
     ThemeListItemComponent,
     AsideComponent
   ],
   imports: [
     BrowserModule, 
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
   ],
   providers: [
-    storageServiceProvider, 
     UserService, 
     ThemeService, 
     PostService,
