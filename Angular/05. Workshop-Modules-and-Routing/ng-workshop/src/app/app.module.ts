ba6583045a409c458dc99e5e5cf42f513755eb5e
mikegsCoder <mikegsid@gmail.com>
2024-04-24 10:47:14 +0300

    Add Home component

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/app.module.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 import { AsideComponent } from './shared/aside/aside.component';
+
+import { CoreModule } from './core/core.module';
+
 import { UserService } from './user.service';
 import { ThemeService } from './theme.service';
 import { PostService } from './post.service';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     ThemeListComponent,
     ThemeListItemComponent,
     AsideComponent
