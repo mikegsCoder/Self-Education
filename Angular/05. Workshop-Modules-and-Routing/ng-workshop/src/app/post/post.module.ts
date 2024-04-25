8240935e28091d0302f6ed705a807d9ad2540a8d
mikegsCoder <mikegsid@gmail.com>
2024-04-25 10:44:38 +0300

    Add post-list component

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/post/post.module.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/post/post.module.ts
@@ -1,15 +1,18 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PostListComponent } from './post-list/post-list.component';
 import { PostService } from './post.service';
 
 @NgModule({
   declarations: [
+    PostListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    PostListComponent
   ]
 })
 export class PostModule {
