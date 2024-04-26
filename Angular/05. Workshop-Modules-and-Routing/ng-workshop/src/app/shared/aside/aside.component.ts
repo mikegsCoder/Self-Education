fed63be12e1a1a3ccda9addf7e9b705d285697b3
mikegsCoder <mikegsid@gmail.com>
2024-04-26 11:22:13 +0300

    Fixed aside and post list

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/shared/aside/aside.component.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/shared/aside/aside.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from '../../post.service';
-import { IPost } from '../interfaces/post';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css'],
 })
-export class AsideComponent implements OnInit {
-  postList: IPost[];
-
-  constructor(private postService: PostService) {}
-
-  ngOnInit(): void {
-    this.postService.loadPostList(5).subscribe((postList) => {
-      this.postList = postList;
-    });
-  }
+export class AsideComponent {
+  @Input() title: string;
 }
