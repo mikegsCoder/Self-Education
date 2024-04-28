1a6d6690ef9b1856044c1a2d9e85e8181bc2793b
mikegsCoder <mikegsid@gmail.com>
2024-04-28 10:21:34 +0300

    Add theme routing module

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme-list-item/theme-list-item.component.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme-list-item/theme-list-item.component.ts
@@ -12,7 +12,5 @@ export class ThemeListItemComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme.module.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ThemeService } from './theme.service';
 import { SharedModule } from '../shared/shared.module';
 import { PostModule } from '../post/post.module';
+import { ThemeRouterModule } from './theme-routing.module';
 
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 
@@ -15,6 +16,7 @@ import { ThemeListItemComponent } from './theme-list-item/theme-list-item.compon
     CommonModule,
     SharedModule,
     PostModule,
+    ThemeRouterModule
   ],
   providers: [
     ThemeService
