f0370fba29731951aa7d5583936970ac4c08de12
mikegsCoder <mikegsid@gmail.com>
2024-04-29 07:29:40 +0300

    Add route for theme component

--- Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme-routing.module.ts
+++ Angular/05. Workshop-Modules-and-Routing/ng-workshop/src/app/theme/theme-routing.module.ts
@@ -1,6 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
+import { ThemeComponent } from './theme/theme.component';
 
 const routes: Routes = [
+  {
+    path: 'theme',
+    pathMatch: 'full',
+    component: ThemeComponent,
+    data: {
+      title: 'THEME'
+    }
+  },
 ];
+
 
 export const ThemeRouterModule = RouterModule.forChild(routes);
