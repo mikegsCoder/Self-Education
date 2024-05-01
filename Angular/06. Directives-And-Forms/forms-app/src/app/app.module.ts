6e2ae7c4472629e2ea397496517192147ed8031c
mikegsCoder <mikegsid@gmail.com>
2024-05-01 10:36:14 +0300

    Add home component

--- Angular/06. Directives-And-Forms/forms-app/src/app/app.module.ts
+++ Angular/06. Directives-And-Forms/forms-app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
