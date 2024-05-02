61674e97b82d83e88fb5547b50f204c4c586d4b6
mikegsCoder <mikegsid@gmail.com>
2024-05-02 10:24:36 +0300

    Add route for login component

--- Angular/06. Directives-And-Forms/forms-app/src/app/app-routing.module.ts
+++ Angular/06. Directives-And-Forms/forms-app/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
