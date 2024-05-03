f737bacd2c21a45983b7bdfe307ba28a58c40e25
mikegsCoder <mikegsid@gmail.com>
2024-05-03 10:23:46 +0300

    Add route for register component

--- Angular/06. Directives-And-Forms/forms-app/src/app/app-routing.module.ts
+++ Angular/06. Directives-And-Forms/forms-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
