a0871dc6fe9d931ba8d11ba4faf7e305d621e8ba
mikegsCoder <mikegsid@gmail.com>
2024-05-04 10:54:00 +0300

    Add email-validator directive

--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/shared/shared.module.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsideComponent } from './aside/aside.component';
 import { LoaderComponent } from './loader/loader.component';
+import { EmailValidatorDirective } from './email-validator.directive';
 
 @NgModule({
   declarations: [
     AsideComponent,
-    LoaderComponent
+    LoaderComponent,
+    EmailValidatorDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
     AsideComponent,
-    LoaderComponent
+    LoaderComponent,
+    EmailValidatorDirective
   ]
 })
 export class SharedModule { }
