200abb30e474e651a0e4175ce83940de8c2511c5
mikegsCoder <mikegsid@gmail.com>
2024-05-04 10:50:29 +0300

    Add rePasswordValidatorFactory

--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/shared/validators.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/shared/validators.ts
@@ -6,3 +6,10 @@ export function emailValidator(control: AbstractControl): ValidationErrors | nul
   const isValidEmail = /^[a-zA-Z0-9\.-]{6,}@\w+\.(com|bg)$/.test(value);
   return isValidEmail ? null : { emailValidator: true };
 }
+
+export function rePasswordValidatorFactory(targetControl: AbstractControl): ValidatorFn {
+  return function rePasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const areTheSame = targetControl.value === control.value;
+    return areTheSame ? null : { rePasswordValidator: true };
+  };
+}
