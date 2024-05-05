6e80105aadfcb8b38f7f48c45849e377313b0178
mikegsCoder <mikegsid@gmail.com>
2024-05-05 10:37:28 +0300

    Add register form validation

--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/register/register.component.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/register/register.component.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { emailValidator, rePasswordValidatorFactory } from 'src/app/shared/validators';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: ['../../../form-styles.css', './register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+  isLoading = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    const passwordControl = this.fb.control('', [Validators.required, Validators.minLength(4)]);
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, emailValidator]],
+      tel: [''],
+      password: passwordControl,
+      rePassword: ['', [Validators.required, Validators.minLength(5), rePasswordValidatorFactory(passwordControl)]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  submitHandler(): void {
+    const data = this.form.value;
+    this.isLoading = true;
+    this.userService.register(data).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.isLoading = true;
+        console.error(err);
+      }
+    });
+  }
 }
--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/user.module.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
@@ -19,7 +20,9 @@ import { UserService } from './user.service';
   imports: [
     CommonModule,
     SharedModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     UserService
