c9cfdffa2c16a1100c617cfb64f4bd6fd9f45310
mikegsCoder <mikegsid@gmail.com>
2024-05-06 07:08:58 +0300

    Add login form validations

--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/login/login.component.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/user/login/login.component.ts
@@ -5,55 +5,25 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css', '../../../form-styles.css']
 })
 export class LoginComponent implements OnInit {
+
   isLoading = false;
-  errorMessage: string = null;
+  errorMessage = '';
 
-  form = {
-    email: {
-      touched: false,
-      value: '',
-    },
-    password: {
-      touched: false,
-      value: '',
-    },
-  };
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
-  get showEmailError(): boolean {
-    return this.form.email.touched && this.form.email.value.length === 0;
+  ngOnInit(): void {
   }
 
-  get showPasswordError(): boolean {
-    return this.form.password.touched && this.form.password.value.length === 0;
-  }
-
-  get hasFormErrors(): boolean {
-    return (
-      this.form.email.value.length === 0 ||
-      this.form.password.value.length === 0
-    );
-  }
-
-  constructor(private userService: UserService, private router: Router) {}
-
-  ngOnInit(): void {}
-
-  updateInputValue(name: 'email' | 'password', value: string): void {
-    this.form[name].touched = true;
-    this.form[name].value = value;
-  }
-
-  submitFormHandler(): void {
-    const {
-      email: { value: email },
-      password: { value: password },
-    } = this.form;
+  submitFormHandler(formValue: { email: string, password: string }): void {
     this.isLoading = true;
     this.errorMessage = '';
-    this.userService.login({ email, password }).subscribe({
+    this.userService.login(formValue).subscribe({
       next: (data) => {
         this.isLoading = false;
         this.router.navigate(['/']);
@@ -61,7 +31,7 @@ export class LoginComponent implements OnInit {
       error: (err) => {
         this.errorMessage = 'ERROR!';
         this.isLoading = false;
-      },
+      }
     });
   }
 }
