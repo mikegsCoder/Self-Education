abf83f0bb29342ff14a46af760169e5abc4fcf9c
mikegsCoder <mikegsid@gmail.com>
2024-05-07 07:35:19 +0300

    Add AuthGuard for routes in theme routing module

--- Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/theme/theme-routing.module.ts
+++ Angular/07. Workshop-Directives-and-Forms/ng-workshop/src/app/theme/theme-routing.module.ts
@@ -1,33 +1,40 @@
 import { Routes, RouterModule } from '@angular/router';
-
-import { ThemeComponent } from './theme/theme.component';
-import { NewComponent } from './new/new.component';
 import { DetailComponent } from './detail/detail.component';
+import { NewComponent } from './new/new.component';
+import { ThemeComponent } from './theme/theme.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'theme',
-    pathMatch: 'full',
-    component: ThemeComponent,
-    data: {
-      title: 'THEME'
-    }
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ThemeComponent,
+        data: {
+          title: 'THEME',
+        },
+      },
+      {
+        path: 'new',
+        component: NewComponent,
+        data: {
+          title: 'NEW THEME',
+          isLogged: true,
+        },
+      },
+      {
+        path: 'detail/:id',
+        component: DetailComponent,
+        data: {
+          title: 'THEME DETAIL',
+          isLogged: true,
+        },
+      },
+    ],
   },
-  {
-    path: 'theme/new',
-    component: NewComponent,
-    data: {
-      title: 'NEW THEME'
-    }
-  },
-  {
-    path: 'theme/detail/:id',
-    component: DetailComponent,
-    data: {
-      title: 'THEME DETAIL'
-    }
-  }
 ];
-
 
 export const ThemeRouterModule = RouterModule.forChild(routes);
