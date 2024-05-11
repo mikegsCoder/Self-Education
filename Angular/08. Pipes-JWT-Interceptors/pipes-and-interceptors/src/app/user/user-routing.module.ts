6a1118f3d0f5bd5e97ea45c447cb0b2478d6e95f
mikegsCoder <mikegsid@gmail.com>
2024-05-11 10:30:09 +0300

    Add detail component

--- Angular/08. Pipes-JWT-Interceptors/pipes-and-interceptors/src/app/user/user-routing.module.ts
+++ Angular/08. Pipes-JWT-Interceptors/pipes-and-interceptors/src/app/user/user-routing.module.ts
@@ -2,6 +2,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { UserListResolver } from './user-list.resolver';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'list',
     resolve: [UserListResolver],
     component: ListComponent
+  },
+  {
+    path: 'detail/:id',
+    component: DetailComponent
   }
 ];
 
--- Angular/08. Pipes-JWT-Interceptors/pipes-and-interceptors/src/app/user/user.module.ts
+++ Angular/08. Pipes-JWT-Interceptors/pipes-and-interceptors/src/app/user/user.module.ts
@@ -4,14 +4,11 @@ import { ListComponent } from './list/list.component';
 import { SharedModule } from '../shared/shared.module';
 import { UserListResolver } from './user-list.resolver';
 import { UserRoutingModule } from './user-routing.module';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [ListComponent, DetailComponent],
   providers: [UserListResolver],
-  imports: [
-    CommonModule,
-    UserRoutingModule,
-    SharedModule
-  ]
+  imports: [CommonModule, UserRoutingModule, SharedModule],
 })
-export class UserModule { }
+export class UserModule {}
