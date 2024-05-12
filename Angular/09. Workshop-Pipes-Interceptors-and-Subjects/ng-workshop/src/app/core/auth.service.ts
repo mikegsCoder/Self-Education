86241d52005664ddb18a89af074a3374849706b6
mikegsCoder <mikegsid@gmail.com>
2024-05-12 10:58:16 +0300

    Add auth service

---
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/core/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IUser } from '../shared/interfaces';
+
+@Injectable()
+export class AuthService {
+
+  private _currentUser: BehaviorSubject<IUser | null> = new BehaviorSubject(undefined);
+  currentUser$ = this._currentUser.asObservable();
+  isLogged$ = this.currentUser$.pipe(map(user => !!user));
+  isReady$ = this.currentUser$.pipe(map(user => user !== undefined));
+
+  constructor(private http: HttpClient) { }
+
+  updateCurrentUser(user: IUser | null): void {
+    this._currentUser.next(user);
+  }
+}
