69a5c036b54a4e684ee784f2943db7bc256c60e3
mikegsCoder <mikegsid@gmail.com>
2024-05-14 07:34:48 +0300

    Add routes for user and theme module

--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/app-routing.module.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,16 @@ const routes: Routes = [
     data: {
       title: 'HOME'
     }
+  },
+  {
+    path: 'user',
+    canActivateChild: [AuthGuard],
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'theme',
+    canActivateChild: [AuthGuard],
+    loadChildren: () => import('./theme/theme.module').then(m => m.ThemeModule)
   },
   {
     path: '**',
