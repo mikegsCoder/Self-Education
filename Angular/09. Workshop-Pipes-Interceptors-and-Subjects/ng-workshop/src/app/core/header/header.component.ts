469a449994d7ade5ab5fbd21163b69741d5487ce
mikegsCoder <mikegsid@gmail.com>
2024-05-15 07:36:20 +0300

    Fixed header component

--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/core/header/header.component.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/core/header/header.component.ts
@@ -5,24 +5,19 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnDestroy {
-
-  hideNavigation = false;
-
   isLogged$ = this.authService.isLogged$;
   isReady$ = this.authService.isReady$;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   logoutHandler(): void {
-    this.authService.logout().subscribe(() => this.router.navigate(['/user/login']));
+    this.authService
+      .logout()
+      .subscribe(() => this.router.navigate(['/user/login']));
   }
 
-  ngOnDestroy(): void {
-  }
+  ngOnDestroy(): void {}
 }
