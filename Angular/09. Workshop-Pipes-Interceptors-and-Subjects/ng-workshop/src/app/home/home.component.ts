89322503de368aac35978c2df48065bc07738681
mikegsCoder <mikegsid@gmail.com>
2024-05-15 07:19:32 +0300

    Updated Home component

--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/home/home.component.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/home/home.component.ts
@@ -1,28 +1,23 @@
 import { Component, DoCheck } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { UserService } from '../user/user.service';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements DoCheck {
-  isLogged = false;
+export class HomeComponent {
+  isLogged$ = this.authService.isLogged$;
 
   url: string;
 
-  constructor(private userService: UserService, router: Router) {
+  constructor(private authService: AuthService, router: Router) {
     router.events
       .pipe(filter((e) => e instanceof NavigationEnd))
       .subscribe((e: NavigationEnd) => {
-        this.url = e.url; // get current url
-        //console.log(this.url);
+        this.url = e.url;
       });
-  }
-
-  ngDoCheck(): void {
-    this.isLogged = this.userService.isLogged;
   }
 }
