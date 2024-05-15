7d3d478d8c2a46bc7d382cbac4d956a20efe78f5
mikegsCoder <mikegsid@gmail.com>
2024-05-15 07:30:06 +0300

    Updated login, register and profile components

--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/user/login/login.component.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/user/login/login.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from '../user.service';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css', '../../../form-styles.css']
+  styleUrls: ['../../../form-styles.css', './login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   isLoading = false;
   errorMessage = '';
 
-  constructor(
-    private userService: UserService,
-    private router: Router
-  ) { }
+  constructor(private userService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeHandler(data: any): void {
+    console.log(data);
   }
 
-  submitFormHandler(formValue: { email: string, password: string }): void {
+  submitFormHandler(formValue: { email: string; password: string }): void {
     this.isLoading = true;
     this.errorMessage = '';
     this.userService.login(formValue).subscribe({
@@ -29,9 +28,9 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/']);
       },
       error: (err) => {
-        this.errorMessage = 'ERROR!';
+        this.errorMessage = err.error.message;
         this.isLoading = false;
-      }
+      },
     });
   }
 }
--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/user/register/register.component.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/user/register/register.component.ts
@@ -1,48 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { emailValidator, rePasswordValidatorFactory } from 'src/app/shared/validators';
-import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
+import {
+  emailValidator,
+  rePasswordValidatorFactory,
+} from 'src/app/shared/validators';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['../../../form-styles.css', './register.component.css']
+  styleUrls: ['../../../form-styles.css', './register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+
   isLoading = false;
 
   constructor(
     private fb: FormBuilder,
-    private userService: UserService,
+    private authService: AuthService,
     private router: Router
   ) {
-    const passwordControl = this.fb.control('', [Validators.required, Validators.minLength(4)]);
+    const passwordControl = this.fb.control('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]);
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(5)]],
       email: ['', [Validators.required, emailValidator]],
       tel: [''],
       password: passwordControl,
-      rePassword: ['', [Validators.required, Validators.minLength(5), rePasswordValidatorFactory(passwordControl)]]
+      rePassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          rePasswordValidatorFactory(passwordControl),
+        ],
+      ],
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   submitHandler(): void {
     const data = this.form.value;
     this.isLoading = true;
-    this.userService.register(data).subscribe({
+
+    this.authService.register(data).subscribe({
       next: () => {
         this.isLoading = false;
         this.router.navigate(['/']);
       },
       error: (err) => {
-        this.isLoading = true;
+        this.isLoading = false;
         console.error(err);
-      }
+      },
     });
   }
 }
