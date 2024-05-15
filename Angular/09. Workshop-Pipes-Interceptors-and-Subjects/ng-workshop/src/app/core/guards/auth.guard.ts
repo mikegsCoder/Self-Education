119c67773be010e4b1366c6c707c5de5f2e82de9
mikegsCoder <mikegsid@gmail.com>
2024-05-15 07:26:08 +0300

    Updated auth guard

--- Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/core/guards/auth.guard.ts
+++ Angular/09. Workshop-Pipes-Interceptors-and-Subjects/ng-workshop/src/app/core/guards/auth.guard.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
-import { UserService } from 'src/app/user/user.service';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { first, map, switchMap, tap } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
 @Injectable()
-export class AuthGuard {
-  constructor(private userService: UserService, private router: Router) {}
+export class AuthGuard implements CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): boolean {
-    const isLoggedFromData = childRoute.data['isLogged'];
-    if (
-      typeof isLoggedFromData === 'boolean' &&
-      isLoggedFromData === this.userService.isLogged
-    ) {
-      return true;
-    }
-    const url = this.router.url;
-    this.router.navigateByUrl(url);
-    return false;
+  ): Observable<boolean> {
+    return this.authService.currentUser$.pipe(
+      switchMap((user) =>
+        user === undefined ? this.authService.authenticate() : [user]
+      ),
+      map((user) => {
+        const isLoggedFromData = childRoute.data['isLogged'];
+        return (
+          typeof isLoggedFromData !== 'boolean' || isLoggedFromData === !!user
+        );
+      }),
+      tap((canContinue) => {
+        if (canContinue) {
+          return;
+        }
+        const url = this.router.url;
+        this.router.navigateByUrl(url);
+      }),
+      first()
+    );
   }
 }
