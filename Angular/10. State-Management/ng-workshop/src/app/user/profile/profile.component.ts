64010c69f06f17fc6598803e5ce8101ee35d180b
mikegsCoder <mikegsid@gmail.com>
2024-05-20 07:25:18 +0300

    Refactored profile component

--- Angular/10. State-Management/ng-workshop/src/app/user/profile/profile.component.ts
+++ Angular/10. State-Management/ng-workshop/src/app/user/profile/profile.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/core/auth.service';
+import { IUserModuleState } from '../+store';
+import {
+  userProfileSetEditMode,
+  userProfileSetErrorMessage,
+  userProfileSetLoading,
+} from '../+store/actions';
 import { UserService } from '../user.service';
 
 @Component({
@@ -8,28 +15,34 @@ import { UserService } from '../user.service';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  inEditMode = false;
+  inEditMode$ = this.store.select((state) => state.user.profile.isEditMode);
+  isLoading$ = this.store.select((state) => state.user.profile.isLoading);
   currentUser$ = this.authService.currentUser$;
 
   constructor(
     private authService: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private store: Store<IUserModuleState>
   ) {}
 
   ngOnInit(): void {
     this.userService.getCurrentUserProfile().subscribe();
   }
 
-  toggleEditMode(): void {
-    this.inEditMode = !this.inEditMode;
+  toggleEditMode(currentValue): void {
+    this.store.dispatch(userProfileSetEditMode({ isEdit: !currentValue }));
   }
 
   submitHandler(data: any): void {
+    this.store.dispatch(userProfileSetLoading({ isLoading: true }));
     this.userService.updateProfile(data).subscribe({
       next: () => {
-        this.inEditMode = false;
+        this.store.dispatch(userProfileSetEditMode({ isEdit: false }));
       },
       error: (err) => {
+        this.store.dispatch(
+          userProfileSetErrorMessage({ message: err.error.message })
+        );
         console.error(err);
       },
     });
