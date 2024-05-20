289c6b5c974f7b4096435c1ac5394bdd3004bb4d
mikegsCoder <mikegsid@gmail.com>
2024-05-20 07:32:19 +0300

    Fixed header component

--- Angular/10. State-Management/ng-workshop/src/app/core/header/header.component.ts
+++ Angular/10. State-Management/ng-workshop/src/app/core/header/header.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+
+  hideNavigation = false;  
+  
   isLogged$ = this.authService.isLogged$;
   isReady$ = this.authService.isReady$;
-  currentUser$ = this.authService.currentUser$;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+  
   logoutHandler(): void {
-    this.authService
-      .logout()
-      .subscribe(() => this.router.navigate(['/user/login']));
+    this.authService.logout().subscribe(() => this.router.navigate(['/user/login']));
+  }
+  
+  ngOnDestroy(): void {
   }
 }
