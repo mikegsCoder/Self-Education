ae4d1a54baa306bfa4d69a67656c433c71670689
mikegsCoder <mikegsid@gmail.com>
2024-05-20 07:18:55 +0300

    Refactored login component

--- Angular/10. State-Management/ng-workshop/src/app/user/login/login.component.ts
+++ Angular/10. State-Management/ng-workshop/src/app/user/login/login.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/core/auth.service';
+import { IUserModuleState } from '../+store';
+import {
+  userLoginSetErrorMessage,
+  userLoginSetLoading,
+} from '../+store/actions';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +14,14 @@ import { AuthService } from 'src/app/core/auth.service';
   styleUrls: ['../../../form-styles.css', './login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  isLoading = false;
-  errorMessage = '';
+  isLoading$ = this.store.select((state) => state.user.login.isLoading);
+  errorMessage$ = this.store.select((state) => state.user.login.errorMessage);
 
-  constructor(private userService: AuthService, private router: Router) {}
+  constructor(
+    private userService: AuthService,
+    private router: Router,
+    private store: Store<IUserModuleState>
+  ) {}
 
   ngOnInit(): void {}
 
@@ -20,16 +30,19 @@ export class LoginComponent implements OnInit {
   }
 
   submitFormHandler(formValue: { email: string; password: string }): void {
-    this.isLoading = true;
-    this.errorMessage = '';
+    this.store.dispatch(userLoginSetLoading({ isLoading: true }));
+    this.store.dispatch(userLoginSetErrorMessage({ message: '' }));
+
     this.userService.login(formValue).subscribe({
       next: (data) => {
-        this.isLoading = false;
+        this.store.dispatch(userLoginSetLoading({ isLoading: false }));
         this.router.navigate(['/']);
       },
       error: (err) => {
-        this.errorMessage = err.error.message;
-        this.isLoading = false;
+        this.store.dispatch(userLoginSetLoading({ isLoading: false }));
+        this.store.dispatch(
+          userLoginSetErrorMessage({ message: err.error.message })
+        );
       },
     });
   }
