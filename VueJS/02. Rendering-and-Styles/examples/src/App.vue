b04472494cfa28373ed57f47275e5cd740e65971
mikegsCoder <mikegsid@gmail.com>
2024-07-10 07:43:27 +0300

    Add example with function

--- VueJS/02. Rendering-and-Styles/examples/src/App.vue
+++ VueJS/02. Rendering-and-Styles/examples/src/App.vue
@@ -3,9 +3,22 @@ export default {
   data() {
     return {
       isDay: false,
+      weather: '',
     };
   },
   methods: {
+    getWeather(weather) {
+      switch (weather) {
+        case 'cloudy':
+          return '⛅ Couldy';
+        case 'rainy':
+          return '🌧️ Rainy';
+        case 'stormy':
+          return '🌩️ Stormy';
+        default:
+          return '☀️ Sunny';
+      }
+    },
   },
 };
 </script>
@@ -23,4 +36,25 @@ export default {
     🌑 It's dark outside!
   </h1>
 </div>
+<hr>
+  <div>
+    <div v-if="weather === 'cloudy'">
+      ⛅ Couldy
+    </div>
+
+    <div v-else-if="weather === 'rainy'">
+      🌧️ Rainy
+    </div>
+
+    <div v-else-if="weather === 'stormy'">
+      🌩️ Stormy
+    </div>
+
+    <div v-else>
+      ☀️ Sunny
+    </div>
+
+    <h2>Example with function</h2>
+    <p>Today's weather is: {{ getWeather(weather) }}</p>
+  </div>
 </template>
