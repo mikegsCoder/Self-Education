c81b4420ee8b1217d947fcbca01ed7193acf9e75
mikegsCoder <mikegsid@gmail.com>
2024-07-10 07:38:37 +0300

    Add examples initial commit

---
+++ VueJS/02. Rendering-and-Styles/examples/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      isDay: false,
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<template>
+<div>
+  <button @click="isDay = !isDay">
+    Toggle
+  </button>
+
+  <h1 v-if="isDay">
+    ☀️ It's sunny outside!
+  </h1>
+  <h1 v-else>
+    🌑 It's dark outside!
+  </h1>
+</div>
+</template>
