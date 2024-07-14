ce2387bf9cc2654e394d62599bf751e10b10fd49
mikegsCoder <mikegsid@gmail.com>
2024-07-14 15:15:23 +0300

    Add timer app initial commit

---
+++ VueJS/02. Rendering-and-Styles/timer-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Timer from './components/Timer.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Timer />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ VueJS/02. Rendering-and-Styles/timer-app/src/components/Timer.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <h2>Simple Timer App</h2>
+</template>
+
+<style scoped>
+</style>
