3a159f8b23da8e4b1350df00a734f5fc2db0a07e
mikegsCoder <mikegsid@gmail.com>
2024-07-14 15:19:52 +0300

    Add data and setTimer

--- VueJS/02. Rendering-and-Styles/timer-app/src/components/Timer.vue
+++ VueJS/02. Rendering-and-Styles/timer-app/src/components/Timer.vue
@@ -1,16 +1,40 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      timerValue: '00:00:25',
+      timerIsStarted: false,
+      currentTime: 0,
+      pauseBtnText: 'Stop',
+      isPaused: false,
+      timerMax: 0,
+      warrningMsg: '',
+    };
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    setTimer(event) {
+      this.timerValue = event.target.value;
+    },
+  },
 };
 </script>
 
 <template>
   <h2>Simple Timer App</h2>
+
+  <div class="container">
+    <label for="time">Set Time</label>
+    <input
+      id="time"
+      :value="timerValue"
+      placeholder="HH:MM:SS"
+      type="text"
+      @change="setTimer"
+    >
+  </div>
+
 </template>
 
 <style scoped>
