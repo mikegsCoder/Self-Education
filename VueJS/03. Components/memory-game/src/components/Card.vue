f403d7dca6ff80840c11cdb48ef1e46aef1d7288
mikegsCoder <mikegsid@gmail.com>
2024-07-17 15:45:11 +0300

    Add card component

---
+++ VueJS/03. Components/memory-game/src/components/Card.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    idx: {
+      type: Number,
+      required: true,
+    },
+    activeItems: {
+      type: Array,
+    },
+    guessedItems: {
+      type: Array,
+    },
+  },
+  emits: ['onClick'],
+  computed: {},
+  methods: {},
+};
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
