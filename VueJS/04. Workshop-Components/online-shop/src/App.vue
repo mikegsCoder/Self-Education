f30f30632be259c5421bc295f0892efec9a3588f
mikegsCoder <mikegsid@gmail.com>
2024-07-23 07:40:11 +0300

    Add AppFooter

--- VueJS/04. Workshop-Components/online-shop/src/App.vue
+++ VueJS/04. Workshop-Components/online-shop/src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
     AppHeader,
+    AppFooter
   },
   data() {
     return {};
---
+++ VueJS/04. Workshop-Components/online-shop/src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {}
+</script>
+
+<template>
+<footer class="footer">
+  <div>
+    <section>VueJS workshop project</section>
+  </div>
+</footer>
+</template>
+
+
+<style scoped></style>
