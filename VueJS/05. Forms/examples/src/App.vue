55fd3ccfc6d41b6006ce4a53ab9bfb431f3c79f0
mikegsCoder <mikegsid@gmail.com>
2024-07-28 10:53:03 +0300

    Add forms examples

---
+++ VueJS/05. Forms/examples/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      age: null,
+    };
+  },
+};
+</script>
+
+<template>
+<h1>Input elements examples</h1>
+<form action="" class="container">
+  <div>
+    <label for="name">Name</label>
+    <input id="name" v-model="name" type="text">
+  </div>
+  <div>
+    <label for="age">Age</label>
+    <input id="age" v-model="age" type="number">
+  </div>
+</form>
+</template>
+
+<style scoped></style>
