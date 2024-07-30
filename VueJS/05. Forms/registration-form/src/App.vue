5da8dc930539134ff0a85399e46971dd7be646d5
mikegsCoder <mikegsid@gmail.com>
2024-07-30 07:34:05 +0300

    Add registration form initial commit

---
+++ VueJS/05. Forms/registration-form/src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import { useVuelidate } from '@vuelidate/core';
+import { email, required, between, helpers } from '@vuelidate/validators';
+
+export default {
+	// components: { AppInput, UserName },
+	setup() {
+		return { v$: useVuelidate() };
+	},
+	data() {
+		return {
+			data: {
+				fName: '',
+			},
+			formData: null,
+		};
+	},
+	methods: {},
+	validations() {},
+};
+</script>
+
+<template>
+	<form class="form" action="" @submit.prevent="onSubmit">
+		<div class="two-cols">
+			<!-- firstName -->
+			<div>
+				<label for="fname">First name</label>
+				<input id="fname" v-model="v$.data.fName.$model" type="text">
+				<div v-for="error of v$.data.fName.$errors" :key="error.$uid" class="input-errors">
+					<div class="error-msg">
+						{{ error.$message }}
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<button>Submit</button>
+	</form>
+
+	<pre v-if="formData">
+    <code>{{ formData }}</code>
+  </pre>
+</template>
+
+<style>
+</style>
