b0e6eab455a7ad3ebfd2d288b7ba6b0456d74fbe
mikegsCoder <mikegsid@gmail.com>
2024-08-11 09:35:21 +0300

    Add Directives HTTP and routing examples initial commit

---
+++ VueJS/07. Directives-HTTP-and-Routing/examples/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      pages: [{
+        url: '/',
+        text: 'Home',
+      }],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="page in pages" :key="page.url">
+          <router-link :to="page.url">
+            {{ page.text }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <router-view />
+
+  <footer>
+    <p>my footer</p>
+  </footer>
+</template>
