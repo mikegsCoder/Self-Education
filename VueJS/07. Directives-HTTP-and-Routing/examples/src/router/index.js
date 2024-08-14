9f3de5580ff3a4c21cd137c86eb58e16718f787b
mikegsCoder <mikegsid@gmail.com>
2024-08-14 07:40:35 +0300

    Add product route

--- VueJS/07. Directives-HTTP-and-Routing/examples/src/router/index.js
+++ VueJS/07. Directives-HTTP-and-Routing/examples/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Product from '../views/Product.vue';
 import Products from '../views/Products.vue';
 import Directives from '../views/Directives.vue';
 import NotFound from '../views/NotFound.vue';
@@ -12,6 +13,11 @@ const routes = [
   {
     path: '/products',
     component: Products,
+  },
+  {
+    path: '/products/:id',
+    name: 'singleProduct',
+    component: Product,
   },
   {
     path: '/directives',
