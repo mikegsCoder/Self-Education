d6ffd7cb1034095a26c525d0e7d4671235d76a9c
mikegsCoder <mikegsid@gmail.com>
2024-08-14 07:45:28 +0300

    Add profile container route

--- VueJS/07. Directives-HTTP-and-Routing/examples/src/router/index.js
+++ VueJS/07. Directives-HTTP-and-Routing/examples/src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import Home from '../views/Home.vue';
 import Product from '../views/Product.vue';
 import Products from '../views/Products.vue';
 import Directives from '../views/Directives.vue';
 import NotFound from '../views/NotFound.vue';
+
+import ProfileContainer from '../views/Profile/ProfileContainer.vue';
 
 const routes = [
   {
@@ -22,6 +25,11 @@ const routes = [
   {
     path: '/directives',
     component: Directives,
+  },
+  {
+    path: '/profile',
+    component: ProfileContainer,
+    children: [],
   },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
