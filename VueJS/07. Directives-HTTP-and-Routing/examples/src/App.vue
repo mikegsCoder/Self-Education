bb148f866796433257cd6feb987cddd13d4ac7f4
mikegsCoder <mikegsid@gmail.com>
2024-08-15 10:08:55 +0300

    Add profile

--- VueJS/07. Directives-HTTP-and-Routing/examples/src/App.vue
+++ VueJS/07. Directives-HTTP-and-Routing/examples/src/App.vue
@@ -2,36 +2,44 @@
 export default {
   data() {
     return {
-      pages: [{
-        url: '/',
-        text: 'Home',
-      }, {
-        url: '/products',
-        text: 'Products',
-      }, {
-        url: '/directives',
-        text: 'Directives',
-      }],
+      pages: [
+        {
+          url: '/',
+          text: 'Home',
+        },
+        {
+          url: '/products',
+          text: 'Products',
+        },
+        {
+          url: '/directives',
+          text: 'Directives',
+        },
+        {
+          url: '/profile',
+          text: 'profile',
+        }
+      ],
     };
   },
 };
 </script>
 
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li v-for="page in pages" :key="page.url">
-          <router-link :to="page.url">
-            {{ page.text }}
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <router-view />
+<header>
+  <nav>
+    <ul>
+      <li v-for="page in pages" :key="page.url">
+        <router-link :to="page.url">
+          {{ page.text }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</header>
+<router-view />
 
-  <footer>
-    <p>my footer</p>
-  </footer>
+<footer>
+  <p>my footer</p>
+</footer>
 </template>
