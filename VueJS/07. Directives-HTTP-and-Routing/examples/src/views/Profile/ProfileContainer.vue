9cd57b44c8b8c861aeb37f74539ba5b39c68d0c9
mikegsCoder <mikegsid@gmail.com>
2024-08-16 11:12:31 +0300

    Add profile container links

--- VueJS/07. Directives-HTTP-and-Routing/examples/src/views/Profile/ProfileContainer.vue
+++ VueJS/07. Directives-HTTP-and-Routing/examples/src/views/Profile/ProfileContainer.vue
@@ -1,11 +1,32 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pages: [
+        {
+          url: 'my-profile',
+          text: 'My Profile',
+        },
+        {
+          url: 'change-pass',
+          text: 'Change Password',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <h1>profile container</h1>
+  <ul>
+    <li v-for="page in pages" :key="page.url">
+      <router-link :to="`${/profile/}` + page.url">
+        {{ page.text }}
+      </router-link>
+    </li>
+  </ul>
   <router-view />
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
