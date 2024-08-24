2146557ea16f4331b57289ad4543d7e97ef99a76
mikegsCoder <mikegsid@gmail.com>
2024-08-24 11:26:43 +0300

    Add RouterView

--- VueJS/09. Workshop-Routing-and-State-Management/online-shop/src/App.vue
+++ VueJS/09. Workshop-Routing-and-State-Management/online-shop/src/App.vue
@@ -1,47 +1,26 @@
 <script>
+import { RouterView } from 'vue-router';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
-import Home from './views/Home.vue';
-import Contacts from './views/Contacts.vue';
-import About from './views/About.vue';
-import Cart from './views/Cart.vue';
-import Shop from './views/Shop/Shop.vue';
-import Register from './views/Register/Register.vue';
 
 export default {
   components: {
     AppHeader,
     AppFooter,
-    Home,
-    Contacts,
-    About,
-    Cart,
-    Shop,
-    Register,
+    RouterView,
   },
   data() {
-    return {
-      view: 'Register',
-      cartProducts: [],
-    };
-  },
-  methods: {
-    onNavSelect(selectedView) {
-      this.view = selectedView;
-    },
-    onAddToCart(productId) {
-      this.cartProducts.push(productId);
-    },
+    return {};
   },
 };
 </script>
 
 <template>
-<AppHeader :cart-products="cartProducts" @on-select="onNavSelect" />
-<main>
-  <component :is="view" :cart-products="cartProducts" @on-add-to-cart="onAddToCart" />
-</main>
-<AppFooter />
+  <AppHeader />
+  <main>
+    <RouterView />
+  </main>
+  <AppFooter />
 </template>
 
 <style scoped>
