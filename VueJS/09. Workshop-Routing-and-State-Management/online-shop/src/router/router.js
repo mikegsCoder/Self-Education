5db1dbc08b2bea390513cf566733a0a7a17c3aef
mikegsCoder <mikegsid@gmail.com>
2024-08-28 07:51:49 +0300

    Add register route

--- VueJS/09. Workshop-Routing-and-State-Management/online-shop/src/router/router.js
+++ VueJS/09. Workshop-Routing-and-State-Management/online-shop/src/router/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { isUser, isGuest } from '../guards/guards';
 
 import Home from '../views/Home.vue';
 import Shop from '../views/Shop/Shop.vue';
 import About from '../views/About.vue';
 import Contacts from '../views/Contacts.vue';
 import Cart from '../views/Cart.vue';
+import Register from '../views/Register/Register.vue';
 
 const routes = [
   { path: '/', component: Home },
@@ -12,6 +14,7 @@ const routes = [
   { path: '/about', component: About },
   { path: '/contacts', component: Contacts },
   { path: '/cart', component: Cart },
+  { path: '/register', component: Register, beforeEnter: isGuest },
 ];
 
 const router = createRouter({
