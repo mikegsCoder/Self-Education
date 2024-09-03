c5cd45f1ff68027daf2088f12e9ca343bf2bb9d3
mikegsCoder <mikegsid@gmail.com>
2024-09-03 07:50:30 +0300

    Removed constants

--- VueJS/09. Workshop-Routing-and-State-Management/online-shop/src/constants/categories.js
+++
@@ -1,9 +0,0 @@
-export const categories = [
-  { name: 'Women\'s Dresses', value: 'womens-dresses' },
-  { name: 'Women\'s Shoes', value: 'womens-shoes' },
-  { name: 'Men\'s Shirts', value: 'mens-shirts' },
-  { name: 'Men\'s Shoes', value: 'mens-shoes' },
-  { name: 'Men\'s Watches', value: 'mens-watches' },
-  { name: 'Women\'s Watches', value: 'womens-watches' },
-  { name: 'Sunglasses', value: 'sunglasses' },
-];
