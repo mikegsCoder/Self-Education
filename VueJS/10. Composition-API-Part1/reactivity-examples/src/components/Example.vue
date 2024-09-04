5df5cb280fc9b920507a271d8b414730a2f06208
mikegsCoder <mikegsid@gmail.com>
2024-09-04 08:03:34 +0300

    Add userData and computed numberOfBooks

--- VueJS/10. Composition-API-Part1/reactivity-examples/src/components/Example.vue
+++ VueJS/10. Composition-API-Part1/reactivity-examples/src/components/Example.vue
@@ -1,14 +1,22 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import AppButton from './AppButton.vue';
 
 const username = ref('Mihail');
-let userData = reactive({});
+let userData = reactive({
+  name: 'Mike',
+  location: 'Varna',
+  books: ['Lord of the rings', 'Manga', 'Harry Potter'],
+});
 
 function handleChange(event) {
   username.value = event.target.value;
   userData.name = event.target.value;
 }
+
+const numberOfBooks = computed(() => {
+  return userData.books.length;
+});
 
 function onMyCustomBtn() {
   console.log('My custom button was clicked!');
@@ -19,6 +27,13 @@ function onMyCustomBtn() {
   <div>
     <h1>Welcome, {{ username }}</h1>
     <input type="text" :value="username" @input="handleChange">
+
+    <h2>I have {{ numberOfBooks }} books in my bag</h2>
+    <ul>
+      <li v-for="book in userData.books" :key="book">
+        {{ book }}
+      </li>
+    </ul>
 
     <AppButton :text="username" type="secondary" @on-click="onMyCustomBtn" />
   </div>
