5bbef2ca9d24014d0a28b77a80f8666229df40a7
mikegsCoder <mikegsid@gmail.com>
2024-09-04 08:00:13 +0300

    Add reactivity examples initial commit

---
+++ VueJS/10. Composition-API-Part1/reactivity-examples/src/components/AppButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed, toRefs } from 'vue';
+
+const props = defineProps({
+  text: String,
+  type: {
+    type: String,
+    default: 'secondary',
+  },
+  outline: Boolean,
+});
+
+const emit = defineEmits(['onClick']);
+
+const { text, type, outline } = toRefs(props);
+
+const upperName = computed(() => {
+  return text.value.toUpperCase();
+});
+</script>
+
+<template>
+  <span>{{ upperName }}</span>
+  <button :class="[type, outline ? 'outline' : '']" @click="emit('onClick')">
+    Get {{ text }}'s data
+  </button>
+</template>
+
+<style scoped>
+</style>
---
+++ VueJS/10. Composition-API-Part1/reactivity-examples/src/components/Example.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive, ref } from 'vue';
+import AppButton from './AppButton.vue';
+
+const username = ref('Mihail');
+let userData = reactive({});
+
+function handleChange(event) {
+  username.value = event.target.value;
+  userData.name = event.target.value;
+}
+
+function onMyCustomBtn() {
+  console.log('My custom button was clicked!');
+}
+</script>
+
+<template>
+  <div>
+    <h1>Welcome, {{ username }}</h1>
+    <input type="text" :value="username" @input="handleChange">
+
+    <AppButton :text="username" type="secondary" @on-click="onMyCustomBtn" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
