1419ab14c0b239ac22bbaed9144c51636b9e23e0
mikegsCoder <mikegsid@gmail.com>
2024-09-06 12:05:32 +0300

    Add Memory game with composition API initial commit

---
+++ VueJS/10. Composition-API-Part1/memory-game-composition-api/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+let interval = null;
+
+const defaultTimer = ref(60000);
+const timer = ref(0);
+const selectedItem = ref([]);
+const guessedItems = ref([]);
+const game = reactive({
+  isStarted: false,
+  hasWon: false,
+  hasLost: false,
+});
+
+function onStart() {
+  game.isStarted = true;
+  game.hasWon = false;
+  game.hasLost = false;
+  startGameTimer();
+}
+</script>
+
+<template>
+  <div class="container">
+    <button v-if="!game.isStarted" @click="onStart">
+      Start game
+    </button>
+
+    <template v-else>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+</style>
