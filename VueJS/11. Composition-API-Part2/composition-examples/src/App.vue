70b7caf35fe08d2c8148c2d6206a1f5f0707cc55
mikegsCoder <mikegsid@gmail.com>
2024-09-12 16:06:55 +0300

    Add ComponentsTabs

--- VueJS/11. Composition-API-Part2/composition-examples/src/App.vue
+++ VueJS/11. Composition-API-Part2/composition-examples/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import ComponentsTabs from './components/ComponentsTabs.vue';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
+  <ComponentsTabs />
   <HelloWorld />
 </template>
 
