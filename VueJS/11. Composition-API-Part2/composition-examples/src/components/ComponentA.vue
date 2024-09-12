dd45b8921aefb1ba40dc4eaf88f668f54eeb0f41
mikegsCoder <mikegsid@gmail.com>
2024-09-12 16:03:04 +0300

    Add components A, B and C

---
+++ VueJS/11. Composition-API-Part2/composition-examples/src/components/ComponentA.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    component A
+  </div>
+</template>
+
+<style scoped></style>
---
+++ VueJS/11. Composition-API-Part2/composition-examples/src/components/ComponentB.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    component B
+  </div>
+</template>
+
+<style scoped></style>
