dac44187317b841fb26b39f3aec022925c37c3cd
mikegsCoder <mikegsid@gmail.com>
2024-09-13 13:32:38 +0300

    Add ListTodos

--- VueJS/11. Composition-API-Part2/composition-examples/src/App.vue
+++ VueJS/11. Composition-API-Part2/composition-examples/src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import ComponentsTabs from './components/ComponentsTabs.vue';
 import HelloWorld from './components/HelloWorld.vue';
+import ListTodos from './components/ListTodos.vue';
 </script>
 
 <template>
   <ComponentsTabs />
+
+  <Suspense>
+    <ListTodos />
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
+  
   <HelloWorld />
 </template>
 
