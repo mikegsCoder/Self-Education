af0acb3d80dd11c6dc3eb5ab660cbee575321ffa
mikegsCoder <mikegsid@gmail.com>
2024-09-16 07:35:37 +0300

    Add styles

--- VueJS/11. Composition-API-Part2/register-example/src/components/Register.vue
+++ VueJS/11. Composition-API-Part2/register-example/src/components/Register.vue
@@ -33,42 +33,57 @@ async function onSubmit() {
 </script>
 
 <template>
-  <form action="" @submit.prevent="onSubmit">
-    <InputText v-model="v$.firstName.$model" type="text" placeholder="firstName" />
-    <ul v-if="v$.firstName.$errors.length">
-      <li v-for="error in v$.firstName.$errors" :key="error.$uid">
-        {{ error.$message }}
-      </li>
-    </ul>
-    <InputText v-model="v$.lastName.$model" type="text" placeholder="lastName" />
-    <ul v-if="v$.lastName.$errors.length">
-      <li v-for="error in v$.lastName.$errors" :key="error.$uid">
-        {{ error.$message }}
-      </li>
-    </ul>
-    <InputText v-model="v$.maidenName.$model" type="text" placeholder="maidenName" />
-    <ul v-if="v$.maidenName.$errors.length">
-      <li v-for="error in v$.maidenName.$errors" :key="error.$uid">
-        {{ error.$message }}
-      </li>
-    </ul>
+<form action="" @submit.prevent="onSubmit">
+  <InputText v-model="v$.firstName.$model" type="text" placeholder="firstName" />
+  <ul v-if="v$.firstName.$errors.length">
+    <li v-for="error in v$.firstName.$errors" :key="error.$uid">
+      {{ error.$message }}
+    </li>
+  </ul>
+  <InputText v-model="v$.lastName.$model" type="text" placeholder="lastName" />
+  <ul v-if="v$.lastName.$errors.length">
+    <li v-for="error in v$.lastName.$errors" :key="error.$uid">
+      {{ error.$message }}
+    </li>
+  </ul>
+  <InputText v-model="v$.maidenName.$model" type="text" placeholder="maidenName" />
+  <ul v-if="v$.maidenName.$errors.length">
+    <li v-for="error in v$.maidenName.$errors" :key="error.$uid">
+      {{ error.$message }}
+    </li>
+  </ul>
 
-    <InputNumber v-model="v$.age.$model" />
-    <ul v-if="v$.age.$errors.length">
-      <li v-for="error in v$.age.$errors" :key="error.$uid">
-        {{ error.$message }}
-      </li>
-    </ul>
-    <div>
-      <Button outlined severity="secondary" type="button">
-        Cancel
-      </Button>
-      <Button type="submit">
-        Submit
-      </Button>
-    </div>
-  </form>
+  <InputNumber v-model="v$.age.$model" />
+  <ul v-if="v$.age.$errors.length">
+    <li v-for="error in v$.age.$errors" :key="error.$uid">
+      {{ error.$message }}
+    </li>
+  </ul>
+  <div>
+    <Button outlined severity="secondary" type="button">
+      Cancel
+    </Button>
+    <Button type="submit">
+      Submit
+    </Button>
+  </div>
+</form>
 </template>
 
 <style scoped>
+form {
+  display: grid;
+  gap: 1rem;
+  max-width: 600px;
+  margin: 4rem auto;
+  border-radius: 1rem;
+  padding: 1.5rem 0.75rem;
+  border: 1px solid gray;
+}
+
+form>div {
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+}
 </style>
