d06f59af0b0dd221183a13c50e433c13f7b697bd
mikegsCoder <mikegsid@gmail.com>
2024-09-18 07:56:59 +0300

    Add pinia

--- VueJS/12. Workshop-Composition/online-shop/src/main.js
+++ VueJS/12. Workshop-Composition/online-shop/src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router/router';
 import 'primevue/resources/themes/lara-light-green/theme.css';
 import 'primeicons/primeicons.css';
 
 const app = createApp(App);
+const pinia = createPinia();
 app.use(PrimeVue);
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
