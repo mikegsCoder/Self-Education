6040c239809d5a4788ac04a8b58a4066b7aa9b78
mikegsCoder <mikegsid@gmail.com>
2024-09-18 07:51:58 +0300

    Add Home

---
+++ VueJS/12. Workshop-Composition/online-shop/src/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from 'primevue/card';
+</script>
+
+<template>
+<Card>
+  <template #title>
+    <h2>Welcome to Fashion Emporium!</h2>
+  </template>
+  <template #content>
+    <p>
+      Discover the latest trends and express your unique style with our curated collection of fashion-forward
+      clothing, footwear, and accessories.
+    </p>
+    <p>Explore our catalog and find the perfect pieces to elevate your wardrobe.</p>
+  </template>
+</Card>
+</template>
+
+<style scoped></style>
