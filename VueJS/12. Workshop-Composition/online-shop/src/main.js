c3e9fbd863d6618f0c5740de37b6cc63f782dadc
mikegsCoder <mikegsid@gmail.com>
2024-09-20 07:39:35 +0300

    Add persisted profile from user store

--- VueJS/12. Workshop-Composition/online-shop/src/main.js
+++ VueJS/12. Workshop-Composition/online-shop/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import { createPinia } from 'pinia';
+import { useUserStore } from '../src/stores/userStore.js';
 import App from './App.vue';
 import router from './router/router';
 import 'primevue/resources/themes/lara-light-green/theme.css';
@@ -12,4 +13,6 @@ app.use(PrimeVue);
 app.use(pinia);
 app.use(router);
 
+const userStore = useUserStore();
+userStore.getPersistedProfile();
 app.mount('#app');
