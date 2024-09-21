014ccffaec0e6a7b2e18f199ccba91222d53bd83
mikegsCoder <mikegsid@gmail.com>
2024-09-21 11:27:36 +0300

    Add addToCart

--- VueJS/12. Workshop-Composition/online-shop/src/stores/cartStore.js
+++ VueJS/12. Workshop-Composition/online-shop/src/stores/cartStore.js
@@ -6,8 +6,19 @@ export const useCartStore = defineStore('cart', () => {
 
   const getProduct = computed(() => productId => products.value.find(prod => prod.id === productId));
 
+  function addToCart(selectedId) {
+    const productInCart = products.value.find(prod => prod.id === selectedId);
+    if (!productInCart) {
+      products.value.push({ id: selectedId, quantity: 1 });
+    }
+    else {
+      productInCart.quantity += 1;
+    }
+  };
+  
   return {
     products,
     getProduct,
+    addToCart
   };
 });
