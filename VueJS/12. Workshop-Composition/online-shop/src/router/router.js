fa2a3698d944bea48266305a3e636067488da432
mikegsCoder <mikegsid@gmail.com>
2024-09-24 07:30:49 +0300

    Add about route

--- VueJS/12. Workshop-Composition/online-shop/src/router/router.js
+++ VueJS/12. Workshop-Composition/online-shop/src/router/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('../views/Home.vue');
+const About = () => import('../views/About.vue');
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/about', component: About },
 ];
 
 const router = createRouter({
