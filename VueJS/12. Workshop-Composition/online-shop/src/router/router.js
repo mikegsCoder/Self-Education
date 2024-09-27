b6ea7979d3a147035e4ad72a209f668c71ac8a09
mikegsCoder <mikegsid@gmail.com>
2024-09-27 07:15:51 +0300

    Add shop route

--- VueJS/12. Workshop-Composition/online-shop/src/router/router.js
+++ VueJS/12. Workshop-Composition/online-shop/src/router/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('../views/Home.vue');
+const Shop = () => import('../views/Shop/Shop.vue');
 const About = () => import('../views/About.vue');
 const Contacts = () => import('../views/Contacts.vue');
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/shop', component: Shop },
   { path: '/about', component: About },
   { path: '/contacts', component: Contacts },
 ];
