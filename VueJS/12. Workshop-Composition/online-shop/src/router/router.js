06b697e1edd4506a6408988e8c324fe75cd7f258
mikegsCoder <mikegsid@gmail.com>
2024-09-29 12:18:05 +0300

    Add register route

--- VueJS/12. Workshop-Composition/online-shop/src/router/router.js
+++ VueJS/12. Workshop-Composition/online-shop/src/router/router.js
@@ -4,12 +4,14 @@ const Home = () => import('../views/Home.vue');
 const Shop = () => import('../views/Shop/Shop.vue');
 const About = () => import('../views/About.vue');
 const Contacts = () => import('../views/Contacts.vue');
+const Register = () => import('../views/Register/Register.vue');
 
 const routes = [
   { path: '/', component: Home },
   { path: '/shop', component: Shop },
   { path: '/about', component: About },
   { path: '/contacts', component: Contacts },
+  { path: '/register', component: Register },
 ];
 
 const router = createRouter({
