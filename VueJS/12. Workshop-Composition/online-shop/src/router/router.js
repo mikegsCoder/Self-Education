927ede468752c14a23e3bbff6ee1bd9ff0dbd85e
mikegsCoder <mikegsid@gmail.com>
2024-10-04 06:55:44 +0300

    Add favorites route

--- VueJS/12. Workshop-Composition/online-shop/src/router/router.js
+++ VueJS/12. Workshop-Composition/online-shop/src/router/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from '../stores/userStore';
 
 const Home = () => import('../views/Home.vue');
 const Shop = () => import('../views/Shop/Shop.vue');
@@ -8,6 +9,7 @@ const Contacts = () => import('../views/Contacts.vue');
 const Register = () => import('../views/Register/Register.vue');
 const Login = () => import('../views/Login.vue');
 const Profile = () => import('../views/Profile.vue');
+const Favourites = () => import('../views/Favourites.vue');
 
 function validateUser() {
   const userStore = useUserStore();
@@ -26,6 +28,7 @@ const routes = [
     return userStore.isAuthenticated ? { path: '/profile' } : true;
   } },
   { path: '/profile', component: Profile, beforeEnter: validateUser },
+  { path: '/favourites', component: Favourites, beforeEnter: validateUser },
 ];
 
 const router = createRouter({
