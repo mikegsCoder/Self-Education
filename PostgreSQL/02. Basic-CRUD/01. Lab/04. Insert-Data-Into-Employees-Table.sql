dd0d78d6704b32be5c6dc7bca2fe564152fd6128
mikegsCoder <mikegsid@gmail.com>
2024-12-14 07:34:39 +0200

    Add Insert data into employees table problem solution

---
+++ PostgreSQL/02. Basic-CRUD/01. Lab/04. Insert-Data-Into-Employees-Table.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees	(first_name, last_name, job_title, department_id, salary)
+VALUES 
+	('Samantha','Young', 'Housekeeping', 5, 700),
+	('Roger', 'Palmer', 'Waiter', 2, 645.27);
+
+SELECT * FROM employees;
