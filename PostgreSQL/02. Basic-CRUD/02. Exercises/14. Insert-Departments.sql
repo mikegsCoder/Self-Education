005333fe3bedf534745006136c66dcccbb10d46f
mikegsCoder <mikegsid@gmail.com>
2024-12-20 08:50:55 +0200

    Add Insert departments problem solution

---
+++ PostgreSQL/02. Basic-CRUD/02. Exercises/14. Insert-Departments.sql
@@ -0,0 +1,9 @@
+INSERT INTO departments (id, department, manager_id) 
+VALUES
+	(10, 'Finance', 4),
+	(11, 'Information Services', 32),
+	(12, 'Document Control', 90),
+	(13, 'Quality Assurance', 254),
+	(14, 'Facilities and Maintenance', 238),
+	(15, 'Shipping and Receiving', 81),
+	(16, 'Executive', 193);
