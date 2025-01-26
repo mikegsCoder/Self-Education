2b9556e1e610c919ce7f5068ff415a5ad8bd32a9
mikegsCoder <mikegsid@gmail.com>
2025-01-26 09:07:24 +0200

    Add Primary key problem solution

---
+++ PostgreSQL/05. Table-Relations/02. Exercises/01. Primary-Key.sql
@@ -0,0 +1,15 @@
+/* create table */
+CREATE TABLE IF NOT EXISTS products (
+  product_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO products (product_name) 
+VALUES
+	('Broccoli'),
+	('Shampoo'),
+	('Toothpaste'),
+	('Candy');
+	
+/* define priary key */
+ALTER TABLE products
+ADD COLUMN "id" SERIAL PRIMARY KEY;
