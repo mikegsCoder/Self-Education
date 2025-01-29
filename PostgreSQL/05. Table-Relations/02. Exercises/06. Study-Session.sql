2e79f8eb5327375ec5bcecad063ad106c3a264ca
mikegsCoder <mikegsid@gmail.com>
2025-01-29 07:33:35 +0200

    Add students exams table with data

--- PostgreSQL/05. Table-Relations/02. Exercises/06. Study-Session.sql
+++ PostgreSQL/05. Table-Relations/02. Exercises/06. Study-Session.sql
@@ -40,3 +40,26 @@ VALUES
 	('Creative Hall', 103),
 	('Masterclass Hall', 103),
 	('Information Security Hall', 103);
+
+/* students exams table */
+CREATE TABLE IF NOT EXISTS students_exams (
+  student_id INT,
+  exam_id INT,
+ 	CONSTRAINT pk_students_exams
+		PRIMARY KEY(student_id, exam_id),
+	CONSTRAINT fk_students_exams_students
+		FOREIGN KEY(student_id)
+		REFERENCES students("id"),
+	CONSTRAINT fk_students_exams_exams
+		FOREIGN KEY(exam_id)
+		REFERENCES exams("id")
+);
+
+INSERT INTO students_exams (student_id, exam_id) 
+VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103), 
+	(2, 102),
+	(2, 103);
