9e05011047cf012354d711ac9e3fd422ad9ac1d7
mikegsCoder <mikegsid@gmail.com>
2025-01-30 07:39:03 +0200

    Add orders and item types tables

--- PostgreSQL/05. Table-Relations/02. Exercises/07. Online-Store.sql
+++ PostgreSQL/05. Table-Relations/02. Exercises/07. Online-Store.sql
@@ -14,3 +14,18 @@ CREATE TABLE IF NOT EXISTS customers (
 		FOREIGN KEY (city_id) 
 		REFERENCES cities("id")
 );
+
+/* orders table */
+CREATE TABLE IF NOT EXISTS orders (
+  "id" SERIAL PRIMARY KEY,
+	customer_id INT,
+	CONSTRAINT fk_orders_customers
+		FOREIGN KEY (customer_id) 
+		REFERENCES customers("id")
+);
+
+/* item types table */
+CREATE TABLE IF NOT EXISTS item_types (
+  "id" SERIAL PRIMARY KEY,
+  item_type_name VARCHAR(50)
+);
