76bc4bf464f1be8b443033040bcd4da413b698a9
mikegsCoder <mikegsid@gmail.com>
2025-02-01 09:27:58 +0200

    Delete customer with id equals to 1

--- PostgreSQL/05. Table-Relations/02. Exercises/10. Set-Null.sql
+++ PostgreSQL/05. Table-Relations/02. Exercises/10. Set-Null.sql
@@ -30,3 +30,7 @@ VALUES
 	(1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
   (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
   (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
+
+/* delete customer with "id" = 1 */ 
+DELETE FROM customers
+WHERE customers.id = 1;
