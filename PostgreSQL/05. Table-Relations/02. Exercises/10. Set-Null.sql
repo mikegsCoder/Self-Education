da6cf6f8f65f5bdb04782a491389c53f27834b0c
mikegsCoder <mikegsid@gmail.com>
2025-02-01 09:25:53 +0200

    Insert data into customers and contacts tables

--- PostgreSQL/05. Table-Relations/02. Exercises/10. Set-Null.sql
+++ PostgreSQL/05. Table-Relations/02. Exercises/10. Set-Null.sql
@@ -17,3 +17,16 @@ CREATE TABLE contacts(
 	  ON DELETE SET NULL
 	  ON UPDATE CASCADE
 );
+
+/* insert into customers table */
+INSERT INTO customers (customer_name) 
+VALUES
+	('BlueBird Inc'),
+	('Dolphin LLC');
+	
+/* insert into contacts table */
+INSERT INTO contacts (customer_id, contact_name, phone, email) 
+VALUES
+	(1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
+  (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
+  (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
