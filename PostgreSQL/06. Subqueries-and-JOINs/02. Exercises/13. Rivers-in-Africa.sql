caa1dd4a4e99ed29f91301ede255528989fd5c51
mikegsCoder <mikegsid@gmail.com>
2025-02-09 13:37:12 +0200

    Add Rivers in Africa problem solution

---
+++ PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/13. Rivers-in-Africa.sql
@@ -0,0 +1,11 @@
+SELECT 
+	c.country_name,
+	r.river_name
+FROM countries AS c
+LEFT OUTER JOIN countries_rivers AS cr 
+	ON cr.country_code =  c.country_code
+LEFT OUTER JOIN rivers AS r
+	ON r."id" = cr.river_id
+WHERE c.continent_code ='AF'
+ORDER BY c.country_name
+LIMIT 5;
