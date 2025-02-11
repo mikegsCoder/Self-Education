1a1cd49ac4144117774b56a9a4bf745f3418b319
mikegsCoder <mikegsid@gmail.com>
2025-02-11 07:27:12 +0200

    Alter table and update

--- PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/16. Monasteries-by-Country.sql
+++ PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/16. Monasteries-by-Country.sql
@@ -29,3 +29,25 @@ VALUES
   ('Pa-Auk Forest Monastery', 'MM'),
   ('Taktsang Palphug Monastery', 'BT'),
   ('Sümela Monastery', 'TR');
+
+ALTER TABLE countries
+ADD COLUMN 
+  three_rivers BOOLEAN NOT NULL DEFAULT false;
+
+
+UPDATE countries
+SET three_rivers = true
+WHERE 
+	country_code IN (
+	  SELECT rivers_count.code
+	  FROM (
+			SELECT
+			  c.country_code AS code,
+			  COUNT(cr.river_id) AS countries_rivers
+			FROM countries AS c
+			JOIN countries_rivers AS cr
+				USING(country_code)
+			GROUP BY c.country_code
+		  ) AS rivers_count
+	  WHERE rivers_count.countries_rivers > 3
+	);
