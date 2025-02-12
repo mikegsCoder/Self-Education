81e5c04ceb56e75790a7e2342654b5c01bf84340
mikegsCoder <mikegsid@gmail.com>
2025-02-12 07:28:15 +0200

    Add select

--- PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/17. Monasteries-by-Continents-and-Countries.sql
+++ PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/17. Monasteries-by-Continents-and-Countries.sql
@@ -19,3 +19,20 @@ INSERT INTO monasteries (monastery_name, country_code)
 	  FROM countries
 	  WHERE country_name = 'Myanmar'
 	);
+
+SELECT
+	con.continent_name AS "Continent Name",
+	c.country_name AS "Country Name",
+	COUNT(m."id") AS "Monasteries Count"
+FROM continents AS con
+JOIN countries AS c
+	USING(continent_code)
+LEFT JOIN monasteries AS m
+	USING(country_code)
+WHERE c.three_rivers = false
+GROUP BY 
+	c.country_name, 
+	con.continent_name
+ORDER BY 
+	"Monasteries Count" DESC, 
+	"Country Name";
