b76b359b28556df5e10bc5fcbe6b01724ee69c5f
mikegsCoder <mikegsid@gmail.com>
2025-02-12 07:26:47 +0200

    Insert data

--- PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/17. Monasteries-by-Continents-and-Countries.sql
+++ PostgreSQL/06. Subqueries-and-JOINs/02. Exercises/17. Monasteries-by-Continents-and-Countries.sql
@@ -1,3 +1,21 @@
 UPDATE countries
 SET country_name = 'Burma'
 WHERE country_name = 'Myanmar';
+
+INSERT INTO monasteries (monastery_name, country_code)
+	(
+	  SELECT
+	  	'Hanga Abbey',
+	  	country_code
+	  FROM countries
+	  WHERE country_name = 'Tanzania'
+	);
+
+INSERT INTO monasteries (monastery_name, country_code)
+	(
+	  SELECT
+	  	'Myin-Tin-Daik',
+	  	country_code
+	  FROM countries
+	  WHERE country_name = 'Myanmar'
+	);
