d8578a63cef4fd821d955f1eaa2b3a25aadbe630
mikegsCoder <mikegsid@gmail.com>
2025-02-22 10:08:10 +0200

    Add function trigger_fn_insert_new_entry_into_logs

--- PostgreSQL/07. Database-Programmability/02. Exercises/12. Log-Accounts-Trigger.sql
+++ PostgreSQL/07. Database-Programmability/02. Exercises/12. Log-Accounts-Trigger.sql
@@ -4,3 +4,22 @@ CREATE TABLE logs (
   old_sum DECIMAL(19, 4),
   new_sum DECIMAL(19, 4)
 );
+
+CREATE OR REPLACE FUNCTION trigger_fn_insert_new_entry_into_logs()
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  INSERT INTO logs (
+	  account_id, 
+	  old_sum, 
+	  new_sum
+  )
+  VALUES (
+	  OLD."id", 
+	  OLD.balance, 
+	  NEW.balance
+  );
+  RETURN NEW;
+END;
+$$;
