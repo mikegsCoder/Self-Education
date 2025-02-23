4a4a86311e3c86c8b8577c54a924929f89665429
mikegsCoder <mikegsid@gmail.com>
2025-02-23 09:00:12 +0200

    Add function

--- PostgreSQL/07. Database-Programmability/02. Exercises/13. Notification-Email-on-Balance-Change.sql
+++ PostgreSQL/07. Database-Programmability/02. Exercises/13. Notification-Email-on-Balance-Change.sql
@@ -4,3 +4,27 @@ CREATE TABLE notification_emails (
   subject VARCHAR(50),
   body TEXT
 );
+
+CREATE OR REPLACE FUNCTION trigger_fn_send_email_on_balance_change()
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  INSERT INTO notification_emails (
+	recipent_id,
+    subject,
+    body
+  )
+  VALUES (
+	NEW.account_id,
+    CONCAT(
+		'Balance change for account: ', NEW.account_id
+	),
+    CONCAT(
+		'On ', CURRENT_DATE, ' your balance was changed from ', OLD.old_sum, ' to ', NEW.new_sum, '.'
+	)
+  );
+  RETURN NEW;
+END;
+$$;
+
