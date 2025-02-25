2ae767c98cf84e18802184ee663d766b5cdbba19
mikegsCoder <mikegsid@gmail.com>
2025-02-25 07:29:23 +0200

    Add CREATE TABLE volunteers

--- PostgreSQL/08. Exams/01. Exam-I/01. DDL.sql
+++ PostgreSQL/08. Exams/01. Exam-I/01. DDL.sql
@@ -42,3 +42,22 @@ CREATE TABLE IF NOT EXISTS volunteers_departments (
   "id" SERIAL PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS volunteers (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  address VARCHAR(50),
+  animal_id INT,
+  department_id INT NOT NULL,
+  CONSTRAINT fk_volunteers_animal_id
+	FOREIGN KEY (animal_id) 
+	REFERENCES animals("id")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+  CONSTRAINT fk_volunteers_department_id
+	FOREIGN KEY (department_id) 
+	REFERENCES volunteers_departments("id")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
