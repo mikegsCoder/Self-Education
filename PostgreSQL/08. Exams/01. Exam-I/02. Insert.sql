d2fdcb82b65c51a2f0cbe6df3454f3a50c105e89
mikegsCoder <mikegsid@gmail.com>
2025-02-26 07:32:18 +0200

    Add INSERT INTO volunteers

---
+++ PostgreSQL/08. Exams/01. Exam-I/02. Insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO volunteers ("name", phone_number, address, animal_id, department_id) 
+VALUES 
+	('Anita Kostova', '0896365412', 'Sofia, 5 Rosa str.', 15, 1),	
+	('Dimitur Stoev', '0877564223', NULL, 42, 4),
+	('Kalina Evtimova', '0896321112', 'Silistra, 21 Breza str.', 9, 7),	
+	('Stoyan Tomov', '0898564100', 'Montana, 1 Bor str.', 18, 8),
+	('Boryana Mileva', '0888112233', NULL, 31, 5);
