33566ab5383faf11196188e4ce4e1d245f7c1889
mikegsCoder <mikegsid@gmail.com>
2025-02-27 07:16:35 +0200

    Completed Delete problem solution

--- PostgreSQL/08. Exams/01. Exam-I/04. Delete.sql
+++ PostgreSQL/08. Exams/01. Exam-I/04. Delete.sql
@@ -1,3 +1,8 @@
 -- Delete records from referencing tables
 DELETE FROM volunteers
 WHERE department_id = 3;
+
+-- Delete records from volunteers_departments table
+DELETE FROM volunteers_departments
+WHERE "id" = 3
+RETURNING *;
