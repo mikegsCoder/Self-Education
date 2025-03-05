f29736cebe3a13ab85b1f69a2649b3b5d69c02ae
mikegsCoder <mikegsid@gmail.com>
2025-03-05 07:36:17 +0200

    Add Update problem solution

---
+++ PostgreSQL/08. Exams/02. Exam-II/03. Update.sql
@@ -0,0 +1,13 @@
+UPDATE cars
+SET "condition" = 'C'
+WHERE (mileage >= 800000 OR mileage IS NULL) 
+	AND "year" <= 2010 
+	AND make != 'Mercedes-Benz';
+		
+SELECT 
+	"id", 
+	"condition" 
+FROM cars
+WHERE (mileage >= 800000 OR mileage IS NULL)
+  AND "year" <= 2010
+  AND make != 'Mercedes-Benz';
