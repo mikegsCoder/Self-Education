cc2e3e11676235eefea61b986d8c9ad2bd9d3270
mikegsCoder <mikegsid@gmail.com>
2025-03-06 06:36:03 +0200

    Add Drivers and cars problem solution

---
+++ PostgreSQL/08. Exams/02. Exam-II/06. Drivers-and-Cars.sql
@@ -0,0 +1,15 @@
+SELECT
+  d.first_name,
+  d.last_name,
+  c.make,
+  c.model,
+  c.mileage
+FROM drivers AS d
+JOIN cars_drivers AS cd 
+	ON cd.driver_id = d."id"
+JOIN cars AS c 
+	ON cd.car_id = c."id"
+WHERE c.mileage IS NOT NULL
+ORDER BY
+  c.mileage DESC,
+  d.first_name;
