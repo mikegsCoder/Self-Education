dfda81eec181b8a2c707cc1d97f943ae61105958
mikegsCoder <mikegsid@gmail.com>
2025-03-08 09:10:18 +0200

    Add procedure usp_courses_by_address

--- PostgreSQL/08. Exams/02. Exam-II/11. Full-Info-for-Address.sql
+++ PostgreSQL/08. Exams/02. Exam-II/11. Full-Info-for-Address.sql
@@ -8,3 +8,47 @@ CREATE TABLE search_results (
   category_name VARCHAR(50)
 );
 
+CREATE OR REPLACE PROCEDURE usp_courses_by_address(
+  IN address_name VARCHAR(100)
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  -- Clear results table:
+  TRUNCATE TABLE search_results;
+
+  -- Insert new results into the table:
+  INSERT INTO search_results (
+    address_name,
+    full_name,
+    level_of_bill,
+    make,
+    "condition",
+    category_name
+  )
+  SELECT 
+	  a.name, 
+	  cl.full_name,
+    CASE
+      WHEN cou.bill <= 20 THEN 'Low'
+      WHEN cou.bill <= 30 THEN 'Medium'
+      ELSE 'High'
+    END AS level_of_bill,
+    c.make, 
+	  c."condition", 
+	  cat."name"
+  FROM courses cou
+  INNER JOIN addresses AS a 
+	  ON cou.from_address_id = a."id"
+  INNER JOIN clients AS cl 
+	  ON cou.client_id = cl.id
+  INNER JOIN cars AS c 
+	  ON cou.car_id = c.id
+  INNER JOIN categories AS cat 
+	  ON c.category_id = cat.id
+  WHERE	a."name" = address_name
+  ORDER BY 
+	c.make, 
+	cl.full_name;
+END;
+$$;
