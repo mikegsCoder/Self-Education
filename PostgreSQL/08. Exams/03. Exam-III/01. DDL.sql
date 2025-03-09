7f08bf966b9c06f68e3ac17b3403aebe28e05526
mikegsCoder <mikegsid@gmail.com>
2025-03-09 09:05:53 +0200

    Add table addresses

--- PostgreSQL/08. Exams/03. Exam-III/01. DDL.sql
+++ PostgreSQL/08. Exams/03. Exam-III/01. DDL.sql
@@ -2,3 +2,14 @@ CREATE TABLE IF NOT EXISTS categories (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS addresses (
+  "id" SERIAL PRIMARY KEY,
+  street_name VARCHAR(100) NOT NULL,
+  street_number INT NOT NULL,
+  town VARCHAR(30) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  zip_code INT NOT NULL,
+  CONSTRAINT addresses_street_number_check CHECK (street_number > 0),
+  CONSTRAINT addresses_zip_code_check CHECK (zip_code > 0)
+);
