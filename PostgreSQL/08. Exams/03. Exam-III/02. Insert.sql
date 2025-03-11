fa903fbe3807b4f1b0a1c145a9ad37821c5ca2a3
mikegsCoder <mikegsid@gmail.com>
2025-03-11 07:23:52 +0200

    Add insert into publishers

--- PostgreSQL/08. Exams/03. Exam-III/02. Insert.sql
+++ PostgreSQL/08. Exams/03. Exam-III/02. Insert.sql
@@ -5,3 +5,9 @@ VALUES
 	('Catan: Starfarers', 2021, 9.87, 7, 13, 6),
 	('Bleeding Kansas', 2020, 3.25, 3, 7, 4),
 	('One Small Step', 2019, 5.75, 5, 9, 2);
+
+INSERT INTO publishers ("name", address_id, website, phone)
+VALUES
+	('Agman Games', 5, 'www.agmangames.com', '+16546135542'),
+	('Amethyst Games', 7, 'www.amethystgames.com', '+15558889992'),
+	('BattleBooks', 13, 'www.battlebooks.com', '+12345678907');
