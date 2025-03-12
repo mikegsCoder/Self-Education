bb20f6bc3af95081a02ff9bfc5428a0888409e76
mikegsCoder <mikegsid@gmail.com>
2025-03-12 07:21:41 +0200

    Add update board_games

--- PostgreSQL/08. Exams/03. Exam-III/03. Update.sql
+++ PostgreSQL/08. Exams/03. Exam-III/03. Update.sql
@@ -3,3 +3,8 @@ SET max_players = max_players + 1
 WHERE min_players = 2 
 	AND max_players = 2
 RETURNING *;
+
+UPDATE board_games
+SET "name" = "name" || ' V2'
+WHERE release_year >= 2020
+RETURNING *;
