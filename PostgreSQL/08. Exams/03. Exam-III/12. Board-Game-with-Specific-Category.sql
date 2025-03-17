7b5cc31dcf9f411b37ca0e37dc5432b67b9b4106
mikegsCoder <mikegsid@gmail.com>
2025-03-17 07:14:52 +0200

    Add usp_search_by_category procedure

--- PostgreSQL/08. Exams/03. Exam-III/12. Board-Game-with-Specific-Category.sql
+++ PostgreSQL/08. Exams/03. Exam-III/12. Board-Game-with-Specific-Category.sql
@@ -8,3 +8,44 @@ CREATE TABLE search_results (
   min_players VARCHAR(50),
   max_players VARCHAR(50)
 );
+
+CREATE OR REPLACE PROCEDURE usp_search_by_category(
+	IN category VARCHAR(50)
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  -- Clear results table
+  TRUNCATE TABLE search_results;
+    
+  -- Insert new results into the table
+  INSERT INTO search_results (
+		"name", 
+		release_year, 
+		rating, 
+		category_name, 
+		publisher_name, 
+		min_players, 
+		max_players
+	)
+  SELECT 
+    bg."name",
+    bg.release_year,
+    bg.rating,
+    c."name" AS category_name,
+    p."name" AS publisher_name,
+    CONCAT(pr.min_players, ' people'),
+    CONCAT(pr.max_players, ' people')
+  FROM board_games AS bg
+  JOIN publishers AS p 
+	  ON bg.publisher_id = p.id
+  JOIN categories AS c 
+	  ON bg.category_id = c."id"
+  JOIN players_ranges AS pr 
+	  ON bg.players_range_id = pr."id"
+  WHERE c."name" = category
+  ORDER BY 
+	  publisher_name, 
+	  release_year DESC;
+END;
+$$;
