400cf2de0632a3bf18c18e85d9293d059db8bd70
mikegsCoder <mikegsid@gmail.com>
2025-06-20 06:34:44 +0300

    Add creating graphical interface example app

---
+++ MAUI/02. Creating-Graphical-Interface/HelloApp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace HelloApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+             return new Window(new AppShell());
+        }
+    }
+}
---
+++ MAUI/02. Creating-Graphical-Interface/HelloApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            string xaml = "<Label Text=\".NET MAUI\" FontSize=\"24\" />";
+
+            header.LoadFromXaml(xaml);
+        }
+    }
+}
