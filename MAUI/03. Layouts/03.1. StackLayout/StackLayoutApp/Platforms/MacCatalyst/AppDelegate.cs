f623a1f5be8290406b5bd0573576489a99dbb414
mikegsCoder <mikegsid@gmail.com>
2025-06-21 08:19:54 +0300

    Add StackLayoutApp

---
+++ MAUI/03. Layouts/03.1. StackLayout/StackLayoutApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StackLayoutApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
