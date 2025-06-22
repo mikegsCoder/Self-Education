6f7350b33f32aec2d50a0fb7f775ecf6f739b408
mikegsCoder <mikegsid@gmail.com>
2025-06-22 07:55:34 +0300

    Code cleanup

--- MAUI/03. Layouts/03.1. StackLayout/StackLayoutApp/StackLayoutStartPage.cs
+++ MAUI/03. Layouts/03.1. StackLayout/StackLayoutApp/StackLayoutStartPage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StackLayoutApp
+﻿namespace StackLayoutApp
 {
     public class StackLayoutStartPage : ContentPage
     {
