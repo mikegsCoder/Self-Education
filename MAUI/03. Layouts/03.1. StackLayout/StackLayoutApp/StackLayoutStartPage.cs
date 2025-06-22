1d4fc80bda17e885f50f9cfa2aa8ddbc2c87bb18
mikegsCoder <mikegsid@gmail.com>
2025-06-22 07:51:39 +0300

    Add StackLayout in C# code example

---
+++ MAUI/03. Layouts/03.1. StackLayout/StackLayoutApp/StackLayoutStartPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackLayoutApp
+{
+    public class StackLayoutStartPage : ContentPage
+    {
+        Label label1 = new Label()
+        {
+            Text = "First label",
+            TextColor = Colors.Red
+        };
+
+        Label label2 = new Label()
+        {
+            Text = "Second label",
+            TextColor = Colors.Blue
+        };
+
+        Label label3 = new Label()
+        {
+            Text = "Third label",
+            TextColor = Colors.Green
+        };
+
+        public StackLayoutStartPage()
+        {
+            //StackLayout example:
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { label1, label2, label3 }
+            };
+
+            stackLayout.Orientation = StackOrientation.Horizontal;
+        }
+    }
+}
