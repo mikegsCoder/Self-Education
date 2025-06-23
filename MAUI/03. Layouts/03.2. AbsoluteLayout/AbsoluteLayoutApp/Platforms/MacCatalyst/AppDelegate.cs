380a76c0f54ea9e7841ca93ed1a071d2263386d8
mikegsCoder <mikegsid@gmail.com>
2025-06-23 06:29:19 +0300

    Add AbsoluteLayoutApp

---
+++ MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AbsoluteLayoutApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
