ef7a808eab2d2568a1f569b68c0fc8de77306685
mikegsCoder <mikegsid@gmail.com>
2025-06-24 06:52:10 +0300

    Add AbsoluteLayout example in C# code

---
+++ MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/AbsoluteLayoutStartPage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteLayoutApp
+{
+    public class AbsoluteLayoutStartPage : ContentPage
+    {
+        public AbsoluteLayoutStartPage()
+        {
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+
+            // AbsoluteLayout example:
+            // define elements:
+            BoxView boxView = new BoxView { Color = Colors.LightBlue };
+            Label label = new Label { Text = ".NET MAUI Absolute Layout", FontSize = 20 };
+
+            // add elements:
+            absoluteLayout.Add(boxView);
+            absoluteLayout.Add(label);
+
+            // set location and size:
+            absoluteLayout.SetLayoutBounds(
+                boxView,
+                new Rect(30, 70, 390, 100)
+            );
+
+            absoluteLayout.SetLayoutBounds(
+                label,
+                //new Rect(50, 100, 300, 40)
+                new Rect(50, 100, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
+            );
+        }
+    }
+}
--- MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/App.xaml.cs
+++ MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/App.xaml.cs
@@ -9,7 +9,9 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage());
+            //return new Window(new MainPage());
+
+            return new Window(new AbsoluteLayoutStartPage());
         }
     }
 }
