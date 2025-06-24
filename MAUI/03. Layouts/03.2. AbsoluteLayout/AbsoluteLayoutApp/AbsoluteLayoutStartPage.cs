25713dbdbcc2fbf72f8d20debf3cadbb3bbb6f62
mikegsCoder <mikegsid@gmail.com>
2025-06-24 06:54:03 +0300

    Add new example

--- MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/AbsoluteLayoutStartPage.cs
+++ MAUI/03. Layouts/03.2. AbsoluteLayout/AbsoluteLayoutApp/AbsoluteLayoutStartPage.cs
@@ -14,25 +14,48 @@ namespace AbsoluteLayoutApp
             AbsoluteLayout absoluteLayout = new AbsoluteLayout();
 
             // AbsoluteLayout example:
+            //// define elements:
+            //BoxView boxView = new BoxView { Color = Colors.LightBlue };
+            //Label label = new Label { Text = ".NET MAUI Absolute Layout", FontSize = 20 };
+
+            //// add elements:
+            //absoluteLayout.Add(boxView);
+            //absoluteLayout.Add(label);
+
+            //// set location and size:
+            //absoluteLayout.SetLayoutBounds(
+            //    boxView,
+            //    new Rect(30, 70, 390, 100)
+            //);
+
+            //absoluteLayout.SetLayoutBounds(
+            //    label,
+            //    //new Rect(50, 100, 300, 40)
+            //    new Rect(50, 100, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
+            //);
+
+            // example with AbsoluteLayout.SetLayoutFlags():
             // define elements:
-            BoxView boxView = new BoxView { Color = Colors.LightBlue };
-            Label label = new Label { Text = ".NET MAUI Absolute Layout", FontSize = 20 };
+            BoxView redBox = new BoxView { BackgroundColor = Colors.Red };
+            BoxView greenBox = new BoxView { BackgroundColor = Colors.Green };
+            BoxView blueBox = new BoxView { BackgroundColor = Colors.Blue };
 
-            // add elements:
-            absoluteLayout.Add(boxView);
-            absoluteLayout.Add(label);
+            AbsoluteLayout.SetLayoutBounds(redBox, new Rect(0.2, 0.4, 50, 80));
+            // set proportional coordinates:
+            AbsoluteLayout.SetLayoutFlags(redBox, AbsoluteLayoutFlags.PositionProportional);
 
-            // set location and size:
-            absoluteLayout.SetLayoutBounds(
-                boxView,
-                new Rect(30, 70, 390, 100)
-            );
+            AbsoluteLayout.SetLayoutBounds(greenBox, new Rect(1, 0.2, 50, 80));
+            AbsoluteLayout.SetLayoutFlags(greenBox, AbsoluteLayoutFlags.PositionProportional);
 
-            absoluteLayout.SetLayoutBounds(
-                label,
-                //new Rect(50, 100, 300, 40)
-                new Rect(50, 100, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
-            );
+            AbsoluteLayout.SetLayoutBounds(blueBox, new Rect(0.4, 0.8, 0.2, 0.2));
+            // set proportional coordinates and size:
+            AbsoluteLayout.SetLayoutFlags(blueBox, AbsoluteLayoutFlags.All);
+
+            absoluteLayout.Children.Add(redBox);
+            absoluteLayout.Children.Add(greenBox);
+            absoluteLayout.Children.Add(blueBox);
+
+            Content = absoluteLayout;
         }
     }
 }
