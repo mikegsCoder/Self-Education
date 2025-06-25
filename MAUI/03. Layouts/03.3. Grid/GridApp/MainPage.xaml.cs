a9b6aa9f9a833eab224a930933e1803dd45e8640
mikegsCoder <mikegsid@gmail.com>
2025-06-25 06:21:44 +0300

    Add GridApp

---
+++ MAUI/03. Layouts/03.3. Grid/GridApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GridApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/03. Layouts/03.3. Grid/GridApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GridApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
