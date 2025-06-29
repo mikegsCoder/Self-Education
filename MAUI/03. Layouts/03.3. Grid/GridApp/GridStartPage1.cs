c5bcd8729dda9ddb4bc24abb6c4871ece767fdeb
mikegsCoder <mikegsid@gmail.com>
2025-06-29 08:35:07 +0300

    Add Grid example in C# code

---
+++ MAUI/03. Layouts/03.3. Grid/GridApp/GridStartPage1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridApp
+{
+    public class GridStartPage1 : ContentPage
+    {
+        public GridStartPage1()
+        {
+            // Grid example:
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition(),
+                    new RowDefinition()
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition(),
+                    new ColumnDefinition(),
+                    new ColumnDefinition()
+                }
+            };
+
+            grid.Add(new BoxView { Color = Colors.Red }, 0, 0);
+            grid.Add(new BoxView { Color = Colors.Blue }, 0, 1);
+
+            grid.Add(new BoxView { Color = Colors.Teal }, 1, 0);
+            grid.Add(new BoxView { Color = Colors.Green }, 1, 1);
+
+            grid.Add(new BoxView { Color = Colors.Olive }, 2, 0);
+            grid.Add(new BoxView { Color = Colors.Pink }, 2, 1);
+
+            Content = grid;
+        }
+    }
+}
