d1171007e93f4fb99cbdef2b9ebad6fb28fd1ae5
mikegsCoder <mikegsid@gmail.com>
2025-07-03 18:22:17 +0300

    Add margin and padding example in C# code

---
+++ MAUI/04. Controls/04.01. Size-And-Positioning/SizeAndPositioningApp/StartPage2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeAndPositioningApp
+{
+    public class StartPage2 : ContentPage
+    {
+        public StartPage2()
+        {
+            // margin and padding example:
+            var stackLayout = new StackLayout
+            {
+                Padding = new Thickness(60),
+                Children =
+                {
+                    new BoxView { Color = Colors.Blue, Margin = new Thickness (50), HeightRequest = 100 },
+                    new BoxView { Color = Colors.Red, Margin = new Thickness (50), HeightRequest = 100 }
+                }
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
