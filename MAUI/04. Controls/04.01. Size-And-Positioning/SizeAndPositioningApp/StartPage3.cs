4ac98e1c54bceb06cfb47242a6d81d7a6388ba7f
mikegsCoder <mikegsid@gmail.com>
2025-07-03 18:23:29 +0300

    Add another example in C# code

---
+++ MAUI/04. Controls/04.01. Size-And-Positioning/SizeAndPositioningApp/StartPage3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeAndPositioningApp
+{
+    public class StartPage3 : ContentPage
+    {
+        public StartPage3()
+        {
+            // more margin and padding examples:
+            var stackLayout = new StackLayout
+            {
+                Padding = new Thickness(0, 20, 0, 0),
+                Children = 
+                {
+                    new BoxView { Color = Colors.Green, Margin = new Thickness (20) },
+                    new BoxView { Color = Colors.Blue, Margin = new Thickness (10, 25) },
+                    new BoxView { Color = Colors.Red, Margin = new Thickness (0, 20, 15, 5) }
+                }
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
