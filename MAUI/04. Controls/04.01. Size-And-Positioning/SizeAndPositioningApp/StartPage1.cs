0033a8b6637b8c3334e1b111cf59d5f6cbf3b613
mikegsCoder <mikegsid@gmail.com>
2025-07-03 18:20:52 +0300

    Add size example in C# code

---
+++ MAUI/04. Controls/04.01. Size-And-Positioning/SizeAndPositioningApp/StartPage1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeAndPositioningApp
+{
+    public class StartPage1 : ContentPage
+    {
+        public StartPage1()
+        {
+            // size example:
+            Button button = new Button
+            {
+                WidthRequest = 100,
+                HeightRequest = 50,
+                Text = "Click"
+            };
+
+            Content = new Grid { Children = { button } };
+        }
+    }
+}
