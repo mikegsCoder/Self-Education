55b9477c39c3e13742d0f7b0c051bffd9e99b9ac
mikegsCoder <mikegsid@gmail.com>
2025-07-04 06:32:14 +0300

    Add new horizontal and vertical alignment example in C# code

---
+++ MAUI/04. Controls/04.01. Size-And-Positioning/SizeAndPositioningApp/StartPage5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeAndPositioningApp
+{
+    public class StartPage5 : ContentPage
+    {
+        public StartPage5()
+        {
+            // horizontal and vertical alignment example:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            header.VerticalOptions = LayoutOptions.Center;
+            header.HorizontalOptions = LayoutOptions.Center;
+
+            Content = header;
+        }
+    }
+}
