bf6667eecb3a0ba4f4f3fdbb774927dcac254f0f
mikegsCoder <mikegsid@gmail.com>
2025-07-04 06:29:54 +0300

    Add new example in C# code

---
+++ MAUI/04. Controls/04.01. Size-And-Positioning/SizeAndPositioningApp/StartPage4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeAndPositioningApp
+{
+    public class StartPage4 : ContentPage
+    {
+        public StartPage4()
+        {
+            // set padding top for iOS:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+
+            Content = header;
+
+            if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            }
+        }
+    }
+}
