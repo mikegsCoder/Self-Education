6c9e2c557e2a6ebe9f1fa02ae5dff0fd7efc38b4
mikegsCoder <mikegsid@gmail.com>
2025-07-05 09:11:46 +0300

    Add colors exampe in C# code

---
+++ MAUI/04. Controls/04.02. Work-With-Colors/ColorsApp/StartPage1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsApp
+{
+    public class StartPage1 : ContentPage
+    {
+        public StartPage1()
+        {
+            // work with colors example:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+
+            header.BackgroundColor = Colors.LightBlue;  // background color
+            header.TextColor = Colors.DarkBlue;         // text color
+            
+            Content = header;
+        }
+    }
+}
