b7e7da74e44f1f0bfb8dfa0523bab085f954c9e6
mikegsCoder <mikegsid@gmail.com>
2025-07-06 09:13:46 +0300

    Add work with colors example in C# code

---
+++ MAUI/04. Controls/04.02. Work-With-Colors/ColorsApp/StartPage2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsApp
+{
+    public class StartPage2 : ContentPage
+    {
+        public StartPage2()
+        {
+            // work with colors another example:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+
+            header.BackgroundColor = new Color(178, 223, 219);  // background color
+            header.TextColor = new Color(0, 77, 64);            // text color
+            
+            Content = header;
+        }
+    }
+}
