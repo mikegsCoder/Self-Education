b622f09a0be7ca8f1a524fc6ba9f3656f20dd8a9
mikegsCoder <mikegsid@gmail.com>
2025-07-07 06:26:13 +0300

    Add another example

---
+++ MAUI/04. Controls/04.02. Work-With-Colors/ColorsApp/StartPage4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsApp
+{
+    public class StartPage4 : ContentPage
+    {
+        public StartPage4()
+        {
+            // work with colors more examples:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            
+            Color customColor = Colors.Teal;
+            customColor = customColor.WithAlpha(0.5f);
+            customColor = customColor.WithHue(0.5f);
+            customColor = customColor.WithLuminosity(0.9f);
+
+            header.BackgroundColor = customColor;
+
+            Content = header;
+        }
+    }
+}
