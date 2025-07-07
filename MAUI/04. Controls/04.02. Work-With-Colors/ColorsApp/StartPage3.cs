7a00924f805db92723167c6030f80d059e76d430
mikegsCoder <mikegsid@gmail.com>
2025-07-07 06:24:09 +0300

    Add another example in C# code

---
+++ MAUI/04. Controls/04.02. Work-With-Colors/ColorsApp/StartPage3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsApp
+{
+    public class StartPage3 : ContentPage
+    {
+        public StartPage3()
+        {
+            // work with colors FromArgb() example:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+
+            header.BackgroundColor = Color.FromArgb("#B2DFDB");  // background color
+            header.TextColor = Color.FromArgb("#aa004D40");      // text color
+            
+            Content = header;
+        }
+    }
+}
