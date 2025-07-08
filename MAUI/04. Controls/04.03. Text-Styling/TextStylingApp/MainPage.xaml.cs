38d6cfec937627869852de4639415b0730d89185
mikegsCoder <mikegsid@gmail.com>
2025-07-08 06:33:38 +0300

    Add Text styling app

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TextStylingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TextStylingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
