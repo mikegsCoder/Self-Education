24c369f3bffba2bc8fa95dd781d6741ecb8fd274
mikegsCoder <mikegsid@gmail.com>
2025-07-09 06:29:51 +0300

    Add text styling example in C# code

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/StartPage1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStylingApp
+{
+    public class StartPage1 : ContentPage
+    {
+        public StartPage1()
+        {
+            // text styling example:
+            Label header = new Label() { Text = ".NET MAUI in Arial" };
+            header.FontFamily = "Arial";
+
+            Content = header;
+        }
+    }
+}
