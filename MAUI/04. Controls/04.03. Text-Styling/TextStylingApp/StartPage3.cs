532cf54a360fa35f351773e7bea75acab59a2aa6
mikegsCoder <mikegsid@gmail.com>
2025-07-10 06:42:25 +0300

    Add FontAttributes example in C# code

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/StartPage3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStylingApp
+{
+    public class StartPage3 : ContentPage
+    {
+        public StartPage3()
+        {
+            // FontAttributes example:
+            Label label1 = new Label
+            {
+                Text = "Bold",
+                FontAttributes = FontAttributes.Bold
+            };
+
+            Label label2 = new Label
+            {
+                Text = "Bold and Italics",
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+
+            StackLayout layout = new StackLayout() { label1, label2 };
+
+            Content = layout;
+        }
+    }
+}
