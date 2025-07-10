7252f4bba7d7a1516a6689f5f28c91ce40c46f62
mikegsCoder <mikegsid@gmail.com>
2025-07-10 06:40:51 +0300

    Add FontSize example in C# code

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/StartPage2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStylingApp
+{
+    public class StartPage2 : ContentPage
+    {
+        public StartPage2()
+        {
+            // FontSize example:
+            Label header = new Label() { Text = ".NET MAUI" };
+            header.FontSize = 26;
+
+            Content = header;
+        }
+    }
+}
