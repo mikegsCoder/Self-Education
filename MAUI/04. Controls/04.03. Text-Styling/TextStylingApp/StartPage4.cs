d75328ecbe78349146d5d3d45035da12794f6910
mikegsCoder <mikegsid@gmail.com>
2025-07-10 06:44:29 +0300

    Add fonts example in C# code

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/StartPage4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStylingApp
+{
+    public class StartPage4 : ContentPage
+    {
+        public StartPage4()
+        {
+            // fonts example:
+            Label label = new Label
+            {
+                Text = "OpenSansSemibold",
+                FontSize = 22,
+                FontFamily = "OpenSansSemibold" 
+            };
+
+            Content = label;
+        }
+    }
+}
