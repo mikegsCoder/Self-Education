7eaeb143a38d301bdaef5ed4f4630406dbf022ef
mikegsCoder <mikegsid@gmail.com>
2025-07-11 06:32:08 +0300

    Add new text alignment example

---
+++ MAUI/04. Controls/04.03. Text-Styling/TextStylingApp/StartPage5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStylingApp
+{
+    public class StartPage5 : ContentPage
+    {
+        public StartPage5()
+        {
+            // text alignment example:
+            Label header = new Label() { Text = "Hello from MAUI!" };
+            
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+
+            Content = header;
+        }
+    }
+}
