f0ef3e51afd2337ca7b8f1d6248f9cd9738ae74d
mikegsCoder <mikegsid@gmail.com>
2025-07-12 09:39:44 +0300

    Add ButtonsApp

---
+++ MAUI/04. Controls/04.04. Buttons/ButtonsApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ButtonsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Clicked!";
+        }
+    }
+
+}
