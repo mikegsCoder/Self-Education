4a4688609bfacee608a566cfa1c23b23d990b136
mikegsCoder <mikegsid@gmail.com>
2025-07-12 09:42:22 +0300

    Add new example in C# code

---
+++ MAUI/04. Controls/04.04. Buttons/ButtonsApp/StartPage.cs
@@ -0,0 +1,32 @@
+﻿namespace ButtonsApp
+{
+    public class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Grid grid = new Grid();
+
+            Button button = new Button
+            {
+                Text = "Click me",
+                FontSize = 22,
+                BorderWidth = 1,
+                BackgroundColor = Colors.LightPink,
+                TextColor = Colors.DarkRed,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            button.Clicked += OnButtonClicked;
+
+            grid.Children.Add(button);
+            Content = grid;
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Clicked!";
+        }
+    }
+}
