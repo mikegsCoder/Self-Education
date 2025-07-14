5fa8a97aca4b96de236d6b2f3e3efd0f4ea05357
mikegsCoder <mikegsid@gmail.com>
2025-07-14 06:41:26 +0300

    Add Label example in C# code

---
+++ MAUI/04. Controls/04.05. Label/LabelApp/StartPage1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // Label example:
+        public StartPage1()
+        {
+            Grid grid = new Grid();
+
+            Label label = new Label
+            {
+                Text = "Hello from MAUI!",
+                TextDecorations = TextDecorations.Underline,
+                CharacterSpacing = 2,
+                FontAttributes = FontAttributes.Bold,
+                FontFamily = "Helvetica",
+                FontSize = 22,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            grid.Children.Add(label);
+
+            Content = grid;
+        }
+    }
+}
