4d3b88116c3fd6d07a5df0be91f09f55d067d78f
mikegsCoder <mikegsid@gmail.com>
2025-07-14 06:43:56 +0300

    Add FormattedString example in C# code

---
+++ MAUI/04. Controls/04.05. Label/LabelApp/StartPage2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelApp
+{
+    public class StartPage2 : ContentPage
+    {
+        //FormattedString example:
+        public StartPage2()
+        {
+            Grid grid = new Grid();
+            Label header = new Label();
+            FormattedString formattedString = new FormattedString();
+            
+            formattedString.Spans.Add(new Span
+            {
+                Text = "Hello ",
+                FontSize = 22
+            });
+
+            formattedString.Spans.Add(new Span
+            {
+                Text = "from",
+                TextColor = Colors.DarkRed,
+                BackgroundColor = Colors.LightPink,
+            });
+
+            formattedString.Spans.Add(new Span
+            {
+                Text = " MAUI!",
+                FontAttributes = FontAttributes.Bold
+            });
+
+            header.FormattedText = formattedString;
+
+            header.VerticalOptions = LayoutOptions.Center;
+            header.HorizontalOptions = LayoutOptions.Center;
+
+            grid.Children.Add(header);
+
+            Content = grid;
+        }
+    }
+}
