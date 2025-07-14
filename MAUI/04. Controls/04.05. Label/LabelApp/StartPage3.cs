869dc791fdd411d519da2233ca565f28f54fd0ce
mikegsCoder <mikegsid@gmail.com>
2025-07-14 06:49:01 +0300

    Add multi line text example in C# code

---
+++ MAUI/04. Controls/04.05. Label/LabelApp/StartPage3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelApp
+{
+    public class StartPage3 : ContentPage
+    {
+        //Multi line text example:
+        public StartPage3()
+        {
+            Grid grid = new Grid();
+
+            Label header = new Label();
+
+            header.Text = "First line,\n" +
+                "Second line,\n" +
+                "Third line,\n" +
+                "Fourth line.\n";
+
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+            header.FontSize = 22;
+
+            grid.Children.Add(header);
+
+            Content = grid;
+        }
+    }
+}
