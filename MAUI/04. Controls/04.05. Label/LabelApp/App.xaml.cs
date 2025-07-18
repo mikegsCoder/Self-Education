4eeede3feb9e571d31a8eb0ebc83be1ebb33dae2
mikegsCoder <mikegsid@gmail.com>
2025-07-18 16:35:30 +0300

    Add Label click handling example

--- MAUI/04. Controls/04.05. Label/LabelApp/App.xaml.cs
+++ MAUI/04. Controls/04.05. Label/LabelApp/App.xaml.cs
@@ -13,7 +13,8 @@
 
             //return new Window(new StartPage1());
             //return new Window(new StartPage2());
-            return new Window(new StartPage3());
+            //return new Window(new StartPage3());
+            return new Window(new StartPage4());
         }
     }
 }
---
+++ MAUI/04. Controls/04.05. Label/LabelApp/StartPage4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelApp
+{
+    public class StartPage4 : ContentPage
+    {
+        //Label click handling example:
+        public StartPage4()
+        {
+            Grid grid = new Grid();
+
+            Label label = new Label
+            {
+                Text = "Hello from MAUI!",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = 22
+            };
+
+            TapGestureRecognizer tapGesture = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 2
+            };
+
+            int count = 0;  // click counter
+
+            tapGesture.Tapped += (s, e) =>
+            {
+                count++;
+                label.Text = $"Clicked {count} times";
+            };
+
+            label.GestureRecognizers.Add(tapGesture);
+
+            grid.Children.Add(label);
+
+            Content = grid;
+        }
+    }
+}
