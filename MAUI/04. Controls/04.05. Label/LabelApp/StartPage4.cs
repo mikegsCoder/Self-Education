25972d41a7520265b99d886788999bf68be84af9
mikegsCoder <mikegsid@gmail.com>
2025-07-18 16:36:40 +0300

    Code cleanup

--- MAUI/04. Controls/04.05. Label/LabelApp/StartPage4.cs
+++ MAUI/04. Controls/04.05. Label/LabelApp/StartPage4.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LabelApp
+﻿namespace LabelApp
 {
     public class StartPage4 : ContentPage
     {
