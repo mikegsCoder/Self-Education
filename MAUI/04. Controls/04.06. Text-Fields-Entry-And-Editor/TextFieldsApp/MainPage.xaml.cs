9b4195ba8237d168abfe185ccd545f531b1b15f2
mikegsCoder <mikegsid@gmail.com>
2025-07-19 07:49:03 +0300

    Add TextFieldsApp

---
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace TextFieldsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //void nameEntry_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    nameLabel.Text = nameEntry.Text;
+
+        //    string oldText = e.OldTextValue;
+        //    string newText = e.NewTextValue;
+        //}
+    }
+
+}
---
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TextFieldsApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
