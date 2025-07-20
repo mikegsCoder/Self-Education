5dfca8c81ca79433e483808a85e8a7cd8fc86d3a
mikegsCoder <mikegsid@gmail.com>
2025-07-20 12:26:33 +0300

    Add TextChanged example in C# code

---
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/StartPage2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TextFieldsApp
+{
+    public class StartPage2 : ContentPage
+    {
+        Label nameLabel;
+        Entry nameEntry;
+
+        // TextChanged example:
+        public StartPage2()
+        {
+            StackLayout stackLayout = new StackLayout();
+
+            nameEntry = new Entry { FontSize = 22 };
+            nameEntry.TextChanged += nameEntry_TextChanged;
+            nameLabel = new Label { FontSize = 22 };
+
+            stackLayout.Children.Add(nameEntry);
+            stackLayout.Children.Add(nameLabel);
+
+            Content = stackLayout;
+        }
+
+        void nameEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            nameLabel.Text = nameEntry.Text;
+        }
+    }
+}
