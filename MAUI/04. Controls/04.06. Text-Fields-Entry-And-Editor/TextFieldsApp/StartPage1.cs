946109418657b358f2848ecc6ea99cbb75a5f5f4
mikegsCoder <mikegsid@gmail.com>
2025-07-20 12:24:29 +0300

    Add Entry example in C# code

---
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/StartPage1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFieldsApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // Entry example:
+        public StartPage1()
+        {
+            StackLayout stackLayout = new StackLayout();
+
+            Entry entry = new Entry
+            {
+                Placeholder = "Enter your email",
+                FontFamily = "Helvetica",
+                FontSize = 22,
+                MaxLength = 20,
+                Keyboard = Keyboard.Email
+            };
+
+            stackLayout.Children.Add(entry);
+
+            Content = stackLayout;
+        }
+    }
+}
