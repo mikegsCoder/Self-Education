2a7c6376c709796011cf95e034e809fecb47c32f
mikegsCoder <mikegsid@gmail.com>
2025-07-20 12:27:33 +0300

    Code cleanup

--- MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/StartPage1.cs
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/StartPage1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextFieldsApp
+﻿namespace TextFieldsApp
 {
     public class StartPage1 : ContentPage
     {
