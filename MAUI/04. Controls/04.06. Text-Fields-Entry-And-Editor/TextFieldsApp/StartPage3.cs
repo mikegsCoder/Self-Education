e3ab242e1e66ad330b17461f50825e7d23f7c72d
mikegsCoder <mikegsid@gmail.com>
2025-07-21 06:16:35 +0300

    Add Editor example

---
+++ MAUI/04. Controls/04.06. Text-Fields-Entry-And-Editor/TextFieldsApp/StartPage3.cs
@@ -0,0 +1,17 @@
+﻿namespace TextFieldsApp
+{
+    public class StartPage3 : ContentPage
+    {
+        // Editor example:
+        public StartPage3()
+        {
+            StackLayout stackLayout = new StackLayout();
+
+            Editor textEditor = new Editor { HeightRequest = 200, FontSize = 16 };
+            
+            stackLayout.Children.Add(textEditor);
+
+            Content = stackLayout;
+        }
+    }
+}
