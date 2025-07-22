a33e16af66143349fcc3714ee2ccd000d6f202cb
mikegsCoder <mikegsid@gmail.com>
2025-07-22 06:56:21 +0300

    Add BoxView example in C# code

---
+++ MAUI/04. Controls/04.07. BoxView/BoxViewApp/StartPage1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxViewApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // BoxView example:
+        public StartPage1()
+        {
+            BoxView boxView = new BoxView
+            {
+                Color = Colors.LightBlue,
+                CornerRadius = 8,
+                WidthRequest = 150,
+                HeightRequest = 150,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Grid grid = new Grid();
+
+            grid.Children.Add(boxView);
+
+            Content = grid;
+        }
+    }
+}
