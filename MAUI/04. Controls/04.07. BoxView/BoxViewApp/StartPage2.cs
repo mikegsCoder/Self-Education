0675902b576a79d37548497ce7362aeafd6d2905
mikegsCoder <mikegsid@gmail.com>
2025-07-23 06:40:25 +0300

    Add new BoxView example in C# code

---
+++ MAUI/04. Controls/04.07. BoxView/BoxViewApp/StartPage2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxViewApp
+{
+    public class StartPage2 : ContentPage
+    {
+        // BoxView example:
+        public StartPage2()
+        {
+            Label label1 = new Label
+            {
+                Text = ".NET MAUI",
+                FontSize = 22,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Label label2 = new Label
+            {
+                Text = "Multiplatform Application User Interface",
+                FontSize = 18,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            BoxView boxView1 = new BoxView
+            {
+                Color = Colors.Gray,
+                HeightRequest = 2,
+                Margin = new Thickness(1),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            BoxView boxView2 = new BoxView
+            {
+                Color = Colors.Gray,
+                HeightRequest = 2,
+                Margin = 1,
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Children = { label1, boxView1, boxView2, label2 },
+                Margin = new Thickness(20),
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
