b597da0874c978cd55fb48767cea23648ff8989b
mikegsCoder <mikegsid@gmail.com>
2025-07-24 06:41:17 +0300

    Add ScrollView example

---
+++ MAUI/04. Controls/04.08. ScrollView/ScrollViewApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ScrollViewApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
