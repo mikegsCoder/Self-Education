5b7c35e1743474be6b3ffd2304ce2749ce87d09c
mikegsCoder <mikegsid@gmail.com>
2025-07-27 14:44:19 +0300

    Add ImageApp

---
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ImageApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
