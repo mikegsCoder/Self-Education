68705c391c1ce114068fd4e6a45e42f75c881193
mikegsCoder <mikegsid@gmail.com>
2025-07-28 06:46:18 +0300

    Add Image FromUri example 2

--- MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage2.cs
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage2.cs
@@ -12,10 +12,13 @@ namespace ImageApp
         public StartPage2()
         {
             // example 1:
-            Image image = new Image
-            {
-                Source = ImageSource.FromUri(new Uri("https://images.rawpixel.com/image_png_social_landscape/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdXB3azYxNzMxNjE4LXdpa2ltZWRpYS1pbWFnZS1qb2I2MjNfMi1sMXBxYmwxMy5wbmc.png"))
-            };
+            //Image image = new Image
+            //{
+            //    Source = ImageSource.FromUri(new Uri("https://images.rawpixel.com/image_png_social_landscape/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdXB3azYxNzMxNjE4LXdpa2ltZWRpYS1pbWFnZS1qb2I2MjNfMi1sMXBxYmwxMy5wbmc.png"))
+            //};
+
+            // example 2:
+            Image image = new Image { Source = "https://images.rawpixel.com/image_png_social_landscape/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvdXB3azYxNzMxNjE4LXdpa2ltZWRpYS1pbWFnZS1qb2I2MjNfMi1sMXBxYmwxMy5wbmc.png" };
 
             Content = image;
         }
