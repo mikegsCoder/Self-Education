776af887c53bbacebafcce24ab1a21ba794716c7
mikegsCoder <mikegsid@gmail.com>
2025-07-28 06:36:29 +0300

    Add Image FromFile example

---
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // Image FromFile example:
+        public StartPage1()
+        {
+            // example 1:
+            Image image = new Image
+            {
+                Source = ImageSource.FromFile("forest.jpg")
+            };
+
+            Content = image;
+        }
+    }
+}
