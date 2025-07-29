49d1df9995773888ec5812783c88c8621bdddc54
mikegsCoder <mikegsid@gmail.com>
2025-07-29 06:34:12 +0300

    Add Image from embedded resource example

---
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp
+{
+    public class StartPage4 : ContentPage
+    {
+        // Image from embedded resource example:
+        public StartPage4()
+        {
+            Image image = new Image
+            {
+                Source = ImageSource.FromResource("ImageApp.Resources.Images.forest1.jpg")
+            };
+
+            Content = image;
+        }
+    }
+}
