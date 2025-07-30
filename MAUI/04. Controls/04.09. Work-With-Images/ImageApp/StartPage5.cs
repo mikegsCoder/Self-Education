b92f08db2c7d5670c17297d0ad4313d4687ea6ac
mikegsCoder <mikegsid@gmail.com>
2025-07-30 06:38:35 +0300

    Code cleanup

--- MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage5.cs
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage5.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ImageApp
+﻿namespace ImageApp
 {
     public class StartPage5 : ContentPage
     {
