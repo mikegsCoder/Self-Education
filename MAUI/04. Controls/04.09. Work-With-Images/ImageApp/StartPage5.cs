ee950678b8517e035c4ea394df8e7033ce0018da
mikegsCoder <mikegsid@gmail.com>
2025-07-30 06:37:38 +0300

    Add Image Aspect example in C# code

---
+++ MAUI/04. Controls/04.09. Work-With-Images/ImageApp/StartPage5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp
+{
+    public class StartPage5 : ContentPage
+    {
+        // Image Aspect example:
+        public StartPage5()
+        {
+            Grid grid = new Grid { Padding = 2 };
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+
+            var fileName = "forest.jpg";
+
+            grid.Add(new Image { Source = fileName, Aspect = Aspect.AspectFit }, 0, 0);
+            grid.Add(new Image { Source = fileName, Aspect = Aspect.AspectFill }, 1, 0);
+            grid.Add(new Image { Source = fileName, Aspect = Aspect.Fill }, 0, 1);
+            grid.Add(new Image { Source = fileName, Aspect = Aspect.Center }, 1, 1);
+
+            Content = grid;
+        }
+    }
+}
