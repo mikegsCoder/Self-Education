d7d4a18cbf84e0a1f54b5cd7e70b1ec86826b255
mikegsCoder <mikegsid@gmail.com>
2025-08-01 06:52:50 +0300

    Add Border RoundRectangle example in C# code

---
+++ MAUI/04. Controls/04.10. Border/BorderApp/StartPage2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderApp
+{
+    public class StartPage2 : ContentPage
+    {
+        // Border RoundRectangle example:
+        public StartPage2()
+        {
+            Grid grid = new Grid() { Padding = 20 };
+
+            Border border = new Border
+            {
+                Stroke = Colors.Gray,
+                StrokeShape = new RoundRectangle { CornerRadius = 20 },
+                Content = new Label
+                {
+                    Text = "Hello from MAUI!",
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center
+                },
+                BackgroundColor = Color.FromArgb("#e1e1e1")
+            };
+
+            grid.Add(border);
+
+            Content = grid;
+        }
+    }
+}
