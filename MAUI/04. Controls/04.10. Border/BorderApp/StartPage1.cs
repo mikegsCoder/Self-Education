8ead7d5beda149dc18fb60cd5e9a13de1ad0960d
mikegsCoder <mikegsid@gmail.com>
2025-08-01 06:50:10 +0300

    Add new Border example in C# code

---
+++ MAUI/04. Controls/04.10. Border/BorderApp/StartPage1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // Border example:
+        public StartPage1()
+        {
+            Grid grid = new Grid() { Padding = 20 };
+
+            Border border = new Border
+            {
+                Stroke = Colors.Gray,
+                Content = new Label
+                {
+                    Text = "Hello from MAUI!",
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center
+                },
+                BackgroundColor = Color.FromArgb("#e1e1e1")
+            };
+
+            grid.Add(border);
+
+            Content = grid;
+        }
+    }
+}
