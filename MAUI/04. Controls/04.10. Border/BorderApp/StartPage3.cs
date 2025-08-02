48087a82c7db82867de6245d260fe0d3f92475d1
mikegsCoder <mikegsid@gmail.com>
2025-08-02 09:57:51 +0300

    Add new Border example in C# code

---
+++ MAUI/04. Controls/04.10. Border/BorderApp/StartPage3.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderApp
+{
+    public class StartPage3 : ContentPage
+    {
+        // Border example:
+        public StartPage3()
+        {
+            Grid grid = new Grid() { Padding = 20 };
+
+            Border border = new Border
+            {
+                Stroke = Colors.Gray,
+                StrokeShape = new RoundRectangle { CornerRadius = 20 },
+                BackgroundColor = Color.FromArgb("#e1e1e1")
+            };
+
+            Label header = new Label()
+            {
+                Text = ".NET MAUI",
+                HorizontalTextAlignment = TextAlignment.Center,
+                FontSize = 24
+            };
+
+            BoxView separator = new BoxView()
+            {
+                Color = Colors.DarkGray,
+                HeightRequest = 2,
+                Margin = 10
+            };
+
+            Label content = new Label()
+            {
+                Text = ".NET MAUI - technology for creating cross-platform applications.",
+                FontSize = 18
+            };
+
+            StackLayout layout = new StackLayout();
+
+            layout.Children.Add(header);
+            layout.Children.Add(separator);
+            layout.Children.Add(content);
+
+            border.Content = layout;
+
+            grid.Add(border);
+
+            Content = grid;
+        }
+    }
+}
