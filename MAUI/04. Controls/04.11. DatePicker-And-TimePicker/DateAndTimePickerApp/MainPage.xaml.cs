d0922f81c70642838dbb4d3c162f8d303748ec18
mikegsCoder <mikegsid@gmail.com>
2025-08-03 08:39:53 +0300

    Add DateAndTimePickerApp

---
+++ MAUI/04. Controls/04.11. DatePicker-And-TimePicker/DateAndTimePickerApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace DateAndTimePickerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DateSelected(object sender, DateChangedEventArgs e)
+        {
+            if (label is { })
+                label.Text = $"Your choice is: {e.NewDate.ToString("d")}";
+        }
+    }
+}
