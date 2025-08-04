ea63cb89796232a776dd31c9e25c3549f7ccae9d
mikegsCoder <mikegsid@gmail.com>
2025-08-04 06:49:35 +0300

    Add DatePicker example in C# code

---
+++ MAUI/04. Controls/04.11. DatePicker-And-TimePicker/DateAndTimePickerApp/StartPage1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTimePickerApp
+{
+    public class StartPage1 : ContentPage
+    {
+        Label label;
+
+        // DatePicker example:
+        public StartPage1()
+        {
+            label = new Label { Text = "Choose date" };
+
+            DatePicker datePicker = new DatePicker
+            {
+                Format = "d",
+                MaximumDate = DateTime.Now.AddDays(5),
+                MinimumDate = DateTime.Now.AddDays(-5)
+            };
+
+            datePicker.DateSelected += DateSelected;
+
+            StackLayout stack = new StackLayout 
+            { 
+                Children = { label, datePicker }, 
+                Padding = 20 
+            };
+            
+            Content = stack;
+        }
+
+        void DateSelected(object sender, DateChangedEventArgs e)
+        {
+            label.Text = $"Your choice is: {e.NewDate.ToString("d")}";
+        }
+    }
+}
