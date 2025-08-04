028f94854f2665cc46189ac97fced82db389f47d
mikegsCoder <mikegsid@gmail.com>
2025-08-04 06:51:52 +0300

    Add TimePicker example in C# code

---
+++ MAUI/04. Controls/04.11. DatePicker-And-TimePicker/DateAndTimePickerApp/StartPage2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTimePickerApp
+{
+    public class StartPage2 : ContentPage
+    {
+        Label label;
+        TimePicker timePicker;
+
+        // TimePicker example:
+        public StartPage2()
+        {
+            label = new Label 
+            { 
+                Text = "Choose time", 
+                FontSize = 20 
+            };
+
+            timePicker = new TimePicker() { Time = new TimeSpan(17, 0, 0) };
+            timePicker.PropertyChanged += TimePicker_PropertyChanged;
+
+            StackLayout stack = new StackLayout 
+            { 
+                Children = { label, timePicker }, 
+                Padding = 20 
+            };
+            
+            Content = stack;
+        }
+
+        private void TimePicker_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Time")
+            {
+                label.Text = $"Your choice is: {timePicker.Time}.";
+            }
+        }
+    }
+}
