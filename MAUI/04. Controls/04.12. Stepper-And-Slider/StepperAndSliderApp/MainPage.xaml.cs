0ea2676ac43e069e127c08bb1f14842e1f6fc12e
mikegsCoder <mikegsid@gmail.com>
2025-08-05 06:51:36 +0300

    Add StepperAndSliderApp

---
+++ MAUI/04. Controls/04.12. Stepper-And-Slider/StepperAndSliderApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StepperAndSliderApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            header.Text = $"Your choice is: {e.NewValue:F1}";
+        }
+    }
+}
