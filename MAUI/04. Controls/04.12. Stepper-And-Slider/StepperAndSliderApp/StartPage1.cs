78004ff6d1a8acc5c7e930c1fb8a7d13beceb310
mikegsCoder <mikegsid@gmail.com>
2025-08-06 06:46:47 +0300

    Add Stepper example in C# code

---
+++ MAUI/04. Controls/04.12. Stepper-And-Slider/StepperAndSliderApp/StartPage1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepperAndSliderApp
+{
+    public class StartPage1 : ContentPage
+    {
+        Label header;
+
+        // Stepper example:
+        public StartPage1()
+        {
+            header = new Label { FontSize = 18, Margin = 5 };
+
+            Stepper stepper = new Stepper
+            {
+                Minimum = 0,
+                Maximum = 10,
+                Increment = 0.1,
+                VerticalOptions = LayoutOptions.Start
+            };
+
+            stepper.ValueChanged += OnStepperValueChanged;
+
+            Content = new StackLayout { Children = { stepper, header }, Orientation = StackOrientation.Horizontal };
+        }
+
+        private void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            header.Text = $"Your choice is: {e.NewValue:F1}";
+        }
+    }
+}
