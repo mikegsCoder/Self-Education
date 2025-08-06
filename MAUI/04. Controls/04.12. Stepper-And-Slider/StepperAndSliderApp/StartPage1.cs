e142dee33cadc7f00cf84c238c8e2eebc7d6a50c
mikegsCoder <mikegsid@gmail.com>
2025-08-06 06:53:46 +0300

    Code cleanup

--- MAUI/04. Controls/04.12. Stepper-And-Slider/StepperAndSliderApp/StartPage1.cs
+++ MAUI/04. Controls/04.12. Stepper-And-Slider/StepperAndSliderApp/StartPage1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StepperAndSliderApp
+﻿namespace StepperAndSliderApp
 {
     public class StartPage1 : ContentPage
     {
