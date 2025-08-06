c510002cb7f2d120aacc2d68655a768deff41cf2
mikegsCoder <mikegsid@gmail.com>
2025-08-06 06:52:42 +0300

    Add Slider example in C# code

---
+++ MAUI/04. Controls/04.12. Stepper-And-Slider/StepperAndSliderApp/StartPage2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepperAndSliderApp
+{
+    public class StartPage2 : ContentPage
+    {
+        Label header;
+
+        // Slider example:
+        public StartPage2()
+        {
+            header = new Label { FontSize = 18, Margin = 5 };
+
+            Slider slider = new Slider
+            {
+                Maximum = 50,
+                Minimum = 0,
+                Value = 30,
+                ThumbColor = Colors.DeepPink,
+                MinimumTrackColor = Colors.DeepPink,
+                MaximumTrackColor = Colors.LightPink
+            };
+
+            slider.ValueChanged += OnSliderValueChanged;
+
+            Content = new StackLayout { Children = { header, slider }, Padding = 20 };
+        }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            header.Text = $"Your choice is: {e.NewValue:F1}";
+        }
+    }
+}
