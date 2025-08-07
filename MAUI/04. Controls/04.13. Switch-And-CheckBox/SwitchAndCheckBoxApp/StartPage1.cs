8303f9a7d2bcb2512f45622a763e485db0a78c0c
mikegsCoder <mikegsid@gmail.com>
2025-08-07 06:42:14 +0300

    Add Switch example in C# code

---
+++ MAUI/04. Controls/04.13. Switch-And-CheckBox/SwitchAndCheckBoxApp/StartPage1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchAndCheckBoxApp
+{
+    public class StartPage1 : ContentPage
+    {
+        Label label;
+
+        // Switch example:
+        public StartPage1()
+        {
+            Switch switcher = new Switch { IsToggled = true };
+
+            switcher.Toggled += switcher_Toggled;
+
+            label = new Label
+            {
+                FontSize = 16,
+                Text = $"Value: {switcher.IsToggled}"
+            };
+
+            Content = new StackLayout { Children = { switcher, label }, Padding = 8 };
+        }
+
+        private void switcher_Toggled(object sender, ToggledEventArgs e)
+        {
+            label.Text = $"Value: {e.Value}";
+        }
+    }
+}
