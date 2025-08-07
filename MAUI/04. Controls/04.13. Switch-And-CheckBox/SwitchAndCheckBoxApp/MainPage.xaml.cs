75ec248f3d333dca3a82dc75a655a19853592fe0
mikegsCoder <mikegsid@gmail.com>
2025-08-07 06:35:59 +0300

    Add SwitchAndCheckBoxApp

---
+++ MAUI/04. Controls/04.13. Switch-And-CheckBox/SwitchAndCheckBoxApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SwitchAndCheckBoxApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void switcher_Toggled(object sender, ToggledEventArgs e)
+        {
+            label.Text = $"Value: {e.Value}";
+        }
+    }
+}
