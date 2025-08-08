ccb551e2919b7bcd6c0b5c072553548dac21efff
mikegsCoder <mikegsid@gmail.com>
2025-08-08 06:57:09 +0300

    Add CheckBox example

---
+++ MAUI/04. Controls/04.13. Switch-And-CheckBox/SwitchAndCheckBoxApp/StartPage2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchAndCheckBoxApp
+{
+    public class StartPage2 : ContentPage
+    {
+        Label statusLabel;
+
+        // CheckBox example:
+        public StartPage2()
+        {
+            CheckBox statusCheckBox = new CheckBox { IsChecked = false };
+
+            statusCheckBox.CheckedChanged += CheckBox_CheckedChanged;
+
+            Label statusHeaderLabel = new Label { FontSize = 16, Text = "On/Off", Margin = 4 };
+            
+            StackLayout checkboxStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { statusCheckBox, statusHeaderLabel }
+            };
+
+            statusLabel = new Label
+            {
+                FontSize = 18,
+                Text = "Status: Off"
+            };
+
+            Content = new StackLayout { Children = { statusLabel, checkboxStack }, Padding = 8 };
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            statusLabel.Text = $"Status: {(e.Value ? "On" : "Off")}";
+        }
+    }
+}
