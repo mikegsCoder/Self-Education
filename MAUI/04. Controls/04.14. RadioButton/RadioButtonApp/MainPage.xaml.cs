23b5be61bd4085937783be0dd5ce5075df2568e7
mikegsCoder <mikegsid@gmail.com>
2025-08-09 09:27:45 +0300

    Add RadioButton example

--- MAUI/04. Controls/04.14. RadioButton/RadioButtonApp/MainPage.xaml.cs
+++ MAUI/04. Controls/04.14. RadioButton/RadioButtonApp/MainPage.xaml.cs
@@ -7,5 +7,15 @@
         {
             InitializeComponent();
         }
+
+        private void OnLanguageCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton selectedRadioButton = ((RadioButton)sender);
+
+            if (header != null)
+            {
+                header.Text = $"Your choice is: {selectedRadioButton.Value}";
+            }
+        }
     }
 }
