df7c4803cfd7f0287b08e2a833e935130bb8b31c
mikegsCoder <mikegsid@gmail.com>
2025-08-09 09:24:48 +0300

    Add RadioButtonApp

---
+++ MAUI/04. Controls/04.14. RadioButton/RadioButtonApp/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RadioButtonApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
