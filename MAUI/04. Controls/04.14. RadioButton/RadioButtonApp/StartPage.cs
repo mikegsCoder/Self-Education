dfff6e93b7c663f8b2ad0f032f66f309bc4f8982
mikegsCoder <mikegsid@gmail.com>
2025-08-10 08:05:36 +0300

    Add new RadioButton example in C# code

---
+++ MAUI/04. Controls/04.14. RadioButton/RadioButtonApp/StartPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioButtonApp
+{
+    public class StartPage : ContentPage
+    {
+        Label header = new Label { Text = "Choose a technology:" };
+
+        // RadioButton example:
+        public StartPage()
+        {
+            string[] technologies = { "C#", "JavaScript", "TypeScript" };
+
+            StackLayout stackLayout = new StackLayout();
+            stackLayout.Add(header);
+
+            foreach (var technology in technologies)
+            {
+                RadioButton langRadioButton = new RadioButton { GroupName = "technologies", Content = technology, Value = technology };
+                
+                langRadioButton.CheckedChanged += OnLanguageCheckedChanged;
+                
+                stackLayout.Add(langRadioButton);
+            }
+
+            Content = stackLayout;
+        }
+
+        private void OnLanguageCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton selectedRadioButton = (RadioButton)sender;
+
+            if (header != null)
+            {
+                header.Text = $"Your choice is: {selectedRadioButton.Value}";
+            }
+        }
+    }
+}
