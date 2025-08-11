d15fe1025b45033d93de80c5de9b5dd0f9c25a0a
mikegsCoder <mikegsid@gmail.com>
2025-08-11 06:47:21 +0300

    Add PickerApp

---
+++ MAUI/04. Controls/04.15. Picker/PickerApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PickerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            header.Text = $"Your choice is: {languagePicker.SelectedItem}";
+        }
+    }
+}
