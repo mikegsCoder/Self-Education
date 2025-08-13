e019274d5712157b8173293d6e40e7f62bccad6f
mikegsCoder <mikegsid@gmail.com>
2025-08-13 09:22:53 +0300

    Add TableView example in C# code

---
+++ MAUI/04. Controls/04.16. TableView/TableViewApp/StartPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableViewApp
+{
+    public class StartPage : ContentPage
+    {
+        // TableView example:
+        public StartPage()
+        {
+            TableView tableView = new TableView
+            {
+                Intent = TableIntent.Data,
+
+                Root = new TableRoot("Software Develoment")
+                {
+                    new TableSection ("Programming languages")
+                    {
+                        new TextCell{ Text = "C#", Detail = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tristique id enim quis posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."},
+                        new TextCell{ Text = "JavaScript", Detail = "Pellentesque condimentum in mauris vel tristique. Vivamus tincidunt justo justo, eu tincidunt purus accumsan vel. Nulla gravida maximus sagittis."}
+                    },
+                    new TableSection ("Databases")
+                    {
+                        new TextCell{ Text = "MS SQL Server", Detail = "Suspendisse posuere cursus lacus eu tempus. Phasellus tincidunt ipsum vel vestibulum pellentesque. Ut quis odio at augue mattis porttitor eget ac orci."},
+                        new TextCell{ Text = "MongoDB", Detail = "Praesent rhoncus non ante vel eleifend. Vivamus in ligula porta, lacinia ligula imperdiet, viverra mauris. Nam euismod odio a auctor tempor."},
+                    }
+                }
+            };
+
+            Content = new StackLayout { Children = { tableView }, Padding = 10 };
+        }
+    }
+}
