4a99ea6d667d8fbe34bc412657cf05e5e80c699b
mikegsCoder <mikegsid@gmail.com>
2025-08-14 09:51:22 +0300

    Add WebViewApp

---
+++ MAUI/04. Controls/04.17. WebView/WebViewApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WebViewApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
