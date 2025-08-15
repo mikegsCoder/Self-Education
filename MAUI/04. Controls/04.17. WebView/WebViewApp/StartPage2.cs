4a5645c4d18edf881efab01a60349aa42394a98b
mikegsCoder <mikegsid@gmail.com>
2025-08-15 08:44:25 +0300

    Add new WebView example in C# code

---
+++ MAUI/04. Controls/04.17. WebView/WebViewApp/StartPage2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebViewApp
+{
+    public class StartPage2 : ContentPage
+    {
+        // WebView load local HTML example:
+        public StartPage2()
+        {
+            WebView webView = new WebView();
+
+            var htmlSource = new HtmlWebViewSource();
+
+            htmlSource.Html = @"
+                      <h1>Hello World!</h1>
+                      <p>Hello .NET MAUI!</p>
+                      ";
+
+            webView.Source = htmlSource;
+
+            Content = webView;
+        }
+    }
+}
