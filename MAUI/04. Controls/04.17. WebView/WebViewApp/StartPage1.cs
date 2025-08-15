eec0d25dc8083147c9ff4148b32ebbc7f3b9a183
mikegsCoder <mikegsid@gmail.com>
2025-08-15 08:42:53 +0300

    Add new WebView example in C# code

---
+++ MAUI/04. Controls/04.17. WebView/WebViewApp/StartPage1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebViewApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // WebView example:
+        public StartPage1()
+        {
+            WebView webView = new WebView
+            {
+                //Source = "https://mikegscoder.github.io/"
+                Source = new UrlWebViewSource { Url = "https://mikegscoder.github.io/" },
+            };
+
+            Content = webView;
+        }
+    }
+}
