d553699a6f7b2267598ebb915a482d8d2b80006d
mikegsCoder <mikegsid@gmail.com>
2025-08-17 09:20:23 +0300

    Add DisplayActionSheet example

--- MAUI/04. Controls/04.18. Popup-Windows/PopupWindowsApp/App.xaml.cs
+++ MAUI/04. Controls/04.18. Popup-Windows/PopupWindowsApp/App.xaml.cs
@@ -9,7 +9,8 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new StartPage1());
+            //return new Window(new StartPage1());
+            return new Window(new StartPage2());
         }
     }
 }
---
+++ MAUI/04. Controls/04.18. Popup-Windows/PopupWindowsApp/StartPage2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupWindowsApp
+{
+    public class StartPage2 : ContentPage
+    {
+        Label actionLabel;
+
+        // DisplayActionSheet example:
+        public StartPage2()
+        {
+            Button alertButton = new Button { Text = "Alert", HorizontalOptions = LayoutOptions.Center };
+            
+            alertButton.Clicked += AlertButton_Clicked;
+            
+            actionLabel = new Label();
+
+            Content = new StackLayout { Children = { alertButton, actionLabel } };
+        }
+
+        private async void AlertButton_Clicked(object sender, EventArgs e)
+        {
+            //var action = await DisplayActionSheet("Choose Technology", "Cancel", "Ok", "C#", "JavaScript", "TypeScript");
+            var action = await DisplayActionSheet("Choose Technology", "Cancel", null, "C#", "JavaScript", "TypeScript");
+            
+            actionLabel.Text = action;
+        }
+    }
+}
