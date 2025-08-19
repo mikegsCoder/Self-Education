5458417d5449247c572d450ff962d66cf154df7f
mikegsCoder <mikegsid@gmail.com>
2025-08-19 06:57:47 +0300

    Add ProgressIndicatorApp

---
+++ MAUI/04. Controls/04.19. ProgressBar-And-ActivityIndicator/ProgressIndicatorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgressIndicatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
