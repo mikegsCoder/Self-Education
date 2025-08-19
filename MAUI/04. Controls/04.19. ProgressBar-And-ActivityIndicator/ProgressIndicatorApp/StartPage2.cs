77ed7ab9a20b2ac3c9559550935b793603e588b7
mikegsCoder <mikegsid@gmail.com>
2025-08-19 07:04:13 +0300

    Add new ProgressBar example in C# code

---
+++ MAUI/04. Controls/04.19. ProgressBar-And-ActivityIndicator/ProgressIndicatorApp/StartPage2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressIndicatorApp
+{
+    public class StartPage2 : ContentPage
+    {
+        ProgressBar progressBar = new ProgressBar { ProgressColor = Colors.SeaGreen };
+        Label label = new Label();
+
+        // ProgressBar example:
+        public StartPage2()
+        {
+            StackLayout stackLayout = new StackLayout { Padding = 20 };
+
+            stackLayout.Children.Add(label);
+            stackLayout.Children.Add(progressBar);
+
+            Content = stackLayout;
+        }
+
+        protected override async void OnAppearing()
+        {
+            while (progressBar.Progress < 0.9)
+            {
+                progressBar.Progress += 0.1;
+
+                label.Text = $"Progress value: {Math.Round(progressBar.Progress, 1) * 100} %";
+                
+                await Task.Delay(1000);
+            }
+
+            label.Text = "Progress finished!";
+
+            base.OnAppearing();
+        }
+    }
+}
