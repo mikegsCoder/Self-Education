f5bca17d9158dc87f55e64b7c575009045577db3
mikegsCoder <mikegsid@gmail.com>
2025-08-20 06:49:17 +0300

    Add ActivityIndicator example in C# code

---
+++ MAUI/04. Controls/04.19. ProgressBar-And-ActivityIndicator/ProgressIndicatorApp/StartPage3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressIndicatorApp
+{
+    public class StartPage3 : ContentPage
+    {
+        ActivityIndicator activityIndicator = new ActivityIndicator { IsRunning = true, Color = Colors.SeaGreen };
+        Label label = new Label();
+
+        // ActivityIndicator example:
+        public StartPage3()
+        {
+            StackLayout stackLayout = new StackLayout { Padding = 20 };
+
+            stackLayout.Children.Add(label);
+            stackLayout.Children.Add(activityIndicator);
+
+            Content = stackLayout;
+        }
+
+        protected override async void OnAppearing()
+        {
+            int count = 0;
+
+            while (count != 100)
+            {
+                label.Text = $"Process value: {count} %";
+
+                await Task.Delay(2000);
+
+                count += 10;
+            }
+
+            label.Text = "Process finished!";
+            activityIndicator.IsRunning = false;
+
+            base.OnAppearing();
+        }
+    }
+}
