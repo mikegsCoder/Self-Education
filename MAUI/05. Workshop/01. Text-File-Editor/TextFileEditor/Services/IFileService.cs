fc9eaea618f29268e701218b60aca81d71056a92
mikegsCoder <mikegsid@gmail.com>
2025-08-23 06:37:20 +0300

    Add interface IFileService

---
+++ MAUI/05. Workshop/01. Text-File-Editor/TextFileEditor/Services/IFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileEditor.Services
+{
+    public interface IFileService
+    {
+        public string ReadAllText();
+
+        public void SaveAllText(string text);
+
+        public void AddToZip();
+
+        public void ExtractFromZip();
+    }
+}
