5513cdebaa45b75998c6f74fbc28996b63125846
mikegsCoder <mikegsid@gmail.com>
2025-08-23 06:33:59 +0300

    Add AppConstants

---
+++ MAUI/05. Workshop/01. Text-File-Editor/TextFileEditor/AppConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace TextFileEditor
+{
+    public static class AppConstants
+    {
+        // File path and file names:
+        public const string FilePath = @"../../../../";
+        public const string TxtFileName = FilePath + "TextFile.txt";
+        public const string ZipFileName = FilePath + "TextFile.zip";
+    }
+}
