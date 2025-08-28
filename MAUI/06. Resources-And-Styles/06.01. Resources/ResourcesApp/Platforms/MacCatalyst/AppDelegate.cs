df56110fdd9d1d63373511e85de1ab919c3fe048
mikegsCoder <mikegsid@gmail.com>
2025-08-28 06:52:08 +0300

    Add ResourcesApp

---
+++ MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ResourcesApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
