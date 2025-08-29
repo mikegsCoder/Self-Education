b26d275cce72ce1bbce82d1d4971ee12b5ac3819
mikegsCoder <mikegsid@gmail.com>
2025-08-29 06:54:55 +0300

    Add Application resources example

--- MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/App.xaml.cs
+++ MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/App.xaml.cs
@@ -10,7 +10,8 @@
         protected override Window CreateWindow(IActivationState? activationState)
         {
             //return new Window(new MainPage1());
-            return new Window(new MainPage2());
+            //return new Window(new MainPage2());
+            return new Window(new MainPage3());
         }
     }
 }
