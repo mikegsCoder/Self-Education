e049cdccf1f05ac72be318c7a40c74f36db9b834
mikegsCoder <mikegsid@gmail.com>
2025-08-29 06:58:35 +0300

    Add Dynamic resources example

---
+++ MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/MainPage4.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ResourcesApp
+{
+    public partial class MainPage4 : ContentPage
+    {
+        public MainPage4()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorChange(object sender, EventArgs e)
+        {
+            Color textColor = (Color)Resources["textColor"];
+
+            Resources["textColor"] = textColor.ToHex() == "#1976D2" 
+                ? Color.FromArgb("#00838F") 
+                : Color.FromArgb("#1976D2");
+        }
+    }
+}
