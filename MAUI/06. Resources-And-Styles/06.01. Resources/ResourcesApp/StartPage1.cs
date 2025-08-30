2faf9cf0ce36d821777455a8bbbbeeaf0fc75939
mikegsCoder <mikegsid@gmail.com>
2025-08-30 09:55:22 +0300

    Add new example in C# code

---
+++ MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/StartPage1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourcesApp
+{
+    public class StartPage1 : ContentPage
+    {
+        //ResourcesApp example:
+        public StartPage1()
+        {
+            Color textColor = Color.FromArgb("#004D40");
+            Color backColor = Color.FromArgb("#80CBC4");
+            int margin = 10;
+
+            // create ResourceDictionary:
+            ResourceDictionary resDict = new ResourceDictionary();
+
+            // add resources:
+            resDict.Add("textColor", textColor);
+            resDict.Add("backColor", backColor);
+            resDict.Add("margin", margin);
+
+            // set ResourceDictionary for current page:
+            this.Resources = resDict;
+
+            Button iosButton = new Button { Text = "iOS" };
+            // get resource from ResourceDictionary:
+            iosButton.TextColor = (Color)Resources["textColor"];
+            iosButton.BackgroundColor = (Color)Resources["backColor"];
+            iosButton.Margin = (int)Resources["margin"];
+
+            Button androidButton = new Button { Text = "Android" };
+            androidButton.TextColor = (Color)Resources["textColor"];
+            androidButton.BackgroundColor = (Color)Resources["backColor"];
+            androidButton.Margin = (int)Resources["margin"];
+
+            Content = new StackLayout
+            {
+                Padding = 10,
+                Children = { iosButton, androidButton }
+            };
+        }
+    }
+}
