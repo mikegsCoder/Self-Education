e672b767d5d17ca83ee6f9340a6355b960d2399f
mikegsCoder <mikegsid@gmail.com>
2025-08-30 09:57:20 +0300

    Add new dynamic resources example in C# code

---
+++ MAUI/06. Resources-And-Styles/06.01. Resources/ResourcesApp/StartPage2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourcesApp
+{
+    public class StartPage2 : ContentPage
+    {
+        //Dynamic resources example:
+        public StartPage2()
+        {
+            ResourceDictionary resDict = new ResourceDictionary();
+            // add resource into ResourceDictionary:
+            resDict.Add("textColor", Color.FromArgb("#1976D2"));
+
+            // set ResourceDictionary:
+            this.Resources = resDict;
+
+            Button androidButton = new Button
+            {
+                Text = "Change",
+                WidthRequest = 120,
+                BackgroundColor = Colors.White
+            };
+
+            // set dynamic resource:
+            androidButton.SetDynamicResource(Button.TextColorProperty, "textColor");
+            androidButton.Clicked += ColorChange;
+
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = { androidButton }
+            };
+        }
+
+        private void ColorChange(object sender, EventArgs e)
+        {
+            Color textColor = (Color)Resources["textColor"];
+
+            Resources["textColor"] = textColor.ToHex() == "#1976D2" 
+                ? Color.FromArgb("#00838F") 
+                : Color.FromArgb("#1976D2");
+        }
+    }
+}
