6ad57bffb8045de5b3e3b2dcabb1404ed69b8475
mikegsCoder <mikegsid@gmail.com>
2025-08-31 07:48:12 +0300

    Add StylesApp

---
+++ MAUI/06. Resources-And-Styles/06.02. Styles/StylesApp/MainPage1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StylesApp
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/06. Resources-And-Styles/06.02. Styles/StylesApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StylesApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
