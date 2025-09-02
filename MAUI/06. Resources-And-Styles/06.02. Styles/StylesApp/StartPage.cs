1ceaf43760329bb169ca736fabb0c6455a7c3b81
mikegsCoder <mikegsid@gmail.com>
2025-09-02 06:53:40 +0300

    Add new styles example in C# code

---
+++ MAUI/06. Resources-And-Styles/06.02. Styles/StylesApp/StartPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StylesApp
+{
+    public class StartPage : ContentPage
+    {
+        //Styles example:
+        public StartPage()
+        {
+            Style buttonStyle = new Style(typeof(Button))
+            {
+                Setters =
+                {
+                    new Setter
+                    {
+                        Property = Button.TextColorProperty,
+                        Value = Color.FromArgb("#004D40")
+                    },
+                    new Setter
+                    {
+                        Property = Button.BackgroundColorProperty,
+                        Value = Color.FromArgb("#80CBC4")
+                    },
+                    new Setter
+                    {
+                        Property = Button.MarginProperty,
+                        Value = 10
+                    }
+                }
+            };
+
+            Button button1 = new Button { Text = "iOS", Style = buttonStyle };
+            Button button2 = new Button { Text = "Android", Style = buttonStyle };
+
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = { button1, button2 }
+            };
+        }
+    }
+}
