c3e2d2e9ab26e042dc171a9239f0e9e2c2747b5d
mikegsCoder <mikegsid@gmail.com>
2025-09-04 06:35:26 +0300

    Add CssStylingApp

---
+++ MAUI/06. Resources-And-Styles/06.04. Styling-With-CSS/CssStylingApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls.StyleSheets;
+using System.Reflection;
+
+namespace CssStylingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
