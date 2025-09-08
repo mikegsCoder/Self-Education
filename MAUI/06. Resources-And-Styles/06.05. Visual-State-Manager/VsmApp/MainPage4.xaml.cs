6414e74e971d4bf6d6a28142b0d1001707c11165
mikegsCoder <mikegsid@gmail.com>
2025-09-08 17:53:58 +0300

    Add VisualStateManager custom state example

---
+++ MAUI/06. Resources-And-Styles/06.05. Visual-State-Manager/VsmApp/MainPage4.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VsmApp
+{
+    public partial class MainPage4 : ContentPage
+    {
+        public MainPage4()
+        {
+            InitializeComponent();
+
+            SetState(false);    // initial invalid state
+        }
+
+        private void entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // validation with regex:
+            bool isValid = Regex.IsMatch(e.NewTextValue, @"^\+[1-9]-\d{3}-\d{3}-\d{4}$");
+           
+            SetState(isValid);
+        }
+
+        // set state:
+        void SetState(bool isValid)
+        {
+            string visualState = isValid ? "Valid" : "Invalid";
+            VisualStateManager.GoToState(myEntry, visualState);
+        }
+    }
+}
