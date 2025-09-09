46f2025007f57919ad9f96911c7b1298c913078a
mikegsCoder <mikegsid@gmail.com>
2025-09-09 07:16:08 +0300

    Add new binding context example in C# code

---
+++ MAUI/07. Binding/07.01. BindingContext-And-Binding object/BindingContextApp/BindingContextApp/StartPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingContextApp
+{
+    public class StartPage : ContentPage
+    {
+        //Binding context examle:
+        public StartPage()
+        {
+            Label label = new Label();
+            Entry entry = new Entry();
+
+            // binding source - entry, binding target - label:
+            label.BindingContext = entry;
+            label.SetBinding(Label.TextProperty, "Text");
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Children = { label, entry },
+                Padding = 20
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
