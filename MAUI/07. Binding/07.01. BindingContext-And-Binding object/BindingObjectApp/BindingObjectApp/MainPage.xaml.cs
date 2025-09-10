1b34ab990a2e834909b3278c75516b5ba042f76a
mikegsCoder <mikegsid@gmail.com>
2025-09-10 07:02:57 +0300

    Add BindingObjectApp

---
+++ MAUI/07. Binding/07.01. BindingContext-And-Binding object/BindingObjectApp/BindingObjectApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BindingObjectApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.01. BindingContext-And-Binding object/BindingObjectApp/BindingObjectApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BindingObjectApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
