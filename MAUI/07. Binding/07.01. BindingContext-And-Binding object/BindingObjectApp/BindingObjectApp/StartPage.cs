b4bfe4e00bf05774c29f5aef3af8600b1a911778
mikegsCoder <mikegsid@gmail.com>
2025-09-10 07:05:25 +0300

    Add new binding object example in C# code

---
+++ MAUI/07. Binding/07.01. BindingContext-And-Binding object/BindingObjectApp/BindingObjectApp/StartPage.cs
@@ -0,0 +1,26 @@
+﻿namespace BindingObjectApp
+{
+    public class StartPage : ContentPage
+    {
+        //Binding object example:
+        public StartPage()
+        {
+            Label label = new Label();
+            Entry entry = new Entry();
+
+            // creating binding object with source and path:
+            Binding binding = new Binding { Source = entry, Path = "Text" };
+
+            // set binding:
+            label.SetBinding(Label.TextProperty, binding);
+
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { entry, label },
+                Padding = 20
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
