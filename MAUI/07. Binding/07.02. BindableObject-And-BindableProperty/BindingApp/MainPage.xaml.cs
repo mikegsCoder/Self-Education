7e3160bb7d56c3ea91cd06bdd7e159827f9069fb
mikegsCoder <mikegsid@gmail.com>
2025-09-11 06:57:42 +0300

    Add BindingApp

---
+++ MAUI/07. Binding/07.02. BindableObject-And-BindableProperty/BindingApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BindingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.02. BindableObject-And-BindableProperty/BindingApp/TagLabel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingApp
+{
+    //Bindable property example:
+    public class TagLabel : Label
+    {
+        public static readonly BindableProperty TagProperty =
+            BindableProperty.Create("Tag",  // property name
+                typeof(string),             // property return type 
+                typeof(TagLabel),           // type
+                "0"                         // default setting
+            );
+
+        public string Tag
+        {
+            set
+            {
+                SetValue(TagProperty, value);
+            }
+            get
+            {
+                return (string)GetValue(TagProperty);
+            }
+        }
+    }
+}
