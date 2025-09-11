b4396e9f8e8e32e4adbcebdca4435e6bea56f37f
mikegsCoder <mikegsid@gmail.com>
2025-09-11 07:00:31 +0300

    Add bindable property example in C# code

---
+++ MAUI/07. Binding/07.02. BindableObject-And-BindableProperty/BindingApp/StartPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingApp
+{
+    public class StartPage : ContentPage
+    {
+        //Bindable property example:
+        public StartPage()
+        {
+            TagLabel tagLabel = new TagLabel();
+            Entry entry = new Entry();
+
+            // binding source - entry, binding target - tagLabel:
+            tagLabel.BindingContext = entry;
+            // set binding:
+            tagLabel.SetBinding(TagLabel.TagProperty, "Text");
+            tagLabel.SetBinding(TagLabel.TextProperty, "Text");
+
+            Label simpleLabel = new Label();
+
+            simpleLabel.BindingContext = tagLabel;
+
+            // set binding:
+            simpleLabel.SetBinding(Label.TextProperty, "Tag");
+
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { tagLabel, entry, simpleLabel },
+                Padding = 20
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
