e9c80120475bfc1c357f81e65011a0342bd0a06f
mikegsCoder <mikegsid@gmail.com>
2025-09-12 06:53:33 +0300

    Add BindingModeApp

---
+++ MAUI/07. Binding/07.03. BindingMode/BindingModeApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BindingModeApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
