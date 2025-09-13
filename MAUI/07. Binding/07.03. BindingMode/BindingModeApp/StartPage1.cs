b2d0d2c5a96181abaf512198bb70f32260fbfe02
mikegsCoder <mikegsid@gmail.com>
2025-09-13 07:34:17 +0300

    Add new TwoWay binding example in C# code

---
+++ MAUI/07. Binding/07.03. BindingMode/BindingModeApp/StartPage1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingModeApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // BindingContext TwoWay binding mode example:
+        public StartPage1()
+        {
+            Entry entry1 = new Entry { Margin = 10 };
+            Entry entry2 = new Entry { Margin = 10 };
+
+            entry2.BindingContext = entry1;
+
+            entry2.SetBinding(Entry.TextProperty, "Text", BindingMode.TwoWay);
+            
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { entry1, entry2 },
+                Padding = 10
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
