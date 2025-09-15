f0122e789f5d588c536e8d7233e7f5564a51557f
mikegsCoder <mikegsid@gmail.com>
2025-09-15 06:57:59 +0300

    Add binding object StringFormat example

---
+++ MAUI/07. Binding/07.04. Format-Binding-Value/FormatBindingApp/StartPage2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatBindingApp
+{
+    public class StartPage2 : ContentPage
+    {
+        //Binding object StringFormat example:
+        public StartPage2()
+        {
+            Entry entry = new Entry();
+            Label label = new Label();
+
+            Binding binding = new Binding { Source = entry, Path = "Text", StringFormat = "Message: {0}" };
+            label.SetBinding(Label.TextProperty, binding);
+            
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { entry, label },
+                Padding = 20
+            };
+            
+            Content = stackLayout;
+        }
+    }
+}
