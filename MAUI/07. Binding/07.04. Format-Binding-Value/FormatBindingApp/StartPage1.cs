be7f65fb2c1045c5159f4992991ea669ad484d05
mikegsCoder <mikegsid@gmail.com>
2025-09-15 06:55:43 +0300

    Add SetBinding format example

---
+++ MAUI/07. Binding/07.04. Format-Binding-Value/FormatBindingApp/StartPage1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatBindingApp
+{
+    public class StartPage1 : ContentPage
+    {
+        //SetBinding format example:
+        public StartPage1()
+        {
+            Entry entry = new Entry();
+            Label label = new Label();
+
+            label.BindingContext = entry;
+            label.SetBinding(Label.TextProperty, "Text", stringFormat: "Message: {0}");
+            
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { entry, label },
+                Padding = 20
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
