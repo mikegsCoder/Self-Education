6ab6b023cead01119f2bdbacf02de5b1d929869e
mikegsCoder <mikegsid@gmail.com>
2025-09-16 06:35:15 +0300

    Add ConvertersApp

---
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ConvertersApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
