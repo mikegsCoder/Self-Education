56af92498bd5d91073761bbe0c6f7fd2fbbe51dc
mikegsCoder <mikegsid@gmail.com>
2025-09-16 06:45:53 +0300

    Add custom convertion example

---
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/Converters/StringToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertersApp.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string colorStr = value?.ToString().Trim().ToLower();
+
+            switch (colorStr)
+            {
+                case "red": return Colors.Red;
+                case "green": return Colors.Green;
+                case "blue": return Colors.Blue;
+                default: return Colors.Gray;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = value as Color;
+            var colorHex = color.ToHex();
+
+            switch (colorHex)
+            {
+                case "#FF0000": return "red";
+                case "#00FF00": return "green";
+                case "#0000FF": return "blue";
+                default: return "gray";
+            }
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/MainPage2.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConvertersApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
