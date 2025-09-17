d05c455fde8cef2870c4ac549ea5e679590635ce
mikegsCoder <mikegsid@gmail.com>
2025-09-17 06:47:57 +0300

    Add object binding custom conversion example

--- MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/App.xaml.cs
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/App.xaml.cs
@@ -13,7 +13,8 @@
             //return new Window(new MainPage2());
 
             //return new Window(new StartPage1());
-            return new Window(new StartPage2());
+            //return new Window(new StartPage2());
+            return new Window(new StartPage3());
         }
     }
 }
---
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/StartPage3.cs
@@ -0,0 +1,30 @@
+﻿using ConvertersApp.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertersApp
+{
+    public class StartPage3 : ContentPage
+    {
+        //Object Binding custom conversion example:
+        public StartPage3()
+        {
+            Label label = new Label();
+            DatePicker datePicker = new DatePicker();
+
+            Binding binding = new Binding { Source = datePicker, Path = "Date", Converter = new DateTimeToLocalDateConverter() };
+            label.SetBinding(Label.TextProperty, binding);
+            
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { label, datePicker },
+                Padding = 20
+            };
+            
+            Content = stackLayout;
+        }
+    }
+}
