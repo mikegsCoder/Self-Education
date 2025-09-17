e8fd87904749181880867af51b12d7947e2b1f45
mikegsCoder <mikegsid@gmail.com>
2025-09-17 06:45:52 +0300

    Add custom conversion example

---
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/StartPage2.cs
@@ -0,0 +1,30 @@
+﻿using ConvertersApp.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertersApp
+{
+    public class StartPage2 : ContentPage
+    {
+        //Custom conversion example:
+        public StartPage2()
+        {
+            Label label = new Label();
+            DatePicker datePicker = new DatePicker();
+
+            label.BindingContext = datePicker;
+            label.SetBinding(Label.TextProperty, "Date", converter: new DateTimeToLocalDateConverter());
+            
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { label, datePicker },
+                Padding = 20
+            };
+            
+            Content = stackLayout;
+        }
+    }
+}
