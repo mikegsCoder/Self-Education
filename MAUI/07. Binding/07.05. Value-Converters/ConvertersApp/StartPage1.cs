16ed7071ba05755087c33910821f04d6f1a2555f
mikegsCoder <mikegsid@gmail.com>
2025-09-18 06:57:41 +0300

    Code cleanup

--- MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/StartPage1.cs
+++ MAUI/07. Binding/07.05. Value-Converters/ConvertersApp/StartPage1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConvertersApp
+﻿namespace ConvertersApp
 {
     public class StartPage1 : ContentPage
     {
