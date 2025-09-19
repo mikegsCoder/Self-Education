ecc7f81b57ace70202b1b53c5bd655435cd17ba5
mikegsCoder <mikegsid@gmail.com>
2025-09-19 06:44:50 +0300

    Add converter properties styling example

---
+++ MAUI/07. Binding/07.06. Converter-Properties-And-Parameters/ConverterPropertiesApp/ConverterPropertiesApp/MainPage2.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConverterPropertiesApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
