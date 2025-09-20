5b98f40a6006adc9c0c9ce49a300a048fcdaf712
mikegsCoder <mikegsid@gmail.com>
2025-09-20 08:08:38 +0300

    Add ConverterParametersApp

---
+++ MAUI/07. Binding/07.06. Converter-Properties-And-Parameters/ConverterParametersApp/ConverterParametersApp/Converters/StringToCurrencyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterParametersApp.Converters
+{
+    public class StringToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter.ToString() == "euro")
+            {
+                return $"{value} €";
+            }
+
+            return $"{value} $";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace("  €", "").Replace(" $", "");
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.06. Converter-Properties-And-Parameters/ConverterParametersApp/ConverterParametersApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace ConverterParametersApp
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
