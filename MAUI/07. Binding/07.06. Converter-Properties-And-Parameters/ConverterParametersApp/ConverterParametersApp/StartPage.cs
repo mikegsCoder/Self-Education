5a1af32b29f5a8ac0eda949b2922d60d8606502d
mikegsCoder <mikegsid@gmail.com>
2025-09-20 08:11:26 +0300

    Add new converter parameters example in C# code

---
+++ MAUI/07. Binding/07.06. Converter-Properties-And-Parameters/ConverterParametersApp/ConverterParametersApp/StartPage.cs
@@ -0,0 +1,47 @@
+﻿using ConverterParametersApp.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterParametersApp
+{
+    public class StartPage : ContentPage
+    {
+        //Converter parameter example:
+        public StartPage()
+        {
+            Entry entry = new Entry();
+            Label euroLabel = new Label();
+            Label dollarLabel = new Label();
+
+            Binding euroBinding = new Binding
+            {
+                Source = entry,
+                Path = "Text",
+                Converter = new StringToCurrencyConverter(),
+                ConverterParameter = "euro"
+            };
+
+            Binding dollarBinding = new Binding
+            {
+                Source = entry,
+                Path = "Text",
+                Converter = new StringToCurrencyConverter(),
+                ConverterParameter = "dollar"
+            };
+
+            euroLabel.SetBinding(Label.TextProperty, euroBinding);
+            dollarLabel.SetBinding(Label.TextProperty, dollarBinding);
+
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { entry, euroLabel, dollarLabel },
+                Padding = 20
+            };
+
+            Content = stackLayout;
+        }
+    }
+}
