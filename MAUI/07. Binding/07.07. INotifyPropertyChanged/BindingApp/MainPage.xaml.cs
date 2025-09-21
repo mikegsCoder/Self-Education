25bad2eb92d9f6ee69196294c70d109051943205
mikegsCoder <mikegsid@gmail.com>
2025-09-21 08:40:37 +0300

    Add BindingApp

---
+++ MAUI/07. Binding/07.07. INotifyPropertyChanged/BindingApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace BindingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateAge(object sender, EventArgs e)
+        {
+            var person = Resources["person"] as Person;
+
+            person.Age++;
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.07. INotifyPropertyChanged/BindingApp/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingApp
+{
+    //INotifyPropertyChanged example:
+    public class Person : INotifyPropertyChanged
+    {
+        string name = "";
+        int age;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
