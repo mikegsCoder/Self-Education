355c3ab2d25e1b35944869b1253a044ba5706251
mikegsCoder <mikegsid@gmail.com>
2025-09-23 07:30:15 +0300

    Add RelativeBindingApp

---
+++ MAUI/07. Binding/07.08. Relative-Binding/RelativeBindingApp/Converters/StringToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelativeBindingApp.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Color.TryParse(value.ToString(), out var color)) return color;
+            else return Colors.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Color)value).ToHex();
+        }
+    }
+}
---
+++ MAUI/07. Binding/07.08. Relative-Binding/RelativeBindingApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RelativeBindingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
