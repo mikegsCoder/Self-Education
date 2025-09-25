71c5e41ba52d5515e7dcdec09fd01f0ec6d910ff
mikegsCoder <mikegsid@gmail.com>
2025-09-25 06:54:46 +0300

    Add CalculatorApp

---
+++ MAUI/08. Workshop/CalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace CalculatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
