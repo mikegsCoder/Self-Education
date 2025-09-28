b45aa7a32a403bec90f587a36f1084c687feb8bd
mikegsCoder <mikegsid@gmail.com>
2025-09-28 09:59:56 +0300

    Add MainPageContext

---
+++ MAUI/08. Workshop/CalculatorApp/DataContexts/MainPageContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.DataContexts
+{
+    public class MainPageContext : INotifyPropertyChanged
+    {
+        private string displayText = "";
+
+        public string DisplayText
+        {
+            get { return displayText; }
+            set
+            {
+                displayText = value == "-" ? "" : value;
+                NotifyPropertyChanged(nameof(DisplayText));
+            }
+        }
+
+        public void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
