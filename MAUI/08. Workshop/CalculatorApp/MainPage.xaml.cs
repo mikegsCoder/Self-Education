33ee91f84fe2ba21211ac49241d9c82f43c8de15
mikegsCoder <mikegsid@gmail.com>
2025-09-28 09:56:50 +0300

    Add calculations and operations

--- MAUI/08. Workshop/CalculatorApp/MainPage.xaml.cs
+++ MAUI/08. Workshop/CalculatorApp/MainPage.xaml.cs
@@ -5,9 +5,19 @@ namespace CalculatorApp
 {
     public partial class MainPage : ContentPage
     {
+        private double number1 = 0;
+        private double number2 = 0;
+        private string operation = "";
+
+        private HashSet<string> calculations;
+        private HashSet<string> operations;
+
         public MainPage()
         {
             InitializeComponent();
+
+            calculations = new HashSet<string>() { "=", "1/x", "ln", "x²", "√", "cos", "sin", "tan" };
+            operations = new HashSet<string>() { "÷", "×", "−", "+", "xʸ", "ʸ√x" };
         }
 
         private void Button_Click(object sender, EventArgs e)
