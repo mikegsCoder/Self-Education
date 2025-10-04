1775582fba6433c9f1aca2b45bc4868471c9b49e
mikegsCoder <mikegsid@gmail.com>
2025-10-04 08:18:12 +0300

    Add PropertyTriggersApp

---
+++ MAUI/09. Triggers/09.01. Property-Triggers/PropertyTriggersApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PropertyTriggersApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage1());
+        }
+    }
+}
---
+++ MAUI/09. Triggers/09.01. Property-Triggers/PropertyTriggersApp/MainPage1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PropertyTriggersApp
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
