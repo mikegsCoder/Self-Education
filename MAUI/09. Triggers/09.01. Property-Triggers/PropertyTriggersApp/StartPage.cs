d9244282762c6f61bf3cf8439e149c845eaa1792
mikegsCoder <mikegsid@gmail.com>
2025-10-06 06:58:40 +0300

    Add property trigger example in C# code

---
+++ MAUI/09. Triggers/09.01. Property-Triggers/PropertyTriggersApp/StartPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTriggersApp
+{
+    public class StartPage : ContentPage
+    {
+        //Property Trigger example:
+        public StartPage()
+        {
+            Entry entry = new Entry 
+            { 
+                Text = "Hello from MAUI!", 
+                TextColor = Colors.LightGray 
+            };
+
+            // Create trigger for Entry:
+            var trigger = new Trigger(typeof(Entry))
+            {
+                Property = IsFocusedProperty,
+                Value = true
+            };
+
+            // Add Setter for TextColorProperty:
+            trigger.Setters.Add(new Setter
+            {
+                Property = Entry.TextColorProperty,
+                Value = Colors.Black
+            });
+
+            // Add Trigger to triggers collection:
+            entry.Triggers.Add(trigger);
+
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = { entry }
+            };
+        }
+    }
+}
