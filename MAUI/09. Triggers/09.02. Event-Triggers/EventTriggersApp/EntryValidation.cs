38bf5af7043cfc445558559966c1ebd9f51b7ac6
mikegsCoder <mikegsid@gmail.com>
2025-10-07 07:04:25 +0300

    Add EventTriggersApp

---
+++ MAUI/09. Triggers/09.02. Event-Triggers/EventTriggersApp/EntryValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTriggersApp
+{
+    public class EntryValidation : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (!int.TryParse(sender.Text, out int number))
+            { 
+                sender.TextColor = Colors.Red;
+            }
+            else
+            {
+                sender.TextColor = Colors.Green;
+            }
+        }
+    }
+}
---
+++ MAUI/09. Triggers/09.02. Event-Triggers/EventTriggersApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventTriggersApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
