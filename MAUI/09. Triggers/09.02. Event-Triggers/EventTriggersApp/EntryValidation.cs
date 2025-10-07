32633cfaa86c61b305b71455f6b5b3b53ae6130f
mikegsCoder <mikegsid@gmail.com>
2025-10-07 07:09:23 +0300

    Code cleanup

--- MAUI/09. Triggers/09.02. Event-Triggers/EventTriggersApp/EntryValidation.cs
+++ MAUI/09. Triggers/09.02. Event-Triggers/EventTriggersApp/EntryValidation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventTriggersApp
+﻿namespace EventTriggersApp
 {
     public class EntryValidation : TriggerAction<Entry>
     {
