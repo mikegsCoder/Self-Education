247a24b2b84db43de95f2bbb1d99ea897a4d4aae
mikegsCoder <mikegsid@gmail.com>
2025-10-08 07:07:42 +0300

    Add new event trigger example in C# code

---
+++ MAUI/09. Triggers/09.02. Event-Triggers/EventTriggersApp/StartPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTriggersApp
+{
+    //Event trigger example:
+    public class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Entry entry = new Entry();
+
+            var trigger = new EventTrigger()
+            {
+                Event = "TextChanged"
+            };
+
+            trigger.Actions.Add(new EntryValidation());
+            entry.Triggers.Add(trigger);
+
+            Content = new StackLayout
+            {
+                Children = { entry }
+            };
+        }
+    }
+}
