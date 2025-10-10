b57a1ba9a1cc4b2580e711e255237a478a811989
mikegsCoder <mikegsid@gmail.com>
2025-10-10 06:48:00 +0300

    Add new data trigger example in XAML

---
+++ MAUI/09. Triggers/09.03. Data-Triggers/DataTriggersApp/MainPage3.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTriggersApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public MainPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
