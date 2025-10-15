f6cdbcfb9659a1ded32341b153a54a752ac79846
mikegsCoder <mikegsid@gmail.com>
2025-10-15 06:56:47 +0300

    Add new ListView example

---
+++ MAUI/10. ListView-And-Work-With-Data/10.01. ListView/ListViewApp/MainPage4.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ListViewApp
+{
+    public partial class MainPage4 : ContentPage
+    {
+        // ItemSelected example:
+        public MainPage4()
+        {
+            InitializeComponent();
+
+            usersList.ItemsSource = new List<string> { "Tom", "Bob", "Sam", "Alice" };
+        }
+
+        private void usersList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            selected.Text = $"Chosen: {e.SelectedItem}";
+        }
+    }
+}
