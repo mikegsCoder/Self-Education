b5476df8cbb3fca78988d1ec9007fb1018b2a661
mikegsCoder <mikegsid@gmail.com>
2025-10-15 06:54:49 +0300

    Add new ListView example

---
+++ MAUI/10. ListView-And-Work-With-Data/10.01. ListView/ListViewApp/MainPage3.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ListViewApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public List<string> Users { get; set; }
+
+        public MainPage3()
+        {
+            InitializeComponent();
+
+            Users = new List<string> { "Tom", "Bob", "Sam", "Alice" };
+
+            BindingContext = this;
+        }
+    }
+}
