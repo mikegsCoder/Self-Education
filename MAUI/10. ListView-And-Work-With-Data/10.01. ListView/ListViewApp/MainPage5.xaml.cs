86ea979c080d02fac2933cb8dd0fe099fe28fd60
mikegsCoder <mikegsid@gmail.com>
2025-10-16 08:28:46 +0300

    Add ItemTapped example

---
+++ MAUI/10. ListView-And-Work-With-Data/10.01. ListView/ListViewApp/MainPage5.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ListViewApp
+{
+    public partial class MainPage5 : ContentPage
+    {
+        // ItemTapped example:
+        public MainPage5()
+        {
+            InitializeComponent();
+
+            usersList.ItemsSource = new List<string> { "Tom", "Bob", "Sam", "Alice" };
+        }
+
+        private void usersList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            selected.Text = $"Clicked: {e.Item}";
+        }
+    }
+}
