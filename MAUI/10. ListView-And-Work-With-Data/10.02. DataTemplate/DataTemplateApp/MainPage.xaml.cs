1f8438f4af0e101eb1e3b524a38cac09ce37ac2f
mikegsCoder <mikegsid@gmail.com>
2025-10-19 09:03:47 +0300

    Add DataTemplateApp

---
+++ MAUI/10. ListView-And-Work-With-Data/10.02. DataTemplate/DataTemplateApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTemplateApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/10. ListView-And-Work-With-Data/10.02. DataTemplate/DataTemplateApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataTemplateApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
