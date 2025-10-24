1bb687e6308764d20298d2221b47493143684d3f
mikegsCoder <mikegsid@gmail.com>
2025-10-24 06:54:06 +0300

    Add ViewCell example

---
+++ MAUI/10. ListView-And-Work-With-Data/10.04. ImageCell-And-ViewCell/ImgViewCellApp/MainPage2.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImgViewCellApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
