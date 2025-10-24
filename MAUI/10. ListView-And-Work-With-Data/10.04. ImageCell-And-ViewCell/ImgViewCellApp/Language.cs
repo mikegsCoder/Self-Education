fe4a361f01a7bbf320a71b8277c7e4df4ccfbc54
mikegsCoder <mikegsid@gmail.com>
2025-10-24 06:51:15 +0300

    Add ImgViewCellApp

---
+++ MAUI/10. ListView-And-Work-With-Data/10.04. ImageCell-And-ViewCell/ImgViewCellApp/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgViewCellApp
+{
+    public class Language
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string ImagePath { get; set; } = "";
+    }
+}
---
+++ MAUI/10. ListView-And-Work-With-Data/10.04. ImageCell-And-ViewCell/ImgViewCellApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ImgViewCellApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
