1690483ecb3fe50a1f15a3ad6d605095d529c8d2
mikegsCoder <mikegsid@gmail.com>
2025-10-27 05:41:48 +0200

    Add CustomCell example in C# code

---
+++ MAUI/10. ListView-And-Work-With-Data/10.05. CustomCell/CustomCellApp/StartPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCellApp
+{
+    public class StartPage : ContentPage
+    {
+        // CustomCell example:
+        public StartPage()
+        {
+            ListView listView = new ListView();
+
+            // set data source:
+            listView.ItemsSource = new List<Language>
+            {
+                new Language {Name="C#", ImagePath="csharp.png", Description = "C# description ..." },
+                new Language {Name="JavaScript", ImagePath="js.png", Description = "JS description ..." },
+                new Language {Name="TypeScript", ImagePath="ts.png", Description = "TS description ..." },
+            };
+
+            // set data template
+            listView.ItemTemplate = new DataTemplate(() =>
+            {
+                CustomCell customCell = new CustomCell { ImageHeight = 40, ImageWidth = 40 };
+                customCell.SetBinding(CustomCell.TitleProperty, "Name");
+                customCell.SetBinding(CustomCell.DetailProperty, "Description");
+                customCell.SetBinding(CustomCell.ImagePathProperty, "ImagePath");
+                
+                return customCell;
+            });
+
+            Label header = new Label { FontSize = 18, Text = "Programming languages:" };
+            
+            Content = new StackLayout { Children = { header, listView }, Padding = 7 };
+        }
+    }
+}
