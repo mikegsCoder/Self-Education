4129845b7472f961e38d58636f87c7f2e772cf2d
mikegsCoder <mikegsid@gmail.com>
2025-10-28 06:30:30 +0200

    Add ObservableCollectionApp

---
+++ MAUI/10. ListView-And-Work-With-Data/10.06. ObservableCollection/ObservableCollectionApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ObservableCollectionApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<User> Users { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<User>
+            {
+                new User {Name="Tom", Age=38 },
+                new User {Name = "Bob", Age = 42}
+            };
+
+            BindingContext = this; // binding current object
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            int.TryParse(ageEntry.Text, out var age);
+
+            Users.Add(new User { Name = nameEntry.Text, Age = age });
+            
+            nameEntry.Text = ageEntry.Text = "";
+        }
+    }
+}
