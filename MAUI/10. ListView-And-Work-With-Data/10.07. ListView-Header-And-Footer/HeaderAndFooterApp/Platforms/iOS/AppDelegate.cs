74a884d53c6abf44cd87fc5150c8f3a1d7822799
mikegsCoder <mikegsid@gmail.com>
2025-10-30 06:14:48 +0200

    Add HeaderAndFooterApp

---
+++ MAUI/10. ListView-And-Work-With-Data/10.07. ListView-Header-And-Footer/HeaderAndFooterApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HeaderAndFooterApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
