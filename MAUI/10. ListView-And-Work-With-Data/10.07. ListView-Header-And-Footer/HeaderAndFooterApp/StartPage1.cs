89a04100be5b4d62d706c93274fca5d6e21c3c50
mikegsCoder <mikegsid@gmail.com>
2025-10-31 06:09:25 +0200

    Add Header and Footer example in C# code

---
+++ MAUI/10. ListView-And-Work-With-Data/10.07. ListView-Header-And-Footer/HeaderAndFooterApp/StartPage1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaderAndFooterApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // Header and Footer example:
+        public StartPage1()
+        {
+            ListView listView = new ListView();
+
+            listView.RowHeight = 25;
+            listView.Header = "User list:";
+            listView.Footer = "April 2025";
+
+            // set data source:
+            listView.ItemsSource = new List<string>() { "Tom", "Sam", "Bob", "Alice" };
+            
+            Content = new StackLayout 
+            { 
+                Children = { listView }, 
+                Padding = 7 
+            };
+        }
+    }
+}
