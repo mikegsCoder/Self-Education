2a424edeba8196ad245a6a7204bb47b53716c5ef
mikegsCoder <mikegsid@gmail.com>
2025-11-03 06:31:55 +0200

    Add GroupingApp

---
+++ MAUI/10. ListView-And-Work-With-Data/10.08. Grouping/GroupingApp/Grouping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupingApp
+{
+    public class Grouping<K, T> : ObservableCollection<T>
+    {
+        public K Name { get; private set; }
+
+        public Grouping(K name, IEnumerable<T> items) : base(items)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ MAUI/10. ListView-And-Work-With-Data/10.08. Grouping/GroupingApp/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupingApp
+{
+    public class User
+    {
+        public string Name { get; set; } = "";
+        public string Company { get; set; } = "";
+    }
+}
