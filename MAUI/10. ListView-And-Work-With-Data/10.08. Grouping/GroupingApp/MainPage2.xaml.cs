feba29d0c47c80ff75479ef539864259a1b3e52f
mikegsCoder <mikegsid@gmail.com>
2025-11-04 06:21:58 +0200

    Add new grouping example

---
+++ MAUI/10. ListView-And-Work-With-Data/10.08. Grouping/GroupingApp/MainPage2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GroupingApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        // Grouping collection:
+        public ObservableCollection<Grouping<string, User>> UserGroups { get; set; }
+
+        public MainPage2()
+        {
+            InitializeComponent();
+
+            // initial data:
+            var users = new List<User>
+            {
+                new User {Name="Tom", Company="Microsoft" },
+                new User {Name="Sam", Company="Google" },
+                new User {Name="Alice", Company="Microsoft" },
+                new User {Name="Bob", Company="JetBrains" },
+                new User {Name="Kate", Company="Google" },
+            };
+
+            // set groups:
+            var groups = users.GroupBy(p => p.Company).Select(g => new Grouping<string, User>(g.Key, g));
+
+            // set UserGroups:
+            UserGroups = new ObservableCollection<Grouping<string, User>>(groups);
+
+            BindingContext = this;
+        }
+    }
+}
