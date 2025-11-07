e26f3ba0e18faed6269820a3aa8823259915c8e2
mikegsCoder <mikegsid@gmail.com>
2025-11-07 06:40:23 +0200

    Add private fields

--- MAUI/11. Workshop/StopWatch/MainPage.xaml.cs
+++ MAUI/11. Workshop/StopWatch/MainPage.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace StopWatch
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace StopWatch
 {
     public partial class MainPage : ContentPage
     {
+        private IDispatcherTimer dt;
+        private Stopwatch sw;
+        private ObservableCollection<string> elapsedTimes;
+        private string currentTime;
+
         public MainPage()
         {
             InitializeComponent();
+
+            dt = Application.Current.Dispatcher.CreateTimer();
+            sw = new Stopwatch();
+            currentTime = string.Empty;
+            elapsedTimes = new ObservableCollection<string>();
+
+            clockTxtLbl.Text = "00:00:00";
+            elapsedTimesList.ItemsSource = elapsedTimes;
+
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1); // milliseconds
         }
 
         private void startbtn_Click(object sender, EventArgs e)
