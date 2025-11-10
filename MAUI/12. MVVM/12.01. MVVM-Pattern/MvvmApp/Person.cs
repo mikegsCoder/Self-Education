16075edcd15e359b06a9907cf25d77247dc2a3be
mikegsCoder <mikegsid@gmail.com>
2025-11-10 06:36:57 +0200

    Code cleanup

--- MAUI/12. MVVM/12.01. MVVM-Pattern/MvvmApp/Person.cs
+++ MAUI/12. MVVM/12.01. MVVM-Pattern/MvvmApp/Person.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MvvmApp
+﻿namespace MvvmApp
 {
     public class Person
     {
