660249eb077b26aa13befc299270271f33e2f691
mikegsCoder <mikegsid@gmail.com>
2025-11-11 06:31:58 +0200

    Add ICommandApp

---
+++ MAUI/12. MVVM/12.02. ICommand/ICommandApp/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ICommandApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string name = "";
+        int age;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ICommand AddCommand { get; set; }
+        public ObservableCollection<Person> People { get; } = new();
+
+        public MainViewModel()
+        {
+            // set AddCommand:
+            AddCommand = new Command(() =>
+            {
+                People.Add(new Person(Name, Age));
+                Name = "";
+                Age = 0;
+            });
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
