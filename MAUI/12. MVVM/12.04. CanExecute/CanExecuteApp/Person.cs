7efd2d73db87f6329fc9ba3f9cd9dd1d110f8949
mikegsCoder <mikegsid@gmail.com>
2025-11-13 06:35:18 +0200

    Code cleanup

--- MAUI/12. MVVM/12.04. CanExecute/CanExecuteApp/Person.cs
+++ MAUI/12. MVVM/12.04. CanExecute/CanExecuteApp/Person.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CanExecuteApp
+﻿namespace CanExecuteApp
 {
     public class Person
     {
