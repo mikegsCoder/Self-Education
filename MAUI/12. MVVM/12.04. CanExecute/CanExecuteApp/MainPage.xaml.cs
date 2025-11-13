71ea728995da14e824c13bc32ac479c97a5f5a22
mikegsCoder <mikegsid@gmail.com>
2025-11-13 06:27:01 +0200

    Add CanExecuteApp

---
+++ MAUI/12. MVVM/12.04. CanExecute/CanExecuteApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CanExecuteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ MAUI/12. MVVM/12.04. CanExecute/CanExecuteApp/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CanExecuteApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string name = "";
+        int age;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ICommand AddCommand { get; set; }
+        public ObservableCollection<Person> People { get; } = new();
+
+        public MainViewModel()
+        {
+            // Set AddCommand:
+            AddCommand = new Command(() =>
+            {
+                People.Add(new Person(Name, Age));
+                Name = "";
+                Age = 0;
+            },
+            // Func<bool> CanExecute :
+            () => Age > 0 && Age < 110 && Name.Length > 2);
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            
+            ((Command)AddCommand).ChangeCanExecute();
+        }
+    }
+}
