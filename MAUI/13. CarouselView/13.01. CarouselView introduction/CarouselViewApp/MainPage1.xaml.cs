da580fc1d05cb0a91b7e1d5e0cb88b15cb729ee9
mikegsCoder <mikegsid@gmail.com>
2025-11-17 07:09:18 +0200

    Add CarouselViewApp

---
+++ MAUI/13. CarouselView/13.01. CarouselView introduction/CarouselViewApp/MainPage1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarouselViewApp
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
