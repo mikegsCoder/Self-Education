748d8c546b8c45c382d3c4728faa0344e92b844f
mikegsCoder <mikegsid@gmail.com>
2025-11-21 06:48:10 +0200

    Add CurrentItemChangedCommand example

---
+++ MAUI/13. CarouselView/13.02. CarouselView-Item-Selection-Handling/CarouselViewApp/MainPage3.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace CarouselViewApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public ICommand SelectCommand { get; set; }
+
+        public MainPage3()
+        {
+            InitializeComponent();
+
+            SelectCommand = new Command<Product?>(p =>
+            {
+                header.Text = $"You have selected: {p?.Name}";
+            });
+
+            BindingContext = this;
+        }
+    }
+}
