021ac736572bf2aae18798b64779d55daff2f510
mikegsCoder <mikegsid@gmail.com>
2025-11-21 06:44:33 +0200

    Add CurrentItemChanged example

---
+++ MAUI/13. CarouselView/13.02. CarouselView-Item-Selection-Handling/CarouselViewApp/MainPage2.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace CarouselViewApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+
+        private void carouselView_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
+        {
+            Product? current = e.CurrentItem as Product;
+            Product? previous = e.PreviousItem as Product;
+
+            header.Text = $"Current: {current?.Name}  Previous: {previous?.Name}";
+        }
+    }
+}
