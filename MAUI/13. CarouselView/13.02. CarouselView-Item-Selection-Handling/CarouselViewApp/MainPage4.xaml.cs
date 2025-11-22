0c9f6681baab8d182a142bb83f8a00418ead50ff
mikegsCoder <mikegsid@gmail.com>
2025-11-22 08:29:40 +0200

    Add Select command example

---
+++ MAUI/13. CarouselView/13.02. CarouselView-Item-Selection-Handling/CarouselViewApp/MainPage4.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace CarouselViewApp
+{
+    public partial class MainPage4 : ContentPage
+    {
+        public ICommand SelectCommand { get; set; }
+
+        public MainPage4()
+        {
+            InitializeComponent();
+
+            SelectCommand = new Command<Product>(async p =>
+            {
+                await DisplayAlert("Notification", $"You have selected: {p.Name}", "ОK");
+            });
+
+            BindingContext = this;
+        }
+    }
+}
