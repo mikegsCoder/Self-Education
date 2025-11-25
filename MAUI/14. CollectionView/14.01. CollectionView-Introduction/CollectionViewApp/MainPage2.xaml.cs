90dd4bdcba2391be614c926a72d08bdac8d2013f
mikegsCoder <mikegsid@gmail.com>
2025-11-25 06:40:13 +0200

    Add new CollectionView example

---
+++ MAUI/14. CollectionView/14.01. CollectionView-Introduction/CollectionViewApp/MainPage2.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewApp
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
