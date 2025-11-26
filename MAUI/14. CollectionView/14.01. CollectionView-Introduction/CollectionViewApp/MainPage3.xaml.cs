2e4738bfd85032637ce887fe4b804ddaef6e2e94
mikegsCoder <mikegsid@gmail.com>
2025-11-26 06:42:51 +0200

    Add CollectionView Header and Footer example

---
+++ MAUI/14. CollectionView/14.01. CollectionView-Introduction/CollectionViewApp/MainPage3.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public MainPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
