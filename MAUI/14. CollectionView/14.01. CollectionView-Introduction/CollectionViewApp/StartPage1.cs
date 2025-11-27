4636b2a1ae6a3b390b34812b08278c1a21bc6872
mikegsCoder <mikegsid@gmail.com>
2025-11-27 06:41:09 +0200

    Add new CollectionView example in C# code

---
+++ MAUI/14. CollectionView/14.01. CollectionView-Introduction/CollectionViewApp/StartPage1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // CollectionView example:
+        public StartPage1()
+        {
+            CollectionView collectionView = new CollectionView { Margin = 5 };
+            
+            collectionView.ItemsSource = new string[] { "Tom", "Sam", "Bob", "Alice", "Kate" };
+            
+            Content = collectionView;
+        }
+    }
+}
