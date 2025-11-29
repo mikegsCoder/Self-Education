63383062b98d021558eea65d059518ca513111c4
mikegsCoder <mikegsid@gmail.com>
2025-11-29 10:40:39 +0200

    Add ItemsLayoutApp

---
+++ MAUI/14. CollectionView/14.02. ItemsLayout/ItemsLayoutApp/MainPage1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ItemsLayoutApp
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
