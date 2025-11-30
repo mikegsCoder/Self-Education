36a12ce23cc8d8d65e8db46e518a27d414497eaf
mikegsCoder <mikegsid@gmail.com>
2025-11-30 10:54:45 +0200

    Add new ItemsLayout example

---
+++ MAUI/14. CollectionView/14.02. ItemsLayout/ItemsLayoutApp/MainPage3.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ItemsLayoutApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public MainPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
