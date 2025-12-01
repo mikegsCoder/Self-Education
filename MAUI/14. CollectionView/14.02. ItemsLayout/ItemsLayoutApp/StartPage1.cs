9c3c77e61558d22cd51b7fdf4713cc0194d7c860
mikegsCoder <mikegsid@gmail.com>
2025-12-01 06:41:31 +0200

    Add new ItemsLayout example in C# code

---
+++ MAUI/14. CollectionView/14.02. ItemsLayout/ItemsLayoutApp/StartPage1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsLayoutApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // ItemsLayout example:
+        public StartPage1()
+        {
+            CollectionView collectionView = new CollectionView();
+            collectionView.ItemsLayout = LinearItemsLayout.Vertical;
+            
+            // set data source:
+            collectionView.ItemsSource = new List<Person>
+            {
+                new  Person { Name="Tom", Age=38, Company ="Microsoft" },
+                new  Person { Name="Sam", Age=25,  Company ="Google" },
+                new  Person { Name="Bob", Age=42,  Company ="JetBrains" },
+                new  Person { Name="Alice", Age=33,  Company ="Microsoft" },
+                new  Person { Name="Kate", Age=29,  Company ="Google" },
+                new  Person { Name="Amelia", Age=35,  Company ="JetBrains" },
+            };
+
+            // set data template:
+            collectionView.ItemTemplate = new DataTemplate(() =>
+            {
+                var nameLabel = new Label { FontSize = 20, TextColor = Color.FromArgb("#006064"), Margin = 10 };
+                nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                var ageLabel = new Label();
+                ageLabel.SetBinding(Label.TextProperty, new Binding { Path = "Age", StringFormat = "Age: {0}" });
+
+                var companyLabel = new Label();
+                companyLabel.SetBinding(Label.TextProperty, "Company");
+
+                return new StackLayout
+                {
+                    Children = { nameLabel, ageLabel, companyLabel },
+                    Margin = 20
+                };
+            });
+
+            Content = collectionView;
+        }
+    }
+}
