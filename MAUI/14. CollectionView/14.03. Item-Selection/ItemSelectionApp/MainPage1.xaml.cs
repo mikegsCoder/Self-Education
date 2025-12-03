e768e24ba3b2f5b707d5a0bd1caede1ea2e408c3
mikegsCoder <mikegsid@gmail.com>
2025-12-03 06:43:50 +0200

    Add ItemSelectionApp

---
+++ MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/MainPage1.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ItemSelectionApp
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
