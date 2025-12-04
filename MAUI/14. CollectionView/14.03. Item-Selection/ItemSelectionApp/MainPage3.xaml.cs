fdf3b222f58a1ab38fb1936e3e661cb05e59a3ac
mikegsCoder <mikegsid@gmail.com>
2025-12-04 06:49:14 +0200

    Add SelectionChangedCommand example

---
+++ MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/MainPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace ItemSelectionApp
+{
+    public partial class MainPage3 : ContentPage
+    {
+        public ICommand SelectCommand { get; set; }
+        public MainPage3()
+        {
+            InitializeComponent();
+
+            SelectCommand = new Command<Person?>(p =>
+            {
+                selectedLabel.Text = $"Selected: {p?.Name}";
+            });
+
+            BindingContext = this;
+        }
+    }
+}
