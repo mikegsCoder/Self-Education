c05f38db2404395d1c9b3681f0075e393f8064f3
mikegsCoder <mikegsid@gmail.com>
2025-12-05 06:48:41 +0200

    App SelectionMode Multiple example

---
+++ MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/MainPage4.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace ItemSelectionApp
+{
+    public partial class MainPage4 : ContentPage
+    {
+        public ICommand SelectCommand { get; set; }
+        public MainPage4()
+        {
+            InitializeComponent();
+
+            SelectCommand = new Command<IList<object>>(people =>
+            {
+                string text = "Selected: ";
+
+                foreach (var p in people)
+                {
+                    if (p is Person person) text = $"{text} {person.Name},";
+                }
+
+                selectedLabel.Text = text;
+            });
+
+            BindingContext = this;
+        }
+    }
+}
