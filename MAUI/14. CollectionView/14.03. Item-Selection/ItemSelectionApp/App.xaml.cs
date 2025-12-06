da2b950793e18467860cabf609de49e57a8c4269
mikegsCoder <mikegsid@gmail.com>
2025-12-06 08:37:12 +0200

    Add new SelectedItem example

--- MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/App.xaml.cs
+++ MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/App.xaml.cs
@@ -13,7 +13,9 @@
             //return new Window(new MainPage2());
             //return new Window(new MainPage3());
             //return new Window(new MainPage4());
-            return new Window(new MainPage5());
+            //return new Window(new MainPage5());
+
+            return new Window(new StartPage1());
         }
     }
 }
---
+++ MAUI/14. CollectionView/14.03. Item-Selection/ItemSelectionApp/StartPage1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemSelectionApp
+{
+    public class StartPage1 : ContentPage
+    {
+        // SelectedItem example:
+        public StartPage1()
+        {
+            CollectionView collectionView = new CollectionView { VerticalOptions = LayoutOptions.Start };
+
+            // set SelectionMode:
+            collectionView.SelectionMode = SelectionMode.Single;
+            
+            // set data source:
+            collectionView.ItemsSource = new List<Person>
+            {
+                new  Person { Name="Tom", Company ="Microsoft" },
+                new  Person { Name="Sam", Company ="Google" },
+                new  Person { Name="Bob", Company ="JetBrains" },
+                new  Person { Name="Alice", Company ="Microsoft" },
+                new  Person { Name="Kate", Company ="Google" },
+                new  Person { Name="Amelia", Company ="JetBrains" }
+            };
+
+            // set data template:
+            collectionView.ItemTemplate = new DataTemplate(() =>
+            {
+                var nameLabel = new Label { FontSize = 20, TextColor = Color.FromArgb("#006064") };
+                nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                var companyLabel = new Label();
+                companyLabel.SetBinding(Label.TextProperty, "Company");
+
+                return new StackLayout
+                {
+                    Children = { nameLabel, companyLabel },
+                    Margin = 8
+                };
+            });
+
+            // label for the selected item:
+            Label selectedLabel = new Label { Margin = 8, FontSize = 18 };
+            selectedLabel.SetBinding(Label.TextProperty,
+                new Binding { Source = collectionView, Path = "SelectedItem.Name", StringFormat = "Selected: {0}" });
+            
+            Content = new StackLayout { selectedLabel, collectionView };
+        }
+    }
+}
