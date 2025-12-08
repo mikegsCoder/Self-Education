405ab0554980885e5b8d8c13c0cbe8c412841fb2
mikegsCoder <mikegsid@gmail.com>
2025-12-08 06:29:25 +0200

    Add GroupingApp

---
+++ MAUI/14. CollectionView/14.04. Grouping/GroupingApp/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupingApp
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public string Company { get; set; } = "";
+    }
+}
