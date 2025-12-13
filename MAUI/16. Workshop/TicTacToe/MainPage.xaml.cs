38b78afedf86a354c170e19e18f95df1dadcd59c
mikegsCoder <mikegsid@gmail.com>
2025-12-13 09:13:23 +0200

    Add TicTacToe application

---
+++ MAUI/16. Workshop/TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+using System.Xml;
+
+namespace TicTacToe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
