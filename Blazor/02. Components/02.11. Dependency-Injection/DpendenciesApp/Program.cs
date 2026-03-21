eb803a8ef3cbabd046f83abfd30bf487a7b6badb
mikegsCoder <mikegsid@gmail.com>
2026-03-21 07:58:17 +0200

    Add DependenciesApp initial commit

---
+++ Blazor/02. Components/02.11. Dependency-Injection/DpendenciesApp/Program.cs
@@ -0,0 +1,37 @@
+using DpendenciesApp;
+using DpendenciesApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Show all default services:
+Console.WriteLine("Blazor default services:");
+
+foreach (var service in builder.Services)
+{
+    Console.WriteLine(service.ServiceType);
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
