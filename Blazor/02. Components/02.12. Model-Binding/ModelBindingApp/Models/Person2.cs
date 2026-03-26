1fd9b1b83416a0c77662da83279e5414cf1274ba
mikegsCoder <mikegsid@gmail.com>
2026-03-26 06:45:10 +0200

    Add Person2

---
+++ Blazor/02. Components/02.12. Model-Binding/ModelBindingApp/Models/Person2.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelBindingApp.Models
+{
+    public class Person2
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public DateTime Day { get; set; } = DateTime.Now;
+        public bool Working { get; set; }
+        public string Subject { get; set; } = "";
+        public string[] Courses { get; set; } = [];
+    }
+}
