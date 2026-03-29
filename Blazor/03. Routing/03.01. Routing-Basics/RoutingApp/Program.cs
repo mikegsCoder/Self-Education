86a3870a1d68372841ee4e3bd95164d2a77fbc85
mikegsCoder <mikegsid@gmail.com>
2026-03-29 09:54:21 +0300

    Add RoutingApp

---
+++ Blazor/03. Routing/03.01. Routing-Basics/RoutingApp/Program.cs
@@ -0,0 +1,19 @@
+using RoutingApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.UseStatusCodePagesWithReExecute("/404");
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
