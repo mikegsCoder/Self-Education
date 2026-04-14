307f53f8efaaff61059f6fd376b8766400f0ff0f
mikegsCoder <mikegsid@gmail.com>
2026-04-14 09:29:26 +0300

    Add Student

---
+++ Blazor/04. Forms-And-Validation/04.01. Input-Components/InputComponentsApp/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace InputComponentsApp
+{
+    public class Student
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now; 
+        public bool HasWork { get; set; }
+        public string Subject { get; set; } = "";
+        public string[] Courses { get; set; } = [];
+    }
+}
