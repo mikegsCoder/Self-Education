79c5e8690ee91a6edc2eeb61409822b7c5286d5f
mikegsCoder <mikegsid@gmail.com>
2026-04-19 09:55:14 +0300

    Add Person class with data annotations

---
+++ Blazor/04. Forms-And-Validation/04.03. DataAnnotationsValidator/DataAnnotationsValidatorApp/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationsValidatorApp
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Name must be between {2} and {1} characters long")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        [Range(1, 110, ErrorMessage = "Age must be between {2} and {1}")]
+        public int Age { get; set; }
+    }
+}
