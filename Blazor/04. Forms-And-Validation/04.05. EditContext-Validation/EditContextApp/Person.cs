71e6c830cdc3571ea3d49f81e064efcac9e819cb
mikegsCoder <mikegsid@gmail.com>
2026-04-24 06:51:58 +0300

    Add Person class

---
+++ Blazor/04. Forms-And-Validation/04.05. EditContext-Validation/EditContextApp/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EditContextApp
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [Range(1, 110)]
+        public int Age { get; set; }
+    }
+}
