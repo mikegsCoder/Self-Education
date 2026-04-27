ab6236f508c80a1ac88139e9964052736397c4f2
mikegsCoder <mikegsid@gmail.com>
2026-04-27 06:58:41 +0300

    Add Person class

---
+++ Blazor/04. Forms-And-Validation/04.06. Custom-Validation/CustomValidationApp/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidationApp
+{
+    public class Person
+    {
+        [PersonNameValidator(new[] { "admin" })]
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [Range(1, 110)]
+        public int Age { get; set; }
+    }
+}
