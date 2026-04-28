e74264eb061b655cf4d7ff9edb4a2d17abd6316a
mikegsCoder <mikegsid@gmail.com>
2026-04-28 07:02:13 +0300

    Add PersonNameValidator

---
+++ Blazor/04. Forms-And-Validation/04.06. Custom-Validation/CustomValidationApp/PersonNameValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomValidationApp
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PersonNameValidator : ValidationAttribute
+    {
+        string[] names;
+
+        public PersonNameValidator(string[] names)
+        {
+            this.names = names;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (names.Contains(value?.ToString()))
+            {
+                return new ValidationResult("Invalid name provided!");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
