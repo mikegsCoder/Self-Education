090c9820567dabb302a12979dc01610528038d2b
mikegsCoder <mikegsid@gmail.com>
2026-05-03 09:49:59 +0300

    Add Person class

---
+++ Blazor/05. HTTP-Requests/05.02. HTTP-Client-in-Blazor-WebAsembly/HttpClientWebAsemblyApp/HttpClientWebAsemblyApp/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace HttpClientWebAsemblyApp
+{
+    public class Person
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+    }
+}
