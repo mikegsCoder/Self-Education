bafc378e6fa20847d7100637d8bc76b4a4d7c32b
mikegsCoder <mikegsid@gmail.com>
2026-05-04 10:04:30 +0300

    Map Http POST request

--- Blazor/05. HTTP-Requests/05.02. HTTP-Client-in-Blazor-WebAsembly/HttpClientWebAsemblyApp/HttpClientWebAsemblyApp/Program.cs
+++ Blazor/05. HTTP-Requests/05.02. HTTP-Client-in-Blazor-WebAsembly/HttpClientWebAsemblyApp/HttpClientWebAsemblyApp/Program.cs
@@ -33,6 +33,24 @@ app.UseAntiforgery();
 // testing HTTP GET request:
 app.MapGet("/time", () => DateTime.Now.ToShortTimeString());
 
+// testing HTTP POST request:
+app.MapPost("/user", (Person user) =>
+{
+    // Name must be between 3 and 20 characters long
+    if (user.Name.Length < 3 || user.Name.Length > 20)
+        return Results.BadRequest(new { details = "Name must be between 3 and 20 characters long." });
+
+    // Age must be between 1 and 110
+    if (user.Age < 1 || user.Age > 110)
+        return Results.BadRequest(new { details = "Age must be between 1 and 110." });
+
+    // if everything is ok create new user id
+    user.Id = Guid.NewGuid().ToString();
+
+    // send new user object as JSON
+    return Results.Json(user);
+});
+
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode();
 
