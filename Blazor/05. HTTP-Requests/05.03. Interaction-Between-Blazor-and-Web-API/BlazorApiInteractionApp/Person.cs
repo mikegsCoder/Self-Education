bff488f9c47a71e3903f4471679ebed4ed17966e
mikegsCoder <mikegsid@gmail.com>
2026-05-08 10:31:27 +0300

    Add Person class

---
+++ Blazor/05. HTTP-Requests/05.03. Interaction-Between-Blazor-and-Web-API/BlazorApiInteractionApp/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApiInteractionApp
+{
+    public class Person
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+    }
+}
