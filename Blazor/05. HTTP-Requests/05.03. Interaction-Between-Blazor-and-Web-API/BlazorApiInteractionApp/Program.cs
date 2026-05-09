8148b0b83f341405e03ed95987f998b6bba90283
mikegsCoder <mikegsid@gmail.com>
2026-05-09 10:00:01 +0300

    Add initial data

--- Blazor/05. HTTP-Requests/05.03. Interaction-Between-Blazor-and-Web-API/BlazorApiInteractionApp/Program.cs
+++ Blazor/05. HTTP-Requests/05.03. Interaction-Between-Blazor-and-Web-API/BlazorApiInteractionApp/Program.cs
@@ -1,6 +1,13 @@
 using BlazorApiInteractionApp;
 using BlazorApiInteractionApp.Components;
 
+// initial data
+List<Person> users = [
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+];
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
